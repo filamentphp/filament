3a48b2923b8fc1974c3679c5fb95127d8acf9ba1
Ryan Scherler <ryan@eastslope.studio>
2020-03-12 16:49:07 -0700

    Logout redirect and set alert

--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -44,4 +44,30 @@ class LoginController extends Controller
 
         return view('alpine::auth.login', compact('title'));
     }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new Response('', 204)
+            : redirect(route('alpine.auth.login'))->with('alert', [
+                'type' => 'success',
+                'message' => trans('alpine::auth.logout'),
+            ]);
+    }
 }
