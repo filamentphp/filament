50a1a5ac312116320ee55aa65ac5ea19b26aa0a6
Ryan Scherler <ryan@eastslope.studio>
2020-03-17 16:42:09 -0700

    .

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,9 +1,8 @@
 @extends('alpine::layouts.app', ['classes' => ['body' => '']])
 
 @section('content')
-    <div class="h-screen flex overflow-hidden bg-gray-100" x-data="{ sidebarOpen: false }" @keydown.window.escape="sidebarOpen = false">
-        
-        <!-- Off-canvas menu for mobile -->
+    <div class="h-screen flex overflow-hidden bg-gray-100" x-data="{ sidebarOpen: false }" @keydown.window.escape="sidebarOpen = false">    
+        {{-- Off-canvas menu for mobile --}}
         <div class="md:hidden">
             <div @click="sidebarOpen = false" class="fixed inset-0 z-30 bg-gray-600 opacity-0 pointer-events-none transition-opacity ease-linear duration-300" :class="{'opacity-75 pointer-events-auto': sidebarOpen, 'opacity-0 pointer-events-none': !sidebarOpen}"></div>
             <div class="fixed inset-y-0 left-0 flex flex-col z-40 max-w-xs w-full bg-gray-800 transform ease-in-out duration-300 -translate-x-full" :class="{'translate-x-0': sidebarOpen, '-translate-x-full': !sidebarOpen}">
@@ -25,9 +24,8 @@
                     @include('alpine::partials.user-actions')
                 </div>
             </div>
-        </div>
-        
-        <!-- Static sidebar for desktop -->
+        </div>   
+        {{-- Static sidebar for desktop --}}
         <div class="hidden md:flex md:flex-shrink-0">
             <div class="flex flex-col w-64 bg-gray-800">
                 <div class="h-0 flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
@@ -44,7 +42,7 @@
                 </div>
             </div>
         </div>
-
+        {{-- Main content --}}
         <div class="flex flex-col w-0 flex-1 overflow-hidden">
             <div class="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
                 <button @click.stop="sidebarOpen = true" class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
@@ -54,8 +52,7 @@
             <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabindex="0" x-data x-init="$el.focus()">
                 <div class="max-w-7xl mx-auto p-4 sm:px-6 lg:px-8">
                     <h1 class="text-2xl font-semibold text-gray-900">@yield('title')</h1>
-                </div>
-                
+                </div>       
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                     @yield('main')
                 </div>
