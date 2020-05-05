3d5906ccc896dadfbd1d049f8a797cdab1296afb
Ryan Scherler <ryan@eastslope.studio>
2020-05-05 11:25:48 -0700

    Use nicolaslopezj/searchable trait for easy model search

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -2,11 +2,12 @@
 
 namespace Filament\Models;
 
+use Nicolaslopezj\Searchable\SearchableTrait;
 use Plank\Metable\Metable;
 
 class Permission extends \Spatie\Permission\Models\Permission
 {
-    use Metable;
+    use SearchableTrait, Metable;
 
     /**
      * The attributes that are mass assignable.
@@ -19,11 +20,23 @@ class Permission extends \Spatie\Permission\Models\Permission
         'guard_name',
     ];
     
-    public static function search($query)
-    {
-        return empty($query) ? static::query()
-            : static::where('id', $query)
-                ->orWhere('name', 'like', '%'.$query.'%')
-                ->orWhere('description', 'like', '%'.$query.'%');
-    }
+    /**
+     * Searchable rules.
+     *
+     * @var array
+     */
+    protected $searchable = [
+        /**
+         * Columns and their priority in search results.
+         * Columns with higher values are more important.
+         * Columns with equal values have equal importance.
+         *
+         * @var array
+         */
+        'columns' => [
+            'permissions.name' => 10,
+            'permissions.description' => 10,
+            'permissions.id' => 1,
+        ],
+    ];
 }
