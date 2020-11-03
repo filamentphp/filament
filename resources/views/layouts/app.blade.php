6585feaf7ce60c103548287e0361680b6aba2c56
Ryan Scherler <ryan@eastslope.studio>
2020-11-03 13:55:02 -0800

    prod assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,21 +6,21 @@
     <a href="#content" class="sr-only">Skip to content</a>
     <div class="flex min-h-screen" role="group" tabindex="-1">
         <header role="banner" class="bg-gray-900 text-gray-500 w-56 flex flex-col space-y-4">
-            <a href="{{ url('/') }}" rel="home" class="px-4 py-3 flex items-center space-x-4 transition-colors duration-200 hover:text-gray-400 hover:bg-black" target="_blank" rel="noopener noreferrer">
+            <a href="{{ url('/') }}" rel="home" class="px-4 py-3 flex items-center space-x-4 transition-colors duration-200 hover:text-white" target="_blank" rel="noopener noreferrer">
                 <x-filament::app-icon class="w-10 h-10 fill-current" />
                 <span class="text-sm leading-tight font-semibold">{{ config('app.name') }}</span>
             </a>
             <x-filament::nav />
-            <x-filament::dropdown class="flex-grow flex items-center space-x-3 p-4" id="dropdown-user">
+            <x-filament::dropdown class="w-full text-left flex-grow flex items-center p-4 space-x-3 text-gray-600 transition-colors duration-200 hover:text-white hover:bg-gray-800" id="dropdown-user">
+                <img src="{{ Auth::user()->avatar(32) }}" alt="{{ Auth::user()->name }}" srcset="{{ Auth::user()->avatar(32) }} 1x, {{ Auth::user()->avatar(64) }} 2x" class="flex-shrink-0 w-8 h-8 rounded" />
+                <span class="flex-grow text-sm leading-tight font-semibold">{{ Auth::user()->name }}</span>
+
                 <x-slot name="content">
                     <ul class="list-dropdown" aria-label="{{ __('User actions') }}">
                         <li><a href="#">{{ __('Edit Profile') }}</a></li>
                         <li><livewire:filament-logout /></li>
                     </ul>
                 </x-slot>
-
-                <img src="{{ Auth::user()->avatar(32) }}" alt="{{ Auth::user()->name }}" srcset="{{ Auth::user()->avatar(32) }} 1x, {{ Auth::user()->avatar(64) }} 2x" class="flex-shrink-0 w-8 h-8 rounded" />
-                <span class="flex-grow text-sm leading-tight font-semibold">{{ Auth::user()->name }}</span>
             </x-filament::dropdown>
         </header>
         <div class="flex-grow flex flex-col">
