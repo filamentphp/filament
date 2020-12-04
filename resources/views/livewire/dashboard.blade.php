b755ed62ef812c80ade01fb0c5f63c190f753859
Ryan Scherler <ryan@eastslope.studio>
2020-12-03 19:00:55 -0800

    .

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,5 +1,5 @@
 <div class="card">
-    <div class="p-4 border border-gray-200 border-dashed rounded-lg h-64 flex items-center justify-center">
-        <span class="text-xs text-gray-400 font-mono">{{ config('app.name') }} Dashboard</span>
+    <div class="p-4 border border-gray-300 border-dashed rounded-lg h-64 flex items-center justify-center">
+        <span class="text-xs text-gray-600 font-mono">{{ config('app.name') }} Dashboard</span>
     </div>
 </div>
