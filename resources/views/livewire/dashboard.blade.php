6c15b204e2e2c8431598c964dac9c1aa9cd97d1c
Ryan Scherler <ryan@eastslope.studio>
2020-12-20 17:04:55 -0800

    cleanup

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,4 +1,4 @@
-<div class="card">
+<div class="p-4 md:p-6 bg-white shadow rounded overflow-hidden">
     <div class="p-4 border border-gray-300 border-dashed rounded-lg h-64 flex items-center justify-center">
         <span class="text-xs text-gray-600 font-mono">{{ config('app.name') }} Dashboard</span>
     </div>
