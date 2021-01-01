a1eaad8f4826e6f17c73dc9d7e58525302bdc8f2
Ryan Scherler <ryan@eastslope.studio>
2020-12-31 17:13:39 -0800

    Prod assets

--- resources/views/fields/file.blade.php
+++ resources/views/fields/file.blade.php
@@ -37,7 +37,7 @@
         @if ($field->value)
             <div class="overflow-hidden">
                 <ol 
-                    class="relative flex flex-wrap -m-2" 
+                    class="flex flex-wrap -m-2" 
                     @if ($field->sortMethod)
                         wire:sortable="{{ $field->sortMethod }}"
                     @endif
@@ -52,12 +52,22 @@
                         >
                             <div class="p-2 bg-white shadow-sm rounded border border-gray-300 flex items-center space-x-4">
                                 @if (exif_imagetype(Filament::storage()->path($file)))
-                                    <a href="{{ Filament::storage()->url($file) }}" target="_blank" rel="noopener noreferrer" class="flex-shrink-0">
-                                        <x-filament-image :src="$file" alt="{{ $file }}" :manipulations="[ 'w' => 48, 'h' => 48, 'fit' => 'crop' ]" width="48px" height="48px" loading="lazy" class="w-12 h-12 rounded" />    
+                                    <a 
+                                        href="{{ Filament::storage()->url($file) }}" 
+                                        target="_blank" 
+                                        rel="noopener noreferrer" 
+                                        class="flex-shrink-0" 
+                                    >
+                                        <x-filament-image :src="$file" alt="{{ $file }}" :manipulations="[ 'w' => 48, 'h' => 48, 'fit' => 'crop' ]" class="w-12 h-12 rounded" />    
                                     </a>
                                 @endif
                                 <div class="flex-grow overflow-scroll flex flex-col leading-tight">
-                                    <a href="{{ Filament::storage()->url($file) }}" target="_blank" rel="noopener noreferrer" class="text-sm link">
+                                    <a 
+                                        href="{{ Filament::storage()->url($file) }}" 
+                                        target="_blank" 
+                                        rel="noopener noreferrer" 
+                                        class="text-sm link"
+                                    >
                                         {{ $file }}
                                     </a>
                                     <dl class="font-mono text-xs text-gray-500">
