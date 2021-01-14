b185e5d264ec9961284b6dd0f8d3c886a7a72278
Ryan Scherler <ryan@eastslopestudio.com>
2021-01-14 00:43:03 -0800

    Update progress.blade.php

--- resources/views/components/progress.blade.php
+++ resources/views/components/progress.blade.php
@@ -1,10 +1,10 @@
 @props([
     'max' => 100,
-    'progress' => 0,
+    'progress',
 ])
 <div {{ $attributes }}>
     <progress max="{{ $max }}" x-bind:value="{{ $progress }}" class="sr-only"></progress>
     <div class="rounded-full border border-gray-200 overflow-hidden" aria-hidden="true">
-        <div class="h-1 bg-gray-500" :style="`width: ${ {{ ($progress / $max) * 100 }} }%`"></div>
+        <div class="h-1 bg-gray-500" :style="`width: ${ {{ ((int) $progress / (int) $max) * 100 }} }%`"></div>
     </div>  
 </div>
