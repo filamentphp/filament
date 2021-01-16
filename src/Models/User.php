feab9ac539034fbed8b13ee0cebccd7d69a9154f
Dan Harrin <git@danharrin.com>
2021-01-16 00:48:28 +0000

    Create user model

---
+++ src/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Filament\Models;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+    use Notifiable;
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function sendPasswordResetNotification($token): void
+    {
+        $notification = new ResetPassword($token);
+        $notification->createUrlUsing(function ($notifiable, $token) {
+            return route('filament.password.reset', [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ]);
+        });
+
+        $this->notify($notification);
+    }
+}
