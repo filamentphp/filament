b5fb3fd11e9d84b3bb3671c4ea926a3bbcddb85c
Dan Harrin <git@danharrin.com>
2021-02-02 20:28:03 +0000

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-filament::form :fields="$this->fields()" class="space-y-6">
     <x-filament::button type="submit" class="btn-primary w-full" wire:loading.attr="disabled">
-    <x-filament::loader class="w-6 h-6 absolute left-0 ml-2 pointer-events-none" wire:loading />
+        <x-filament::loader class="w-6 h-6 absolute left-0 ml-2 pointer-events-none" wire:loading />
 
         {{ __('filament::auth.login') }}
     </x-filament::button>
