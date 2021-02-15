c12d1d24b120173380ddb774e996d3a924479fb6
Ryan Scherler <ryan@eastslope.studio>
2021-02-14 16:56:45 -0800

    widgets wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,19 @@
         <x-slot name="actions">
             <x-filament::dropdown class="cursor-pointer font-medium border rounded transition duration-200 shadow-sm inline-block relative focus:ring focus:ring-opacity-50 border-gray-300 from-gray-100 to-gray-200 text-gray-800 hover:to-gray-100 bg-gradient-to-b focus:ring-secondary-200 text-sm py-2 px-4 flex items-center space-x-1">
                 <x-slot name="button">
+                    <x-heroicon-o-plus class="w-4 h-4" aria-hidden="true" />
                     <span>Add Widget</span>
                     <x-heroicon-o-chevron-down class="w-4 h-4" aria-hidden="true" />
                 </x-slot>
 
                 <x-filament::dropdown-link button>
-                    test
+                    Quick Links
+                </x-filament::dropdown-link>
+                <x-filament::dropdown-link button>
+                    Activity
+                </x-filament::dropdown-link>
+                <x-filament::dropdown-link button>
+                    Chart
                 </x-filament::dropdown-link>
             </x-filament::dropdown>
         </x-slot>
@@ -23,26 +30,60 @@
                     </x-filament::dropdown-link>
                 </x-slot>
 
-                <ol class="divide-y divide-gray-200">
-                    @for ($i = 1; $i <= 5; $i++)
-                        <li class="py-2.5 flex items-center justify-between space-x-3">
-                            <button class="flex-shrink-0 flex text-gray-300 hover:text-gray-600 transition-colors duration-200">
-                                <x-filamenticon-o-sort class="w-3 h-3" />
-                            </button>
+                <div class="space-y-4">
+                    <ol class="divide-y divide-gray-200">
+                        @for ($i = 1; $i <= 5; $i++)
+                            <li class="py-2.5 flex items-center justify-between space-x-3">
+                                <button class="flex-shrink-0 flex text-gray-300 hover:text-gray-600 transition-colors duration-200">
+                                    <x-filamenticon-o-sort class="w-3 h-3" />
+                                </button>
 
-                            <div class="flex-grow text-sm leading-tight">
-                                <a href="#" class="link">Quick Link #{{ $i }}</a>
-                            </div>
+                                <div class="flex-grow text-sm leading-tight">
+                                    <a href="#" class="link">Quick Link #{{ $i }}</a>
+                                </div>
 
-                            <button class="flex-shrink-0 text-gray-300 hover:text-danger-600 transition-colors duration-200">
-                                <x-heroicon-o-x class="w-4 h-4" />
-                            </button>
-                        </li>
-                    @endfor
-                </ol>
+                                <button class="flex-shrink-0 text-gray-300 hover:text-danger-600 transition-colors duration-200">
+                                    <x-heroicon-o-x class="w-4 h-4" />
+                                </button>
+                            </li>
+                        @endfor
+                    </ol>
+                    
+                    <div class="flex justify-end">
+                        <x-filament::modal>
+                            <x-slot name="trigger">
+                                <x-filament::button 
+                                    size="small"
+                                    class="flex items-center space-x-1"
+                                    @click="open = !open"
+                                >
+                                    <x-heroicon-o-plus class="w-4 h-4" aria-hidden="true" />
+                                    <span>Add Link</span>
+                                </x-filament::button>
+                            </x-slot>
+    
+                            <x-filament::card class="space-y-5 max-w-2xl">
+                                <x-filament::card-header title="Add Quick Link?">
+                                    <form>
+                                        Form...
+                                    </form>
+                                </x-filament::card-header>
+            
+                                <div class="space-y-3 sm:space-y-0 sm:flex sm:space-x-3 sm:justify-end">
+                                    <x-filament::button
+                                        x-on:click="open = false"
+                                        wire:loading.attr="disabled"
+                                    >
+                                        Cancel
+                                    </x-filament::button>
+                                </div>
+                            </x-filament::card>
+                        </x-filament::modal>
+                    </div>
+                </div>
             </x-filament::widget>
 
-            <x-filament::widget title="Bar Chart" :columns="2">
+            <x-filament::widget title="Chart" :columns="2">
                 <x-slot name="settings">
                     <x-filament::dropdown-link button>
                         Chart Setting...
