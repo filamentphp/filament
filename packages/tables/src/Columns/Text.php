cf36f7279435d485b94129ddb9b8f9a969f165d8
Dan Harrin <git@danharrin.com>
2021-02-16 12:40:12 +0000

    Formatting column helpers

--- packages/tables/src/Columns/Text.php
+++ packages/tables/src/Columns/Text.php
@@ -2,11 +2,40 @@
 
 namespace Filament\Tables\Columns;
 
+use Carbon\Carbon;
+
 class Text extends Column
 {
+    public $default;
+
     public $formatUsing;
 
     public $url;
+
+    public function date($format = 'F j, Y')
+    {
+        $this->formatUsing = function ($value) use ($format) {
+            $value = Carbon::parse($value)->format($format);
+
+            return $value;
+        };
+
+        return $this;
+    }
+
+    public function dateTime($format = 'F j, Y H:i:s')
+    {
+        $this->date($format);
+
+        return $this;
+    }
+
+    public function default($default)
+    {
+        $this->default = $default;
+
+        return $this;
+    }
 
     public function formatUsing($callback)
     {
@@ -30,6 +59,10 @@ class Text extends Column
     {
         $value = parent::getValue($record);
 
+        if ($value === null) {
+            $this->default;
+        }
+
         if ($this->formatUsing) {
             $callback = $this->formatUsing;
 
@@ -47,6 +80,22 @@ class Text extends Column
             }
 
             return $value;
+        };
+
+        return $this;
+    }
+
+    public function price($locale = null)
+    {
+        $this->formatUsing = function ($value) use ($locale) {
+            if (! is_numeric($value)) return null;
+
+            setlocale(LC_MONETARY, $locale ?: locale_get_default());
+
+            $formatter = numfmt_create('en_US', \NumberFormatter::CURRENCY);
+            $formatter->setAttribute(\NumberFormatter::FRACTION_DIGITS, 2);
+
+            return numfmt_format_currency($formatter, $value, trim(localeconv()['int_curr_symbol']));
         };
 
         return $this;
