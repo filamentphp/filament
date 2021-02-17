85761e8c8521b701957b9f1d876f81ea8f7f2eb8
Ryan Scherler <ryan@eastslope.studio>
2021-02-16 16:51:47 -0800

    widgets wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,29 +42,40 @@
                                 </button>
 
                                 <div class="flex-grow text-sm leading-tight">
-                                    <a href="#" class="link">Quick Link #{{ $i }}</a>
+                                    <a href="#" class="text-xs font-mono link">Link #{{ $i }} Label</a>
                                 </div>
 
                                 <button class="flex-shrink-0 text-gray-300 hover:text-danger-600 transition-colors duration-200">
-                                    <x-heroicon-o-x class="w-4 h-4" />
+                                    <span class="sr-only">Remove Link</span>
+                                    <x-heroicon-o-x class="flex-shrink-0 w-4 h-4" />
                                 </button>
                             </li>
                         @endfor
                     </ol>
 
-                    <div class="flex justify-end">
-                        <x-filament::button
-                            size="small"
-                            class="flex items-center space-x-1"
-                        >
-                            <x-heroicon-o-plus class="w-4 h-4" aria-hidden="true" />
-                            <span>Add Link</span>
-                        </x-filament::button>
-                    </div>
+                    <form class="flex justify-between space-x-4">
+                        <div class="flex-grow grid grid-cols-2 gap-4">
+                            <input 
+                                placeholder="Label"
+                                class="py-1 border-b text-xs font-mono border-gray-200 placeholder-opacity-100 placeholder-gray-500 focus:placeholder-gray-300 outline-none focus:border-gray-400"
+                            >
+                            <input 
+                                placeholder="URL"
+                                class="py-1 border-b text-xs font-mono border-gray-200 placeholder-opacity-100 placeholder-gray-500 focus:placeholder-gray-300 outline-none focus:border-gray-400"
+                            >
+                        </div>
+                        <button class="flex-shrink-0 flex items-center space-x-1 text-gray-500 hover:text-success-600 transition-colors duration-200">
+                            <span class="text-xs">Add Link</span>
+                            <x-heroicon-o-plus class="flex-shrink-0 w-4 h-4" />
+                        </button>
+                    </form>
                 </div>
             </x-filament::widget>
-            <x-filament::widget title="Activity">
-                widget content...
+            <x-filament::widget 
+                title="Activity"
+                :columns="2"
+            >
+                <span class="text-xs font-mono">filters and a paginated table (10 or so results)</span>
             </x-filament::widget>
         </x-filament::widgets>
     </x-filament::app-content>
