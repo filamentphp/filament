cbd8f329f354ff34fbc9e735247e6f09618245ed
Dan Harrin <git@danharrin.com>
2021-02-25 20:29:31 +0000

    wip

---
+++ src/Resources/Forms/Components/BelongsToSelect.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Filament\Resources\Forms\Components;
+
+use Illuminate\Support\Str;
+
+class BelongsToSelect extends Select
+{
+    protected $view = 'forms::components.select';
+
+    public function relationship($relationshipName, $displayColumnName, $callback = null)
+    {
+        $this->getDisplayValueUsing(function ($value) use ($callback, $displayColumnName, $relationshipName) {
+            $relationship = (new $this->model())->{$relationshipName}();
+
+            $query = $relationship->getRelated();
+
+            if ($callback) {
+                $query = $callback($query);
+            }
+
+            $record = $query->where($relationship->getOwnerKeyName(), $value)->first();
+
+            return $record ? $record->getAttributeValue($displayColumnName) : null;
+        });
+
+        $this->getOptionSearchResultsUsing(function ($search) use ($callback, $displayColumnName, $relationshipName) {
+            $relationship = (new $this->model())->{$relationshipName}();
+
+            $query = $relationship->getRelated();
+
+            if ($callback) {
+                $query = $callback($query);
+            }
+
+            return $query
+                ->where($displayColumnName, 'like', "%{$search}%")
+                ->pluck($displayColumnName, $relationship->getOwnerKeyName())
+                ->toArray();
+        });
+
+        $this->label(
+            (string) Str::of($relationshipName)
+                ->before('.')
+                ->kebab()
+                ->replace(['-', '_'], ' ')
+                ->ucfirst(),
+        );
+
+        $setUpRules = function ($component) use ($relationshipName) {
+            $relationship = (new $component->model())->{$relationshipName}();
+
+            $model = get_class($relationship->getModel());
+            $column = $relationship->getOwnerKeyName();
+
+            $component->addRules([$component->name => "exists:{$model},{$column}"]);
+        };
+
+        if ($this->model) {
+            $setUpRules($this);
+        } else {
+            $this->pendingModelModifications[] = $setUpRules;
+        }
+
+        return $this;
+    }
+}
