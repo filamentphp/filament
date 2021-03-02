03d2e131dd7ea3f7d43eaf47c5d67ed37d5cf43a
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-02 20:23:59 +0000

    feature: add DocBlocks to Filament facade

--- src/Filament.php
+++ src/Filament.php
@@ -4,6 +4,14 @@ namespace Filament;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void registerPage(string $page)
+ * @method static void registerResource(string $resource) 
+ * @method static void registerRole(string $role)
+ * @method static void registerWidget(string $widget)
+ *
+ * @see \Filament\FilamentManager
+ */
 class Filament extends Facade
 {
     protected static function getFacadeAccessor()
