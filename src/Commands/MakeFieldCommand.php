ad1b0950b56aa804399cd2932440b20e2a0c4fe6
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-02 11:50:08 +0000

    feature: MakeField command

---
+++ src/Commands/MakeFieldCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Filament\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class MakeFieldCommand extends Command
+{
+    use Concerns\CanManipulateFiles;
+
+    protected $description = 'Creates a Filament field class and view.';
+
+    protected $signature = 'make:filament-field {name}';
+
+    public function handle()
+    {
+        $field = (string) Str::of($this->argument('name'))
+            ->trim('/')
+            ->trim('\\')
+            ->trim(' ')
+            ->replace('/', '\\');
+        $fieldClass = (string) Str::of($field)->afterLast('\\');
+        $fieldNamespace = Str::of($field)->contains('\\') ?
+            (string) Str::of($field)->beforeLast('\\') :
+            '';
+
+        $view = Str::of($field)
+            ->prepend('filament\\fields\\')
+            ->explode('\\')
+            ->map(fn ($segment) => Str::kebab($segment))
+            ->implode('.');
+
+        $path = app_path(
+            (string) Str::of($field)
+                ->prepend('Filament\\Fields\\')
+                ->replace('\\', '/')
+                ->append('.php'),
+        );
+
+        $viewPath = resource_path(
+            (string) Str::of($view)
+                ->replace('.', '/')
+                ->prepend('views/')
+                ->append('.blade.php'),
+        );
+
+        if ($this->checkForCollision([
+            $path,
+            $viewPath,
+        ])) return;
+
+        $this->copyStubToApp('Field', $path, [
+            'class' => $fieldClass,
+            'namespace' => 'App\\Filament\\Fields' . ($fieldNamespace !== '' ? "\\{$fieldNamespace}" : ''),
+            'view' => $view,
+        ]);
+
+        $this->copyStubToApp('FieldView', $viewPath);
+
+        $this->info("Successfully created {$field}!");
+    }
+}
