213f25c7fb9fa3ca996143c469c417b10de5bb42
Dan Harrin <git@danharrin.com>
2021-03-03 10:20:30 +0000

    Set checkbox default to false

--- packages/forms/src/Components/Checkbox.php
+++ packages/forms/src/Components/Checkbox.php
@@ -5,4 +5,11 @@ namespace Filament\Forms\Components;
 class Checkbox extends Field
 {
     use Concerns\CanBeAutofocused;
+
+    public function __construct($name)
+    {
+        parent::__construct($name);
+
+        $this->default(false);
+    }
 }
