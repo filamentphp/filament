f73306e2bf3574216f5c52b0c60386b1f195b1e7
Emanuel Quimper <quimperemanuel@gmail.com>
2021-03-04 12:28:51 -0500

    Add extraAttributes to textArea

--- packages/forms/resources/views/components/textarea.blade.php
+++ packages/forms/resources/views/components/textarea.blade.php
@@ -18,5 +18,6 @@
         {!! $formComponent->placeholder ? "placeholder=\"{$formComponent->placeholder}\"" : null !!}
         {!! $formComponent->required ? 'required' : null !!}
         class="block w-full rounded shadow-sm placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-secondary-300 focus:ring focus:ring-secondary-200 focus:ring-opacity-50 {{ $errors->has($formComponent->name) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
+        {!! Filament\format_attributes($formComponent->extraAttributes) !!}
     ></textarea>
 </x-forms::field-group>
