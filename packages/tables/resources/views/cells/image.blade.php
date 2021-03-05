4b4d473490ebdeceb1d3115b3cbebd9d1fd2a904
danilopolani <danilo.polani@gmail.com>
2021-03-05 16:52:20 +0100

    add image column type

---
+++ packages/tables/resources/views/cells/image.blade.php
@@ -0,0 +1,21 @@
+<a
+    href="{{ $column->getValue($record) }}"
+    target="_blank"
+    @if ($column->cover)
+        class="block bg-center {{ $column->getClass() }}"
+        style="
+            {{ $column->getWidth() ? "width: {$column->getWidth()};" : '' }}
+            {{ $column->getHeight() ? "height: {$column->getHeight()};" : '' }}
+            background-image: url({{ $column->getValue($record) }});"
+    @endif
+>
+    @if (!$column->cover)
+        <img
+            src="{{ $column->getValue($record) }}"
+            class="{{ $column->getClass() }}"
+            style="
+                {{ $column->getWidth() ? "width: {$column->getWidth()};" : '' }}
+                {{ $column->getHeight() ? "height: {$column->getHeight()};" : '' }}"
+        >
+    @endif
+</a>
---
+++ packages/tables/src/Columns/Image.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Filament\Tables\Columns;
+
+class Image extends Column
+{
+    public $width = null;
+
+    public $height = null;
+
+    public string $class = '';
+
+    public bool $rounded = false;
+
+    public bool $cover = false;
+
+    public function width($width): self
+    {
+        return tap($this, fn () => $this->width = $width);
+    }
+
+    public function height($height): self
+    {
+        return tap($this, fn () => $this->height = $height);
+    }
+
+    public function square(int $size): self
+    {
+        return tap($this, function () use ($size) {
+            $this->width = $size;
+            $this->height = $size;
+        });
+    }
+
+    public function class(string $class): self
+    {
+        return tap($this, fn () => $this->class = $class);
+    }
+
+    public function rounded(): self
+    {
+        return tap($this, fn () => $this->rounded = true);
+    }
+
+    public function cover(): self
+    {
+        return tap($this, fn () => $this->cover = true);
+    }
+
+    public function getWidth(): string|int|null
+    {
+        return $this->width && is_numeric($this->width) ? "{$this->width}px" : $this->width;
+    }
+
+    public function getHeight(): string|int|null
+    {
+        return $this->height && is_numeric($this->height) ? "{$this->height}px" : $this->height;
+    }
+
+    public function getClass(): string
+    {
+        $class = $this->class;
+
+        if ($this->rounded) $class .= ' rounded-full';
+        if ($this->cover) $class .= ' bg-cover';
+
+        return $class;
+    }
+}
