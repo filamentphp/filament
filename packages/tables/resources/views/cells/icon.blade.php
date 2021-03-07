9e2434f40716aadebb39abd26b42b60acb1de692
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-07 20:16:54 +0000

    fix: add action to icon

--- packages/tables/resources/views/cells/icon.blade.php
+++ packages/tables/resources/views/cells/icon.blade.php
@@ -1,7 +1,24 @@
-@foreach($column->options as $icon => $callback)
-    @if($callback($column->getValue($record)))
-        <x-dynamic-component :component="$icon" class="{{ $classes ?? '' }} w-6 h-6" />
+@php
+$iconToShow = null;
 
-        @break
-    @endif
-@endforeach
+foreach ($column->options as $icon => $callback) {
+    if (! $callback($column->getValue($record))) {
+        continue;
+    }
+
+    $iconToShow = $icon;
+
+    break;
+}
+@endphp
+
+@if($column->action)
+    <button
+        wire:click="{{ $column->action }}('{{ $record->getKey() }}')"
+        type="button"
+    >
+        <x-dynamic-component :component="$iconToShow" class="{{ $classes ?? '' }} w-6 h-6" />
+    </button>
+@else
+    <x-dynamic-component :component="$iconToShow" class="{{ $classes ?? '' }} w-6 h-6" />
+@endif
