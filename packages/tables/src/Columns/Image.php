5c78e93d909bb4409a24c3199ca6935715f62d82
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-07 20:29:54 +0000

    fix: allow using valid urls in image column

--- packages/tables/src/Columns/Image.php
+++ packages/tables/src/Columns/Image.php
@@ -55,6 +55,10 @@ class Image extends Column
     {
         $path = $this->getValue($record);
 
+        if (filter_var($path, FILTER_VALIDATE_EMAIL) !== false) {
+            return $path;
+        }
+
         if (! $path) return null;
 
         return Storage::disk($this->disk)->url($path);
