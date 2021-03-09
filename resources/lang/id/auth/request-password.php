2f04c57cd951d88165a610ca28a115d90b4491bd
rama-adi <ramatabanan@gmail.com>
2021-03-10 04:47:12 +0800

    Added indonesian translation

---
+++ resources/lang/id/auth/request-password.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    'buttons' => [
+
+        'submit' => [
+            'label' => 'Kirim link reset sandi',
+        ],
+
+    ],
+
+    'form' => [
+
+        'email' => [
+            'hint' => 'Kembali ke login',
+            'label' => 'Alamat email',
+        ],
+
+    ],
+
+    'messages' => [
+
+        'throttled' => 'Terlalu banyak upaya masuk. Silahkan coba lagi dalam :seconds detik.',
+
+        'passwords' => [
+            'sent' => 'Kami sudah mengirimkan email yang berisi tautan untuk mereset sandi anda!',
+            'throttled' => 'Harap tunggu sebelum mencoba lagi.',
+            'user' => 'Kami tidak menemukan pengguna dengan alamat email tersebut.',
+        ],
+
+    ],
+
+    'title' => 'Reset kata sandi',
+
+];
---
+++ resources/lang/id/edit-account.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'buttons' => [
+
+        'save' => [
+            'label' => 'Simpan',
+        ],
+
+    ],
+
+    'messages' => [
+        'saved' => 'Tersimpan!',
+    ],
+
+    'title' => 'Akun',
+
+];
