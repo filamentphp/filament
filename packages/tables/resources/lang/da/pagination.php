44646a3301d3e00d0035e53b8a7d72144e292f93
Hans Nielsen <hans.nielsen@gmail.com>
2021-03-09 18:33:49 +0100

    Added danish translation files

---
+++ packages/tables/resources/lang/da/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'buttons' => [
+        'goToPage' => 'Gå til side :page',
+        'next' => 'Næste',
+        'previous' => 'Forrige',
+    ],
+
+    'label' => 'Paginering Navigation',
+
+    'overview' => [
+        'Viser',
+        'til',
+        'af',
+        'resultater',
+    ],
+
+];
---
+++ packages/tables/resources/lang/da/table.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    'delete' => [
+        'button' => [
+            'label' => 'Slet valgt',
+        ],
+
+        'modal' => [
+            'buttons' => [
+                'cancel' => [
+                    'label' => 'Annullere',
+                ],
+
+                'delete' => [
+                    'label' => 'Slet valgt',
+                ],
+            ],
+
+            'description' => 'Er du sikker på, at du vil slette de valgte poster? Denne handling kan ikke fortrydes.',
+
+            'heading' => 'Slet de valgte poster?',
+        ],
+    ],
+
+    'filter' => [
+        'placeholder' => 'Filter',
+    ],
+
+    'messages' => [
+        'noRecords' => 'Ingen resultater',
+    ],
+
+    'pagination' => [
+        'fields' => [
+            'recordsPerPage' => [
+                'label' => 'per side',
+            ],
+        ],
+    ],
+
+    'record' => [
+        'button' => [
+            'label' => 'Redigere',
+        ],
+    ],
+
+    'search' => [
+        'placeholder' => 'Søg',
+    ],
+
+];
