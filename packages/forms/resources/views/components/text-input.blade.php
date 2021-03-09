f165dea5b602003d917aef8be8631ca8af43a04d
Dan Harrin <git@danharrin.com>
2021-03-09 21:38:04 +0000

    formatting

--- packages/forms/resources/views/components/text-input.blade.php
+++ packages/forms/resources/views/components/text-input.blade.php
@@ -8,11 +8,12 @@
     :required="$formComponent->required"
 >
     <div class="flex border-gray-300 rounded shadow-sm">
-        @if($formComponent->prefix)
+        @if ($formComponent->prefix)
             <span class="inline-flex items-center px-3 text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50 sm:text-sm">
                 {{ $formComponent->prefix }}
             </span>
         @endif
+
         <input
             {!! $formComponent->autocomplete ? "autocomplete=\"{$formComponent->autocomplete}\"" : null !!}
             {!! $formComponent->autofocus ? 'autofocus' : null !!}
@@ -24,7 +25,7 @@
             {!! $formComponent->placeholder ? "placeholder=\"{$formComponent->placeholder}\"" : null !!}
             {!! $formComponent->required ? 'required' : null !!}
             {!! $formComponent->type ? "type=\"{$formComponent->type}\"" : null !!}
-            class="block w-full rounded-r-md placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ $errors->has($formComponent->name) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }} {{ !$formComponent->prefix ? 'rounded-l-md' : null }}"
+            class="block w-full rounded-r-md placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ ! $formComponent->prefix ? 'rounded-l-md' : null }} {{ $errors->has($formComponent->name) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
             {!! Filament\format_attributes($formComponent->extraAttributes) !!}
         />
     </div>
