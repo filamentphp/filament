3383e587029e6bc3e21481111d873fc852185ffe
sebapastore <sebastianerp@gmail.com>
2021-03-09 10:42:10 -0300

    Add spanish translation to forms and tables packages

---
+++ packages/tables/resources/lang/es/table.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    'delete' => [
+
+        'button' => [
+            'label' => 'Eliminar seleccionados',
+        ],
+
+        'modal' => [
+
+            'buttons' => [
+
+                'cancel' => [
+                    'label' => 'Cancelar',
+                ],
+
+                'delete' => [
+                    'label' => 'Eliminar seleccionados',
+                ],
+
+            ],
+
+            'description' => '¿Está seguro que desea eliminar los registros seleccionados? Esta acción no se puede deshacer.',
+
+            'heading' => '¿Eliminar los registros seleccionados?',
+
+        ],
+
+    ],
+
+    'filter' => [
+        'placeholder' => 'Filtro',
+    ],
+
+    'messages' => [
+        'noRecords' => 'No se encontraron registros',
+    ],
+
+    'pagination' => [
+
+        'fields' => [
+
+            'recordsPerPage' => [
+                'label' => 'por página',
+            ],
+
+        ],
+
+    ],
+
+    'record' => [
+
+        'button' => [
+            'label' => 'Editar',
+        ],
+
+    ],
+
+    'search' => [
+        'placeholder' => 'Buscar',
+    ],
+
+];
