a4b6e34164d5480cfcf94556735459a1864bc430
Emanuel Quimper <quimperemanuel@gmail.com>
2021-03-08 22:03:18 -0500

    Add french to table resource

---
+++ packages/tables/resources/lang/fr/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'buttons' => [
+        'goToPage' => 'Aller à la page :page',
+        'next' => 'Suivant',
+        'previous' => 'Précédent',
+    ],
+
+    'label' => 'Navigation par pagination',
+
+    'overview' => [
+        'Montrer',
+        'à',
+        'de',
+        'résultats',
+    ],
+
+];
