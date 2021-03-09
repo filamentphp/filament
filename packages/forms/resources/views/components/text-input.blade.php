39ee6e160e284623334b3b105beb8c1d959cd004
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-09 11:32:18 +0000

    feature: add support for input prefixes

--- packages/forms/resources/views/components/text-input.blade.php
+++ packages/forms/resources/views/components/text-input.blade.php
@@ -7,18 +7,25 @@
     :label="__($formComponent->label)"
     :required="$formComponent->required"
 >
-    <input
-        {!! $formComponent->autocomplete ? "autocomplete=\"{$formComponent->autocomplete}\"" : null !!}
-        {!! $formComponent->autofocus ? 'autofocus' : null !!}
-        {!! $formComponent->disabled ? 'disabled' : null !!}
-        {!! $formComponent->id ? "id=\"{$formComponent->id}\"" : null !!}
-        {!! $formComponent->name ? "{$formComponent->nameAttribute}=\"{$formComponent->name}\"" : null !!}
-        {!! $formComponent->maxLength ? "maxlength=\"{$formComponent->maxLength}\"" : null !!}
-        {!! $formComponent->minLength ? "minlength=\"{$formComponent->minLength}\"" : null !!}
-        {!! $formComponent->placeholder ? "placeholder=\"{$formComponent->placeholder}\"" : null !!}
-        {!! $formComponent->required ? 'required' : null !!}
-        {!! $formComponent->type ? "type=\"{$formComponent->type}\"" : null !!}
-        class="block w-full rounded shadow-sm placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ $errors->has($formComponent->name) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
-        {!! Filament\format_attributes($formComponent->extraAttributes) !!}
-    />
+    <div class="flex border-gray-300 rounded shadow-sm">
+        @if($formComponent->prefix)
+            <span class="inline-flex items-center px-3 text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50 sm:text-sm">
+                {{ $formComponent->prefix }}
+            </span>
+        @endif
+        <input
+            {!! $formComponent->autocomplete ? "autocomplete=\"{$formComponent->autocomplete}\"" : null !!}
+            {!! $formComponent->autofocus ? 'autofocus' : null !!}
+            {!! $formComponent->disabled ? 'disabled' : null !!}
+            {!! $formComponent->id ? "id=\"{$formComponent->id}\"" : null !!}
+            {!! $formComponent->name ? "{$formComponent->nameAttribute}=\"{$formComponent->name}\"" : null !!}
+            {!! $formComponent->maxLength ? "maxlength=\"{$formComponent->maxLength}\"" : null !!}
+            {!! $formComponent->minLength ? "minlength=\"{$formComponent->minLength}\"" : null !!}
+            {!! $formComponent->placeholder ? "placeholder=\"{$formComponent->placeholder}\"" : null !!}
+            {!! $formComponent->required ? 'required' : null !!}
+            {!! $formComponent->type ? "type=\"{$formComponent->type}\"" : null !!}
+            class="block w-full rounded-r-md placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ $errors->has($formComponent->name) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
+            {!! Filament\format_attributes($formComponent->extraAttributes) !!}
+        />
+    </div>
 </x-forms::field-group>
