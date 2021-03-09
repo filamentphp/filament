13b8949e14762a5cf5052bac3864f6f81023dbb2
Dennis <dennissmink@gmail.com>
2021-03-09 11:21:00 +0100

    more wip

---
+++ packages/forms/resources/lang/nl/fields.php
@@ -0,0 +1,88 @@
+<?php
+
+return [
+
+    'dateTimePicker' => [
+
+        'abbreviatedDays' => [
+            'friday' => 'Vrij',
+            'monday' => 'Maa',
+            'saturday' => 'Za',
+            'sunday' => 'Zo',
+            'thursday' => 'Don',
+            'tuesday' => 'Din',
+            'wednesday' => 'Woe',
+        ],
+
+        'months' => [
+            'april' => 'April',
+            'august' => 'Augustus',
+            'december' => 'December',
+            'february' => 'Februari',
+            'january' => 'Januari',
+            'july' => 'Juli',
+            'june' => 'Juni',
+            'march' => 'Maart',
+            'may' => 'Mei',
+            'november' => 'November',
+            'october' => 'October',
+            'september' => 'September',
+        ],
+
+    ],
+
+    'multiSelect' => [
+        'emptyOptionsMessage' => 'Geen opties om te selecteren.',
+        'noSearchResultsMessage' => 'Er zijn geen resultaten voor je zoekopdracht.',
+        'placeholder' => 'Selecteer een optie',
+    ],
+
+    'richEditor' => [
+
+        'dialogs' => [
+
+            'link' => [
+
+                'buttons' => [
+                    'link' => 'Link',
+                    'unlink' => 'Unlink',
+                ],
+
+                'label' => 'URL',
+
+                'placeholder' => 'Voer een URL in',
+
+            ],
+
+        ],
+
+        'toolbarButtons' => [
+            'attachFiles' => 'Voeg bestanden toe',
+            'bold' => 'Dikgedrukt',
+            'bullet' => 'Bullets',
+            'code' => 'Code',
+            'heading' => 'Hoofdtekst',
+            'italic' => 'Schuingedrukt',
+            'link' => 'Link',
+            'number' => 'Nummers',
+            'quote' => 'Quote',
+            'redo' => 'Opnieuw doen',
+            'strike' => 'Doorgehaald',
+            'subheading' => 'Sub hoofdtekst',
+            'title' => 'Titel',
+            'undo' => 'Ongedaan maken',
+        ],
+
+    ],
+
+    'select' => [
+        'emptyOptionsMessage' => 'Start met typen om te zoeken...',
+        'noSearchResultsMessage' => 'Er zijn geen resultaten voor je zoekopdracht.',
+        'placeholder' => 'Selecteer een optie',
+    ],
+
+    'tags' => [
+        'placeholder' => 'Nieuwe tag',
+    ],
+
+];
