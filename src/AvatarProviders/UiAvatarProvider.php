b5f2419be781c3e6b5c996ab64df57c9329321e1
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-10 18:37:18 +0000

    chore: update colours

--- src/AvatarProviders/UiAvatarProvider.php
+++ src/AvatarProviders/UiAvatarProvider.php
@@ -8,6 +8,6 @@ class UiAvatarProvider implements Contracts\AvatarProvider
 {
     public function get(FilamentUser $user, $size = 48, $dpr = 1)
     {
-        return 'https://ui-avatars.com/api/?name='.urlencode($user->name).'&color=7F9CF5&background=EBF4FF&size='.$size * $dpr;
+        return 'https://ui-avatars.com/api/?name='.urlencode($user->name).'&color=FFFFFF&background=0284c7&size='.$size * $dpr;
     }
 }
