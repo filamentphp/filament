279a9c33cdff5619dd47929f0a40c7bbfdd56c2b
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-10 17:15:05 +0000

    wip

---
+++ src/AvatarProviders/GravatarProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Filament;
+
+use Filament\Models\Contracts\FilamentUser;
+
+class GravatarProvider implements Contracts\AvatarProvider
+{
+    public function get(FilamentUser $user)
+    {
+
+    }
+}
