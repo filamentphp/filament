456af3cf6fe06a2db5c1160179aff5f85447d1fa
Lado Lomidze <lado.lomidze@gmail.com>
2021-03-11 13:45:28 +0400

    add georgian(ka) language translations

---
+++ resources/lang/ka/dashboard.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'title' => 'დეშბორდი',
+
+    'widgets' => [
+
+        'account' => [
+
+            'heading' => 'გამარჯობა, :name',
+
+            'links' => [
+
+                'account' => [
+                    'label' => 'პროფილის რედაქტირება',
+                ],
+
+            ],
+
+        ],
+
+        'filament' => [
+
+            'links' => [
+
+                'documentation' => [
+                    'label' => 'დოკუმენტაცია',
+                ],
+
+                'repository' => [
+                    'label' => 'რეპოზიტორი',
+                ],
+
+            ],
+
+        ],
+
+    ],
+
+];
---
+++ resources/lang/ka/resources/pages/list-records.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'buttons' => [
+
+        'create' => [
+            'label' => 'დამატება',
+        ],
+
+    ],
+
+    'table' => [
+
+        'recordActions' => [
+
+            'edit' => [
+                'label' => 'რედაქტირება',
+            ],
+
+        ],
+
+    ],
+
+];
