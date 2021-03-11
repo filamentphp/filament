04e2ae56b2964fe00675bed05f7a1c1f5cbcb222
Lado Lomidze <lado.lomidze@gmail.com>
2021-03-11 16:20:22 +0400

    add georgian (ka) translations to forms and packages

---
+++ packages/forms/resources/lang/ka/fields.php
@@ -0,0 +1,88 @@
+<?php
+
+return [
+
+    'dateTimePicker' => [
+
+        'abbreviatedDays' => [
+            'friday' => 'პარ',
+            'monday' => 'ორშ',
+            'saturday' => 'შაბ',
+            'sunday' => 'კვი',
+            'thursday' => 'ხუთ',
+            'tuesday' => 'სამ',
+            'wednesday' => 'ოთხ',
+        ],
+
+        'months' => [
+            'april' => 'აპრილი',
+            'august' => 'აგვისტო',
+            'december' => 'სექტემბერი',
+            'february' => 'თებერვარი',
+            'january' => 'იანვარი',
+            'july' => 'ივლისი',
+            'june' => 'ივნისი',
+            'march' => 'მარტი',
+            'may' => 'მაისი',
+            'november' => 'ნოემბერი',
+            'october' => 'ოტქომბერი',
+            'september' => 'სექტემბერი',
+        ],
+
+    ],
+
+    'multiSelect' => [
+        'emptyOptionsMessage' => 'ჩანაწერების სია ცარიელია.',
+        'noSearchResultsMessage' => 'ამ პარამეტრებით არცერთი ჩანაწერი არ მოიძებნა.',
+        'placeholder' => 'აირჩიეთ ჩანაწერი',
+    ],
+
+    'richEditor' => [
+
+        'dialogs' => [
+
+            'link' => [
+
+                'buttons' => [
+                    'link' => 'დამატება',
+                    'unlink' => 'გაუქმება',
+                ],
+
+                'label' => 'ლინკი',
+
+                'placeholder' => 'ჩაწერეთ URL მისამართი',
+
+            ],
+
+        ],
+
+        'toolbarButtons' => [
+            'attachFiles' => 'ფაილების დამატება',
+            'bold' => 'მსხვილი',
+            'bullet' => 'სია',
+            'code' => 'კოდი',
+            'heading' => 'სათაური',
+            'italic' => 'დახრილი',
+            'link' => 'ლინკი',
+            'number' => 'რიცხვები',
+            'quote' => 'ციტატა',
+            'redo' => 'Redo',
+            'strike' => 'ხაზგადასმული',
+            'subheading' => 'ქვესათაური',
+            'title' => 'სათაური',
+            'undo' => 'Undo',
+        ],
+
+    ],
+
+    'select' => [
+        'emptyOptionsMessage' => 'ძიება...',
+        'noSearchResultsMessage' => 'ამ პარამეტრებით არცერთი ჩანაწერი არ მოიძებნა.',
+        'placeholder' => 'აირჩიეთ ჩანაწერი',
+    ],
+
+    'tags' => [
+        'placeholder' => 'ახალი ტეგი',
+    ],
+
+];
