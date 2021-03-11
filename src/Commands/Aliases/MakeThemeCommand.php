c96b61d204fad1a2fbed0dc7e96fef31e2a3f475
Dan Harrin <git@danharrin.com>
2021-03-11 23:19:04 +0000

    add hidden props

--- src/Commands/Aliases/MakeThemeCommand.php
+++ src/Commands/Aliases/MakeThemeCommand.php
@@ -5,4 +5,6 @@ namespace Filament\Commands\Aliases;
 class MakeThemeCommand extends \Filament\Commands\MakeThemeCommand
 {
     protected $signature = 'filament:theme {name}';
+
+    protected $hidden = true;
 }
