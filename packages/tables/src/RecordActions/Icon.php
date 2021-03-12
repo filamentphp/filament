a7cbea4026d06fb94cb58cd76a3980b7c3530b25
Dan Harrin <git@danharrin.com>
2021-03-12 22:44:43 +0000

    Support record action icons

---
+++ packages/tables/src/RecordActions/Icon.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Filament\Tables\RecordActions;
+
+class Icon extends Action
+{
+    use Concerns\CanCallAction;
+    use Concerns\CanOpenUrl;
+
+    protected $icon;
+
+    public function icon($icon)
+    {
+        $this->configure(function () use ($icon) {
+            $this->icon = $icon;
+        });
+
+        return $this;
+    }
+
+    public function getIcon()
+    {
+        return $this->icon;
+    }
+}
---
+++ src/Resources/Tables/RecordActions/Icon.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Filament\Resources\Tables\RecordActions;
+
+class Icon extends \Filament\Tables\RecordActions\Icon
+{
+    //
+}
