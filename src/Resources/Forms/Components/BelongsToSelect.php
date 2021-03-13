2e23aa5467d3ec38cc7bb0806a451751bbb3de33
Dan Harrin <git@danharrin.com>
2021-03-13 08:07:34 +0000

    fixes

--- src/Resources/Forms/Components/BelongsToSelect.php
+++ src/Resources/Forms/Components/BelongsToSelect.php
@@ -6,85 +6,131 @@ use Illuminate\Support\Str;
 
 class BelongsToSelect extends Select
 {
-    public $getOptions;
+    protected $getOptions;
 
-    public $view = 'forms::components.select';
+    protected $displayColumnName;
+
+    protected $relationship;
+
+    protected $view = 'forms::components.select';
+
+    public function getDisplayColumnName()
+    {
+        return $this->displayColumnName;
+    }
+
+    public function getLabel()
+    {
+        if ($this->label === null) {
+            return (string) Str::of($this->getRelationshipName())
+                ->before('.')
+                ->kebab()
+                ->replace(['-', '_'], ' ')
+                ->ucfirst();
+        }
+
+        return parent::getLabel();
+    }
+
+    public function getOptions()
+    {
+        if ($callback = $this->getOptions) {
+            return $callback();
+        }
+
+        return parent::getOptions();
+    }
 
     public function getOptionsUsing($callback)
     {
-        $this->getOptions = $callback;
+        $this->configure(function () use ($callback) {
+            $this->getOptions = $callback;
+        });
 
         return $this;
     }
 
+    public function getRelationship()
+    {
+        $model = $this->getModel();
+
+        return (new $model())->{$this->getRelationshipName()}();
+    }
+
+    public function getRelationshipName()
+    {
+        return $this->relationship;
+    }
+
     public function preload()
     {
-        $options = $this->getOptions();
+        $this->configure(function () {
+            $options = $this->getOptions();
 
-        if (! $options || ! $this->getModel()) return;
+            if (! $options || ! $this->getModel()) return;
 
-        $this->options = $options;
+            $this->options = $options;
+        });
 
         return $this;
     }
 
     public function relationship($relationshipName, $displayColumnName, $callback = null)
     {
-        $this->getDisplayValueUsing(function ($value) use ($displayColumnName, $relationshipName) {
-            $model = $this->getModel();
+        $this->configure(function () use ($callback, $displayColumnName, $relationshipName) {
+            $this->displayColumnName = $displayColumnName;
+            $this->relationship = $relationshipName;
 
-            $relationship = (new $model())->{$relationshipName}();
+            $this->getDisplayValueUsing(function ($value) {
+                $relationship = $this->getRelationship();
 
-            $record = $relationship->getRelated()->where($relationship->getOwnerKeyName(), $value)->first();
+                $record = $relationship->getRelated()->where($relationship->getOwnerKeyName(), $value)->first();
 
-            return $record ? $record->getAttributeValue($displayColumnName) : null;
-        });
+                return $record ? $record->getAttributeValue($this->getDisplayColumnName()) : null;
+            });
 
-        $this->getOptionSearchResultsUsing(function ($search) use ($callback, $displayColumnName, $relationshipName) {
-            $model = $this->getModel();
+            $this->getOptionSearchResultsUsing(function ($search) use ($callback) {
+                $relationship = $this->getRelationship();
 
-            $relationship = (new $model())->{$relationshipName}();
+                $query = $relationship->getRelated();
 
-            $query = $relationship->getRelated();
+                if ($callback) {
+                    $query = $callback($query);
+                }
 
-            if ($callback) {
-                $query = $callback($query);
+                $search = Str::lower($search);
+                $searchOperator = [
+                    'pgsql' => 'ilike',
+                ][$query->getConnection()->getDriverName()] ?? 'like';
+
+                return $query
+                    ->where($this->getDisplayColumnName(), $searchOperator, "%{$search}%")
+                    ->pluck($this->getDisplayColumnName(), $relationship->getOwnerKeyName())
+                    ->toArray();
+            });
+
+            $this->getOptionsUsing(function () use ($callback) {
+                $relationship = $this->getRelationship();
+
+                $query = $relationship->getRelated();
+
+                if ($callback) {
+                    $query = $callback($query);
+                }
+
+                return $query
+                    ->pluck($this->getDisplayColumnName(), $relationship->getOwnerKeyName())
+                    ->toArray();
+            });
+
+            if ($model = $this->getModel()) {
+                $relationship = $this->getRelationship();
+
+                $relatedModelClass = get_class($relationship->getModel());
+
+                $this->addRules([$this->getName() => "exists:{$relatedModelClass},{$relationship->getOwnerKeyName()}"]);
             }
-
-            $search = Str::lower($search);
-            $searchOperator = [
-                'pgsql' => 'ilike',
-            ][$query->getConnection()->getDriverName()] ?? 'like';
-
-            return $query
-                ->where($displayColumnName, $searchOperator, "%{$search}%")
-                ->pluck($displayColumnName, $relationship->getOwnerKeyName())
-                ->toArray();
         });
-
-        $this->getOptionsUsing(function () use ($callback, $displayColumnName, $relationshipName) {
-            $model = $this->getModel();
-
-            $relationship = (new $model())->{$relationshipName}();
-
-            $query = $relationship->getRelated();
-
-            if ($callback) {
-                $query = $callback($query);
-            }
-
-            return $query
-                ->pluck($displayColumnName, $relationship->getOwnerKeyName())
-                ->toArray();
-        });
-
-        $this->label(
-            (string) Str::of($relationshipName)
-                ->before('.')
-                ->kebab()
-                ->replace(['-', '_'], ' ')
-                ->ucfirst(),
-        );
 
         return $this;
     }
