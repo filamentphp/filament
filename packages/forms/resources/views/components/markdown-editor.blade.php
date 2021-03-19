e195cf9c365693812b09dae33f7406cf942e545a
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-19 17:56:38 +0000

    wip [skip-ci]

--- packages/forms/resources/views/components/markdown-editor.blade.php
+++ packages/forms/resources/views/components/markdown-editor.blade.php
@@ -98,12 +98,12 @@
                     <div class="flex items-stretch space-x-1">
                         <x-filament::button size="small" class="text-base">
                             <md-unordered-list class="w-full h-full">
-                                <svg version="1" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M4 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm0 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm0 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm4 3h14v-2H8v2zm0-6h14v-2H8v2zm0-8v2h14V5H8z"/></svg>
+                                <span>&bull;</span>
                             </md-unordered-list>
                         </x-filament::button>
                         <x-filament::button size="small" class="text-base">
                             <md-ordered-list class="w-full h-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M2 17h2v.5H3v1h1v.5H2v1h3v-4H2v1zm1-9h1V4H2v1h1v3zm-1 3h1.8L2 13.1v.9h3v-1H3.2L5 10.9V10H2v1zm5-6v2h14V5H7zm0 14h14v-2H7v2zm0-6h14v-2H7v2z"/></svg>
+                                <span>𝟏𝟐𝟑</span>
                             </md-ordered-list>
                         </x-filament::button>
                     </div>
