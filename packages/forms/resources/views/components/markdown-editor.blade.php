6b7cad8991c21c9c64399ab712b66de4f25be943
Sam Snelling <sam@onroi.com>
2021-03-21 16:13:09 -0500

    Update markdown-editor.blade.php

--- packages/forms/resources/views/components/markdown-editor.blade.php
+++ packages/forms/resources/views/components/markdown-editor.blade.php
@@ -279,12 +279,12 @@
                         x-on:keyup.enter="checkForAutoInsertion"
                         x-on:file-attachment-accepted.window="uploadAttachments"
                         x-ref="textarea"
-                        class="tracking-normal whitespace-pre-wrap font-mono text-sm absolute bg-transparent top-0 left-0 block z-1 w-full h-full min-h-full rounded resize-none shadow-sm placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ $errors->has($formComponent->getName()) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
+                        class="tracking-normal whitespace-pre-wrap overflow-y-hidden font-mono text-sm absolute bg-transparent top-0 left-0 block z-1 w-full h-full min-h-full rounded resize-none shadow-sm placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ $errors->has($formComponent->getName()) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
                     ></textarea>
                 </file-attachment>
 
                 <div
-                    class="w-full h-full font-mono text-sm tracking-normal text-black break-words whitespace-pre-wrap"
+                    class="w-full h-full border border-transparent font-mono text-sm tracking-normal text-black break-words whitespace-pre-wrap"
                     x-ref="overlay"
                     x-html="overlay"
                 ></div>
