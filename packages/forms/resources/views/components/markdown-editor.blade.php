f85a439e40998100b4f3a3de10fce3e76b1f2a3b
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-21 12:30:42 +0000

    fix: overlay markdown height

--- packages/forms/resources/views/components/markdown-editor.blade.php
+++ packages/forms/resources/views/components/markdown-editor.blade.php
@@ -5,6 +5,7 @@
         [x-data^="markdownEditor"] textarea {
             color: transparent;
             caret-color: black;
+            /* -webkit-text-fill-color: transparent; */
         }
     </style>
 @endpushonce
@@ -76,8 +77,6 @@
                 init: function () {
                     this.value = this.$refs.textarea.value
 
-                    this.$refs.overlay.style.padding = window.getComputedStyle(this.$refs.textarea).padding
-
                     this.overlay = mdhl.highlight(this.$refs.textarea.value)
 
                     this.$watch('tab', () => {
@@ -88,7 +87,7 @@
                 },
 
                 resize: function () {
-                    this.$el.style.height = this.$refs.textarea.style.height
+                    this.$refs.overlay.style.height = this.$refs.textarea.scrollHeight + 'px'
 
                     this.overlay = mdhl.highlight(this.value = this.$refs.textarea.value)
                 },
@@ -274,11 +273,16 @@
                         x-on:keyup.enter="checkForAutoInsertion"
                         x-on:file-attachment-accepted.window="uploadAttachments"
                         x-ref="textarea"
-                        class="absolute bg-transparent top-0 left-0 block z-1 w-full h-full min-h-full rounded resize-none shadow-sm placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ $errors->has($formComponent->getName()) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
+                        class="font-mono text-sm absolute bg-transparent top-0 left-0 block z-1 w-full h-full min-h-full rounded resize-none shadow-sm placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ $errors->has($formComponent->getName()) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
                     ></textarea>
                 </file-attachment>
 
-                <div class="w-full h-full text-black break-words" x-ref="overlay" x-html="overlay"></div>
+                <div
+                    class="w-full h-full font-mono text-sm text-black break-words"
+                    style="padding: 8px 12px;"
+                    x-ref="overlay"
+                    x-html="overlay"
+                ></div>
             </div>
 
             <div class="block w-full h-full min-h-full px-6 py-4 border border-gray-300 rounded shadow-sm focus:border-blue-300" x-show="tab === 'preview'" style="min-height: 150px;">
