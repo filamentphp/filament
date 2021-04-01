46ae41122f058805f8705f07437a71189b564bca
Bezhan Salleh <bezhan_salleh@yahoo.com>
2021-04-01 13:58:09 +0430

    Added Persian/Farsi Language Translations

---
+++ resources/lang/fa/edit-account.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'buttons' => [
+
+        'save' => [
+            'label' => 'ذخیره',
+        ],
+
+    ],
+
+    'messages' => [
+        'saved' => 'ذخیره شد!',
+    ],
+
+    'title' => 'حساب',
+
+];
---
+++ resources/lang/fa/resources/relation-manager.php
@@ -0,0 +1,132 @@
+<?php
+
+return [
+
+    'buttons' => [
+
+        'attach' => [
+            'label' => 'پیوستن موجود',
+        ],
+
+        'create' => [
+            'label' => 'جدید',
+        ],
+
+        'detach' => [
+            'label' => 'جدا کردن انتخاب شده',
+        ],
+
+    ],
+
+    'modals' => [
+
+        'attach' => [
+
+            'buttons' => [
+
+                'cancel' => [
+                    'label' => 'لغو',
+                ],
+
+                'attach' => [
+                    'label' => 'پیوست',
+                ],
+
+                'attachAnother' => [
+                    'label' => 'پیوست و پیوستن دیگر',
+                ],
+
+            ],
+
+            'form' => [
+
+                'related' => [
+                    'placeholder' => 'برای جستجو تایپ کنید...',
+                ],
+
+            ],
+
+            'heading' => 'پیوستن موجدی',
+
+            'messages' => [
+                'attached' => 'پیوست شد!',
+            ],
+
+        ],
+
+        'create' => [
+
+            'buttons' => [
+
+                'cancel' => [
+                    'label' => 'لغو',
+                ],
+
+                'create' => [
+                    'label' => 'ایجاد',
+                ],
+
+                'createAnother' => [
+                    'label' => 'ایجاد و ایجاد دیگر',
+                ],
+
+            ],
+
+            'heading' => 'ایجاد',
+
+            'messages' => [
+                'created' => 'ایجاد شد!',
+            ],
+
+        ],
+
+        'detach' => [
+
+            'buttons' => [
+
+                'cancel' => [
+                    'label' => 'لغو',
+                ],
+
+                'detach' => [
+                    'label' => 'جدا کردن انتخاب شده',
+                ],
+
+            ],
+
+            'description' => 'آیا مطمتن اید تا ریکاردهای انتخاب شده را جدا کنید؟ این عمل قابل برگشت نیست.',
+
+
+            'heading' => 'آیا ریکاردهای انتخاب شده جدا شود؟',
+
+            'messages' => [
+                'detached' => 'جدا شد!',
+            ],
+
+        ],
+
+        'edit' => [
+
+            'buttons' => [
+
+                'cancel' => [
+                    'label' => 'لغو',
+                ],
+
+                'save' => [
+                    'label' => 'ذخیره',
+                ],
+
+            ],
+
+            'heading' => 'ویرایش',
+
+            'messages' => [
+                'saved' => 'ذخیره شد!',
+            ],
+
+        ],
+
+    ],
+
+];
