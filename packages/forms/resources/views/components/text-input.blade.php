16b412ae602b42d8b6721fa20728d8a7170089e5
Dan Harrin <git@danharrin.com>
2021-04-12 00:45:01 +0100

    fix inconsistent border rounding

--- packages/forms/resources/views/components/text-input.blade.php
+++ packages/forms/resources/views/components/text-input.blade.php
@@ -9,7 +9,7 @@
 >
     <div class="flex border-gray-300 rounded shadow-sm">
         @if ($formComponent->getPrefix())
-            <span class="inline-flex items-center px-3 text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50 sm:text-sm">
+            <span class="inline-flex items-center px-3 text-gray-500 border border-r-0 border-gray-300 rounded-l bg-gray-50 sm:text-sm">
                 {{ $formComponent->getPrefix() }}
             </span>
         @endif
@@ -25,7 +25,7 @@
             {!! $formComponent->getPlaceholder() ? "placeholder=\"{$formComponent->getPlaceholder()}\"" : null !!}
             {!! $formComponent->isRequired() ? 'required' : null !!}
             {!! $formComponent->getType() ? "type=\"{$formComponent->getType()}\"" : null !!}
-            class="block w-full rounded-r-md placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ ! $formComponent->getPrefix() ? 'rounded-l-md' : null }} {{ $errors->has($formComponent->getName()) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
+            class="block w-full rounded-r placeholder-gray-400 focus:placeholder-gray-500 placeholder-opacity-100 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 {{ ! $formComponent->getPrefix() ? 'rounded-l-md' : null }} {{ $errors->has($formComponent->getName()) ? 'border-danger-600 motion-safe:animate-shake' : 'border-gray-300' }}"
             {!! Filament\format_attributes($formComponent->getExtraAttributes()) !!}
         />
     </div>
