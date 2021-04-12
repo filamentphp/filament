406a1a180e3d7357697e04311ba7b2b89a1ab0f9
tanthammar <tinahammar@gmail.com>
2021-04-12 17:54:54 +0200

    swedish table

---
+++ packages/tables/resources/lang/sv/table.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    'delete' => [
+
+        'button' => [
+            'label' => 'Radera valda',
+        ],
+
+        'modal' => [
+
+            'buttons' => [
+
+                'cancel' => [
+                    'label' => 'Ångra',
+                ],
+
+                'delete' => [
+                    'label' => 'Radera valda',
+                ],
+
+            ],
+
+            'description' => 'Är du säker på att du vill radera de valda raderna? Det går inte att ångra!',
+
+            'heading' => 'Radera de valda raderna?',
+
+        ],
+
+    ],
+
+    'filter' => [
+        'placeholder' => 'Filter',
+    ],
+
+    'messages' => [
+        'noRecords' => 'Finns inga rader',
+    ],
+
+    'pagination' => [
+
+        'fields' => [
+
+            'recordsPerPage' => [
+                'label' => 'per sida',
+            ],
+
+        ],
+
+    ],
+
+    'search' => [
+        'placeholder' => 'Sök',
+    ],
+
+];
