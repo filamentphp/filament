f1e525953cd58c8220eada0e2a12cb60ac9ad153
tanthammar <tinahammar@gmail.com>
2021-04-12 17:41:48 +0200

    start sv

---
+++ packages/forms/resources/lang/sv/fields.php
@@ -0,0 +1,139 @@
+<?php
+
+return [
+
+    'dateTimePicker' => [
+
+        'abbreviatedDays' => [
+            'friday' => 'Fre',
+            'monday' => 'Mån',
+            'saturday' => 'Lör',
+            'sunday' => 'Sön',
+            'thursday' => 'Tor',
+            'tuesday' => 'Tis',
+            'wednesday' => 'Ons',
+        ],
+
+        'months' => [
+            'april' => 'April',
+            'august' => 'Augusti',
+            'december' => 'December',
+            'february' => 'Februari',
+            'january' => 'Januari',
+            'july' => 'Juli',
+            'june' => 'Juni',
+            'march' => 'Mars',
+            'may' => 'Maj',
+            'november' => 'November',
+            'october' => 'Oktober',
+            'september' => 'September',
+        ],
+
+    ],
+
+    'keyValue' => [
+
+        'buttons' => [
+
+            'add' => [
+                'label' => 'Add Row',
+            ],
+
+            'delete' => [
+                'label' => 'Delete Row',
+            ],
+
+            'sort' => [
+                'label' => 'Hold to Sort',
+            ],
+
+        ],
+
+        'fields' => [
+
+            'key' => [
+                'label' => 'Key',
+                'placeholder' => 'Enter a key...',
+            ],
+
+            'value' => [
+                'label' => 'Value',
+                'placeholder' => 'Enter a value...',
+            ],
+
+        ],
+
+    ],
+
+    'markdownEditor' => [
+
+        'toolbarButtons' => [
+            'attachFiles' => 'Attach Files',
+            'bold' => 'Bold',
+            'bullet' => 'Bullets',
+            'code' => 'Code',
+            'italic' => 'Italic',
+            'link' => 'Link',
+            'number' => 'Numbers',
+            'preview' => 'Preview',
+            'strike' => 'Strikethrough',
+            'write' => 'Write',
+        ],
+
+    ],
+
+    'multiSelect' => [
+        'emptyOptionsMessage' => 'No options to choose from.',
+        'noSearchResultsMessage' => 'No options match your search.',
+        'placeholder' => 'Select an option',
+    ],
+
+    'richEditor' => [
+
+        'dialogs' => [
+
+            'link' => [
+
+                'buttons' => [
+                    'link' => 'Link',
+                    'unlink' => 'Unlink',
+                ],
+
+                'label' => 'URL',
+
+                'placeholder' => 'Enter a URL',
+
+            ],
+
+        ],
+
+        'toolbarButtons' => [
+            'attachFiles' => 'Attach Files',
+            'bold' => 'Bold',
+            'bullet' => 'Bullets',
+            'code' => 'Code',
+            'heading' => 'Heading',
+            'italic' => 'Italic',
+            'link' => 'Link',
+            'number' => 'Numbers',
+            'quote' => 'Quote',
+            'redo' => 'Redo',
+            'strike' => 'Strikethrough',
+            'subheading' => 'Subheading',
+            'title' => 'Title',
+            'undo' => 'Undo',
+        ],
+
+    ],
+
+    'select' => [
+        'emptyOptionsMessage' => 'Start typing to search...',
+        'noSearchResultsMessage' => 'No options match your search.',
+        'placeholder' => 'Select an option',
+    ],
+
+    'tags' => [
+        'placeholder' => 'New tag',
+    ],
+
+];
