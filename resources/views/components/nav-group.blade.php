ef4df890ce58308567f9dbc060d7792589a1388d
Dan Pock <self@danpock.me>
2021-04-20 21:07:19 -0400

    ensure nav groups have unique variables

--- resources/views/components/nav-group.blade.php
+++ resources/views/components/nav-group.blade.php
@@ -5,16 +5,19 @@
     'items',
 ])
 
-<li x-data="{ subNavOpen : {{ (request()->is($activeRule)) ? 'true' : 'false' }} }">
+@php
+$subNavLabel = 'subNavOpen' . Str::studly($label);
+@endphp
+<li x-data="{ {{ $subNavLabel }} : {{ (request()->is($activeRule)) ? 'true' : 'false' }} }">
     <a
-        @click="subNavOpen = !subNavOpen"
+        @click="{{ $subNavLabel }} = !{{ $subNavLabel }}"
         {{ $attributes->merge(['class' => 'px-4 py-3 flex items-center space-x-3 rtl:space-x-reverse rounded transition-color duration-200 hover:text-white ' . (request()->is($activeRule) ? 'text-white bg-gray-700' : 'text-current')]) }}
     >
         <x-dynamic-component :component="$icon" class="flex-shrink-0 w-5 h-5" />
 
         <span class="flex-grow text-sm font-medium leading-tight">{{ __($label) }}</span>
     </a>
-    <ol :class="{ 'block' : subNavOpen , 'hidden' : !subNavOpen}"  @click.away="subNavOpen = false">
+    <ol :class="{ 'block' : {{ $subNavLabel }} , 'hidden' : !{{ $subNavLabel }}}"  @click.away="{{ $subNavLabel }} = false">
         @foreach($items as $item)
             <li class="border-l-2 {{ (request()->is($item->activeRule)) ? 'border-gray-700' : 'border-gray-300' }}">
                 <x-filament::nav-link
