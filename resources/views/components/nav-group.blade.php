dbcbc4a0d69e1508d402c58e31537e786a55fc0b
Dan Pock <self@danpock.me>
2021-04-23 10:10:48 -0400

    make dropdown rendering uniform for all groups

--- resources/views/components/nav-group.blade.php
+++ resources/views/components/nav-group.blade.php
@@ -12,27 +12,25 @@
 $subNavLabel = 'subNavOpen' . Str::studly($label);
 @endphp
 <li x-data="{ {{ $subNavLabel }} : {{ (request()->is($activeRule)) ? 'true' : 'false' }} }">
-    @if($isResourceGroup)
     <div class="inline-flex w-full {{ (request()->is($activeRule)) ? 'bg-gray-700' : '' }}">
-    @endif
     <a
         @if($isResourceGroup)
         href="{{ $url }}"
         @else
         @click="{{ $subNavLabel }} = !{{ $subNavLabel }}"
         @endif
-        {{ $attributes->merge(['class' => 'px-4 py-3 flex items-center space-x-3 rtl:space-x-reverse rounded transition-color duration-200 hover:text-white ' . ($isResourceGroup ? 'flex-grow ' : '') . (request()->is($activeRule) ? 'text-white bg-gray-700' : 'text-current')]) }}
+        {{ $attributes->merge(['class' => 'px-4 py-3 flex flex-grow items-center space-x-3 rtl:space-x-reverse rounded transition-color duration-200 hover:text-white ' . (request()->is($activeRule) ? 'text-white bg-gray-700' : 'text-current')]) }}
     >
         <x-dynamic-component :component="$icon" class="flex-shrink-0 w-5 h-5" />
 
         <span class="flex-grow text-sm font-medium leading-tight">{{ __($label) }}</span>
     </a>
-    @if($isResourceGroup && count($items) > 0)
-        <button @click="{{ $subNavLabel }} = !{{ $subNavLabel }}"><x-heroicon-o-chevron-down class="w-4" /></button>
-    @endif
-    @if($isResourceGroup)
+    <button @click="{{ $subNavLabel }} = !{{ $subNavLabel }}" class="px-3">
+        <x-heroicon-o-chevron-down class="w-4" x-show="true === {{ $subNavLabel }}" />
+        <x-heroicon-o-chevron-right class="w-4" x-show="false === {{ $subNavLabel }}" />
+    </button>
+
     </div>
-    @endif
     <ol :class="{ 'block' : {{ $subNavLabel }} , 'hidden' : !{{ $subNavLabel }}}"  @click.away="{{ $subNavLabel }} = false">
         @foreach($items as $item)
             <li class="border-l-2 {{ (request()->is($item->activeRule)) ? 'border-gray-700' : 'border-gray-300' }}">
