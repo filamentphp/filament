6d1e06871e00a71347b44eaa94126bec0ea19020
Bernhard Hörmann <hoermann.b@gmail.com>
2021-05-12 17:01:49 +0200

    Allow to change classes of components (example)

--- packages/forms/src/Components/Concerns/CanChangeClass.php
+++ packages/forms/src/Components/Concerns/CanChangeClass.php
@@ -5,28 +5,42 @@ namespace Filament\Forms\Components\Concerns;
 trait CanChangeClass
 {
     protected $addClasses;
-    protected $removedClasses;
+    protected $removeClasses;
 
     public function getChangedClasses($currentClasses = '')
     {
-        $classes = array_merge(
-            explode(' ', trim($currentClasses)),
-            explode(' ', trim($this->addClasses)),
-        );
-        $removeClasses = explode(' ', trim($this->removedClasses));
+        if(is_string($this->removeClasses)) {
+            $classes = array_merge(
+                explode(' ', trim($currentClasses)),
+                explode(' ', trim($this->addClasses)),
+            );
+            $removeClasses = explode(' ', trim($this->removeClasses));
 
-        $classes = array_filter($classes, function($item) use ($removeClasses) {
-            return !in_array($item, $removeClasses);
-        });
+            $classes = array_filter($classes, function($item) use ($removeClasses) {
+                return !in_array($item, $removeClasses);
+            });
 
-        return implode(' ', $classes);
+            return implode(' ', $classes);
+        }
+        elseif(is_bool($this->removeClasses) && $this->removeClasses) {
+            return $this->addClasses;
+        }
+
+
+        return $currentClasses . ' ' . $this->addClasses;
     }
 
-    public function changeClass($addClasses = '', $removedClasses = '')
+    /**
+     * @param string $addClasses
+     * @param string|boolean $removeClasses
+     *
+     * @return $this
+     */
+    public function changeClass($addClasses = '', $removeClasses = '')
     {
-        $this->configure(function () use ($addClasses, $removedClasses) {
+        $this->configure(function () use ($addClasses, $removeClasses) {
             $this->addClasses = $addClasses;
-            $this->removedClasses = $removedClasses;
+            $this->removeClasses = $removeClasses;
         });
 
         return $this;
