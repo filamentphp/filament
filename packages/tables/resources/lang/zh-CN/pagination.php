081eb3502f00bd9dcd0dd6f91d3653c5d10f7f74
darkwind <darkwind@darkwinddeiMac.local>
2021-05-14 14:29:24 +0800

    add Simplified Chinese lang

---
+++ packages/tables/resources/lang/zh-CN/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'buttons' => [
+        'goToPage' => '跳转到 :page',
+        'next' => '下一页',
+        'previous' => '上一页',
+    ],
+
+    'label' => '分页',
+
+    'overview' => [
+        '显示',
+        '到',
+        '的',
+        '结果',
+    ],
+
+];
