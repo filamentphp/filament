c4f8a9387049c0e85823431d34e7f63e7a3c1802
Dan Harrin <git@danharrin.com>
2021-07-19 18:19:33 +0100

    fix formatting

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,10 @@
-@props(['expanded' => false])
+@props([
+    'expanded' => false,
+])
 
-<div {{ $attributes->class(['bg-white shadow-xl rounded p-4 md:p-6', 'col-span-full' => $expanded]) }}>
+<div {{ $attributes->class([
+    'bg-white shadow-xl rounded p-4 md:p-6',
+    'col-span-full' => $expanded,
+]) }}>
     {{ $slot }}
 </div>
