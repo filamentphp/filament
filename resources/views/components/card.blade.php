9b242e75b0fe7f77f28ba0590eec4b4ae7960643
danilopolani <danilo.polani@gmail.com>
2021-07-19 16:52:34 +0200

    add expanded attribute to card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,5 @@
-<div {{ $attributes->merge(['class' => 'bg-white shadow-xl rounded p-4 md:p-6']) }}>
+@props(['expanded' => false])
+
+<div {{ $attributes->class(['bg-white shadow-xl rounded p-4 md:p-6', 'col-span-full' => $expanded]) }}>
     {{ $slot }}
 </div>
