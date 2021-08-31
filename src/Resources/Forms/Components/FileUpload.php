67fc0c61af574ac72436704f21d93712bdf84162
Elisha Witte <elisha.witte@antwerpes.de>
2021-08-31 10:02:09 +0200

    Remove filename logic

--- src/Resources/Forms/Components/FileUpload.php
+++ src/Resources/Forms/Components/FileUpload.php
@@ -5,13 +5,4 @@ namespace Filament\Resources\Forms\Components;
 class FileUpload extends \Filament\Forms\Components\FileUpload
 {
     use Concerns\InteractsWithResource;
-
-    public $storeAsCallback;
-
-    public function storeAs(callable $storeAsCallback)
-    {
-        $this->storeAsCallback = $storeAsCallback;
-
-        return $this;
-    }
 }
