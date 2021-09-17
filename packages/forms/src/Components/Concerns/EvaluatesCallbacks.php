82f1ff4e5194d90a7279b6740e3a75d756368bac
Ryan Chandler <ryangjchandler@gmail.com>
2021-09-17 13:03:27 +0100

    fix: dont cache evaluation results

--- packages/forms/src/Components/Concerns/EvaluatesCallbacks.php
+++ packages/forms/src/Components/Concerns/EvaluatesCallbacks.php
@@ -6,10 +6,10 @@ use Closure;
 
 trait EvaluatesCallbacks
 {
-    protected function evaluate(&$value, array $parameters = [])
+    protected function evaluate($value, array $parameters = [])
     {
         if ($value instanceof Closure) {
-            return $value = app()->call($value, array_merge([
+            return app()->call($value, array_merge([
                 'component' => $this,
                 'get' => $this->getGetCallback(),
                 'livewire' => $this->getLivewire(),
