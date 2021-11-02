36224c448f357f2c8e28dc69a921a98e33be3749
Dan Harrin <git@danharrin.com>
2021-11-02 19:36:43 +0000

    Update section.blade.php

--- packages/forms/resources/views/components/section.blade.php
+++ packages/forms/resources/views/components/section.blade.php
@@ -1,10 +1,10 @@
 <div
-  @if ($isCollapsible())
-  x-data="{ isCollapsed: {{ $isCollapsed() ? 'true' : 'false' }} }"
-  x-on:expand-concealing-component.window="if ($event.detail.id === $el.id) isCollapsed = false"
-  @endif
-  id="{{ $getId() }}"
-  class="p-6 space-y-6 rounded-xl shadow-sm border border-gray-300"
+    @if ($isCollapsible())
+        x-data="{ isCollapsed: {{ $isCollapsed() ? 'true' : 'false' }} }"
+        x-on:expand-concealing-component.window="if ($event.detail.id === $el.id) isCollapsed = false"
+    @endif
+    id="{{ $getId() }}"
+    class="p-6 space-y-6 rounded-xl shadow-sm border border-gray-300"
 >
     <div class="flex space-x-3">
         <div class="flex-1 space-y-1">
@@ -21,9 +21,9 @@
 
         @if ($isCollapsible())
             <button
-              x-on:click="isCollapsed = ! isCollapsed"
-              type="button"
-              class="flex items-center justify-center w-10 h-10 text-primary-500 transition rounded-full hover:bg-gray-500/5 focus:bg-primary-500/10 focus:outline-none"
+                x-on:click="isCollapsed = ! isCollapsed"
+                type="button"
+                class="flex items-center justify-center w-10 h-10 text-primary-500 transition rounded-full hover:bg-gray-500/5 focus:bg-primary-500/10 focus:outline-none"
             >
                 <svg x-show="isCollapsed" class="h-7 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
@@ -37,10 +37,10 @@
     </div>
 
     <div
-      @if ($isCollapsible())
-      x-show="! isCollapsed"
-      x-bind:aria-expanded="(! isCollapsed).toString()"
-      @endif
+        @if ($isCollapsible())
+            x-show="! isCollapsed"
+            x-bind:aria-expanded="(! isCollapsed).toString()"
+        @endif
     >
         {{ $getChildComponentContainer() }}
     </div>
