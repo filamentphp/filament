ea1992df723f5d508b912be30d23df353eba7a82
Dan Harrin <git@danharrin.com>
2021-11-11 16:31:10 +0000

    wip

--- packages/admin/resources/views/login.blade.php
+++ packages/admin/resources/views/login.blade.php
@@ -2,13 +2,13 @@
     <div class="p-2 max-w-md space-y-8 w-screen">
         <form wire:submit.prevent="submit" class="bg-white space-y-8 shadow border border-gray-300 rounded-2xl p-8">
             <h2 class="font-bold tracking-tight text-center text-gray-900 text-2xl">
-                {{ __(__('filament::login.heading')) }}
+                {{ __('filament::login.heading') }}
             </h2>
 
             {{ $this->form }}
 
             <x-filament::button type="submit" class="w-full">
-                {{ __(__('filament::login.buttons.submit.label')) }}
+                {{ __('filament::login.buttons.submit.label') }}
             </x-filament::button>
         </form>
 
