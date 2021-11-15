7382b9b899c56c4f080c696270ea87da3611f374
Dan Harrin <git@danharrin.com>
2021-11-15 12:29:54 +0000

    Update InteractsWithTable.php

--- packages/tables/src/Concerns/InteractsWithTable.php
+++ packages/tables/src/Concerns/InteractsWithTable.php
@@ -24,7 +24,7 @@ trait InteractsWithTable
 
     protected Table $table;
 
-    public function bootInteractsWithTable(): void
+    public function bootedInteractsWithTable(): void
     {
         $this->table = $this->getTable();
 
