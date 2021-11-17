cb498d3e3d7d2d12f9c1c27777d5b87b419eb9bb
Jeff Greco <jeff@cavera.ca>
2021-11-16 23:53:35 -0500

    Update text-input.blade.php

--- packages/forms/resources/views/components/text-input.blade.php
+++ packages/forms/resources/views/components/text-input.blade.php
@@ -54,7 +54,7 @@
                 {{ $attributes->merge($getExtraAttributes())->class([
                     'block w-full h-10 transition duration-75 rounded-lg shadow-sm focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600',
                     'border-gray-300' => ! $errors->has($getStatePath()),
-                    'border-danger-600 ring-danger-600' => $errors->has($getStatePath()),
+                    'border-danger-600 ring-danger-600 disabled:bg-gray-200' => $errors->has($getStatePath()),
                 ]) }}
             />
         </div>
