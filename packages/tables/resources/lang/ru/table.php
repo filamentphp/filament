7f8b138e3feddca3c91f017a01a243d1237b24a6
Stuart Hallewell <stuart@blackpig.eu>
2021-11-18 21:39:36 +0100

    ku, ru, zh-CN

---
+++ packages/tables/resources/lang/ru/table.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+
+    'fields' => [
+
+        'search_query' => [
+            'label' => 'Поиск',
+            'placeholder' => 'Поиск',
+        ],
+
+    ],
+
+    'pagination' => [
+
+        'label' => 'навигация разбивки на страницы',
+
+        'overview' => 'показ :first до :last из :total результаты',
+
+        'fields' => [
+
+            'records_per_page' => [
+                'label' => 'на страницу',
+            ],
+
+        ],
+
+        'buttons' => [
+
+            'go_to_page' => [
+                'label' => 'Перейти к странице :page',
+            ],
+
+            'next' => [
+                'label' => 'Следующая',
+            ],
+
+            'previous' => [
+                'label' => 'Предыдущая',
+            ],
+
+        ],
+
+    ],
+
+    'buttons' => [
+
+        'filter' => [
+            'label' => 'Фильтр',
+        ],
+
+        'open_actions' => [
+            'label' => 'Open actions',
+        ],
+
+    ],
+
+    'actions' => [
+
+        'modal' => [
+
+            'requires_confirmation_subheading' => 'Are you sure you would like to do this?',
+
+            'buttons' => [
+
+                'cancel' => [
+                    'label' => 'Отменить',
+                ],
+
+                'confirm' => [
+                    'label' => 'Confirm',
+                ],
+
+                'submit' => [
+                    'label' => 'Submit',
+                ],
+
+            ],
+
+        ],
+
+        'buttons' => [
+
+            'select_all' => [
+                'label' => 'Select all :count records',
+            ],
+
+        ],
+
+    ],
+
+    'empty' => [
+        'heading' => 'не найдено записей',
+    ],
+
+];
