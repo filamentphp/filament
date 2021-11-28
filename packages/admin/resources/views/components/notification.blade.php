798253025537c9427aea088d2e48c802f182427a
Dan Harrin <git@danharrin.com>
2021-11-28 17:30:07 +0000

    Toast visibility

--- packages/admin/resources/views/components/notification.blade.php
+++ packages/admin/resources/views/components/notification.blade.php
@@ -11,13 +11,21 @@
         setTimeout(() => $el.remove(), 6000)
     }"
     x-cloak
-    class="fixed inset-x-0 bottom-0 z-10 p-3 pointer-events-none"
+    class="fixed inset-x-0 top-0 z-10 p-3 pointer-events-none"
 >
-    <div class="flex flex-col w-full h-auto sm:max-w-xs ml-auto space-y-2 pointer-events-auto">
+    <div class="flex flex-col h-auto sm:max-w-xs max-w-screen mx-auto space-y-2 pointer-events-auto">
         <div
             x-show="isVisible"
             x-transition
-            class="flex items-start w-full px-3 py-2 space-x-2 text-xs shadow bg-white/50 ring-gray-200 ring-1 rounded-xl backdrop-blur-xl backdrop-saturate-150"
+            @class([
+                'flex items-start px-3 py-2 space-x-2 text-xs shadow ring-1 rounded-xl',
+                match ($status) {
+                    'danger' => 'bg-danger-50 ring-danger-200',
+                    'success' => 'bg-success-50 ring-success-200',
+                    'warning' => 'bg-warning-50 ring-warning-200',
+                    default => 'bg-white ring-gray-200',
+                },
+            ])
         >
             <x-dynamic-component :component="match ($status) {
                 'danger' => 'heroicon-o-x-circle',
@@ -27,16 +35,26 @@
             }" :class="\Illuminate\Support\Arr::toCssClasses([
                 'flex-shrink-0 w-6 h-6',
                 match ($status) {
-                    'danger' => 'text-danger-500',
-                    'success' => 'text-success-500',
-                    'warning' => 'text-warning-500',
-                    default => 'text-primary-500',
-                }
+                    'danger' => 'text-danger-600',
+                    'success' => 'text-success-600',
+                    'warning' => 'text-warning-600',
+                    default => 'text-primary-600',
+                },
             ])" />
 
             <div class="flex-1 space-y-1">
                 <div class="flex items-center justify-between font-medium">
-                    <p class="text-sm leading-6 truncate">
+                    <p
+                        @class([
+                            'text-sm leading-6 truncate',
+                            match ($status) {
+                                'danger' => 'text-danger-900',
+                                'success' => 'text-success-900',
+                                'warning' => 'text-warning-900',
+                                default => 'text-gray-900',
+                            },
+                        ])
+                    >
                         {{ $message }}
                     </p>
                 </div>
