ec0b9955a77203e8d2ccff385ee7a5ab2b76d2c9
Dan Harrin <git@danharrin.com>
2021-12-01 12:29:01 +0000

    wip

--- packages/admin/resources/views/components/header/index.blade.php
+++ packages/admin/resources/views/components/header/index.blade.php
@@ -3,10 +3,10 @@
     'heading',
 ])
 
-<header {{ $attributes->class('space-y-2 items-center justify-between sm:flex sm:space-y-0 sm:space-x-4 sm:py-4') }}>
+<header {{ $attributes->class('space-y-2 items-start justify-between sm:flex sm:space-y-0 sm:space-x-4 sm:py-4') }}>
     <x-filament::header.heading>
         {{ $heading }}
     </x-filament::header.heading>
 
-    <x-filament::actions :actions="$actions" />
+    <x-filament::actions :actions="$actions" class="flex-shrink-0" />
 </header>
