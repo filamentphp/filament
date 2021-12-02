7b59f2261e43ee1f3bf98ed81ea076e8eb14c490
David Palomares <david@mainsip.com>
2021-12-02 10:43:08 +0100

    Move $sort property from stub to base class

--- src/Widgets/Widget.php
+++ src/Widgets/Widget.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class Widget extends Component
 {
+    /**
+     * @var int $sort
+     */
+    public static $sort = 0;
+
     public static $view;
 
     public function render()
