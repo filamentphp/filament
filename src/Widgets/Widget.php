492b50522be73dad50e2d8c423af7ae285bf6e64
Dan Harrin <git@danharrin.com>
2021-12-02 14:19:51 +0000

    Update Widget.php

--- src/Widgets/Widget.php
+++ src/Widgets/Widget.php
@@ -14,7 +14,7 @@ class Widget extends Component
 
     public static function isVisible()
     {
-        return static::$isHidden !== true;
+        return static::$isHidden;
     }
 
     public function render()
