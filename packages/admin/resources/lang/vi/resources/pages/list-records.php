f6bc8a38708b8a41cdcfe6d017f308c2ab94e6a0
Thanh-Son Banh <takashato@gmail.com>
2021-12-15 01:09:11 +0700

    Add Vietnamese translations

---
+++ packages/admin/resources/lang/vi/resources/pages/list-records.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    'breadcrumb' => 'Danh sách',
+
+    'actions' => [
+
+        'create' => [
+            'label' => 'Thêm :label',
+        ],
+
+    ],
+
+    'table' => [
+
+        'actions' => [
+
+            'edit' => [
+                'label' => 'Sửa',
+            ],
+
+            'view' => [
+                'label' => 'Xem',
+            ],
+
+        ],
+
+        'bulk_actions' => [
+
+            'delete' => [
+                'label' => 'Đã xóa những mục đã chọn',
+            ],
+
+        ],
+
+    ],
+
+];
---
+++ packages/forms/resources/lang/vi/components.php
@@ -0,0 +1,156 @@
+<?php
+
+return [
+
+    'builder' => [
+
+        'buttons' => [
+
+            'create_item' => [
+                'label' => 'Thêm vào :label',
+            ],
+
+            'create_item_between' => [
+                'label' => 'Thêm vào giữa',
+            ],
+
+            'delete_item' => [
+                'label' => 'Xóa',
+            ],
+
+            'move_item_down' => [
+                'label' => 'Chuyển xuống',
+            ],
+
+            'move_item_up' => [
+                'label' => 'Chuyển lên',
+            ],
+
+        ],
+
+    ],
+
+    'key_value' => [
+
+        'buttons' => [
+
+            'add' => [
+                'label' => 'Thêm Dòng',
+            ],
+
+            'delete' => [
+                'label' => 'Xóa Dòng',
+            ],
+
+        ],
+
+        'fields' => [
+
+            'key' => [
+                'label' => 'Key',
+            ],
+
+            'value' => [
+                'label' => 'Giá trị',
+            ],
+
+        ],
+
+    ],
+
+    'markdown_editor' => [
+
+        'toolbar_buttons' => [
+            'attach_files' => 'Đính kèm file',
+            'bold' => 'In đậm',
+            'bullet_list' => 'Danh sách dùng dấu chấm',
+            'code_block' => 'Code block',
+            'edit' => 'Xóa',
+            'italic' => 'In nghiêng',
+            'link' => 'Liên kết',
+            'ordered_list' => 'Danh sách đánh số',
+            'preview' => 'Xem trước',
+            'strike' => 'Gạch ngang',
+        ],
+
+    ],
+
+    'multi_select' => [
+        'no_search_results_message' => 'Không có tùy chọn nào phù hợp với tìm kiếm của bạn.',
+        'placeholder' => 'Chọn một mục',
+        'search_prompt' => 'Gõ để tìm kiếm...',
+    ],
+
+    'repeater' => [
+
+        'buttons' => [
+
+            'create_item' => [
+                'label' => 'Thêm vào :label',
+            ],
+
+            'delete_item' => [
+                'label' => 'Xóa',
+            ],
+
+            'move_item_down' => [
+                'label' => 'Chuyển xuống',
+            ],
+
+            'move_item_up' => [
+                'label' => 'Chuyển lên',
+            ],
+
+        ],
+
+    ],
+
+    'rich_editor' => [
+
+        'dialogs' => [
+
+            'link' => [
+
+                'buttons' => [
+                    'link' => 'Liên kết',
+                    'unlink' => 'Bỏ liên kết',
+                ],
+
+                'label' => 'URL',
+
+                'placeholder' => 'Nhập URL',
+
+            ],
+
+        ],
+
+        'toolbar_buttons' => [
+            'attach_files' => 'Đính kèm file',
+            'blockquote' => 'Trích dẫn',
+            'bold' => 'In đậm',
+            'bullet_list' => 'Danh sách dùng dấu chấm',
+            'code_block' => 'Code block',
+            'h1' => 'Tiêu đề chính',
+            'h2' => 'Tiêu đề',
+            'h3' => 'Tiêu đề phụ',
+            'italic' => 'In nghiêng',
+            'link' => 'Liên kết',
+            'ordered_list' => 'Danh sách đánh số',
+            'redo' => 'Redo',
+            'strike' => 'Strikethrough',
+            'undo' => 'Hoàn tác',
+        ],
+
+    ],
+
+    'select' => [
+        'no_search_results_message' => 'Không có tùy chọn nào phù hợp với tìm kiếm của bạn.',
+        'placeholder' => 'Chọn một mục',
+        'search_prompt' => 'Gõ để tìm kiếm...',
+    ],
+
+    'tags_input' => [
+        'placeholder' => 'Thêm thẻ mới',
+    ],
+
+];
