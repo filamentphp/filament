3553e0fd5dbe83e611e6eca3fb87edcaff6ae2cf
Dan Harrin <git@danharrin.com>
2021-12-15 19:57:07 +0000

    Remove namespace option & clarify some docs

--- packages/spatie-laravel-settings-plugin/src/Commands/MakeSettingsPageCommand.php
+++ packages/spatie-laravel-settings-plugin/src/Commands/MakeSettingsPageCommand.php
@@ -2,10 +2,9 @@
 
 namespace Filament\Commands;
 
-use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 
 class MakeSettingsPageCommand extends Command
 {
@@ -14,14 +13,11 @@ class MakeSettingsPageCommand extends Command
 
     protected $description = 'Creates a Filament settings page class.';
 
-    protected $signature = 'make:filament-settings-page
-                            {name? : provide Settings Page name. i.e, `ManageSite`}
-                            {--S|settings= : Provide your Settings class name for the settings page. i.e, `SiteSettings`}
-                            {--N|namespace : Use this flag to Override the namesapace, provide your settings class name with the namespace of your choosing because by default the this command presumes that you have a Settings i.e, `SiteSettings` in your App\Settings directory.}';
+    protected $signature = 'make:filament-settings-page {name?} {settingsClass?}';
 
     public function handle(): int
     {
-        $page = (string) Str::of($this->argument('name') ?? $this->askRequired('Page name (e.g. `ManageSite`)', 'name'))
+        $page = (string) Str::of($this->argument('name') ?? $this->askRequired('Page name (e.g. `ManageFooter`)', 'name'))
             ->trim('/')
             ->trim('\\')
             ->trim(' ')
@@ -30,22 +26,18 @@ class MakeSettingsPageCommand extends Command
         $pageNamespace = Str::of($page)->contains('\\') ?
             (string) Str::of($page)->beforeLast('\\') :
             '';
+
+        $settingsClass = (string) Str::of($this->argument('settingsClass') ?? $this->askRequired('Settings class (e.g. `FooterSettings`)', 'settings class'))
+            ->trim('/')
+            ->trim('\\')
+            ->trim(' ');
+
         $path = app_path(
             (string) Str::of($page)
                 ->prepend('Filament\\Pages\\')
                 ->replace('\\', '/')
                 ->append('.php'),
         );
-
-        $settingsPage = (string) Str::of($this->option('settings') ?? $this->askRequired('Settings class (e.g. `SiteSettings`)', 'settings'))
-            ->trim('/')
-            ->trim('\\')
-            ->trim(' ')
-            ->replace('/', '\\');
-        $settingsNamespace = Str::of($settingsPage)->contains('\\') ?
-        (string) Str::of($settingsPage)->beforeLast('\\') :
-        '';
-        $settingsClass = (string) Str::of($settingsPage)->afterLast('\\');
 
         if ($this->checkForCollision([$path])) {
             return static::INVALID;
@@ -55,10 +47,9 @@ class MakeSettingsPageCommand extends Command
             'class' => $pageClass,
             'namespace' => 'App\\Filament\\Pages' . ($pageNamespace !== '' ? "\\{$pageNamespace}" : ''),
             'settingsClass' => $settingsClass,
-            'settingsNamespace' => $this->option('namespace') ? $settingsNamespace : 'App\\Settings'
         ]);
 
-        $this->info("Successfully created setting's page: App/".Str::after($path,'/app/')."!");
+        $this->info("Successfully created {$page}!");
 
         return static::SUCCESS;
     }
