7f0afdd2efd44b0462bc67761a0b3339a4e5b6ce
Maher El Gamil <maherbusnes@gmail.com>
2021-12-15 22:31:26 +0200

    add missed localization for global search groups

--- packages/admin/resources/views/components/global-search/result-group.blade.php
+++ packages/admin/resources/views/components/global-search/result-group.blade.php
@@ -7,7 +7,7 @@
     <li class="sticky top-0 z-10">
         <header class="px-6 py-2 bg-gray-50/80 backdrop-blur-xl backdrop-saturate-150">
             <p class="text-xs font-bold tracking-wider text-gray-500 uppercase">
-                {{ $label }}
+                {{ __(ucfirst($label)) }}
             </p>
         </header>
     </li>
