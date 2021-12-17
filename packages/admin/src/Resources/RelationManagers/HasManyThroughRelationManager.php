1655125c41b3ef6e2b9d3f8156dd062483cfa211
Dan Harrin <git@danharrin.com>
2021-12-17 20:20:50 +0000

    Update HasManyThroughRelationManager.php

--- packages/admin/src/Resources/RelationManagers/HasManyThroughRelationManager.php
+++ packages/admin/src/Resources/RelationManagers/HasManyThroughRelationManager.php
@@ -8,17 +8,11 @@ use Illuminate\Database\Eloquent\Relations\Relation;
 
 class HasManyThroughRelationManager extends HasManyRelationManager
 {
-    use Concerns\CanCreateRecords;
-    use Concerns\CanDeleteRecords;
-    use Concerns\CanEditRecords;
-
-    protected static string $view = 'filament::resources.relation-managers.has-many-relation-manager';
-
     // https://github.com/laravel/framework/issues/4962
     protected function getTableQuery(): Builder
     {
         $query = parent::getTableQuery();
 
-        return $query->select( $query->getModel()->getTable().'.*');
+        return $query->select($query->getModel()->getTable().'.*');
     }
 }
