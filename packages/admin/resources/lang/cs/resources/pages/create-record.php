8c56e3970731a9131a5af0d10934d3557db63a6b
paral.shean <paral.shean@gmail.com>
2021-12-20 13:52:20 +0100

    admin cs translation

---
+++ packages/admin/resources/lang/cs/resources/pages/create-record.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'title' => 'Vytvořit :label',
+
+    'breadcrumb' => 'Vytvořit',
+
+    'form' => [
+
+        'actions' => [
+
+            'cancel' => [
+                'label' => 'Zrušit',
+            ],
+
+            'create' => [
+                'label' => 'Vytvořit',
+            ],
+
+            'create_and_create_another' => [
+                'label' => 'Vytvořit & vytvořit další',
+            ],
+
+        ],
+
+    ],
+
+    'messages' => [
+        'created' => 'Vytvořit',
+    ],
+
+];
