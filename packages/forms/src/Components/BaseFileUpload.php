38c8445f26ee27efcfced3d7dbcc7119f852f5b0
Roberto Rinaldi <roberto.rinaldi@mgquadro.com>
2021-12-27 11:14:31 +0100

    FileUpload - Delete tmp file on revert and save

--- packages/forms/src/Components/BaseFileUpload.php
+++ packages/forms/src/Components/BaseFileUpload.php
@@ -309,6 +309,10 @@ class BaseFileUpload extends Field
             return null;
         }
 
+        if ($file instanceof TemporaryUploadedFile) {
+            $file->delete();
+        }
+
         unset($files[$fileKey]);
 
         $this->state($files);
@@ -347,12 +351,18 @@ class BaseFileUpload extends Field
             $callback = $this->saveUploadedFileUsing;
 
             if (! $callback) {
+                $file->delete();
+
                 return $file;
             }
 
-            return $this->evaluate($callback, [
+            $newFile = $this->evaluate($callback, [
                 'file' => $file,
             ]);
+
+            $file->delete();
+
+            return $newFile;
         }, $this->getState());
 
         $this->state($state);
