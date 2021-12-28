1f39cb1f212e584f8c1a82d47bab2979423455c1
Dan Harrin <git@danharrin.com>
2021-12-28 21:44:44 +0000

    wip

---
+++ packages/forms/resources/views/components/builder/block-picker.blade.php
@@ -0,0 +1,31 @@
+@props([
+    'blocks',
+    'createAfterItem' => null,
+    'statePath',
+])
+
+<div
+    x-show="isCreateButtonDropdownOpen"
+    x-on:click.away="isCreateButtonDropdownOpen = false"
+    x-transition
+    {{ $attributes->class(['absolute z-10 mt-10 shadow-xl border overflow-hidden rounded-xl w-52']) }}
+>
+    <ul class="py-1 space-y-1 bg-white shadow rounded-xl">
+        @foreach ($blocks as $block)
+            <li>
+                <button
+                    wire:click="dispatchFormEvent('builder::createItem', '{{ $statePath }}', '{{ $block->getName() }}' {{ $createAfterItem ? ", '{$createAfterItem}'" : '' }})"
+                    x-on:click="isCreateButtonDropdownOpen = false"
+                    type="button"
+                    class="flex items-center w-full h-8 px-3 text-sm font-medium focus:outline-none hover:text-white hover:bg-primary-600 focus:bg-primary-700 focus:text-white group"
+                >
+                    @if ($icon = $block->getIcon())
+                        <x-dynamic-component :component="$icon" class="mr-2 -ml-1 text-primary-500 w-5 h-5 group-hover:text-white group-focus:text-white" />
+                    @endif
+
+                    {{ $block->getLabel() }}
+                </button>
+            </li>
+        @endforeach
+    </ul>
+</div>
