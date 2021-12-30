0833fc304a5b53b35f3877a17307522fb6d551d7
Dan Harrin <git@danharrin.com>
2021-12-30 10:08:22 +0000

    Ensure theme CSS overrides plugin CSS

--- packages/admin/resources/views/components/layouts/base.blade.php
+++ packages/admin/resources/views/components/layouts/base.blade.php
@@ -19,8 +19,6 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
 
-        <link rel="stylesheet" href="{{ \Filament\Facades\Filament::getThemeUrl() }}" />
-
         @foreach (\Filament\Facades\Filament::getStyles() as $name => $path)
             @if (Str::of($path)->startsWith(['http://', 'https://']))
                 <link rel="stylesheet" href="{{ $path }}" />
@@ -30,6 +28,8 @@
                 ]) }}" />
             @endif
         @endforeach
+
+        <link rel="stylesheet" href="{{ \Filament\Facades\Filament::getThemeUrl() }}" />
     </head>
 
     <body>
