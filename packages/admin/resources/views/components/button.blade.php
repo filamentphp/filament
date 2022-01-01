288f1cfa40e0d074d862c0d0cd7bdb69f572d7b0
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2022-01-01 13:42:37 +0600

    Fixed button ui issue

--- packages/admin/resources/views/components/button.blade.php
+++ packages/admin/resources/views/components/button.blade.php
@@ -25,7 +25,7 @@
         'w-6 h-6' => $size === 'md',
         'w-7 h-7' => $size === 'lg',
         'w-5 h-5' => $size === 'sm',
-        'mr-1 -ml-2 rtl:ml-1 -mr-2' => ($iconPosition === 'before') && ($size === 'md'),
+        'mr-1 -ml-2 rtl:ml-1 rtl:-mr-2' => ($iconPosition === 'before') && ($size === 'md'),
         'mr-2 -ml-3 rtl:ml-2 rtl:-mr-3' => ($iconPosition === 'before') && ($size === 'lg'),
         'mr-1 -ml-1.5 rtl:ml-1 rtl:-mr-1.5' => ($iconPosition === 'before') && ($size === 'sm'),
         'ml-1 -mr-2 rtl:mr-1 rtl:-ml-2' => ($iconPosition === 'after') && ($size === 'md'),
