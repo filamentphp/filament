706abe1f36ac7f82566e7c493cfeaac90f6a08fc
Dan Harrin <git@danharrin.com>
2022-01-04 20:26:34 +0000

    feature: Text column wrapping

--- packages/tables/src/Columns/TextColumn.php
+++ packages/tables/src/Columns/TextColumn.php
@@ -7,4 +7,18 @@ class TextColumn extends Column
     use Concerns\CanFormatState;
 
     protected string $view = 'tables::columns.text-column';
+
+    protected bool $canWrap = false;
+
+    public function wrap(bool $condition = true): static
+    {
+        $this->canWrap = $condition;
+
+        return $this;
+    }
+
+    public function canWrap(): bool
+    {
+        return $this->canWrap;
+    }
 }
