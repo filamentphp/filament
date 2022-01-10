e6347e21b15ec78c88da465dd827ae84b806ed12
Dan Harrin <git@danharrin.com>
2022-01-10 22:42:45 +0000

    fix: Hidden relation managers error

--- packages/admin/src/Resources/Pages/Concerns/HasRelationManagers.php
+++ packages/admin/src/Resources/Pages/Concerns/HasRelationManagers.php
@@ -2,12 +2,19 @@
 
 namespace Filament\Resources\Pages\Concerns;
 
+use Illuminate\Support\Facades\Gate;
+
 trait HasRelationManagers
 {
     public $activeRelationManager = null;
 
     protected function getRelationManagers(): array
     {
-        return $this->getResource()::getRelations();
+        $managers = $this->getResource()::getRelations();
+
+        return array_filter(
+            $managers,
+            fn (string $manager): bool => $manager::canViewForRecord($this->record),
+        );
     }
 }
