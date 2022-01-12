324ba576ebb3e6d8fd30e90ecab517f74b548557
Tina Hammar <tinahammar@gmail.com>
2022-01-12 16:33:40 +0100

    I want Nunito font

--- packages/admin/resources/views/components/layouts/base.blade.php
+++ packages/admin/resources/views/components/layouts/base.blade.php
@@ -11,6 +11,9 @@
 
         <title>{{ $title ? "{$title} - " : null }} {{ config('app.name') }}</title>
 
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
         <style>[x-cloak] { display: none !important; }</style>
 
         @livewireStyles
