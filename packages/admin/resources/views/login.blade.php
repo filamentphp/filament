6597f0b5a104ae8aab134def6f47d7792626a06f
Sebastiaan Kloos <sebastiaan@codecycler.com>
2022-01-16 12:10:36 +0100

    Adding branding to the login page

--- packages/admin/resources/views/login.blade.php
+++ packages/admin/resources/views/login.blade.php
@@ -1,6 +1,10 @@
 <div class="flex items-center justify-center min-h-screen bg-gray-100 text-gray-900">
     <div class="p-2 max-w-md space-y-8 w-screen">
         <form wire:submit.prevent="authenticate" class="bg-white space-y-8 shadow border border-gray-300 rounded-2xl p-8">
+            <div class="w-full flex justify-center">
+                <x-filament::brand />
+            </div>
+        
             <h2 class="font-bold tracking-tight text-center text-2xl">
                 {{ __('filament::login.heading') }}
             </h2>
