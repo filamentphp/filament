0e9c2d6884b1d26e2bd6fab881ca7e72d206ff47
Dan Harrin <git@danharrin.com>
2022-01-16 13:29:05 +0000

    feature: Widget column span

--- packages/admin/resources/views/widgets/stats-overview-widget.blade.php
+++ packages/admin/resources/views/widgets/stats-overview-widget.blade.php
@@ -1,5 +1,7 @@
-<x-filament::stats :columns="$this->getColumns()" class="col-span-full">
-    @foreach ($this->getCards() as $card)
-        {{ $card }}
-    @endforeach
-</x-filament::stats>
+<x-filament::widget>
+    <x-filament::stats :columns="$this->getColumns()">
+        @foreach ($this->getCards() as $card)
+            {{ $card }}
+        @endforeach
+    </x-filament::stats>
+</x-filament::widget>
