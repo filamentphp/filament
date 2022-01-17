94978a2b3c4cb750a5180b01899550b6f3fbcbb0
Ghossam <houssem.hoho@gmail.com>
2022-01-17 21:51:04 +0100

    Added missing strings to french translation

--- packages/forms/resources/lang/fr/components.php
+++ packages/forms/resources/lang/fr/components.php
@@ -2,10 +2,107 @@
 
 return [
 
+    'builder' => [
+
+        'buttons' => [
+
+            'create_item' => [
+                'label' => 'Ajouter à :label',
+            ],
+
+            'create_item_between' => [
+                'label' => 'Ajouter entre',
+            ],
+
+            'delete_item' => [
+                'label' => 'Supprimer',
+            ],
+
+            'move_item_down' => [
+                'label' => 'Déplacer vers le bas',
+            ],
+
+            'move_item_up' => [
+                'label' => 'Move up',
+            ],
+
+        ],
+
+    ],
+
+    'key_value' => [
+
+        'buttons' => [
+
+            'add' => [
+                'label' => 'Ajouter une ligne',
+            ],
+
+            'delete' => [
+                'label' => 'Supprimer une ligne',
+            ],
+
+        ],
+
+        'fields' => [
+
+            'key' => [
+                'label' => 'Clé',
+            ],
+
+            'value' => [
+                'label' => 'Valeur',
+            ],
+
+        ],
+
+    ],
+
+    'markdown_editor' => [
+
+        'toolbar_buttons' => [
+            'attach_files' => 'Joindre des fichiers',
+            'bold' => 'Gras',
+            'bullet_list' => 'Liste à puces',
+            'code_block' => 'Bloc de code',
+            'edit' => 'Modifier',
+            'italic' => 'Italique',
+            'link' => 'Lien',
+            'ordered_list' => 'Liste numérotée',
+            'preview' => 'Aperçu',
+            'strike' => 'Barré',
+        ],
+
+    ],
+
     'multi_select' => [
         'no_search_results_message' => 'Aucune option ne correspond à votre recherche.',
         'placeholder' => 'Sélectionnez une option',
         'search_prompt' => 'Aucune option à choisir.',
+    ],
+
+    'repeater' => [
+
+        'buttons' => [
+
+            'create_item' => [
+                'label' => 'Ajouter à :label',
+            ],
+
+            'delete_item' => [
+                'label' => 'Supprimer',
+            ],
+
+            'move_item_down' => [
+                'label' => 'Déplacer vers le bas',
+            ],
+
+            'move_item_up' => [
+                'label' => 'Déplacer vers le haut',
+            ],
+
+        ],
+
     ],
 
     'rich_editor' => [
