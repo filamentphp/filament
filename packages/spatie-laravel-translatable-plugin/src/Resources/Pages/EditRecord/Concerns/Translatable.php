39ed6562fb453bc4e4080ba36fabad6e797bc9a0
MohamedSabil83 <me@mohamedsabil83.com>
2022-01-19 01:37:23 +0300

    revert another line

--- packages/spatie-laravel-translatable-plugin/src/Resources/Pages/EditRecord/Concerns/Translatable.php
+++ packages/spatie-laravel-translatable-plugin/src/Resources/Pages/EditRecord/Concerns/Translatable.php
@@ -39,6 +39,7 @@ trait Translatable
         $defaultLocale = $resource::getDefaultTranslatableLocale();
 
         $this->activeFormLocale = in_array($defaultLocale, $availableLocales) ? $defaultLocale : array_intersect($availableLocales, $resourceLocales)[0] ?? $defaultLocale;
+        $this->record->setLocale($this->activeFormLocale);
     }
 
     protected function handleRecordUpdate(Model $record, array $data): Model
