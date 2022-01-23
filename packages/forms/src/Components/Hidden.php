6387686ca542bf1c6bffe665f4fab72b845b8916
Dan Harrin <git@danharrin.com>
2022-01-23 12:54:12 +0000

    Update Hidden.php

--- packages/forms/src/Components/Hidden.php
+++ packages/forms/src/Components/Hidden.php
@@ -5,4 +5,9 @@ namespace Filament\Forms\Components;
 class Hidden extends Field
 {
     protected string $view = 'forms::components.hidden';
+
+    protected function setUp(): void
+    {
+        $this->columnSpan('hidden');
+    }
 }
