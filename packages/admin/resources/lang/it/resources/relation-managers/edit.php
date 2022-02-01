93c32e3d84865c84d6fcca58ca31b072259f53a4
slamservice <marco.mapelli@slamservice.it>
2022-02-01 16:15:52 +0100

    Added italian language

---
+++ packages/admin/resources/lang/it/resources/relation-managers/edit.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'action' => [
+
+        'label' => 'Modifica',
+
+        'modal' => [
+
+            'heading' => 'Modifica :label',
+
+            'actions' => [
+
+                'save' => [
+                    'label' => 'Salva',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'saved' => 'Salvato',
+        ],
+
+    ],
+
+];
