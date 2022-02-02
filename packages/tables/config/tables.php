766a12e6cee997102609bcdebab977c6d1320617
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-02-02 11:36:05 +0100

    Add time() method to CanFormatState

--- packages/tables/config/tables.php
+++ packages/tables/config/tables.php
@@ -4,6 +4,7 @@ return [
 
     'date_format' => 'M j, Y',
     'date_time_format' => 'M j, Y H:i:s',
+    'time_format' => 'H:i',
 
     'default_filesystem_disk' => env('TABLES_FILESYSTEM_DRIVER', 'public'),
 
