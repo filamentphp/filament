73a7a80a48e006fb31e176c692619536daff7929
Dan Harrin <git@danharrin.com>
2022-02-04 20:51:43 +0000

    wip

--- packages/tables/src/Filters/Concerns/HasOptions.php
+++ packages/tables/src/Filters/Concerns/HasOptions.php
@@ -7,9 +7,9 @@ use Illuminate\Contracts\Support\Arrayable;
 
 trait HasOptions
 {
-    protected string |array | Arrayable | Closure $options = [];
+    protected array | Arrayable | string | Closure $options = [];
 
-    public function options(string |array | Arrayable | Closure $options): static
+    public function options(array | Arrayable | string | Closure $options): static
     {
         $this->options = $options;
 
@@ -24,12 +24,8 @@ trait HasOptions
             $options = $this->queriesRelationships() ? $this->getRelationshipOptions() : [];
         }
 
-        if (is_string($options)) {
-            $options = collect($options::cases())
-                ->mapWithKeys(function ($eachEnumInstance) {
-                    return [$eachEnumInstance?->value ?? $eachEnumInstance->name => $eachEnumInstance->name];
-                })
-                ->toArray();
+        if (is_string($options) && class_exists($options)) {
+            $options = collect($options::cases())->mapWithKeys(fn ($case) => [($case?->value ?? $case->name) => $case->name]);
         }
 
         if ($options instanceof Arrayable) {
