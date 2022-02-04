2f841756edb2b772ff2af5134087096a4dcf8022
Dan Harrin <git@danharrin.com>
2022-02-04 20:45:27 +0000

    Update index.blade.php

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -8,7 +8,8 @@
     $isSearchVisible = $isSearchable();
     $isFiltersDropdownVisible = $isFilterable();
 
-    $columnsCount = count($columns) + 1;
+    $columnsCount = count($columns);
+    if (count($actions)) $columnsCount++;
     if ($isSelectionEnabled()) $columnsCount++;
 
     $getHiddenClasses = function (\Filament\Tables\Columns\Column $column): ?string {
@@ -231,7 +232,7 @@
                             </x-tables::header-cell>
                         @endforeach
 
-                        @if ($actions)
+                        @if (count($actions))
                             <th class="w-5"></th>
                         @endif
                     </x-slot>
@@ -282,7 +283,7 @@
                                 </x-tables::cell>
                             @endforeach
 
-                            @if ($actions)
+                            @if (count($actions))
                                 <x-tables::actions-cell :actions="$actions" :record="$record" />
                             @endif
                         </x-tables::row>
