7fce422fbc9e8e1226de2de1f805931e1546cf6a
Dan Harrin <git@danharrin.com>
2022-02-06 15:48:03 +0000

    wip

--- packages/admin/resources/views/components/dropdown/item.blade.php
+++ packages/admin/resources/views/components/dropdown/item.blade.php
@@ -8,7 +8,7 @@
 
 @php
     $buttonClasses = \Illuminate\Support\Arr::toCssClasses([
-        'flex items-center w-full h-8 px-3 text-sm font-medium focus:outline-none hover:text-white focus:text-white group',
+        'flex items-center w-full h-8 px-3 text-sm font-medium focus:outline-none hover:text-white focus:text-white group whitespace-nowrap',
         'hover:bg-primary-600 focus:bg-primary-700' => $color === 'primary' || $color === 'secondary',
         'hover:bg-danger-600 focus:bg-danger-700' => $color === 'danger',
         'hover:bg-success-600 focus:bg-success-700' => $color === 'success',
@@ -23,8 +23,10 @@
         'group-hover:text-warning-100 group-focus:text-warning-100' => $color === 'warning',
     ]);
 
+    $labelClasses = 'truncate';
+
     $iconClasses = \Illuminate\Support\Arr::toCssClasses([
-        'mr-2 -ml-1 rtl:ml-2 rtl:-mr-1 group-hover:text-white group-focus:text-white w-6 h-6',
+        'mr-2 -ml-1 w-6 h-6 flex-shrink-0 rtl:ml-2 rtl:-mr-1 group-hover:text-white group-focus:text-white',
         'text-primary-500' => $color === 'primary',
         'text-danger-500' => $color === 'danger',
         'text-gray-500' => $color === 'secondary',
@@ -43,7 +45,9 @@
                 <x-dynamic-component :component="$icon" :class="$iconClasses" />
             @endif
 
-            <span>{{ $slot }}</span>
+            <span class="{{ $labelClasses }}">
+                {{ $slot }}
+            </span>
 
             @if ($detail)
                 <span class="{{ $detailClasses }}">
@@ -57,7 +61,9 @@
                 <x-dynamic-component :component="$icon" :class="$iconClasses" />
             @endif
 
-            <span>{{ $slot }}</span>
+            <span class="{{ $labelClasses }}">
+                {{ $slot }}
+            </span>
 
             @if ($detail)
                 <span class="{{ $detailClasses }}">
--- packages/tables/resources/views/components/dropdown/item.blade.php
+++ packages/tables/resources/views/components/dropdown/item.blade.php
@@ -9,7 +9,7 @@
 @php
     $buttonClasses = \Illuminate\Support\Arr::toCssClasses([
         'flex items-center w-full h-8 px-3 text-sm font-medium focus:outline-none hover:text-white focus:text-white group whitespace-nowrap',
-        'hover:bg-primary-600 focus:bg-primary-700' => $color === 'primary',
+        'hover:bg-primary-600 focus:bg-primary-700' => $color === 'primary' || $color === 'secondary',
         'hover:bg-danger-600 focus:bg-danger-700' => $color === 'danger',
         'hover:bg-success-600 focus:bg-success-700' => $color === 'success',
         'hover:bg-warning-600 focus:bg-warning-700' => $color === 'warning',
@@ -17,16 +17,19 @@
 
     $detailClasses = \Illuminate\Support\Arr::toCssClasses([
         'ml-auto text-xs text-gray-500',
-        'group-hover:text-primary-100 group-focus:text-primary-100' => $color === 'primary',
+        'group-hover:text-primary-100 group-focus:text-primary-100' => $color === 'primary' || $color === 'secondary',
         'group-hover:text-danger-100 group-focus:text-danger-100' => $color === 'danger',
         'group-hover:text-success-100 group-focus:text-success-100' => $color === 'success',
         'group-hover:text-warning-100 group-focus:text-warning-100' => $color === 'warning',
     ]);
 
+    $labelClasses = 'truncate';
+
     $iconClasses = \Illuminate\Support\Arr::toCssClasses([
-        'mr-2 -ml-1 rtl:ml-2 rtl:-mr-1 group-hover:text-white group-focus:text-white w-6 h-6 min-w-6',
+        'mr-2 -ml-1 w-6 h-6 flex-shrink-0 rtl:ml-2 rtl:-mr-1 group-hover:text-white group-focus:text-white',
         'text-primary-500' => $color === 'primary',
         'text-danger-500' => $color === 'danger',
+        'text-gray-500' => $color === 'secondary',
         'text-success-500' => $color === 'success',
         'text-warning-500' => $color === 'warning',
     ]);
@@ -42,7 +45,9 @@
                 <x-dynamic-component :component="$icon" :class="$iconClasses" />
             @endif
 
-            <span class="truncate">{{ $slot }}</span>
+            <span class="{{ $labelClasses }}">
+                {{ $slot }}
+            </span>
 
             @if ($detail)
                 <span class="{{ $detailClasses }}">
@@ -56,7 +61,9 @@
                 <x-dynamic-component :component="$icon" :class="$iconClasses" />
             @endif
 
-            <span class="truncate">{{ $slot }}</span>
+            <span class="{{ $labelClasses }}">
+                {{ $slot }}
+            </span>
 
             @if ($detail)
                 <span class="{{ $detailClasses }}">
