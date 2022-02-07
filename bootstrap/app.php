9208545499befc7a24a70d09ddeea2b1fe3e9659
Ryan Chandler <ryangjchandler@gmail.com>
2022-02-07 20:32:19 +0000

    fix: manually register providers for larastan

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,3 +1,20 @@
 <?php
 
-return require __DIR__ . '/../vendor/orchestra/testbench-core/laravel/bootstrap/app.php';
+use Filament\FilamentServiceProvider;
+use Filament\Forms\FormsServiceProvider;
+use Filament\Tables\TablesServiceProvider;
+use Livewire\LivewireServiceProvider;
+use Orchestra\Testbench\Foundation\Application;
+
+$app = (new Application())
+    ->configure([
+        'enables_package_discoveries' => true,
+    ])
+    ->createApplication();
+
+$app->register(LivewireServiceProvider::class);
+$app->register(FormsServiceProvider::class);
+$app->register(TablesServiceProvider::class);
+$app->register(FilamentServiceProvider::class);
+
+return $app;
