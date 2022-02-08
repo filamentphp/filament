f3b0e4edac213a171c5996da3e1d4d96b62675ea
Maher El Gamil <maherbusnes@gmail.com>
2022-02-08 11:31:19 +0200

    add missed Arabic phrases

--- packages/admin/resources/lang/ar/layout.php
+++ packages/admin/resources/lang/ar/layout.php
@@ -5,9 +5,19 @@ return [
     'direction' => 'rtl',
 
     'buttons' => [
+
+        'dark_mode' => [
+            'label' => 'تفعيل الوضع الليلي',
+        ],
+
+        'light_mode' => [
+            'label' => 'تفعيل وضع النهار',
+        ],
+
         'logout' => [
             'label' => 'تسجيل الخروج',
         ],
+
     ],
 
 ];
