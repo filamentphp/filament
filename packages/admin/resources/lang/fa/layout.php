ed19d224ca4c560d7315b5cbf67ae973283d4681
amir <amiralizadeh9480@gmail.com>
2022-02-15 11:23:30 +0330

    Add persian translations

---
+++ packages/admin/resources/lang/fa/layout.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'direction' => 'rtl',
+
+    'buttons' => [
+
+        'dark_mode' => [
+            'label' => 'حالت تیره',
+        ],
+
+        'light_mode' => [
+            'label' => 'حالت روشن',
+        ],
+
+        'logout' => [
+            'label' => 'خروج',
+        ],
+
+    ],
+
+];
--- packages/tables/resources/lang/fa/table.php
+++ packages/tables/resources/lang/fa/table.php
@@ -13,7 +13,7 @@ return [
 
     'pagination' => [
 
-        'label' => 'جهت یابی صفحه بندی',
+        'label' => 'صفحه بندی',
 
         'overview' => 'در حال نمایش :first به :last از :total نتایج',
 
@@ -46,11 +46,11 @@ return [
     'buttons' => [
 
         'filter' => [
-            'label' => 'فلتر',
+            'label' => 'فیلتر',
         ],
 
         'open_actions' => [
-            'label' => 'Open actions',
+            'label' => 'بازکردن عملیات',
         ],
 
     ],
@@ -59,7 +59,7 @@ return [
 
         'modal' => [
 
-            'requires_confirmation_subheading' => 'Are you sure you would like to do this?',
+            'requires_confirmation_subheading' => 'آیا برای انجام این کار مطمئن هستید؟',
 
             'buttons' => [
 
@@ -68,11 +68,11 @@ return [
                 ],
 
                 'confirm' => [
-                    'label' => 'Confirm',
+                    'label' => 'تایید',
                 ],
 
                 'submit' => [
-                    'label' => 'Submit',
+                    'label' => 'ثبت',
                 ],
 
             ],
@@ -82,15 +82,41 @@ return [
     ],
 
     'empty' => [
-        'heading' => 'هیچ ریکارد دریافت نشد.',
+        'heading' => 'هیچ آیتمی یافت نشد',
+    ],
+
+    'filters' => [
+
+        'buttons' => [
+
+            'reset' => [
+                'label' => 'پاک کردن فیلترها',
+            ],
+
+        ],
+
+        'multi_select' => [
+            'placeholder' => 'همه',
+        ],
+
+        'select' => [
+            'placeholder' => 'همه',
+        ],
+
     ],
 
     'selection_indicator' => [
 
+        'selected_count' => '1 آیتم انتخاب شده.|:count آیتم انتخاب شده.',
+
         'buttons' => [
 
             'select_all' => [
-                'label' => 'Select all :count',
+                'label' => 'انتخاب همه‌ی :count آیتم',
+            ],
+
+            'deselect_all' => [
+                'label' => 'عدم انتخاب',
             ],
 
         ],
