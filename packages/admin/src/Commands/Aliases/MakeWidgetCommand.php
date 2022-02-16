e9b61337b8c6453a374933f0d856bae90c4ce479
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-02-17 00:10:27 +0430

    Add flags to the command alias as well

--- packages/admin/src/Commands/Aliases/MakeWidgetCommand.php
+++ packages/admin/src/Commands/Aliases/MakeWidgetCommand.php
@@ -8,5 +8,5 @@ class MakeWidgetCommand extends Commands\MakeWidgetCommand
 {
     protected $hidden = true;
 
-    protected $signature = 'filament:widget {name?} {--R|resource=} {--F|force}';
+    protected $signature = 'filament:widget {name?} {--R|resource=} {--F|force} {--C|chart-widget} {--S|stats-widget}';
 }
