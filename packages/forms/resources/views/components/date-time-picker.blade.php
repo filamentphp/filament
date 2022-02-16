c56dda1241c85d1e7517d47e997e2d84b07eda95
Saade <saade@outlook.com.br>
2022-02-16 19:37:19 -0300

    fix: year input allowing more or less than 4 chars

--- packages/forms/resources/views/components/date-time-picker.blade.php
+++ packages/forms/resources/views/components/date-time-picker.blade.php
@@ -111,6 +111,7 @@
 
                             <input
                                 type="number"
+                                inputmode="numeric"
                                 x-model.debounce="focusedYear"
                                 @class([
                                     'w-20 p-0 text-lg text-right border-0 focus:ring-0 focus:outline-none',
