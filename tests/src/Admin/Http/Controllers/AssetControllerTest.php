5766ff50359f5d4bc97d533be8fd7c41586e8721
Dan Harrin <git@danharrin.com>
2022-02-17 22:10:31 +0000

    Update AssetControllerTest.php

--- tests/src/Admin/Http/Controllers/AssetControllerTest.php
+++ tests/src/Admin/Http/Controllers/AssetControllerTest.php
@@ -7,8 +7,7 @@ use function Pest\Laravel\get;
 uses(TestCase::class);
 
 it('can successfully load the assets', function (string $file) {
-    get(route('filament.asset', ['file' => $file]))
-        ->assertOk();
+    get(route('filament.asset', ['file' => $file]))->assertOk();
 })->with([
     ['app.css'],
     ['app.css.map'],
