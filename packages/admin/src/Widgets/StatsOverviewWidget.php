0887bd98a09b4d57d973cbbe07de891fecd9c8fe
Danilo Andrade <mandrade.danilo@gmail.com>
2022-02-21 21:21:58 -0300

    Cache chart data and stats cards to prevent multiple executions

--- packages/admin/src/Widgets/StatsOverviewWidget.php
+++ packages/admin/src/Widgets/StatsOverviewWidget.php
@@ -8,13 +8,25 @@ class StatsOverviewWidget extends Widget
 
     protected static string $view = 'filament::widgets.stats-overview-widget';
 
+    protected array $cachedCards = [];
+
+    public function mount()
+    {
+        $this->cachedCards = $this->getCards();
+    }
+
     protected function getColumns(): int
     {
-        return match ($count = count($this->getCards())) {
+        return match ($count = count($this->getCachedCards())) {
             5, 6, 9, 11 => 3,
             7, 8, 10, 12 => 4,
             default => $count,
         };
+    }
+
+    protected function getCachedCards(): array
+    {
+        return $this->cachedCards;
     }
 
     protected function getCards(): array
