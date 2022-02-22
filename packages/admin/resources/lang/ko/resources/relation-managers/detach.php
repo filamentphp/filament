d4223ccfbe55a82b45b5230cf7fca718d376d02d
silnex <silnex@silnex.kr>
2022-02-22 16:35:24 +0900

    Add korean translations

---
+++ packages/admin/resources/lang/ko/resources/relation-managers/detach.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'action' => [
+
+        'label' => '분리',
+
+        'modal' => [
+            'heading' => ':label 분리',
+        ],
+
+        'messages' => [
+            'detached' => '분리 완료',
+        ],
+
+    ],
+
+    'bulk_action' => [
+
+        'label' => '선택한 목록 분리',
+
+        'modal' => [
+            'heading' => ':label 선택한 목록 분리',
+        ],
+
+        'messages' => [
+            'detached' => '분리 완료',
+        ],
+
+    ],
+
+];
