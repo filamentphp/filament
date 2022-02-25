681bec9a64679613f2c8c0f060985451c678a159
bezhanSalleh <bezhanSalleh@users.noreply.github.com>
2022-02-25 20:15:41 +0000

    chore: styling

--- packages/admin/src/Widgets/StatsOverviewWidget/Card.php
+++ packages/admin/src/Widgets/StatsOverviewWidget/Card.php
@@ -2,10 +2,10 @@
 
 namespace Filament\Widgets\StatsOverviewWidget;
 
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Str;
 use Illuminate\View\Component;
-use Illuminate\Contracts\View\View;
-use Illuminate\Contracts\Support\Htmlable;
 
 class Card extends Component implements Htmlable
 {
