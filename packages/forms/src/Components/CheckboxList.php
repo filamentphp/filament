eee4f964c8652366da4bf6c280a86bf94b0b6d07
Dan Harrin <git@danharrin.com>
2022-03-01 17:56:28 +0000

    wip

--- packages/forms/src/Components/CheckboxList.php
+++ packages/forms/src/Components/CheckboxList.php
@@ -7,9 +7,9 @@ use Illuminate\Contracts\Support\Arrayable;
 
 class CheckboxList extends Field
 {
+    use Concerns\HasOptions;
+    
     protected string $view = 'forms::components.checkbox-list';
-
-    protected array | Closure $options = [];
 
     protected function setUp(): void
     {
@@ -24,23 +24,5 @@ class CheckboxList extends Field
 
             $component->state([]);
         });
-    }
-
-    public function options(array | Arrayable | Closure $options): static
-    {
-        $this->options = $options;
-
-        return $this;
-    }
-
-    public function getOptions(): array
-    {
-        $options = $this->evaluate($this->options);
-
-        if ($options instanceof Arrayable) {
-            $options = $options->toArray();
-        }
-
-        return $options;
     }
 }
