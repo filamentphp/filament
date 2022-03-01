d57ca78cf2de7b7215b832a58c88b83cf71aef58
Dan <dpock32509@gmail.com>
2022-03-01 00:16:25 -0500

    Fix bug

--- packages/admin/src/Resources/Pages/ListRecords/Concerns/CanCreateRecords.php
+++ packages/admin/src/Resources/Pages/ListRecords/Concerns/CanCreateRecords.php
@@ -96,7 +96,7 @@ trait CanCreateRecords
 
     protected function getCreatedNotificationMessage(): ?string
     {
-        return __('filament::resources/pages/list-records.actions.create.messages.created')
+        return __('filament::resources/pages/list-records.actions.create.messages.created');
     }
 
     public function createAndCreateAnother(): void
