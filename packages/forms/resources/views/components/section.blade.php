4d886b9b559dd21a66f6e9a4016fcb664f9a4b0a
Dan Harrin <git@danharrin.com>
2022-03-02 23:04:23 +0000

    wip

--- packages/forms/resources/views/components/section.blade.php
+++ packages/forms/resources/views/components/section.blade.php
@@ -5,7 +5,7 @@
     @endif
     id="{{ $getId() }}"
     {{ $attributes->merge($getExtraAttributes())->class([
-        'p-6 space-y-6 rounded-xl shadow-sm border border-gray-300 filament-forms-section-component bg-white',
+        'p-6 space-y-6 bg-white rounded-xl shadow border border-gray-300 filament-forms-section-component',
         'dark:border-gray-600 dark:bg-gray-800' => config('forms.dark_mode'),
     ]) }}
     {{ $getExtraAlpineAttributeBag() }}
