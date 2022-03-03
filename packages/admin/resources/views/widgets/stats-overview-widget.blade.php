ee92067da390a359446cfd4ba38a05dfb2db10a4
Dan Harrin <git@danharrin.com>
2022-03-03 15:46:22 +0000

    feature: Stats overview polling

--- packages/admin/resources/views/widgets/stats-overview-widget.blade.php
+++ packages/admin/resources/views/widgets/stats-overview-widget.blade.php
@@ -1,7 +1,9 @@
 <x-filament::widget class="filament-stats-overview-widget">
-    <x-filament::stats :columns="$this->getColumns()">
-        @foreach ($this->getCachedCards() as $card)
-            {{ $card }}
-        @endforeach
-    </x-filament::stats>
+    <div {!! ($pollingInterval = $this->getPollingInterval()) ? "wire:poll.{$pollingInterval}" : '' !!}>
+        <x-filament::stats :columns="$this->getColumns()">
+            @foreach ($this->getCachedCards() as $card)
+                {{ $card }}
+            @endforeach
+        </x-filament::stats>
+    </div>
 </x-filament::widget>
