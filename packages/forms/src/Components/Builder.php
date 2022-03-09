e0832251860bf778eb5c93fae8c48f3f9e0fdaaf
Ziyaan Hassan <ziyaan2010@gmail.com>
2022-03-09 23:56:08 +0500

    add ItemBetweenCreationDisabled

--- packages/forms/src/Components/Builder.php
+++ packages/forms/src/Components/Builder.php
@@ -24,6 +24,8 @@ class Builder extends Field
 
     protected bool | Closure $isItemCreationDisabled = false;
 
+    protected bool | Closure $isItemBetweenCreationDisabled = false;
+
     protected bool | Closure $isItemDeletionDisabled = false;
 
     protected function setUp(): void
@@ -196,6 +198,13 @@ class Builder extends Field
         return $this;
     }
 
+    public function disableItemBetweenCreation(bool | Closure $condition = true): static
+    {
+        $this->isItemBetweenCreationDisabled = $condition;
+
+        return $this;
+    }
+
     public function disableItemDeletion(bool | Closure $condition = true): static
     {
         $this->isItemDeletionDisabled = $condition;
@@ -256,6 +265,11 @@ class Builder extends Field
     public function isItemCreationDisabled(): bool
     {
         return $this->evaluate($this->isItemCreationDisabled);
+    }
+    
+    public function isItemBetweenCreationDisabled(): bool
+    {
+        return $this->evaluate($this->isItemBetweenCreationDisabled);
     }
 
     public function isItemDeletionDisabled(): bool
