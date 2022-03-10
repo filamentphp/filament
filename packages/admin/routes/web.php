11d215501dbce4d1e6077e6afd028660e4a6ba3a
RVxLab <RVxLab@users.noreply.github.com>
2022-03-10 13:47:00 +0000

    chore: styling

--- packages/admin/routes/web.php
+++ packages/admin/routes/web.php
@@ -3,7 +3,6 @@
 use Filament\Facades\Filament;
 use Filament\Http\Controllers\AssetController;
 use Filament\Http\Responses\Contracts\LogoutResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 
 Route::domain(config('filament.domain'))
--- tests/src/Admin/Auth/LogoutTest.php
+++ tests/src/Admin/Auth/LogoutTest.php
@@ -16,8 +16,7 @@ it('can log a user out', function () {
 });
 
 it('allows a user to override the logout response', function () {
-    $logoutResponseFake = new class() implements LogoutResponse
-    {
+    $logoutResponseFake = new class () implements LogoutResponse {
         public function toResponse($request)
         {
             return redirect()->to('https://example.com');
