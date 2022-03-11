7645a0d485cd2084d97d994bb4278d3de200717d
alperen ersoy <ersoyalperen@gmail.com>
2022-03-11 17:53:24 +0300

    Added custom validation rule for minLength() and maxLength()

--- packages/forms/src/Components/Concerns/CanBeLengthConstrained.php
+++ packages/forms/src/Components/Concerns/CanBeLengthConstrained.php
@@ -3,6 +3,7 @@
 namespace Filament\Forms\Components\Concerns;
 
 use Closure;
+use Illuminate\Support\Facades\Validator;
 
 trait CanBeLengthConstrained
 {
@@ -12,12 +13,15 @@ trait CanBeLengthConstrained
 
     public function maxLength(int | Closure $length): static
     {
+        Validator::extend('max_length', fn ($attribute, $value, $parameters) =>  strlen((string) $value) <= $parameters[0], __('validation.max.string'));
+        Validator::replacer('max_length', fn ($message, $attribute, $rule, $parameters) =>  str_replace(':max', $parameters[0], $message));
+
         $this->maxLength = $length;
 
         $this->rule(function (): string {
             $length = $this->getMaxLength();
 
-            return "max:{$length}";
+            return "max_length:{$length}";
         });
 
         return $this;
@@ -25,12 +29,15 @@ trait CanBeLengthConstrained
 
     public function minLength(int | Closure $length): static
     {
+        Validator::extend('min_length', fn ($attribute, $value, $parameters) =>  strlen((string) $value) >= $parameters[0], __('validation.min.string'));
+        Validator::replacer('min_length', fn ($message, $attribute, $rule, $parameters) =>  str_replace(':min', $parameters[0], $message));
+
         $this->minLength = $length;
 
         $this->rule(function (): string {
             $length = $this->getMinLength();
 
-            return "min:{$length}";
+            return "min_length:{$length}";
         });
 
         return $this;
