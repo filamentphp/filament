d30a20d360d71bacd251925ed2c4dae81b78d47a
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-03-17 13:13:12 +0100

    Do not use CanOpenUrl trait

--- packages/admin/src/Navigation/NavigationItem.php
+++ packages/admin/src/Navigation/NavigationItem.php
@@ -3,12 +3,9 @@
 namespace Filament\Navigation;
 
 use Closure;
-use Filament\Pages\Actions\Concerns\CanOpenUrl;
 
 class NavigationItem
 {
-    use CanOpenUrl;
-
     protected ?string $group = null;
 
     protected ?Closure $isActiveWhen = null;
@@ -19,44 +16,13 @@ class NavigationItem
 
     protected ?string $badge = null;
 
+    protected bool $shouldOpenUrlInNewTab = false;
+
     protected ?int $sort = null;
 
-    final public function __construct()
-    {
-    }
+    protected string|Closure|null $url = null;
 
-    public static function make(): static
-    {
-        return app(static::class);
-    }
-
-    public function group(?string $group): static
-    {
-        $this->group = $group;
-
-        return $this;
-    }
-
-    public function icon(string $icon): static
-    {
-        $this->icon = $icon;
-
-        return $this;
-    }
-
-    public function isActiveWhen(Closure $callback): static
-    {
-        $this->isActiveWhen = $callback;
-
-        return $this;
-    }
-
-    public function label(string $label): static
-    {
-        $this->label = $label;
-
-        return $this;
-    }
+    final public function __construct() {}
 
     public function badge(?string $badge): static
     {
@@ -65,11 +31,9 @@ class NavigationItem
         return $this;
     }
 
-    public function sort(?int $sort): static
+    public function getBadge(): ?string
     {
-        $this->sort = $sort;
-
-        return $this;
+        return $this->badge;
     }
 
     public function getGroup(): ?string
@@ -87,24 +51,84 @@ class NavigationItem
         return $this->label;
     }
 
-    public function getBadge(): ?string
-    {
-        return $this->badge;
-    }
-
     public function getSort(): int
     {
         return $this->sort ?? -1;
+    }
+
+    public function getUrl(): ?string
+    {
+        return value($this->url);
+    }
+
+    public function group(?string $group): static
+    {
+        $this->group = $group;
+
+        return $this;
+    }
+
+    public function icon(string $icon): static
+    {
+        $this->icon = $icon;
+
+        return $this;
     }
 
     public function isActive(): bool
     {
         $callback = $this->isActiveWhen;
 
-        if ($callback === null) {
+        if ( $callback === null ) {
             return false;
         }
 
         return app()->call($callback);
+    }
+
+    public function isActiveWhen(Closure $callback): static
+    {
+        $this->isActiveWhen = $callback;
+
+        return $this;
+    }
+
+    public function label(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
+    public function openUrlInNewTab(bool $condition = true): static
+    {
+        $this->shouldOpenUrlInNewTab = $condition;
+
+        return $this;
+    }
+
+    public function shouldOpenUrlInNewTab(): bool
+    {
+        return $this->shouldOpenUrlInNewTab;
+    }
+
+    public function sort(?int $sort): static
+    {
+        $this->sort = $sort;
+
+        return $this;
+    }
+
+    public function url(string|Closure|null $url, bool $shouldOpenInNewTab = false): static
+    {
+        $this->shouldOpenUrlInNewTab = $shouldOpenInNewTab;
+        $this->url = $url;
+
+        return $this;
     }
 }
