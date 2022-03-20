ed90d05ff65eeeabaffce1a93750e33d233a0c36
Dan Harrin <git@danharrin.com>
2022-03-20 08:54:54 +0000

    Update Action.php

--- packages/tables/src/Actions/Action.php
+++ packages/tables/src/Actions/Action.php
@@ -12,8 +12,8 @@ use Illuminate\View\Component;
 class Action extends Component implements Htmlable
 {
     use Concerns\BelongsToTable;
-    use Concerns\CanBeHidden;
     use Concerns\CanBeDisabled;
+    use Concerns\CanBeHidden;
     use Concerns\CanBeMounted;
     use Concerns\CanOpenModal;
     use Concerns\CanOpenUrl;
