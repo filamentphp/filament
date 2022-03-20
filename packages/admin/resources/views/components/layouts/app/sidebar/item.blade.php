e17468d2a82112f5c69b28476434f5180e123db1
Dan Harrin <git@danharrin.com>
2022-03-20 20:17:13 +0000

    Update item.blade.php

--- packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
+++ packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
@@ -9,13 +9,13 @@
 <li class="filament-sidebar-item">
     <a
         href="{{ $url }}"
+        {!! $shouldOpenUrlInNewTab ? 'target="_blank"' : '' !!}
         @class([
             'flex items-center gap-3 px-3 py-2 rounded-lg font-medium transition',
             'hover:bg-gray-500/5 focus:bg-gray-500/5' => ! $active,
             'dark:text-gray-300 dark:hover:bg-gray-700' => (! $active) && config('filament.dark_mode'),
             'bg-primary-500 text-white' => $active,
         ])
-        @if($shouldOpenUrlInNewTab) target="_blank" @endif
     >
         <x-dynamic-component :component="$icon" class="h-5 w-5" />
 
