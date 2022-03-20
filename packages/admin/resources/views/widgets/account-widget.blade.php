9193c6b13a822186ccefdf3bf422ecfd9af3a35c
Dan Harrin <git@danharrin.com>
2022-03-20 12:33:46 +0000

    Update account-widget.blade.php

--- packages/admin/resources/views/widgets/account-widget.blade.php
+++ packages/admin/resources/views/widgets/account-widget.blade.php
@@ -15,15 +15,18 @@
                     {{ __('filament::widgets/account-widget.welcome', ['user' => \Filament\Facades\Filament::getUserName($user)]) }}
                 </h2>
 
-                <form method="POST" action="{{ route('filament.auth.logout') }}" class="text-sm">
+                <form action="{{ route('filament.auth.logout') }}" method="post" class="text-sm">
                     @csrf
 
-                    <a href="{{ route('filament.auth.logout') }}" @class([
-                        'text-gray-600 hover:text-primary-500 focus:outline-none focus:underline',
-                        'dark:text-gray-300 dark:hover:text-primary-500' => config('filament.dark_mode'),
-                    ]) onclick="event.preventDefault(); this.closest('form').submit();">
+                    <button
+                        type="submit"
+                        @class([
+                            'text-gray-600 hover:text-primary-500 focus:outline-none focus:underline',
+                            'dark:text-gray-300 dark:hover:text-primary-500' => config('filament.dark_mode'),
+                        ])
+                    >
                         {{ __('filament::widgets/account-widget.buttons.logout.label') }}
-                    </a>
+                    </button>
                 </form>
             </div>
         </div>
