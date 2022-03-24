5a8d14ae88e9d0cfddc96bc712e756044132ed2a
Maher El Gamil <maherelgamil@users.noreply.github.com>
2022-03-24 12:00:51 +0200

    show isdebar group collapase icon when sidebar is collapsed

--- packages/admin/resources/views/components/layouts/app/sidebar/group.blade.php
+++ packages/admin/resources/views/components/layouts/app/sidebar/group.blade.php
@@ -6,27 +6,32 @@
     @if ($label)
         <button
             x-on:click.prevent="$store.sidebar.toggleCollapsedGroup(label)"
-            @if (config('filament.layout.sidebar.is_collapsible_on_desktop'))
-                x-show="$store.sidebar.isOpen"
-            @endif
             class="flex items-center justify-between w-full"
         >
-            <p @class([
-                'font-bold uppercase text-gray-600 text-xs tracking-wider',
-                'dark:text-gray-300' => config('filament.dark_mode'),
-            ])>
+            <p 
+                @if (config('filament.layout.sidebar.is_collapsible_on_desktop'))
+                    x-show="$store.sidebar.isOpen"
+                @endif
+                @class([
+                    'font-bold uppercase text-gray-600 text-xs tracking-wider',
+                    'dark:text-gray-300' => config('filament.dark_mode'),
+                ])
+            >
                 {{ $label }}
             </p>
 
-            <x-heroicon-o-chevron-down :class="\Illuminate\Support\Arr::toCssClasses([
-                'w-3 h-3 text-gray-600',
-                'dark:text-gray-300' => config('filament.dark_mode'),
-            ])" x-show="$store.sidebar.groupIsCollapsed(label)" x-cloak />
+            <div class="ml-3">
 
-            <x-heroicon-o-chevron-up :class="\Illuminate\Support\Arr::toCssClasses([
-                'w-3 h-3 text-gray-600',
-                'dark:text-gray-300' => config('filament.dark_mode'),
-            ])" x-show="! $store.sidebar.groupIsCollapsed(label)" />
+                <x-heroicon-o-chevron-down :class="\Illuminate\Support\Arr::toCssClasses([
+                    'w-3 h-3 text-gray-600',
+                    'dark:text-gray-300' => config('filament.dark_mode'),
+                ])" x-show="$store.sidebar.groupIsCollapsed(label)" x-cloak />
+
+                <x-heroicon-o-chevron-up :class="\Illuminate\Support\Arr::toCssClasses([
+                    'w-3 h-3 text-gray-600',
+                    'dark:text-gray-300' => config('filament.dark_mode'),
+                ])" x-show="! $store.sidebar.groupIsCollapsed(label)" />
+                </div>
         </button>
     @endif
 
