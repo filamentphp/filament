5fe284216ecdafebfc457ae7011607bb80989816
Dennis Koch <info@pixelarbeit.de>
2022-03-24 12:08:45 +0100

    Apply row hover only when clickable

--- packages/tables/resources/views/components/row.blade.php
+++ packages/tables/resources/views/components/row.blade.php
@@ -1,3 +1,13 @@
-<tr {{ $attributes->class(['hover:bg-primary-500/5 dark:hover:bg-primary-500/5 filament-tables-row']) }}>
+@props([
+    'recordUrl' => null,
+])
+<tr
+    {{
+        $attributes->class([
+            'hover:bg-primary-500/5 dark:hover:bg-primary-500/5' => $recordUrl,
+            'filament-tables-row',
+        ])
+    }}
+>
     {{ $slot }}
 </tr>
