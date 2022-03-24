f4519ebee3303d2250d8937fe14defdd1dfcada4
Dennis Koch <info@pixelarbeit.de>
2022-03-24 08:59:57 +0100

    Slight hover effect for table rows

--- packages/tables/resources/views/components/row.blade.php
+++ packages/tables/resources/views/components/row.blade.php
@@ -1,3 +1,3 @@
-<tr {{ $attributes->class(['filament-tables-row']) }}>
+<tr {{ $attributes->class(['hover:bg-primary-500/5 dark:hover:bg-primary-500/5 filament-tables-row']) }}>
     {{ $slot }}
 </tr>
