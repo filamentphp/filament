d16c9f486ef8a5c19d9ae4c4312ab713fc5a5909
Ali Mahdavi <mahdavi095@gmail.com>
2022-03-25 12:27:16 +0430

    Add associate and dissociate to 'fa' translation

---
+++ packages/admin/resources/lang/fa/resources/relation-managers/dissociate.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'action' => [
+
+        'label' => 'جداکردن',
+
+        'modal' => [
+            'heading' => 'جداکردن :label',
+        ],
+
+        'messages' => [
+            'dissociated' => 'جداکردن',
+        ],
+
+    ],
+
+    'bulk_action' => [
+
+        'label' => 'جداکردن انتخاب شده ها',
+
+        'modal' => [
+            'heading' => 'جداکردن انتخاب شده :label',
+        ],
+
+        'messages' => [
+            'dissociated' => 'جدا شد',
+        ],
+
+    ],
+
+];
