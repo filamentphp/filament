1985ac1a22d8e3d703efbcc81238e25fb90d510c
Dennis Koch <info@pixelarbeit.de>
2022-03-26 11:49:49 +0100

    Fix hidden tabs taking up space.
    Fixes #2022

--- packages/forms/resources/views/components/tabs/tab.blade.php
+++ packages/forms/resources/views/components/tabs/tab.blade.php
@@ -3,7 +3,7 @@
     id="{{ $getId() }}"
     role="tabpanel"
     tabindex="0"
-    x-bind:class="{ 'invisible h-0 p-0': tab !== '{{ $getId() }}' }"
+    x-bind:class="{ 'invisible h-0 p-0 overflow-y-hidden': tab !== '{{ $getId() }}' }"
     {{ $attributes->merge($getExtraAttributes())->class(['p-6 focus:outline-none filament-forms-tabs-component-tab']) }}
 >
     {{ $getChildComponentContainer() }}
