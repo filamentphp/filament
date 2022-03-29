783eebe84185bb3220d659711229610271a010d4
danharrin <danharrin@users.noreply.github.com>
2022-03-29 19:28:43 +0000

    chore: styling

--- packages/forms/src/helpers.php
+++ packages/forms/src/helpers.php
@@ -9,7 +9,7 @@ if (! function_exists('Filament\Forms\array_move_after')) {
 
         $indexToMoveAfter = array_search($keyToMoveAfter, $keys);
         $keyToMoveBefore = $keys[$indexToMoveAfter + 1] ?? null;
-        
+
         if (filled($keyToMoveBefore)) {
             return $array;
         }
@@ -35,7 +35,7 @@ if (! function_exists('Filament\Forms\array_move_before')) {
 
         $indexToMoveBefore = array_search($keyToMoveBefore, $keys);
         $keyToMoveAfter = $keys[$indexToMoveBefore - 1] ?? null;
-        
+
         if (filled($keyToMoveAfter)) {
             return $array;
         }
