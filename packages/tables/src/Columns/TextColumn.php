71dde1c326b74685b52c6686b82bf1b9d1fa1ab8
Patrick Schillemat <patrick.schillemat@gmail.com>
2022-03-29 17:31:31 -0400

    Update TextColumn.php
    Addition of ->html() modifier. Purpose of this is to display the returned text from the database with HTML intact (rich text) rather than outputting raw data.

--- packages/tables/src/Columns/TextColumn.php
+++ packages/tables/src/Columns/TextColumn.php
@@ -28,4 +28,9 @@ class TextColumn extends Column
     {
         return implode(', ', $state);
     }
+    
+    public function html() : static
+    {
+        return $this->formatStateUsing(fn ($state) => new \Illuminate\Support\HtmlString($state));
+    }
 }
