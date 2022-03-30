abc530b8a8a322f6930cc9f0e71169293d7ca49e
awcodes <adam@adamwgd.com>
2022-03-30 10:19:30 -0400

    fix: Adds support to Builder Block picker to determine if it should dromp down or up. This prevents overflowing at bottom of screen when open.

--- packages/forms/resources/views/components/builder/block-picker.blade.php
+++ packages/forms/resources/views/components/builder/block-picker.blade.php
@@ -1,18 +1,11 @@
-@props([
-    'blocks',
-    'createAfterItem' => null,
-    'statePath',
-])
+@props(['blocks', 'createAfterItem' => null, 'statePath'])
 
-<div
-    x-show="isCreateButtonDropdownOpen"
-    x-on:click.away="isCreateButtonDropdownOpen = false"
-    x-transition
+<div x-show="isCreateButtonDropdownOpen" x-on:click.away="isCreateButtonDropdownOpen = false" x-data="{ showAbove: false, init() { this.$watch('isCreateButtonDropdownOpen', () => { this.setPosition(); }) }, setPosition() { this.$nextTick(() => { let bounding = this.$el.getBoundingClientRect(); if (bounding.bottom > (window.innerHeight || document.documentElement.clientHeight)) { this.showAbove = true; } }); } }"
+    x-bind:class="{'bottom-0 mb-10': showAbove}" x-transition
     {{ $attributes->class([
         'absolute z-20 mt-10 shadow-xl border overflow-hidden rounded-xl w-52 filament-forms-builder-component-block-picker',
         'dark:border-gray-600' => config('forms.dark_mode'),
-    ]) }}
->
+    ]) }}>
     <ul @class([
         'py-1 space-y-1 bg-white shadow rounded-xl',
         'dark:bg-gray-700 dark:divide-gray-600' => config('forms.dark_mode'),
@@ -21,12 +14,11 @@
             <li>
                 <button
                     wire:click="dispatchFormEvent('builder::createItem', '{{ $statePath }}', '{{ $block->getName() }}' {{ $createAfterItem ? ", '{$createAfterItem}'" : '' }})"
-                    x-on:click="isCreateButtonDropdownOpen = false"
-                    type="button"
-                    class="flex items-center w-full h-8 px-3 text-sm font-medium focus:outline-none hover:text-white hover:bg-primary-600 focus:bg-primary-700 focus:text-white group"
-                >
+                    x-on:click="isCreateButtonDropdownOpen = false" type="button"
+                    class="flex items-center w-full h-8 px-3 text-sm font-medium focus:outline-none hover:text-white hover:bg-primary-600 focus:bg-primary-700 focus:text-white group">
                     @if ($icon = $block->getIcon())
-                        <x-dynamic-component :component="$icon" class="mr-2 -ml-1 text-primary-500 w-5 h-5 group-hover:text-white group-focus:text-white" />
+                        <x-dynamic-component :component="$icon"
+                            class="w-5 h-5 mr-2 -ml-1 text-primary-500 group-hover:text-white group-focus:text-white" />
                     @endif
 
                     {{ $block->getLabel() }}
