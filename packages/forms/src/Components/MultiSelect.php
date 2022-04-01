9b5de696d815f62ddb577d9500db8872887568e1
danharrin <danharrin@users.noreply.github.com>
2022-04-01 18:59:29 +0000

    chore: styling

--- packages/forms/src/Components/MultiSelect.php
+++ packages/forms/src/Components/MultiSelect.php
@@ -2,10 +2,6 @@
 
 namespace Filament\Forms\Components;
 
-use Closure;
-use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Support\HtmlString;
-
 class MultiSelect extends Select
 {
     public function isMultiple(): bool
