81d34bad2f588dc5aabb77d0f18acdfef60accfb
ShaungBhone <phonelaayy@gmail.com>
2022-04-05 22:32:43 +0630

    Update burmese translation

---
+++ packages/admin/resources/lang/my/widgets/filament-info-widget.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'buttons' => [
+
+        'visit_documentation' => [
+            'label' => 'အညွန်း',
+        ],
+
+        'visit_github' => [
+            'label' => 'GitHub',
+        ],
+
+    ],
+
+];
