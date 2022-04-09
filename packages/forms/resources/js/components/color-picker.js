a36c216dc46c1102fcf4af78edf45239e03282ea
Saade <saade@outlook.com.br>
2022-04-09 03:39:55 -0300

    add color picker

---
+++ packages/forms/resources/js/components/color-picker.js
@@ -0,0 +1,45 @@
+import 'vanilla-colorful/hex-color-picker';
+import 'vanilla-colorful/hsl-string-color-picker';
+import 'vanilla-colorful/rgb-string-color-picker';
+import 'vanilla-colorful/rgba-string-color-picker';
+
+import '../../css/components/color-picker.css';
+
+
+export default (Alpine) => {
+    Alpine.data('colorPickerFormComponent', ({
+        state,
+    }) => {
+        return {
+            state,
+
+            isOpen: false,
+
+            init: function () {
+                if( this.state )
+                    this.setState(this.state);
+
+                this.$refs.picker.addEventListener('color-changed', (event) => {
+                    this.setState(event.detail.value);
+                });
+            },
+
+            openPicker: function () {
+                if( this.isDisabled ) return
+
+                this.isOpen = true
+            },
+
+            closePicker: function () {
+                this.isOpen = false
+            },
+
+            setState: function (value) {
+                this.state = value
+
+                this.$refs.input.value = value
+                this.$refs.picker.color = value
+            }
+        }
+    })
+}
---
+++ packages/forms/src/Components/ColorPicker.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Filament\Forms\Components;
+
+use Closure;
+
+class ColorPicker extends Field
+{
+    use Concerns\HasAffixes;
+    use Concerns\HasExtraAlpineAttributes;
+    use Concerns\HasExtraInputAttributes;
+    use Concerns\HasPlaceholder;
+    use Concerns\CanBeInline;
+
+    protected string $view = 'forms::components.color-picker';
+
+    protected string | Closure $format = 'hex';
+
+    protected bool | Closure $preview = true;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->inline(false);
+    }
+
+    public function format(string | Closure $format): static
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    public function preview(bool | Closure $preview = true): static
+    {
+        $this->preview = $preview;
+
+        return $this;
+    }
+
+    public function getFormat(): string
+    {
+        return $this->evaluate($this->format);
+    }
+
+    public function hasPreview(): bool
+    {
+        return (bool) $this->evaluate($this->preview);
+    }
+}
