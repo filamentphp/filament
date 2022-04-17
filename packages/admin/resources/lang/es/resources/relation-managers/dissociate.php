a4b6c9517534d19e091aa12bd1dfd5f96ee0f2df
Pathros <latlovs-github@yahoo.com>
2022-04-17 12:55:26 -0500

    Add Spanish translation for associate and dissociate

---
+++ packages/admin/resources/lang/es/resources/relation-managers/dissociate.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'action' => [
+
+        'label' => 'Disociar',
+
+        'modal' => [
+            'heading' => 'Disociar :label',
+        ],
+
+        'messages' => [
+            'dissociated' => 'Disociada',
+        ],
+
+    ],
+
+    'bulk_action' => [
+
+        'label' => 'Disociar seleccionadas',
+
+        'modal' => [
+            'heading' => 'Disociar :label seleccionada',
+        ],
+
+        'messages' => [
+            'dissociated' => 'Disociada',
+        ],
+
+    ],
+
+];
