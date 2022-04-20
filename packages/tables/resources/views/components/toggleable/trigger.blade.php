c6ea883e768e63df069d926a20e0b4dc90c04cf7
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-20 09:50:47 +0100

    chore: update icon

--- packages/tables/resources/views/components/toggleable/trigger.blade.php
+++ packages/tables/resources/views/components/toggleable/trigger.blade.php
@@ -1,5 +1,5 @@
 <x-tables::icon-button
-    icon="heroicon-o-table"
+    icon="heroicon-o-view-boards"
     x-on:click="isOpen = ! isOpen"
     :label="__('tables::table.buttons.toggle_columns.label')"
     {{ $attributes->class(['filament-tables-column-toggling-trigger']) }}
