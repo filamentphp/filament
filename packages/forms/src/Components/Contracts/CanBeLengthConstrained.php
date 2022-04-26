4f3b9bb0243dd274ff50017efaef5882f2600a53
danharrin <danharrin@users.noreply.github.com>
2022-04-26 21:03:57 +0000

    chore: styling

--- packages/forms/src/Components/Contracts/CanBeLengthConstrained.php
+++ packages/forms/src/Components/Contracts/CanBeLengthConstrained.php
@@ -2,11 +2,6 @@
 
 namespace Filament\Forms\Components\Contracts;
 
-use Closure;
-use Filament\Forms\Components\Component;
-use Filament\Forms\Components\Contracts\CanHaveNumericState;
-use Filament\Forms\Components\Field;
-
 interface CanBeLengthConstrained
 {
     public function getLength(): ?int;
