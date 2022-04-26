7bb1de5fdc413b8137eaf49b7ec250840c4fe8f4
danharrin <danharrin@users.noreply.github.com>
2022-04-26 23:57:27 +0000

    chore: styling

--- packages/support/src/Actions/Modal/Actions/Action.php
+++ packages/support/src/Actions/Modal/Actions/Action.php
@@ -4,17 +4,6 @@ namespace Filament\Support\Actions\Modal\Actions;
 
 use Filament\Support\Actions\BaseAction;
 use Filament\Support\Actions\Concerns\CanBeOutlined;
-use Filament\Support\Actions\Concerns\HasColor;
-use Filament\Support\Actions\Concerns\HasIcon;
-use Filament\Support\Actions\Concerns\HasLabel;
-use Filament\Support\Actions\Concerns\HasName;
-use Filament\Support\Actions\Concerns\HasView;
-use Filament\Support\Concerns\Configurable;
-use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\Traits\Conditionable;
-use Illuminate\Support\Traits\Macroable;
-use Illuminate\Support\Traits\Tappable;
-use Illuminate\View\Component;
 
 abstract class Action extends BaseAction
 {
