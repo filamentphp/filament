830202877980a4b832b007f51761995c4a3588d0
Dan Harrin <git@danharrin.com>
2022-04-27 23:20:41 +0100

    Update Configurable.php

--- packages/support/src/Concerns/Configurable.php
+++ packages/support/src/Concerns/Configurable.php
@@ -2,19 +2,38 @@
 
 namespace Filament\Support\Concerns;
 
+use Closure;
+
 trait Configurable
 {
     protected static array $configurations = [];
 
-    public static function configureUsing(callable $callback): void
+    public static function configureUsing(Closure $callback, ?Closure $during = null)
     {
-        static::$configurations[] = $callback;
+        static::$configurations[static::class] ??= [];
+        static::$configurations[static::class][] = $callback;
+
+        if (! $during) {
+            return;
+        }
+
+        try {
+            return $during();
+        } finally {
+            array_pop(static::$configurations[static::class]);
+        }
     }
 
     public function configure(): static
     {
-        foreach (static::$configurations as $callback) {
-            $callback($this);
+        foreach (static::$configurations as $classToConfigure => $configurationCallbacks) {
+            if (! $this instanceof $classToConfigure) {
+                continue;
+            }
+
+            foreach ($configurationCallbacks as $configure) {
+                $configure($this);
+            }
         }
 
         return $this;
