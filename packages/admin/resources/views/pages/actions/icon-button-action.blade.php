d1243b5b41561ae1008f986c958f1609de7757cc
Tom Janssen <dododedodonl@thor.edu>
2022-04-29 23:34:27 +0200

    add icon button view

---
+++ packages/admin/resources/views/pages/actions/icon-button-action.blade.php
@@ -0,0 +1,28 @@
+@php
+    $action = $getAction();
+    $url = $getUrl();
+
+    if (! $action) {
+        $wireClickAction = null;
+    } elseif ($shouldOpenModal() || ($action instanceof \Closure)) {
+        $wireClickAction = "mountAction('{$getName()}')";
+    } else {
+        $wireClickAction = $action;
+    }
+@endphp
+
+<x-filament::button
+    :form="$getForm()"
+    :tag="((! $action) && $url) ? 'a' : 'button'"
+    :wire:click="$isEnabled() ? $wireClickAction : null"
+    :href="$isEnabled() ? $getUrl() : null"
+    :target="$shouldOpenUrlInNewTab() ? '_blank' : null"
+    :type="$canSubmitForm() ? 'submit' : 'button'"
+    :color="$getColor()"
+    :tooltip="$getTooltip()"
+    :outlined="$isOutlined()"
+    :disabled="$isDisabled()"
+    :icon="$getIcon()"
+    :icon-position="$getIconPosition()"
+    class="filament-page-button-action"
+/>
