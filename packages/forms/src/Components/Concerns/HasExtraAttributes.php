ec8313440cd8173b92821c40797ad75ef7e3c1dc
Dan Harrin <git@danharrin.com>
2022-05-01 16:49:12 +0100

    Update HasExtraAttributes.php

--- packages/forms/src/Components/Concerns/HasExtraAttributes.php
+++ packages/forms/src/Components/Concerns/HasExtraAttributes.php
@@ -4,7 +4,7 @@ namespace Filament\Forms\Components\Concerns;
 
 use Filament\Support\Concerns\HasExtraAttributes as BaseTrait;
 
-/** @deprecated */
+/** @deprecated Use `\Filament\Support\Concerns\HasExtraAttributes` instead. */
 trait HasExtraAttributes
 {
     use BaseTrait;
