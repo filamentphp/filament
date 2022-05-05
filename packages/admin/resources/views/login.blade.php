d2858e7bfd230f10c279cdc603dab622cd71136d
Dan Harrin <git@danharrin.com>
2022-05-05 18:23:54 +0100

    wip

--- packages/admin/resources/views/login.blade.php
+++ packages/admin/resources/views/login.blade.php
@@ -4,8 +4,8 @@
 ])>
     <div class="w-screen max-w-md px-6 -mt-16 space-y-8 md:mt-0 md:px-2">
         <form wire:submit.prevent="authenticate" @class([
-            'p-8 space-y-8 bg-white/60 border border-gray-200 shadow-2xl rounded-2xl relative',
-            'dark:bg-gray-900/70 dark:border-gray-700' => config('filament.dark_mode'),
+            'p-8 space-y-8 bg-white/50 backdrop-blur-xl border border-gray-200 shadow-2xl rounded-2xl relative',
+            'dark:bg-gray-900/50 dark:border-gray-700' => config('filament.dark_mode'),
         ])>
             <div class="flex justify-center w-full">
                 <x-filament::brand />
@@ -21,7 +21,5 @@
                 {{ __('filament::login.buttons.submit.label') }}
             </x-filament::button>
         </form>
-
-        <x-filament::footer />
     </div>
 </div>
