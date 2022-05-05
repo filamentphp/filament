99d21d18b4fa61db6c7da3c4fa3916cc17a7e8e3
Daniël van der Spoel <d.vanderspoel@mediamere.com>
2022-05-05 20:29:22 +0200

    Add missing associate translation file

---
+++ packages/admin/resources/lang/nl/resources/relation-managers/associate.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'action' => [
+
+        'label' => 'Associëren',
+
+        'modal' => [
+
+            'heading' => 'Associëren :label',
+
+            'fields' => [
+
+                'record_ids' => [
+                    'label' => 'Records',
+                ],
+
+            ],
+
+            'actions' => [
+
+                'associate' => [
+                    'label' => 'Associëren',
+                ],
+
+                'associate_and_associate_another' => [
+                    'label' => 'Associëren & nieuwe Associëren',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'associated' => 'Geassocieerd',
+        ],
+
+    ],
+
+];
