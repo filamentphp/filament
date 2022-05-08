087ee6bade711b1329d46666ef583fe04189baab
Daniel Speichert <DSpeichert@users.noreply.github.com>
2022-05-07 20:39:02 -0400

    Update hidden.blade.php

--- packages/forms/resources/views/components/hidden.blade.php
+++ packages/forms/resources/views/components/hidden.blade.php
@@ -2,6 +2,7 @@
     id="{{ $getId() }}"
     {!! $isRequired() ? 'required' : null !!}
     type="hidden"
+    {!! ($autocomplete = $getAutocomplete()) ? "autocomplete=\"{$autocomplete}\"" : null !!}
     {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
     {{ $attributes->merge($getExtraAttributes())->class(['filament-forms-hidden-component']) }}
     dusk="filament.forms.{{ $getStatePath() }}"
