215872c08eb1c1566df2636a45357abbba786708
ziming <ziming@users.noreply.github.com>
2022-05-11 15:05:36 +0800

    edit-record.blade.php to support relationGroup

--- packages/admin/resources/views/resources/pages/edit-record.blade.php
+++ packages/admin/resources/views/resources/pages/edit-record.blade.php
@@ -6,8 +6,15 @@
     </x-filament::form>
 
     @if (count($relationManagers = $this->getRelationManagers()))
-        <x-filament::hr />
 
-        <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagers" :owner-record="$record" />
+        @if (is_array($relationManagers[0]))
+            @foreach ($relationManagers as $relationManagersGroup)
+                <x-filament::hr />
+                <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagersGroup" :owner-record="$record" />
+            @endforeach
+        @else
+            <x-filament::hr />
+            <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagers" :owner-record="$record" />
+        @endif
     @endif
 </x-filament::page>
