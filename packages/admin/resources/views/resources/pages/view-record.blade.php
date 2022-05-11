f6bac878c31a505748c00573fe78d6a0158b28bd
ziming <ziming@users.noreply.github.com>
2022-05-11 15:07:40 +0800

    view-record.blade.php to support relationGroup

--- packages/admin/resources/views/resources/pages/view-record.blade.php
+++ packages/admin/resources/views/resources/pages/view-record.blade.php
@@ -1,9 +1,16 @@
 <x-filament::page :widget-data="['record' => $record]" class="filament-resources-view-record-page">
     {{ $this->form }}
-
+    
     @if (count($relationManagers = $this->getRelationManagers()))
-        <x-filament::hr />
 
-        <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagers" :owner-record="$record" />
+        @if (is_array($relationManagers[0]))
+            @foreach ($relationManagers as $relationManagersGroup)
+                <x-filament::hr />
+                <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagersGroup" :owner-record="$record" />
+            @endforeach
+        @else
+            <x-filament::hr />
+            <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagers" :owner-record="$record" />
+        @endif
     @endif
 </x-filament::page>
