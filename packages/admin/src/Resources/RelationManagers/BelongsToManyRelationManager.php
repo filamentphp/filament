697d5286afaf28226ab4f0a67789a3f1d6972cce
Dan Harrin <git@danharrin.com>
2022-05-11 18:56:08 +0100

    Allow manipulating duplicate pivot records

--- packages/admin/src/Resources/RelationManagers/BelongsToManyRelationManager.php
+++ packages/admin/src/Resources/RelationManagers/BelongsToManyRelationManager.php
@@ -4,8 +4,10 @@ namespace Filament\Resources\RelationManagers;
 
 use Filament\Resources\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Arr;
 
 class BelongsToManyRelationManager extends RelationManager
@@ -80,7 +82,11 @@ class BelongsToManyRelationManager extends RelationManager
         $record->update($data);
 
         if (count($pivotColumns)) {
-            $relationship->updateExistingPivot($record, $pivotData);
+            if ($this->allowsDuplicates()) {
+                $record->{$relationship->getPivotAccessor()}->update($pivotData);
+            } else {
+                $relationship->updateExistingPivot($record, $pivotData);
+            }
         }
 
         return $record;
@@ -94,10 +100,9 @@ class BelongsToManyRelationManager extends RelationManager
         $relationship = $this->getRelationship();
 
         // https://github.com/laravel/framework/issues/4962
-        $query->select(
-            $relationship->getTable().'.*',
-            $query->getModel()->getTable().'.*',
-        );
+        if (! $this->allowsDuplicates()) {
+            $this->selectPivotDataInQuery($query);
+        }
 
         // https://github.com/laravel-filament/filament/issues/2079
         $query->withCasts(
@@ -105,5 +110,50 @@ class BelongsToManyRelationManager extends RelationManager
         );
 
         return $query;
+    }
+
+    protected function resolveTableRecord(?string $key): ?Model
+    {
+        if (! $this->allowsDuplicates()) {
+            return parent::resolveTableRecord($key);
+        }
+
+        /** @var BelongsToMany $relationship */
+        $relationship = $this->getRelationship();
+
+        $pivotClass = $relationship->getPivotClass();
+        $pivotKeyName = app($pivotClass)->getKeyName();
+
+        return $this->selectPivotDataInQuery(
+            $relationship->wherePivot($pivotKeyName, $key),
+        )->first();
+    }
+
+    public function getSelectedTableRecords(): Collection
+    {
+        if (! $this->allowsDuplicates()) {
+            return parent::getSelectedTableRecords();
+        }
+
+        /** @var BelongsToMany $relationship */
+        $relationship = $this->getRelationship();
+
+        $pivotClass = $relationship->getPivotClass();
+        $pivotKeyName = app($pivotClass)->getKeyName();
+
+        return $this->selectPivotDataInQuery(
+            $relationship->wherePivotIn($pivotKeyName, $this->selectedTableRecords),
+        )->get();
+    }
+
+    protected function selectPivotDataInQuery(Builder | Relation $query): Builder | Relation
+    {
+        /** @var BelongsToMany $relationship */
+        $relationship = $this->getRelationship();
+
+        return $query->select(
+            $relationship->getTable().'.*',
+            $query->getModel()->getTable().'.*',
+        );
     }
 }
