de730a40402edff61e335c52c63b8ffa5624d819
Dan Harrin <git@danharrin.com>
2022-05-12 06:30:18 +0100

    Update Builder.php

--- packages/forms/src/Components/Builder.php
+++ packages/forms/src/Components/Builder.php
@@ -242,15 +242,14 @@ class Builder extends Field
     public function getChildComponentContainers(bool $withHidden = false): array
     {
         return collect($this->getState())
+            ->filter(fn (array $itemData): ?Block => $this->hasBlock($itemData['type']))
             ->map(
-                fn ($itemData, $itemIndex): ?Block =>  $this->getBlock($itemData['type'])
-                )
-            ->filter()
-            ->map(
-                fn (Block $block, $itemIndex): ComponentContainer  =>  $block->getChildComponentContainer()
+                fn (array $itemData, $itemIndex): ComponentContainer => $this
+                    ->getBlock($itemData['type'])
+                    ->getChildComponentContainer()
                     ->getClone()
                     ->statePath("{$itemIndex}.data")
-                    ->inlineLabel(false)
+                    ->inlineLabel(false),
             )
             ->toArray();
     }
