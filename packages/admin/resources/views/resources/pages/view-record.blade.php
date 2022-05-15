81336d9c2d84c4a7838f93e9d81f02a3f0d973cd
Dan Harrin <git@danharrin.com>
2022-05-15 21:10:04 +0100

    Revert "view-record.blade.php to support relationGroup"
    This reverts commit f6bac878c31a505748c00573fe78d6a0158b28bd.

--- packages/admin/resources/views/resources/pages/view-record.blade.php
+++ packages/admin/resources/views/resources/pages/view-record.blade.php
@@ -1,16 +1,9 @@
 <x-filament::page :widget-data="['record' => $record]" class="filament-resources-view-record-page">
     {{ $this->form }}
-    
-    @if (count($relationManagers = $this->getRelationManagers()))
 
-        @if (is_array($relationManagers[0]))
-            @foreach ($relationManagers as $relationManagersGroup)
-                <x-filament::hr />
-                <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagersGroup" :owner-record="$record" />
-            @endforeach
-        @else
-            <x-filament::hr />
-            <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagers" :owner-record="$record" />
-        @endif
+    @if (count($relationManagers = $this->getRelationManagers()))
+        <x-filament::hr />
+
+        <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagers" :owner-record="$record" />
     @endif
 </x-filament::page>
