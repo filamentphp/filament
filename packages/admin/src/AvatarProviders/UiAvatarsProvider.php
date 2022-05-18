39f68c56db8d67f963e451177c655afd728604d3
Dan Harrin <git@danharrin.com>
2022-05-18 11:25:23 +0100

    Use fluent methods

--- packages/admin/src/AvatarProviders/UiAvatarsProvider.php
+++ packages/admin/src/AvatarProviders/UiAvatarsProvider.php
@@ -4,12 +4,13 @@ namespace Filament\AvatarProviders;
 
 use Filament\Facades\Filament;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class UiAvatarsProvider implements Contracts\AvatarProvider
 {
     public function get(Model $user): string
     {
-        $name = trim(collect(explode(' ', Filament::getUserName($user)))->map(fn($segment) => $segment[0] ?? '')->join(' '));
+        $name = Str::of(Filament::getUserName($user))->trim()->explode(' ')->map(fn (string $segment): string => $segment[0] ?? '')->join(' ');
         
         return 'https://ui-avatars.com/api/?name=' . urlencode($name) . '&color=FFFFFF&background=111827';
     }
