9c6bf53a453c28b19c33b987ca60dd8b41dfda25
David Vincent <envahisseur@gmail.com>
2022-05-20 13:53:55 +0200

    Add view action to relation's manager

---
+++ packages/admin/src/Resources/RelationManagers/Concerns/CanViewRecords.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Filament\Resources\RelationManagers\Concerns;
+
+use Filament\Facades\Filament;
+use Filament\Tables;
+use Filament\Tables\Actions\Modal\Actions\Action as ModalAction;
+use Illuminate\Database\Eloquent\Model;
+
+trait CanViewRecords
+{
+    protected function canView(Model $record): bool
+    {
+        return $this->can('view', $record);
+    }
+
+    protected function getViewFormSchema(): array
+    {
+        return $this->getResourceForm(columns: 2)->disabled()->getSchema();
+    }
+
+    protected function fillViewForm(): void
+    {
+        $this->callHook('beforeFill');
+
+        $data = $this->getMountedTableActionRecord()->toArray();
+
+        $this->getMountedTableActionForm()->fill($data);
+
+        $this->callHook('afterFill');
+    }
+
+    protected function getViewAction(): ?Tables\Actions\Action
+    {
+        return Filament::makeTableAction('view')
+            ->label(__('filament::resources/relation-managers/view.action.label'))
+            ->form($this->getViewFormSchema())
+            ->mountUsing(fn() => $this->fillViewForm())
+            ->modalCancelAction(
+                ModalAction::make('close')
+                    ->label(__('filament::resources/relation-managers/view.action.modal.actions.close.label'))
+                    ->cancel()
+                    ->color('secondary'),
+            )
+            ->modalActions(fn(Tables\Actions\Action $action): array => [$action->getModalCancelAction()])
+            ->modalHeading(__('filament::resources/relation-managers/view.action.modal.heading', ['label' => static::getRecordLabel()]))
+            ->action(function () {
+            })
+            ->icon('heroicon-s-eye')
+            ->hidden(fn(Model $record): bool => !static::canView($record));
+    }
+}
