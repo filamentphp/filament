64e76f77a79fb4ea6809607232ae63f0bae67afd
Furkan Gezek <furkangezek2014@gmail.com>
2022-05-24 13:18:31 +0300

    wip

--- packages/tables/src/Columns/Concerns/InteractsWithTableQuery.php
+++ packages/tables/src/Columns/Concerns/InteractsWithTableQuery.php
@@ -84,7 +84,7 @@ trait InteractsWithTableQuery
                     $activeLocale = $this->getLivewire()->getActiveTableLocale() ?: app()->getLocale();
 
                     return $query->{"{$whereClause}Raw"}(
-                        "lower(json_extract({$searchColumnName}, \"$.{$this->getLivewire()->activeLocale}\")) {$searchOperator} ?",
+                        "lower(json_extract({$searchColumnName}, \"$.{$activeLocale}\")) {$searchOperator} ?",
                         "%{$search}%",
                     );
                 },
