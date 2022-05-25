b5b17f51aaf05783e5009453fa7d3c60fe61ddbb
danharrin <danharrin@users.noreply.github.com>
2022-05-25 20:46:51 +0000

    chore: styling

--- packages/admin/src/Resources/RelationManagers/Concerns/CanAssociateRecords.php
+++ packages/admin/src/Resources/RelationManagers/Concerns/CanAssociateRecords.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait CanAssociateRecords
