c9e89635fc1bdef13188640343227bec52021daf
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-05-26 21:13:51 +0200

    Extract separate card component

--- packages/admin/resources/views/login.blade.php
+++ packages/admin/resources/views/login.blade.php
@@ -1,25 +1,7 @@
-<div @class([
-    'flex items-center justify-center min-h-screen filament-login-page bg-gray-100 text-gray-900',
-    'dark:bg-gray-900 dark:text-white' => config('filament.dark_mode'),
-])>
-    <div class="w-screen max-w-md px-6 -mt-16 space-y-8 md:mt-0 md:px-2">
-        <form wire:submit.prevent="authenticate" @class([
-            'p-8 space-y-8 bg-white/50 backdrop-blur-xl border border-gray-200 shadow-2xl rounded-2xl relative',
-            'dark:bg-gray-900/50 dark:border-gray-700' => config('filament.dark_mode'),
-        ])>
-            <div class="flex justify-center w-full">
-                <x-filament::brand />
-            </div>
+<form wire:submit.prevent="authenticate" class="space-y-8">
+    {{ $this->form }}
 
-            <h2 class="text-2xl font-bold tracking-tight text-center">
-                {{ __('filament::login.heading') }}
-            </h2>
-
-            {{ $this->form }}
-
-            <x-filament::button type="submit" form="authenticate" class="w-full">
-                {{ __('filament::login.buttons.submit.label') }}
-            </x-filament::button>
-        </form>
-    </div>
-</div>
+    <x-filament::button type="submit" form="authenticate" class="w-full">
+        {{ __('filament::login.buttons.submit.label') }}
+    </x-filament::button>
+</form>
