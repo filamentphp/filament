c73fa8e97a39922b5c5c4545fa4523b488bc04cd
alperen ersoy <ersoyalperen@gmail.com>
2022-05-29 15:16:52 +0300

    Added missing Turkish (tr) translations.

---
+++ packages/admin/resources/lang/tr/resources/relation-managers/dissociate.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'action' => [
+
+        'label' => 'Ayır',
+
+        'modal' => [
+            'heading' => 'Ayır :label',
+        ],
+
+        'messages' => [
+            'dissociated' => 'Ayrıldı',
+        ],
+
+    ],
+
+    'bulk_action' => [
+
+        'label' => 'Seçiliyi ayır',
+
+        'modal' => [
+            'heading' => 'Seçiliyi ayır :label',
+        ],
+
+        'messages' => [
+            'dissociated' => 'Ayrıldı',
+        ],
+
+    ],
+
+];
