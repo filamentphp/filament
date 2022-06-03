302ba6d7e11c4543aded6063907700f11538cebf
Dan Harrin <git@danharrin.com>
2022-06-03 22:14:03 +0100

    wip

--- packages/spatie-laravel-media-library-plugin/src/Tables/Columns/SpatieMediaLibraryImageColumn.php
+++ packages/spatie-laravel-media-library-plugin/src/Tables/Columns/SpatieMediaLibraryImageColumn.php
@@ -3,14 +3,13 @@
 namespace Filament\Tables\Columns;
 
 use Illuminate\Database\Eloquent\Builder;
+use Throwable;
 
 class SpatieMediaLibraryImageColumn extends ImageColumn
 {
     protected ?string $collection = null;
 
     protected string $conversion = '';
-
-    protected string | Closure $visibility = 'public';
 
     public function collection(string $collection): static
     {
@@ -26,13 +25,6 @@ class SpatieMediaLibraryImageColumn extends ImageColumn
         return $this;
     }
 
-    public function visibility(string | Closure | null $visibility): static
-    {
-        $this->visibility = $visibility;
-
-        return $this;
-    }
-
     public function getCollection(): ?string
     {
         return $this->collection ?? 'default';
@@ -41,11 +33,6 @@ class SpatieMediaLibraryImageColumn extends ImageColumn
     public function getConversion(): string
     {
         return $this->conversion ?? '';
-    }
-
-    public function getVisibility(): string
-    {
-        return $this->evaluate($this->visibility);
     }
 
     public function getImagePath(): ?string
@@ -58,12 +45,20 @@ class SpatieMediaLibraryImageColumn extends ImageColumn
 
         $record = $this->getRecord();
 
-        if (! method_exists($record, 'getFirstMediaUrl')) {
-            return $state;
+        if ($this->getVisibility() === 'private' && method_exists($record, 'getFirstTemporaryUrl')) {
+            try {
+                return $record->getFirstTemporaryUrl(
+                    now()->addMinutes(5),
+                    $this->getCollection(),
+                    $this->getConversion(),
+                );
+            } catch (Throwable $exception) {
+                // This driver does not support creating temporary URLs.
+            }
         }
 
-        if ($this->getVisibility() === 'private') {
-            return $record->getFirstTemporaryUrl(now()->addMinutes(5), $this->getCollection(), $this->getConversion());
+        if (! method_exists($record, 'getFirstMediaUrl')) {
+            return $state;
         }
 
         return $record->getFirstMediaUrl($this->getCollection(), $this->getConversion());
