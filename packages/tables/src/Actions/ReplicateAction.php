ff8c5d4875a0030eb241c7967e17587b99d28d09
danharrin <danharrin@users.noreply.github.com>
2022-06-04 19:32:49 +0000

    chore: styling

--- packages/tables/src/Actions/ReplicateAction.php
+++ packages/tables/src/Actions/ReplicateAction.php
@@ -2,11 +2,7 @@
 
 namespace Filament\Tables\Actions;
 
-use Closure;
-use Filament\Facades\Filament;
-use Filament\Forms\ComponentContainer;
 use Filament\Support\Actions\Concerns\CanReplicateRecords;
-use Illuminate\Database\Eloquent\Model;
 
 class ReplicateAction extends Action
 {
