eed7095556d7cdbfaa3edd505f2baef00c0e9189
danharrin <danharrin@users.noreply.github.com>
2022-06-08 21:07:03 +0000

    chore: styling

--- packages/admin/src/Pages/Actions/ReplicateAction.php
+++ packages/admin/src/Pages/Actions/ReplicateAction.php
@@ -2,11 +2,8 @@
 
 namespace Filament\Pages\Actions;
 
-use Filament\Pages\Contracts\HasRecord;
-use Filament\Pages\Page;
 use Filament\Support\Actions\Concerns\CanReplicateRecords;
 use Filament\Support\Actions\Contracts\ReplicatesRecords;
-use Illuminate\Database\Eloquent\Model;
 
 class ReplicateAction extends Action implements ReplicatesRecords
 {
