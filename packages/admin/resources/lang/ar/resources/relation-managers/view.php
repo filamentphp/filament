a2495268b3be2c2d734971cf4cbbda93da81d80e
MohamedSabil83 <me@mohamedsabil83.com>
2022-06-09 13:49:13 +0300

    [AR] Translate view file

---
+++ packages/admin/resources/lang/ar/resources/relation-managers/view.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'action' => [
+
+        'label' => 'عرض',
+
+        'modal' => [
+
+            'heading' => 'عرض :label',
+
+            'actions' => [
+
+                'close' => [
+                    'label' => 'إغلاق',
+                ],
+
+            ],
+
+        ],
+
+    ],
+
+];
