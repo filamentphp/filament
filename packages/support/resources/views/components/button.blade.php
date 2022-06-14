9be0c2e3553efefca1b9eb2b8d2a406d34f5344a
wychoong <67364036+wychoong@users.noreply.github.com>
2022-06-14 17:42:00 +0800

    Update button.blade.php

--- packages/support/resources/views/components/button.blade.php
+++ packages/support/resources/views/components/button.blade.php
@@ -82,14 +82,11 @@
         x-data="{
             form: null,
             label: {{ \Illuminate\Support\Js::from($slot->toHtml()) }},
-            isDisabled: {{ \Illuminate\Support\Js::from((bool) $disabled) }},
             isUploadingFile: false,
         }"
-        @if ($disabled)
-            x-bind:class="{ 'opacity-70': isDisabled }"
-        @else
+        @unless ($disabled)
             x-bind:class="{ 'opacity-70 cursor-wait': isUploadingFile }"
-        @endif
+        @endunless
         x-bind:disabled="isUploadingFile"
         x-init="
             form = $el.closest('form')
