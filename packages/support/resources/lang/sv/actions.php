b955e1fb3de7e64eac55ac7117c6b46dde826b39
Andréas Lundgren <andreas.lundgren@halle.se>
2022-06-14 10:10:45 +0200

    Support

---
+++ packages/support/resources/lang/sv/actions.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'modal' => [
+
+        'requires_confirmation_subheading' => 'Är du säker på att du vill göra det här?',
+
+        'buttons' => [
+
+            'cancel' => [
+                'label' => 'Avbryt',
+            ],
+
+            'confirm' => [
+                'label' => 'Bekräfta',
+            ],
+
+            'submit' => [
+                'label' => 'Skicka',
+            ],
+
+        ],
+
+    ],
+
+];
