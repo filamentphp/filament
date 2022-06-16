c5dad573a21057e9572a9ecb1b4b2bddf24336ec
danharrin <danharrin@users.noreply.github.com>
2022-06-16 05:18:52 +0000

    chore: styling

--- packages/tables/src/Actions/ActionGroup.php
+++ packages/tables/src/Actions/ActionGroup.php
@@ -4,7 +4,6 @@ namespace Filament\Tables\Actions;
 
 use Filament\Support\Actions\ActionGroup as BaseActionGroup;
 use Filament\Support\Actions\Concerns\InteractsWithRecord;
-use Filament\Tables\Actions\Concerns\InteractsWithRecords;
 
 class ActionGroup extends BaseActionGroup
 {
@@ -15,8 +14,8 @@ class ActionGroup extends BaseActionGroup
     public function getActions(): array
     {
         return collect($this->actions)
-            ->mapWithKeys(fn (Action $action): array  => [
-                $action->getName() => $action->grouped()->record($this->getRecord())
+            ->mapWithKeys(fn (Action $action): array => [
+                $action->getName() => $action->grouped()->record($this->getRecord()),
             ])
             ->toArray();
     }
