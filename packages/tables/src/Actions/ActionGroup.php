401ef1c7880a7076cb36530c79bc08d035bfc536
Dan Harrin <git@danharrin.com>
2022-06-16 07:18:19 +0200

    fix: Page action groups with records

--- packages/tables/src/Actions/ActionGroup.php
+++ packages/tables/src/Actions/ActionGroup.php
@@ -3,8 +3,21 @@
 namespace Filament\Tables\Actions;
 
 use Filament\Support\Actions\ActionGroup as BaseActionGroup;
+use Filament\Support\Actions\Concerns\InteractsWithRecord;
+use Filament\Tables\Actions\Concerns\InteractsWithRecords;
 
 class ActionGroup extends BaseActionGroup
 {
+    use InteractsWithRecord;
+
     protected string $view = 'tables::actions.group';
+
+    public function getActions(): array
+    {
+        return collect($this->actions)
+            ->mapWithKeys(fn (Action $action): array  => [
+                $action->getName() => $action->grouped()->record($this->getRecord())
+            ])
+            ->toArray();
+    }
 }
