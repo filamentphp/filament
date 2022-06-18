c32bb65abcd75a91b4b1cc1825d7976eea60b19a
Kamil Stępień <kamilst96@gmail.com>
2022-06-18 22:55:19 +0200

    fix(forms/datepicker): force default cursor
    When the field is disabled, it still shows cursor-text,
    but it should show cursor-default instead.

--- packages/forms/resources/views/components/date-time-picker.blade.php
+++ packages/forms/resources/views/components/date-time-picker.blade.php
@@ -70,6 +70,7 @@
                 @class([
                     'w-full h-full p-0 placeholder-gray-400 bg-transparent border-0 focus:placeholder-gray-500 focus:ring-0 focus:outline-none',
                     'dark:bg-gray-700 dark:placeholder-gray-400' => config('forms.dark_mode'),
+                    'cursor-default' => $isDisabled(),
                 ])
             />
 
