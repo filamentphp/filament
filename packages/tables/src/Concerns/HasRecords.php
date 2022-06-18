278686a59a6c36bdc4eb1df2ba57cd6c14c04828
danharrin <danharrin@users.noreply.github.com>
2022-06-18 18:28:50 +0000

    chore: styling

--- packages/tables/src/Concerns/HasRecords.php
+++ packages/tables/src/Concerns/HasRecords.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Str;
 
 trait HasRecords
