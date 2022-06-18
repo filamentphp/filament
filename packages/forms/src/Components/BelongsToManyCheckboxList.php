9f867e9e30adca943c206653dd1b2c7b3a3d3f65
danharrin <danharrin@users.noreply.github.com>
2022-06-18 13:01:42 +0000

    chore: styling

--- packages/forms/src/Components/BelongsToManyCheckboxList.php
+++ packages/forms/src/Components/BelongsToManyCheckboxList.php
@@ -2,11 +2,6 @@
 
 namespace Filament\Forms\Components;
 
-use Closure;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Support\Str;
-
 /**
  * @deprecated use CheckboxList with the `relationship()` method instead.
  */
