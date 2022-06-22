28a1248b550d2049d83ae1534f06557bc2e9e437
Ryan Chandler <ryangjchandler@gmail.com>
2022-06-22 16:51:21 +0100

    chore: deprecated MultiSelect

--- packages/forms/src/Components/MultiSelect.php
+++ packages/forms/src/Components/MultiSelect.php
@@ -2,6 +2,10 @@
 
 namespace Filament\Forms\Components;
 
+/**
+ * @deprecated Use `\Filament\Forms\Components\Select` and `->multiple()` instead.
+ * @see Select
+ */
 class MultiSelect extends Select
 {
     public function isMultiple(): bool
