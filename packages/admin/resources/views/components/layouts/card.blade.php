10e0391a53fdebde8ed4c54ae7b4d8ea66837608
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-06-29 20:04:20 +0200

    Add default width fallback

--- packages/admin/resources/views/components/layouts/card.blade.php
+++ packages/admin/resources/views/components/layouts/card.blade.php
@@ -22,6 +22,7 @@
                 '5xl' => 'max-w-5xl',
                 '6xl' => 'max-w-6xl',
                 '7xl' => 'max-w-7xl',
+                default => $width,
             }
         ])>
             <div @class([
