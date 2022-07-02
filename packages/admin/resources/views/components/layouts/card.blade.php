7d711c33b8f6eb55aeb3c90f6951dce5bdc64a27
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-07-02 14:10:18 +0200

    Add minimum card padding to prevent the card of bumping to the top or bottom of browsers

--- packages/admin/resources/views/components/layouts/card.blade.php
+++ packages/admin/resources/views/components/layouts/card.blade.php
@@ -4,7 +4,7 @@
 
 <x-filament::layouts.base :title="$title">
     <div @class([
-        'flex items-center justify-center min-h-screen filament-login-page bg-gray-100 text-gray-900',
+        'flex items-center justify-center min-h-screen filament-login-page bg-gray-100 text-gray-900 py-12',
         'dark:bg-gray-900 dark:text-white' => config('filament.dark_mode'),
     ])>
         <div class="w-screen max-w-md px-6 -mt-16 space-y-8 md:mt-0 md:px-2">
