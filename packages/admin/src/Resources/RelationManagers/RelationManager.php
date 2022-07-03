98b8befab972ffec80b2a159f13578a42813ef2a
Dan Harrin <git@danharrin.com>
2022-07-03 18:59:32 +0100

    Update RelationManager.php

--- packages/admin/src/Resources/RelationManagers/RelationManager.php
+++ packages/admin/src/Resources/RelationManagers/RelationManager.php
@@ -294,7 +294,7 @@ class RelationManager extends Component implements Tables\Contracts\HasRelations
 
     public static function getRecordTitle(?Model $record): ?string
     {
-        return $record?->getAttributeValue(static::getRecordTitleAttribute()) ?? $record?->getKey();
+        return $record?->getAttributeValue(static::getRecordTitleAttribute()) ?? static::getModelLabel();
     }
 
     /**
