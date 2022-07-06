c5f1e24c790d5ea7706daab82c2a20eb8ff0aaab
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2022-07-06 16:52:45 +0200

    striped look

--- packages/tables/resources/views/components/row.blade.php
+++ packages/tables/resources/views/components/row.blade.php
@@ -7,6 +7,8 @@
         'hover:bg-gray-50' => $recordUrl,
         'dark:hover:bg-gray-500/10' => $recordUrl && config('tables.dark_mode'),
         'filament-tables-row',
+        'even:bg-gray-100' => config('tables.striped'),
+        'dark:even:bg-gray-900' => config('tables.striped') && config('tables.dark_mode'),
     ]) }}
 >
     {{ $slot }}
