2130c7b869da183bf8efd8a8e58bc4f4bb1f7a48
Bernhard Hörmann <hoermann.b@gmail.com>
2022-07-13 11:48:30 +0200

    CHG: Remove additional calls of config

--- packages/admin/resources/views/components/form/actions.blade.php
+++ packages/admin/resources/views/components/form/actions.blade.php
@@ -5,7 +5,6 @@
 
 <x-filament::pages.actions
     :actions="$actions"
-    :align="config('filament.layout.forms.actions.alignment')"
     :full-width="$fullWidth"
     class="filament-form-actions"
 />
--- packages/admin/resources/views/components/modal/actions.blade.php
+++ packages/admin/resources/views/components/modal/actions.blade.php
@@ -1,6 +1,5 @@
 <x-filament-support::modal.actions
     :attributes="\Filament\Support\prepare_inherited_attributes($attributes)"
-    :align="config('filament.layout.forms.actions.alignment')"
     :dark-mode="config('filament.dark_mode')"
 >
     {{ $slot }}
