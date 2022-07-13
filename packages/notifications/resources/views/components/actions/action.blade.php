684b191c428d3872ff05cb862b438aea4f2250ad
Zep Fietje <hey@zepfietje.com>
2022-07-13 15:56:08 +0200

    Support more base action features

--- packages/notifications/resources/views/components/actions/action.blade.php
+++ packages/notifications/resources/views/components/actions/action.blade.php
@@ -3,16 +3,25 @@
     'component',
 ])
 
+@php
+    $wireClickAction = $action->getEvent()
+        ? '$emit(\''.$action->getEvent().'\','.Js::from($action->getEventData()).')'
+        : null;
+@endphp
+
 <x-dynamic-component
     :component="$component"
     :dark-mode="config('notifications.dark_mode')"
     :attributes="\Filament\Support\prepare_inherited_attributes($attributes)->merge($action->getExtraAttributes())"
     :tag="$action->getUrl() ? 'a' : 'button'"
-    :wire:click="$action->getEvent() ? '$emit(\''.$action->getEvent().'\','.Js::from($action->getEventData()).')' : null"
-    :x-on:click="$action->shouldCloseNotification() ? 'close' : null"
-    :href="$action->getUrl()"
+    :wire:click="$action->isEnabled() ? $wireClickAction : null"
+    :x-on:click="$action->isEnabled() && $action->shouldCloseNotification() ? 'close' : null"
+    :href="$action->isEnabled() ? $action->getUrl() : null"
     :target="$action->shouldOpenUrlInNewTab() ? '_blank' : null"
+    :disabled="$action->isDisabled()"
     :color="$action->getColor()"
+    :icon="$icon ?? $action->getIcon()"
+    :size="$action->getSize() ?? 'sm'"
 >
     {{ $slot }}
 </x-dynamic-component>
