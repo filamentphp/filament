afd1297a8202570c090633d3717d1de8eabb52a6
Dan Harrin <git@danharrin.com>
2022-07-14 10:40:55 +0100

    fix: Action process arguments

---
+++ packages/support/src/Actions/Concerns/HasArguments.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Filament\Support\Actions\Concerns;
+
+use Closure;
+
+trait HasArguments
+{
+    protected array $arguments = [];
+
+    public function arguments(array $arguments): static
+    {
+        $this->arguments = $arguments;
+
+        return $this;
+    }
+
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    public function resetArguments(): static
+    {
+        $this->arguments([]);
+
+        return $this;
+    }
+}
