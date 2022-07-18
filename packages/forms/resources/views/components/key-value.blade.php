5bfbe08bae8d202ebb39e18affa319ad6f40fed0
Zep Fietje <hey@zepfietje.com>
2022-07-18 22:42:42 +0200

    Fix indentation

--- packages/forms/resources/views/components/key-value.blade.php
+++ packages/forms/resources/views/components/key-value.blade.php
@@ -25,32 +25,32 @@
                 'dark:divide-gray-700' => config('forms.dark_mode'),
             ])>
                 <thead>
-                <tr @class([
-                        'bg-gray-50',
-                        'dark:bg-gray-800/60' => config('forms.dark_mode'),
-                    ])>
-                    <th @class([
-                            'px-4 py-2 whitespace-nowrap font-medium text-sm text-gray-600',
-                            'dark:text-gray-300' => config('forms.dark_mode'),
-                        ]) scope="col">
-                        {{ $getKeyLabel() }}
-                    </th>
-
-                    <th @class([
-                            'px-4 py-2 whitespace-nowrap font-medium text-sm text-gray-600',
-                            'dark:text-gray-300' => config('forms.dark_mode'),
-                        ]) scope="col">
-                        {{ $getValueLabel() }}
-                    </th>
-
-                    @if ($canDeleteRows() && $getDeleteButtonLabel() && $isEnabled())
-                        <th class="w-12" scope="col" x-show="rows.length > 1">
-                                <span class="sr-only">
-                                    {{ $getDeleteButtonLabel() }}
-                                </span>
+                    <tr @class([
+                            'bg-gray-50',
+                            'dark:bg-gray-800/60' => config('forms.dark_mode'),
+                        ])>
+                        <th @class([
+                                'px-4 py-2 whitespace-nowrap font-medium text-sm text-gray-600',
+                                'dark:text-gray-300' => config('forms.dark_mode'),
+                            ]) scope="col">
+                            {{ $getKeyLabel() }}
                         </th>
-                    @endif
-                </tr>
+
+                        <th @class([
+                                'px-4 py-2 whitespace-nowrap font-medium text-sm text-gray-600',
+                                'dark:text-gray-300' => config('forms.dark_mode'),
+                            ]) scope="col">
+                            {{ $getValueLabel() }}
+                        </th>
+
+                        @if ($canDeleteRows() && $getDeleteButtonLabel() && $isEnabled())
+                            <th class="w-12" scope="col" x-show="rows.length > 1">
+                                    <span class="sr-only">
+                                        {{ $getDeleteButtonLabel() }}
+                                    </span>
+                            </th>
+                        @endif
+                    </tr>
                 </thead>
 
                 <tbody
@@ -60,52 +60,52 @@
                         'dark:divide-gray-600' => config('forms.dark_mode'),
                     ])
                 >
-                <template x-for="(row, index) in rows" x-bind:key="index" x-ref="rowTemplate">
-                    <tr @class([
-                            'divide-x',
-                            'dark:divide-gray-600' => config('forms.dark_mode'),
-                        ])>
-                        <td>
-                            <input
-                                type="text"
-                                x-model="row.key"
-                                x-on:input.debounce.{{ $getDebounce() }}="updateState"
-                                {!! ($placeholder = $getKeyPlaceholder()) ? "placeholder=\"{$placeholder}\"" : '' !!}
-                                @if ((! $canEditKeys()) || $isDisabled())
-                                disabled
-                                @endif
-                                class="w-full px-4 py-3 font-mono text-sm bg-transparent border-0 focus:ring-0"
-                            >
-                        </td>
-
-                        <td class="whitespace-nowrap">
-                            <input
-                                type="text"
-                                x-model="row.value"
-                                x-on:input.debounce.{{ $getDebounce() }}="updateState"
-                                {!! ($placeholder = $getValuePlaceholder()) ? "placeholder=\"{$placeholder}\"" : '' !!}
-                                @if ((! $canEditValues()) || $isDisabled())
-                                disabled
-                                @endif
-                                class="w-full px-4 py-3 font-mono text-sm bg-transparent border-0 focus:ring-0"
-                            >
-                        </td>
-
-                        @if ($canDeleteRows() && $isEnabled())
-                            <td x-show="rows.length > 1" class="whitespace-nowrap">
-                                <div class="flex items-center justify-center">
-                                    <button
-                                        x-on:click="deleteRow(index)"
-                                        type="button"
-                                        class="text-danger-600 hover:text-danger-700"
-                                    >
-                                        <x-heroicon-o-trash class="w-4 h-4" />
-                                    </button>
-                                </div>
+                    <template x-for="(row, index) in rows" x-bind:key="index" x-ref="rowTemplate">
+                        <tr @class([
+                                'divide-x',
+                                'dark:divide-gray-600' => config('forms.dark_mode'),
+                            ])>
+                            <td>
+                                <input
+                                    type="text"
+                                    x-model="row.key"
+                                    x-on:input.debounce.{{ $getDebounce() }}="updateState"
+                                    {!! ($placeholder = $getKeyPlaceholder()) ? "placeholder=\"{$placeholder}\"" : '' !!}
+                                    @if ((! $canEditKeys()) || $isDisabled())
+                                    disabled
+                                    @endif
+                                    class="w-full px-4 py-3 font-mono text-sm bg-transparent border-0 focus:ring-0"
+                                >
                             </td>
-                        @endif
-                    </tr>
-                </template>
+
+                            <td class="whitespace-nowrap">
+                                <input
+                                    type="text"
+                                    x-model="row.value"
+                                    x-on:input.debounce.{{ $getDebounce() }}="updateState"
+                                    {!! ($placeholder = $getValuePlaceholder()) ? "placeholder=\"{$placeholder}\"" : '' !!}
+                                    @if ((! $canEditValues()) || $isDisabled())
+                                    disabled
+                                    @endif
+                                    class="w-full px-4 py-3 font-mono text-sm bg-transparent border-0 focus:ring-0"
+                                >
+                            </td>
+
+                            @if ($canDeleteRows() && $isEnabled())
+                                <td x-show="rows.length > 1" class="whitespace-nowrap">
+                                    <div class="flex items-center justify-center">
+                                        <button
+                                            x-on:click="deleteRow(index)"
+                                            type="button"
+                                            class="text-danger-600 hover:text-danger-700"
+                                        >
+                                            <x-heroicon-o-trash class="w-4 h-4" />
+                                        </button>
+                                    </div>
+                                </td>
+                            @endif
+                        </tr>
+                    </template>
                 </tbody>
             </table>
 
