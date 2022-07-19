d4f0bb33e6db8eeb9fb197d62891d5bc3d203eff
Dan Harrin <git@danharrin.com>
2022-07-19 07:12:16 +0100

    hotfix: Remove floating UI from filters popover

--- packages/tables/resources/views/components/filters/popover.blade.php
+++ packages/tables/resources/views/components/filters/popover.blade.php
@@ -4,14 +4,15 @@
 ])
 
 <div
-    x-data
+    x-data="{ isOpen: false }"
     {{ $attributes->class(['relative inline-block filament-tables-filters']) }}
 >
     <x-tables::filters.trigger />
 
     <div
-        x-ref="panel"
+        x-show="isOpen"
         x-cloak
+        x-on:click.away="isOpen = false"
         x-float.placement.bottom-end.flip.offset="{ offset: 8 }"
         x-transition:enter="ease duration-300"
         x-transition:enter-start="opacity-0 -translate-y-2"
@@ -20,7 +21,7 @@
         x-transition:leave-start="opacity-100 translate-y-0"
         x-transition:leave-end="opacity-0 translate-y-2"
         @class([
-            'absolute hidden z-10 w-screen pl-12 top-full transition rtl:pl-0 rtl:pr-12',
+            'absolute right-0 z-10 w-screen pl-12 mt-4 top-full transition rtl:right-auto rtl:left-0 rtl:pl-0 rtl:pr-12',
             match ($width) {
                 'xs' => 'max-w-xs',
                 'md' => 'max-w-md',
@@ -42,7 +43,7 @@
         ])>
             <x-tables::icon-button
                 icon="heroicon-o-x"
-                x-on:click="$refs.panel.close"
+                x-on:click="isOpen = ! isOpen"
                 :label=" __('tables::table.filters.buttons.close.label')"
                 color="secondary"
                 {{ $attributes->class(['absolute top-3 right-3 rtl:right-auto rtl:left-3']) }}
--- packages/tables/resources/views/components/filters/trigger.blade.php
+++ packages/tables/resources/views/components/filters/trigger.blade.php
@@ -1,6 +1,6 @@
 <x-tables::icon-button
     icon="heroicon-o-filter"
-    x-on:click="$refs.panel.toggle"
+    x-on:click="isOpen = ! isOpen"
     :label="__('tables::table.buttons.filter.label')"
     {{ $attributes->class(['filament-tables-filters-trigger']) }}
 />
