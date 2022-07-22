811141014445c986b5436ceffbfc52fb7153ca8f
Zep Fietje <hey@zepfietje.com>
2022-07-22 17:45:37 +0200

    Make notifications animation more robust

--- packages/notifications/dist/module.esm.js
+++ packages/notifications/dist/module.esm.js
@@ -1,22 +1,34 @@
 // packages/notifications/resources/js/components/notification.js
 var notification_default = (Alpine) => {
-  Alpine.data("notificationComponent", ({notification}) => ({
+  Alpine.data("notificationComponent", ({$wire, notification}) => ({
     isVisible: false,
     top: null,
+    isClosing: false,
     init: function() {
+      this.top = this.$el.getBoundingClientRect().top;
       this.$nextTick(() => this.isVisible = true);
       if (notification.duration !== null) {
         setTimeout(() => this.close(), notification.duration);
       }
-      Livewire.hook("message.received", () => {
+      Livewire.hook("message.received", (message) => {
+        if (message.component.fingerprint.name !== "notifications") {
+          return;
+        }
         this.top = this.$el.getBoundingClientRect().top;
       });
-      Livewire.hook("message.processed", () => {
+      Livewire.hook("message.processed", (message) => {
+        if (message.component.fingerprint.name !== "notifications") {
+          return;
+        }
+        if (this.isClosing) {
+          return;
+        }
         this.animate();
       });
     },
     animate: function() {
       const top = this.$el.getBoundingClientRect().top;
+      this.$el.getAnimations().forEach((animation) => animation.finish());
       this.$el.animate([
         {transform: `translateY(${this.top - top}px)`},
         {transform: "translateY(0px)"}
@@ -27,7 +39,8 @@ var notification_default = (Alpine) => {
       this.top = top;
     },
     close: function() {
-      this.$wire.close(notification.id);
+      this.isClosing = true;
+      $wire.close(notification.id);
     }
   }));
 };
--- packages/notifications/resources/views/components/notification.blade.php
+++ packages/notifications/resources/views/components/notification.blade.php
@@ -4,7 +4,10 @@
 
 <div
     {{ $attributes }}
-    x-data="notificationComponent({ notification: {{ \Illuminate\Support\Js::from($notification->toArray()) }} })"
+    x-data="notificationComponent({
+        $wire: $wire,
+        notification: {{ \Illuminate\Support\Js::from($notification->toArray()) }},
+    })"
     x-show="isVisible"
     wire:key="notification-{{ $notification->getId() }}"
     dusk="filament.notifications.notification"
