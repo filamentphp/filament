52020da91cc85ea68e06f83d2e3370d5feebbcd9
Andy Ford <AndyTWF@users.noreply.github.com>
2022-07-22 23:26:41 +0100

    Use default text colour
    Co-authored-by: Zep Fietje <hey@zepfietje.com>

--- packages/tables/resources/views/components/header/description.blade.php
+++ packages/tables/resources/views/components/header/description.blade.php
@@ -1,4 +1,4 @@
-<p {{ $attributes->class([
+<p {{ $attributes->class(['filament-tables-header-description']) }}>
     'text-gray-900 filament-tables-header-description',
     'dark:text-gray-100' => config('filament.dark_mode')
 ]) }}>
