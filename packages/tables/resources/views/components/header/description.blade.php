887d922d37d79132b6b89912ef3468d88533796b
Andy Ford <AndyTWF@users.noreply.github.com>
2022-07-23 00:53:01 +0100

    Update packages/tables/resources/views/components/header/description.blade.php
    Co-authored-by: Zep Fietje <hey@zepfietje.com>

--- packages/tables/resources/views/components/header/description.blade.php
+++ packages/tables/resources/views/components/header/description.blade.php
@@ -1,6 +1,3 @@
 <p {{ $attributes->class(['filament-tables-header-description']) }}>
-    'text-gray-900 filament-tables-header-description',
-    'dark:text-gray-100' => config('filament.dark_mode')
-]) }}>
     {{ $slot }}
 </p>
