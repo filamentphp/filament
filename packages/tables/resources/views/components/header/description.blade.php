86306628f9976e27709fccbdff71ffc91af64e16
Andy Ford <andyford993@gmail.com>
2022-07-22 21:55:53 +0100

    fix: relation manager table description colour
    fix #3205
    
    Adds an alternative text colour for when Filament is running in dark mode.

--- packages/tables/resources/views/components/header/description.blade.php
+++ packages/tables/resources/views/components/header/description.blade.php
@@ -1,3 +1,6 @@
-<p {{ $attributes->class(['text-gray-900 filament-tables-header-description']) }}>
+<p {{ $attributes->class([
+    'text-gray-900 filament-tables-header-description',
+    'dark:text-gray-100' => config('filament.dark_mode')
+]) }}>
     {{ $slot }}
 </p>
