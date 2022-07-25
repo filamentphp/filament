211508f73444a0fdd3fd0f88347f7840eafcba5d
Zep Fietje <hey@zepfietje.com>
2022-07-25 14:57:06 +0200

    Fix close button height

--- packages/notifications/resources/views/components/close-button.blade.php
+++ packages/notifications/resources/views/components/close-button.blade.php
@@ -1,4 +1,4 @@
 <x-heroicon-s-x
-    class="filament-notifications-close-button h-5 w-5 cursor-pointer text-gray-400"
+    class="filament-notifications-close-button h-6 w-5 cursor-pointer text-gray-400"
     x-on:click="close"
 />
