68fbd3f4baf739213d83ea2c18f79d194dc43274
AriaieBOY <smr.seddighy@gmail.com>
2022-07-28 15:36:00 +0430

    add localizelabel for table column label

--- packages/tables/src/Columns/Concerns/HasLabel.php
+++ packages/tables/src/Columns/Concerns/HasLabel.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Str;
 trait HasLabel
 {
     protected string | Closure | null $label = null;
+    protected bool | Closure $isLabelLocalized = false;
 
     public function label(string | Closure | null $label): static
     {
@@ -15,13 +16,24 @@ trait HasLabel
 
         return $this;
     }
+    public function localizeLabel(bool | Closure $condition = true): static
+    {
+        $this->isLabelLocalized = $condition;
+
+        return $this;
+    }
+    public function isLabelLocalized(): bool
+    {
+        return $this->evaluate($this->isLabelLocalized);
+    }
 
     public function getLabel(): string
     {
-        return $this->evaluate($this->label) ?? (string) Str::of($this->getName())
+        $label = $this->evaluate($this->label) ?? (string) Str::of($this->getName())
             ->before('.')
             ->kebab()
             ->replace(['-', '_'], ' ')
             ->ucfirst();
+        return ($this->isLabelLocalized())?__($label):$label;
     }
 }
