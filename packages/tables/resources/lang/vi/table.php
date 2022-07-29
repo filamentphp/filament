787c76cbe5289c759c42d6633461a579d37a95c5
Ngô Quốc Đạt <56961917+datlechin@users.noreply.github.com>
2022-07-29 23:12:16 +0700

    Table: vi translations

--- packages/tables/resources/lang/vi/table.php
+++ packages/tables/resources/lang/vi/table.php
@@ -2,6 +2,14 @@
 
 return [
 
+    'columns' => [
+
+        'tags' => [
+            'more' => 'và còn :count',
+        ],
+
+    ],
+
     'fields' => [
 
         'search_query' => [
@@ -45,6 +53,14 @@ return [
 
     'buttons' => [
 
+        'disable_reordering' => [
+            'label' => 'Sắp xếp lại bản ghi thành công',
+        ],
+
+        'enable_reordering' => [
+            'label' => 'Sắp xếp lại bản ghi',
+        ],
+
         'filter' => [
             'label' => 'Lọc',
         ],
@@ -53,18 +69,66 @@ return [
             'label' => 'Xem hành động',
         ],
 
+        'toggle_columns' => [
+            'label' => 'Chuyển đổi cột',
+        ],
+
     ],
 
     'empty' => [
         'heading' => 'Không có dữ liệu nào',
     ],
 
+    'filters' => [
+
+        'buttons' => [
+
+            'reset' => [
+                'label' => 'Đặt lại bộ lọc',
+            ],
+
+            'close' => [
+                'label' => 'Đóng',
+            ],
+
+        ],
+
+        'multi_select' => [
+            'placeholder' => 'Tất cả',
+        ],
+
+        'select' => [
+            'placeholder' => 'Tất cả',
+        ],
+
+        'trashed' => [
+
+            'label' => 'Các bản ghi đã xoá',
+
+            'only_trashed' => 'Chỉ bản ghi đã xoá',
+
+            'with_trashed' => 'Với bản ghi đã xoá',
+
+            'without_trashed' => 'Không có bản ghi bị xóa',
+
+        ],
+
+    ],
+
+    'reorder_indicator' => 'Kéo và thả các bản ghi vào thứ tự.',
+
     'selection_indicator' => [
+
+        'selected_count' => 'đã chọn 1 bản ghi.|:count bản ghi đã chọn.',
 
         'buttons' => [
 
             'select_all' => [
                 'label' => 'Chọn tất cả :count',
+            ],
+
+            'deselect_all' => [
+                'label' => 'Bỏ chọn tất cả',
             ],
 
         ],
