3cbbda6ef0c16c1bc19270986aecff00d9d6f73b
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-07-29 20:57:08 +0430

    reverted code formating

--- packages/forms/resources/views/components/tabs.blade.php
+++ packages/forms/resources/views/components/tabs.blade.php
@@ -1,47 +1,70 @@
-<div x-data="{
+<div
+    x-data="{
 
-    tab: null,
+        tab: null,
 
-    init: function() {
-        this.tab = this.getTabs()[{{ $getActiveTab() }} - 1]
-    },
+        init: function () {
+            this.tab = this.getTabs()[{{ $getActiveTab() }} - 1]
+        },
 
-    getTabs: function() {
-        return JSON.parse(this.$refs.tabsData.value)
-    },
+        getTabs: function () {
+            return JSON.parse(this.$refs.tabsData.value)
+        },
 
-}"
+    }"
     x-on:expand-concealing-component.window="
         if (getTabs().includes($event.detail.id)) {
             tab = $event.detail.id
             $el.scrollIntoView({ behavior: 'smooth', block: 'start' })
         }
     "
-    x-cloak {!! $getId() ? "id=\"{$getId()}\"" : null !!}
+    x-cloak
+    {!! $getId() ? "id=\"{$getId()}\"" : null !!}
     {{ $attributes->merge($getExtraAttributes())->class([
-            'rounded-xl shadow-sm border border-gray-300 bg-white filament-forms-tabs-component',
-            'dark:bg-gray-800 dark:border-gray-700' => config('forms.dark_mode'),
-        ]) }}
-    {{ $getExtraAlpineAttributeBag() }}>
-    <input type="hidden"
-        value='{{ collect($getChildComponentContainer()->getComponents())->filter(static fn(\Filament\Forms\Components\Tabs\Tab $tab): bool => !$tab->isHidden())->map(static fn(\Filament\Forms\Components\Tabs\Tab $tab) => $tab->getId())->values()->toJson() }}'
-        x-ref="tabsData" />
+        'rounded-xl shadow-sm border border-gray-300 bg-white filament-forms-tabs-component',
+        'dark:bg-gray-800 dark:border-gray-700' => config('forms.dark_mode'),
+    ]) }}
+    {{ $getExtraAlpineAttributeBag() }}
+>
+    <input
+        type="hidden"
+        value='{{
+            collect($getChildComponentContainer()->getComponents())
+                ->filter(static fn (\Filament\Forms\Components\Tabs\Tab $tab): bool => ! $tab->isHidden())
+                ->map(static fn (\Filament\Forms\Components\Tabs\Tab $tab) => $tab->getId())
+                ->values()
+                ->toJson()
+        }}'
+        x-ref="tabsData"
+    />
 
-    <div {!! $getLabel() ? 'aria-label="' . $getLabel() . '"' : null !!} role="tablist" @class([
-        'rounded-t-xl flex overflow-y-auto bg-gray-100',
-        'dark:bg-gray-700' => config('forms.dark_mode'),
-    ])>
+    <div
+        {!! $getLabel() ? 'aria-label="' . $getLabel() . '"' : null !!}
+        role="tablist"
+        @class([
+            'rounded-t-xl flex overflow-y-auto bg-gray-100',
+            'dark:bg-gray-700' => config('forms.dark_mode'),
+        ])
+    >
         @foreach ($getChildComponentContainer()->getComponents() as $tab)
-            <button type="button" aria-controls="{{ $tab->getId() }}"
-                x-bind:aria-selected="tab === '{{ $tab->getId() }}'" x-on:click="tab = '{{ $tab->getId() }}'"
-                role="tab" x-bind:tabindex="tab === '{{ $tab->getId() }}' ? 0 : -1"
+            <button
+                type="button"
+                aria-controls="{{ $tab->getId() }}"
+                x-bind:aria-selected="tab === '{{ $tab->getId() }}'"
+                x-on:click="tab = '{{ $tab->getId() }}'"
+                role="tab"
+                x-bind:tabindex="tab === '{{ $tab->getId() }}' ? 0 : -1"
                 class="flex items-center gap-2 shrink-0 p-3 text-sm font-medium"
-                x-bind:class="{ 'bg-white @if (config('forms.dark_mode')) dark:bg-gray-800 @endif': tab === '{{ $tab->getId() }}' }">
+                x-bind:class="{ 'bg-white @if (config('forms.dark_mode')) dark:bg-gray-800 @endif': tab === '{{ $tab->getId() }}' }"
+            >
                 @if ($icon = $tab->getIcon())
-                    <x-dynamic-component :component="$icon" :class="\Illuminate\Support\Arr::toCssClasses([
-                        'h-4 w-4 text-gray-500',
-                        'dark:text-gray-400' => config('forms.dark_mode'),
-                    ])" />
+                    <x-dynamic-component
+                        :component="$icon"
+                        :class="\Illuminate\Support\Arr::toCssClasses([
+                            'h-4 w-4 text-gray-500',
+                            'dark:text-gray-400' => config('forms.dark_mode'),
+                        ])"
+                    />
                 @endif
 
                 {{ $tab->getLabel() }}
