65f8ac6bbb0fdf00ce82dbe09685016f59dc385d
Ngô Quốc Đạt <56961917+datlechin@users.noreply.github.com>
2022-07-30 01:08:38 +0700

    support: vi translations

---
+++ packages/support/resources/lang/vi/actions/dissociate.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Huỷ liên kết',
+
+        'modal' => [
+
+            'heading' => 'Huỷ liên kết :label',
+
+            'actions' => [
+
+                'dissociate' => [
+                    'label' => 'Huỷ liên kết',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'dissociated' => 'Đã huỷ liên kết',
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => 'Huỷ liên kết các mục đã chọn',
+
+        'modal' => [
+
+            'heading' => 'Huỷ liên kết :label',
+
+            'actions' => [
+
+                'dissociate' => [
+                    'label' => 'Huỷ liên kết',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'dissociated' => 'Đã huỷ liên kết',
+        ],
+
+    ],
+
+];
