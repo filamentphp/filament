9cf885a6f9f88f07e22a75b2ea888ac8f9e25dfc
awcodes <adam@adamwgd.com>
2022-08-04 15:05:33 -0400

    fixes date time picker disappearing when set to reactive

--- packages/forms/resources/views/components/date-time-picker.blade.php
+++ packages/forms/resources/views/components/date-time-picker.blade.php
@@ -88,6 +88,10 @@
                 x-ref="panel"
                 x-cloak
                 x-float.placement.bottom-start.offset.flip.shift="{ offset: 8 }"
+                @if (!$getStateBindingModifiers())
+                wire:ignore.self
+                wire:key="reactiveDateTimeInput-{{ $this->id }}"
+                @endif
                 @class([
                     'absolute hidden z-10 my-1 bg-white border border-gray-300 rounded-lg shadow-md',
                     'dark:bg-gray-700 dark:border-gray-600' => config('forms.dark_mode'),
