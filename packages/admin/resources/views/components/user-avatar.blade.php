0a069f61554f4d08e7caf4565e2c78fbf1704204
Dan Harrin <git@danharrin.com>
2022-08-05 13:58:13 +0100

    Update user-avatar.blade.php

--- packages/admin/resources/views/components/user-avatar.blade.php
+++ packages/admin/resources/views/components/user-avatar.blade.php
@@ -2,7 +2,8 @@
     'user' => \Filament\Facades\Filament::auth()->user(),
 ])
 
-<div {{ $attributes->class([
+<div
+    {{ $attributes->class([
         'w-10 h-10 rounded-full bg-gray-200 bg-cover bg-center',
         'dark:bg-gray-900' => config('filament.dark_mode'),
     ]) }}
