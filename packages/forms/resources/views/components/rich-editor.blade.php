70b240bcb3dd76c6338e9afa1aa59de2e210448c
Dennis Koch <info@pixelarbeit.de>
2022-08-05 11:33:39 +0200

    Break words in rich editor

--- packages/forms/resources/views/components/rich-editor.blade.php
+++ packages/forms/resources/views/components/rich-editor.blade.php
@@ -295,7 +295,7 @@
                 x-ref="trix"
                 dusk="filament.forms.{{ $getStatePath() }}"
                 {{ $getExtraInputAttributeBag()->class([
-                    'bg-white block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600 prose max-w-none',
+                    'bg-white block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600 prose max-w-none break-words',
                     'dark:prose-invert dark:bg-gray-700 dark:focus:border-primary-600' => config('forms.dark_mode'),
                     'border-gray-300' => ! $errors->has($getStatePath()),
                     'dark:border-gray-600' => (! $errors->has($getStatePath())) && config('forms.dark_mode'),
