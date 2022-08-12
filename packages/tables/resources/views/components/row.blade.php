12ffb8c6a5cdfe506de79d185aa1d4133100fa26
Zep Fietje <hey@zepfietje.com>
2022-08-12 13:38:24 +0200

    Make merging classes a bit more readable

--- packages/tables/resources/views/components/row.blade.php
+++ packages/tables/resources/views/components/row.blade.php
@@ -5,13 +5,20 @@
 ])
 
 <tr
-    {{ $attributes->class(array_merge([
-        'filament-tables-row',
-        'hover:bg-gray-50' => $recordUrl,
-        'dark:hover:bg-gray-500/10' => $recordUrl && config('tables.dark_mode'),
-        'even:bg-gray-100' => $striped,
-        'dark:even:bg-gray-900' => $striped && config('tables.dark_mode'),
-    ], $recordClasses)) }}
+    {{
+        $attributes->class(
+            array_merge(
+                [
+                    'filament-tables-row',
+                    'hover:bg-gray-50' => $recordUrl,
+                    'dark:hover:bg-gray-500/10' => $recordUrl && config('tables.dark_mode'),
+                    'even:bg-gray-100' => $striped,
+                    'dark:even:bg-gray-900' => $striped && config('tables.dark_mode'),
+                ],
+                $recordClasses,
+            ),
+        )
+    }}
 >
     {{ $slot }}
 </tr>
