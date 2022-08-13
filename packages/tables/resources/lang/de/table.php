7cc796a47bda48d8c15ea52a24eb30e94e69b1d6
danharrin <danharrin@users.noreply.github.com>
2022-08-13 07:17:12 +0000

    chore: styling

--- packages/tables/resources/lang/de/table.php
+++ packages/tables/resources/lang/de/table.php
@@ -117,7 +117,7 @@ return [
         'overview' => ':first bis :last von :total Ergebnissen',
 
     ],
-    
+
     'reorder_indicator' => 'Zum Sortieren die Datensätze per Drag & Drop in die richtige Reihenfolge ziehen.',
 
     'selection_indicator' => [
