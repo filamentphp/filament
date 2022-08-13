8174bc6f9f6db57fa4d700636b324d03089c04f6
Dan Harrin <git@danharrin.com>
2022-08-13 15:40:56 +0100

    Update row.blade.php

--- packages/tables/resources/views/components/row.blade.php
+++ packages/tables/resources/views/components/row.blade.php
@@ -1,24 +1,16 @@
 @props([
-    'recordClasses' => null,
     'recordUrl' => null,
     'striped' => false,
 ])
 
 <tr
-    {{
-        $attributes->class(
-            array_merge(
-                [
-                    'filament-tables-row',
-                    'hover:bg-gray-50' => $recordUrl,
-                    'dark:hover:bg-gray-500/10' => $recordUrl && config('tables.dark_mode'),
-                    'even:bg-gray-100' => $striped,
-                    'dark:even:bg-gray-900' => $striped && config('tables.dark_mode'),
-                ],
-                $recordClasses,
-            ),
-        )
-    }}
+    {{ $attributes->class([
+        'filament-tables-row',
+        'hover:bg-gray-50' => $recordUrl,
+        'dark:hover:bg-gray-500/10' => $recordUrl && config('tables.dark_mode'),
+        'even:bg-gray-100' => $striped,
+        'dark:even:bg-gray-900' => $striped && config('tables.dark_mode'),
+    ]) }}
 >
     {{ $slot }}
 </tr>
