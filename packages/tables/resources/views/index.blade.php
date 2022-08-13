c50b2f0621f17b276d876de3a5865d5dc89b4dad
Dan Harrin <git@danharrin.com>
2022-08-13 15:44:30 +0100

    Update index.blade.php

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -395,9 +395,12 @@
                                 x-bind:class="{
                                     'bg-gray-50 {{ config('tables.dark_mode') ? 'dark:bg-gray-500/10' : '' }}': isRecordSelected('{{ $recordKey }}'),
                                 }"
-                                :class="\Illuminate\Support\Arr::toCssClasses(array_merge([
-                                    'group cursor-move' => $isReordering,
-                                ], $recordClasses))"
+                                :class="\Illuminate\Support\Arr::toCssClasses(array_merge(
+                                    [
+                                        'group cursor-move' => $isReordering,
+                                    ],
+                                    \Illuminate\Support\Arr::wrap($recordClasses),
+                                ))"
                             >
                                 @if ($isReordering)
                                     <x-tables::reorder.cell />
