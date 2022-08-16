6c84b8d3a8898aa2b891996a873e9025e6781f79
Dan Harrin <git@danharrin.com>
2022-08-16 21:40:42 +0100

    fix: RTL markdown wrapping

--- packages/forms/resources/views/components/markdown-editor.blade.php
+++ packages/forms/resources/views/components/markdown-editor.blade.php
@@ -217,7 +217,7 @@
                             {!! $isRequired() ? 'required' : null !!}
                         @endif
                         @class([
-                            'tracking-normal whitespace-pre-wrap overflow-y-hidden font-mono block absolute bg-transparent top-0 text-sm left-0 block z-1 w-full h-full min-h-full resize-none transition duration-75 rounded-lg shadow-sm focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600 caret-black',
+                            'tracking-normal overflow-y-hidden font-mono block absolute bg-transparent top-0 text-sm left-0 block z-1 w-full h-full min-h-full resize-none transition duration-75 rounded-lg shadow-sm focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600 caret-black',
                             'dark:caret-white dark:focus:border-primary-600' => config('forms.dark_mode'),
                             'border-gray-300' => ! $errors->has($getStatePath()),
                             'dark:border-gray-600' => (! $errors->has($getStatePath())) && config('forms.dark_mode'),
@@ -231,7 +231,7 @@
                     x-html="overlay"
                     style="min-height: 150px;"
                     @class([
-                        'w-full h-full rounded-lg px-3 py-2 border border-transparent font-mono tracking-normal bg-white text-sm text-gray-900 break-words whitespace-pre-wrap',
+                        'w-full h-full rounded-lg px-3 py-2 border border-transparent font-mono tracking-normal bg-white text-sm text-gray-900 break-words',
                         'dark:bg-gray-700 dark:border-gray-600 dark:text-white' => config('forms.dark_mode'),
                     ])
                 ></div>
