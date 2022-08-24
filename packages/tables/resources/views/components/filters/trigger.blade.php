31b0b8a2a2ade169f1a7224786ddd6abeb81b2ec
cobraprojects <42706279+cobraprojects@users.noreply.github.com>
2022-08-24 17:11:16 +0200

    Update trigger.blade.php
    change filter icon to be solid only if table is filtered

--- packages/tables/resources/views/components/filters/trigger.blade.php
+++ packages/tables/resources/views/components/filters/trigger.blade.php
@@ -1,5 +1,5 @@
 <x-tables::icon-button
-    icon="heroicon-o-filter"
+    :icon="request()->has('tableFilters') ? 'heroicon-o-filter' : 'heroicon-s-filter'"
     x-on:click="$refs.popoverPanel.toggle"
     :label="__('tables::table.buttons.filter.label')"
     {{ $attributes->class(['filament-tables-filters-trigger']) }}
