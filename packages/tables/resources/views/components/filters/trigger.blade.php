3380336a0c068487ece23f5fc4bbb80932b8973f
cobraprojects <42706279+cobraprojects@users.noreply.github.com>
2022-08-24 19:12:41 +0200

    fix getFilters

--- packages/tables/resources/views/components/filters/trigger.blade.php
+++ packages/tables/resources/views/components/filters/trigger.blade.php
@@ -1,6 +1,13 @@
 @props(['getFilters'])
 <x-tables::icon-button
-    :icon="count($getFilters) ? 'heroicon-s-filter' : 'heroicon-o-filter'"
+    :icon="collect($getFilters)
+    ->mapWithKeys(
+        fn(\Filament\Tables\Filters\BaseFilter $filter): array => [$filter->getName() => $filter->getIndicators()],
+    )
+    ->filter(fn(array $indicators): bool => count($indicators))
+    ->all() 
+    ? 'heroicon-s-filter' 
+    : 'heroicon-o-filter'"
     x-on:click="$refs.popoverPanel.toggle"
     :label="__('tables::table.buttons.filter.label')"
     {{ $attributes->class(['filament-tables-filters-trigger']) }}
