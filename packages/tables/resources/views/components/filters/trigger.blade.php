b53d919c1743d645a122c4213f75fdf3b94fa403
cobraprojects <42706279+cobraprojects@users.noreply.github.com>
2022-08-24 17:16:13 +0200

    Update trigger.blade.php
    fix classes names

--- packages/tables/resources/views/components/filters/trigger.blade.php
+++ packages/tables/resources/views/components/filters/trigger.blade.php
@@ -1,5 +1,5 @@
 <x-tables::icon-button
-    :icon="request()->has('tableFilters') ? 'heroicon-o-filter' : 'heroicon-s-filter'"
+    :icon="request()->has('tableFilters') ? 'heroicon-s-filter' : 'heroicon-o-filter'"
     x-on:click="$refs.popoverPanel.toggle"
     :label="__('tables::table.buttons.filter.label')"
     {{ $attributes->class(['filament-tables-filters-trigger']) }}
