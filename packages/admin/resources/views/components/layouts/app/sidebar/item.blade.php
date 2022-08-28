1b2785994cd30a07135d3b7d31374092c7fa7d55
Marga Rizaldi <margarizaldi@gmail.com>
2022-08-28 23:15:39 +0700

    Change sidebar active item background color
    Change active item background color from `bg-primary-500` to `bg-primary-600` to enhance white text visibility and bring color consistency with primary buttons.

--- packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
+++ packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
@@ -33,7 +33,7 @@
             'flex items-center justify-center gap-3 px-3 py-2 rounded-lg font-medium transition',
             'hover:bg-gray-500/5 focus:bg-gray-500/5' => ! $active,
             'dark:text-gray-300 dark:hover:bg-gray-700' => (! $active) && config('filament.dark_mode'),
-            'bg-primary-500 text-white' => $active,
+            'bg-primary-600 text-white' => $active,
         ])
     >
         <x-dynamic-component :component="$icon" class="h-5 w-5 shrink-0" />
