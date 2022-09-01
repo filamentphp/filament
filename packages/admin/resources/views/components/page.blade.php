d683d2ea968673733c9eb5a4825ebd70cc91ba36
Dan Harrin <git@danharrin.com>
2022-09-01 18:10:50 +0100

    fix: Undefined $wire

--- packages/admin/resources/views/components/page.blade.php
+++ packages/admin/resources/views/components/page.blade.php
@@ -38,15 +38,6 @@
         <x-filament::modal
             id="page-action"
             :wire:key="$action ? $this->id . '.actions.' . $action->getName() . '.modal' : null"
-            x-init="
-                $watch('isOpen', () => {
-                    if (isOpen) {
-                        return
-                    }
-
-                    $wire.mountedAction = null
-                })
-            "
             :visible="filled($action)"
             :width="$action?->getModalWidth()"
             display-classes="block"
