56158f71001b0ee43420170b9bd94ac78c6f1486
danharrin <danharrin@users.noreply.github.com>
2022-09-03 09:07:30 +0000

    chore: fix code style

--- packages/admin/src/Resources/Resource.php
+++ packages/admin/src/Resources/Resource.php
@@ -404,12 +404,12 @@ class Resource
                 method_exists($model, 'isTranslatableAttribute') && $model->isTranslatableAttribute($searchAttribute),
                 function (Builder $query) use ($databaseConnection, $searchAttribute, $searchOperator, $searchQuery, $whereClause): Builder {
                     $activeLocale = app()->getLocale();
-                    
+
                     $searchColumn = match ($databaseConnection->getDriverName()) {
                         'pgsql' => "{$searchAttribute}->>'{$activeLocale}'",
                         default => "json_extract({$searchAttribute}, \"$.{$activeLocale}\")",
                     };
-                    
+
                     return $query->{"{$whereClause}Raw"}(
                         "lower({$searchColumn}) {$searchOperator} ?",
                         "%{$searchQuery}%",
