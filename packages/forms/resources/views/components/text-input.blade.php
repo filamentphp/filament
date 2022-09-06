e1a9ecff58cc3757e44700dd862f7450545f6bd9
Zep Fietje <hey@zepfietje.com>
2022-09-06 16:45:39 +0200

    Use small font size for text input above mobile

--- packages/forms/resources/views/components/text-input.blade.php
+++ packages/forms/resources/views/components/text-input.blade.php
@@ -69,7 +69,7 @@
                     {!! $isRequired() ? 'required' : null !!}
                 @endif
                 {{ $getExtraInputAttributeBag()->class([
-                    'block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70',
+                    'block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 sm:text-sm',
                     'dark:bg-gray-700 dark:text-white dark:focus:border-primary-500' => config('forms.dark_mode'),
                 ]) }}
                 x-bind:class="{
