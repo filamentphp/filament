30526e7b722834ba4491982e7273697700056798
danharrin <danharrin@users.noreply.github.com>
2022-09-12 11:06:31 +0000

    chore: fix code style

--- packages/tables/src/Columns/CheckboxColumn.php
+++ packages/tables/src/Columns/CheckboxColumn.php
@@ -3,8 +3,6 @@
 namespace Filament\Tables\Columns;
 
 use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
-use Filament\Forms\Components\Concerns\HasToggleColors;
-use Filament\Forms\Components\Concerns\HasToggleIcons;
 use Filament\Tables\Columns\Contracts\Editable;
 
 class CheckboxColumn extends Column implements Editable
