dd34f675ab5a9c23f80dff33e274c997c03f657c
Dan Harrin <git@danharrin.com>
2022-09-12 14:04:49 +0300

    wip

--- packages/tables/src/Columns/CheckboxColumn.php
+++ packages/tables/src/Columns/CheckboxColumn.php
@@ -2,17 +2,17 @@
 
 namespace Filament\Tables\Columns;
 
+use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
 use Filament\Forms\Components\Concerns\HasToggleColors;
 use Filament\Forms\Components\Concerns\HasToggleIcons;
 use Filament\Tables\Columns\Contracts\Editable;
 
-class ToggleColumn extends Column implements Editable
+class CheckboxColumn extends Column implements Editable
 {
     use Concerns\CanBeValidated;
-    use HasToggleColors;
-    use HasToggleIcons;
+    use HasExtraInputAttributes;
 
-    protected string $view = 'tables::columns.toggle-column';
+    protected string $view = 'tables::columns.checkbox-column';
 
     protected function setUp(): void
     {
