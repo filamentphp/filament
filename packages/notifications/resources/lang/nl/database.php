c6fb57ab16cf341ea7ed5da0b4db63f3180efb76
Rick de Boer <rickdb@calvintijd.nl>
2022-09-13 13:17:06 +0200

    Add Dutch translations for notifications package

---
+++ packages/notifications/resources/lang/nl/database.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'modal' => [
+
+        'heading' => 'Notificaties',
+
+        'buttons' => [
+
+            'clear' => [
+                'label' => 'Alles wissen',
+            ],
+
+            'mark_all_as_read' => [
+                'label' => 'Alles markeren als gelezen',
+            ],
+
+        ],
+
+        'empty' => [
+            'heading' => 'Er zijn geen notificaties',
+            'description' => 'Kom hier later nog eens terug',
+        ],
+
+    ],
+
+];
