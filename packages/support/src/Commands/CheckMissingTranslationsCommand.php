9002b90dc3ddeecaf9216e223b7bd0dcbf326f26
Saade <saade@outlook.com.br>
2022-09-16 16:33:50 -0300

    wip

--- packages/support/src/Commands/CheckMissingTranslationsCommand.php
+++ packages/support/src/Commands/CheckMissingTranslationsCommand.php
@@ -8,13 +8,11 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Finder\SplFileInfo;
-use function Termwind\{render};
 
 class CheckMissingTranslationsCommand extends Command
 {
     protected $signature = 'filament:check-translations
                             {lang*              : The lang to compare against the base translation. Accepts multiple values.}
-                            {--B|baseLang=en    : The base lang to compare with.}
                             {--D|dir=app        : The directory containing the translations. \'app\' and \'vendor\' are accepted values.}';
 
     protected $description = 'Scan translation files and compare each of them with the base translation. Outputs missing and deprecated translations.';
@@ -25,14 +23,13 @@ class CheckMissingTranslationsCommand extends Command
         $this->scan('forms');
         $this->scan('support');
         $this->scan('tables');
+        $this->scan('notifications');
 
         return self::SUCCESS;
     }
 
     protected function scan(string $package)
     {
-        $baseLang = $this->option('baseLang');
-
         $langRoot = match ($dir = $this->option('dir')) {
             'app' => lang_path("vendor/{$package}"),
             'vendor' => base_path("vendor/filament/{$package}/resources/lang"),
@@ -45,19 +42,23 @@ class CheckMissingTranslationsCommand extends Command
             return;
         }
 
-        $this->announceScanning($package);
-
         $langs = collect($filesystem->directories($langRoot))
             ->mapWithKeys(static fn ($directory) => [$directory => (string) str($directory)->afterLast('/')])
-            ->when($this->argument('lang'), fn (Collection $langs) => $langs->filter(fn ($lang) => in_array($lang, $this->argument('lang'))))
+            ->when(
+                $langs = $this->argument('lang'),
+                fn (Collection $availableLangs) => $availableLangs
+                    ->filter(
+                        fn ($lang) => in_array($lang, $langs)
+                    )
+            )
             ->all();
 
         foreach ($langs as $langDir => $lang) {
             $files = $filesystem->files($langDir);
 
             collect($files)
-                ->mapWithKeys(function (SplFileInfo $file) use ($langRoot, $baseLang) {
-                    $base = require $langRoot . DIRECTORY_SEPARATOR . $baseLang . DIRECTORY_SEPARATOR . $file->getRelativePathname();
+                ->mapWithKeys(function (SplFileInfo $file) use ($langRoot) {
+                    $base = require implode(DIRECTORY_SEPARATOR, [$langRoot, 'en', $file->getRelativePathname()]);
                     $trans = require $file->getPathname();
 
                     $missingKeys = array_diff_key(
@@ -65,7 +66,7 @@ class CheckMissingTranslationsCommand extends Command
                         Arr::dot($trans)
                     );
 
-                    $deprecatedKeys = array_diff_key(
+                    $removedKeys = array_diff_key(
                         Arr::dot($trans),
                         Arr::dot($base)
                     );
@@ -73,56 +74,33 @@ class CheckMissingTranslationsCommand extends Command
                     return [
                         $file->getPathname() => [
                             'missing' => array_keys($missingKeys),
-                            'deprecated' => array_keys($deprecatedKeys),
+                            'removed' => array_keys($removedKeys),
                         ],
                     ];
                 })
-                ->tap(function ($files) use ($lang) {
+                ->tap(function ($files) use ($lang, $package) {
                     $missingCount = $files->sum(fn ($file) => count($file['missing']));
-                    $deprecatedCount = $files->sum(fn ($file) => count($file['deprecated']));
+                    $removedCount = $files->sum(fn ($file) => count($file['removed']));
 
-                    if ($missingCount === 0 && $deprecatedCount === 0) {
-                        $this->banner('success', 'Lang %s has no misssing or deprecated translation keys!', $lang);
-                    } else {
-                        $this->banner('warning', 'Lang %s has %d misssing translation keys and %d deprecated translation keys.', $lang, $missingCount, $deprecatedCount);
+                    if ($missingCount == 0 && $removedCount == 0) {
+                        $this->info("Package filament/{$package} has no misssing or deprecated translation keys for lang {$lang}!\n");
+                    } elseif ($missingCount > 0 && $removedCount > 0) {
+                        $this->warn("Package filament/{$package} has {$missingCount} misssing translation keys and {$removedCount} removed translation keys for lang {$lang}.\n");
+                    } elseif ($missingCount > 0) {
+                        $this->warn("Package filament/{$package} has {$missingCount} misssing translation keys for lang {$lang}.\n");
+                    } elseif ($removedCount > 0) {
+                        $this->warn("Package filament/{$package} has {$removedCount} removed translation keys for lang {$lang}.\n");
                     }
                 })
-                ->filter(static fn ($keys) => count($keys['missing']) || count($keys['deprecated']))
+                ->filter(static fn ($keys) => count($keys['missing']) || count($keys['removed']))
                 ->each(function ($keys, $file) {
                     $this->table(
-                        ['File', 'Missing Keys', 'Deprecated keys'],
+                        ['File', 'Missing Keys', 'Removed keys'],
                         [
-                            [$file, implode(', ', $keys['missing']), implode(', ', $keys['deprecated'])],
+                            [$file, implode(', ', $keys['missing']), implode(', ', $keys['removed'])],
                         ],
                     );
                 });
         }
-    }
-
-    protected function announceScanning(string $package): void
-    {
-        render(<<<HTML
-            <div class="px-2 py-4 mt-2 text-white bg-sky-600">
-                Scanning for missing and deprecated translations inside <span class="font-bold bg-white text-sky-600">&nbsp;filament/{$package}&nbsp;</span> package.
-            </div>
-        HTML);
-    }
-
-    protected function banner(string $style, string $message, ...$replacements): void
-    {
-        $color = match ($style) {
-            'success' => 'green',
-            'warning' => 'orange',
-            'danger' => 'red',
-            default => 'blue'
-        };
-
-        $content = sprintf($message, ...$replacements);
-
-        render(<<<HTML
-            <div class="px-2 py-4 text-white bg-{$color}-600">
-                {$content}
-            </div>
-        HTML);
     }
 }
