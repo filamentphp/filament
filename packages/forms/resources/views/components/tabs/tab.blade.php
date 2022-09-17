0d6c6ad8fff5ab37ffe5d5a260c8ccd1fc500928
Dan Harrin <git@danharrin.com>
2022-09-17 14:48:48 +0300

    Update tab.blade.php

--- packages/forms/resources/views/components/tabs/tab.blade.php
+++ packages/forms/resources/views/components/tabs/tab.blade.php
@@ -6,9 +6,13 @@
     data-tab-panel
     x-bind:class="{ 'invisible h-0 p-0 overflow-y-hidden': tab !== '{{ $getId() }}', 'p-6': tab === '{{ $getId() }}' }"
     x-on:expand-concealing-component.window="
-        if ( ($el.querySelector('[data-validation-error]') ? true : false)
-              && $el.closest('div[data-tab-wrapper]').querySelector('[data-tab-panel]') === $el
-            ) { tab = '{{ $getId() }}' }"
+        if (
+            ($el.querySelector('[data-validation-error]') ? true : false) &&
+            ($el.closest('div[data-tab-wrapper]').querySelector('[data-tab-panel]') === $el)
+        ) {
+            tab = @js($getId())
+        }
+    "
     {{ $attributes->merge($getExtraAttributes())->class(['focus:outline-none filament-forms-tabs-component-tab']) }}
 >
     {{ $getChildComponentContainer() }}
