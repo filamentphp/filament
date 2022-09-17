078272b0ce6453b69409864be3ff618d737c78fc
SalvaTerol <35788666+SalvaTerol@users.noreply.github.com>
2022-09-17 16:27:18 +0200

    add Spanish translation for notifications

---
+++ packages/notifications/resources/lang/es/database.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'modal' => [
+
+        'heading' => 'Notificationes',
+
+        'buttons' => [
+
+            'clear' => [
+                'label' => 'Borrar',
+            ],
+
+            'mark_all_as_read' => [
+                'label' => 'Marcar todas como leídas',
+            ],
+
+        ],
+
+        'empty' => [
+            'heading' => 'No hay notificaciones',
+            'description' => 'Por favor, compruébelo más tarde',
+        ],
+
+    ],
+
+];
