b9f599fe2305efa73792f928e5959f359038a32b
Mathijs Mossink <m.mossink@falcom.nl>
2022-09-19 10:30:17 +0200

    fixed markdown-editor list ident

--- packages/forms/resources/views/components/markdown-editor.blade.php
+++ packages/forms/resources/views/components/markdown-editor.blade.php
@@ -232,7 +232,7 @@
                     style="min-height: 150px;"
                     @class([
                         'w-full h-full rounded-lg px-3 py-2 border border-transparent font-mono tracking-normal bg-white text-sm text-gray-900 break-words',
-                        'dark:bg-gray-700 dark:border-gray-600 dark:text-white' => config('forms.dark_mode'),
+                        'dark:bg-gray-700 dark:border-gray-600 dark:text-white whitespace-pre-wrap' => config('forms.dark_mode'),
                     ])
                 ></div>
             </div>
