cbceab7b823b881bbe2bbca6f0ee0eb6f13329f5
Dan Harrin <git@danharrin.com>
2022-09-21 11:40:40 +0100

    feature: Focus wizard error step

--- packages/forms/src/Components/Wizard.php
+++ packages/forms/src/Components/Wizard.php
@@ -46,12 +46,14 @@ class Wizard extends Component
                         return;
                     }
 
-                    /** @var Step $currentStep */
-                    $currentStep = $component->getChildComponentContainer()->getComponents()[$currentStep];
+                    if (! $component->isSkippable()) {
+                        /** @var Step $currentStep */
+                        $currentStep = $component->getChildComponentContainer()->getComponents()[$currentStep];
 
-                    $currentStep->callBeforeValidation();
-                    $currentStep->getChildComponentContainer()->validate();
-                    $currentStep->callAfterValidation();
+                        $currentStep->callBeforeValidation();
+                        $currentStep->getChildComponentContainer()->validate();
+                        $currentStep->callAfterValidation();
+                    }
 
                     /** @var LivewireComponent $livewire */
                     $livewire = $component->getLivewire();
