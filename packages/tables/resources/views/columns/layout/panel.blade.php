7a06901cadf3c918ed10aa402d354217ae556d38
Alexander Jensen <a.jensen@24bytes.de>
2022-09-24 21:47:26 +0200

    Apply formatting

--- packages/tables/resources/views/columns/layout/panel.blade.php
+++ packages/tables/resources/views/columns/layout/panel.blade.php
@@ -1,8 +1,10 @@
 <div
-    {{ $attributes->merge($getExtraAttributes())->class([
-        'px-4 py-3 bg-gray-100 rounded-lg',
-        'dark:bg-gray-900' => config('forms.dark_mode'),
-    ]) }}
+    {{ $attributes
+        ->merge($getExtraAttributes())
+        ->class([
+            'px-4 py-3 bg-gray-100 rounded-lg',
+            'dark:bg-gray-900' => config('forms.dark_mode'),
+        ]) }}
 >
     <x-tables::columns.layout
         :components="$getComponents()"
