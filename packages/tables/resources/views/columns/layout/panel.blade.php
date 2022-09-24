0a9e3a66abe0eb88c9263e21043b91012b955e69
Alexander Jensen <a.jensen@24bytes.de>
2022-09-24 21:52:01 +0200

    Apply formatting

--- packages/tables/resources/views/columns/layout/panel.blade.php
+++ packages/tables/resources/views/columns/layout/panel.blade.php
@@ -1,10 +1,12 @@
 <div
-    {{ $attributes
-        ->merge($getExtraAttributes())
-        ->class([
-            'px-4 py-3 bg-gray-100 rounded-lg',
-            'dark:bg-gray-900' => config('forms.dark_mode'),
-        ]) }}
+    {{
+        $attributes
+            ->merge($getExtraAttributes())
+            ->class([
+                'px-4 py-3 bg-gray-100 rounded-lg',
+                'dark:bg-gray-900' => config('forms.dark_mode'),
+            ])
+    }}
 >
     <x-tables::columns.layout
         :components="$getComponents()"
--- packages/tables/resources/views/columns/layout/stack.blade.php
+++ packages/tables/resources/views/columns/layout/stack.blade.php
@@ -1,20 +1,22 @@
 <div
-    {{ $attributes
-        ->merge($getExtraAttributes())
-        ->class([
-            'flex flex-col',
-            match ($getAlignment()) {
-                'center' => 'items-center',
-                'right' => 'items-end',
-                default => 'items-start',
-            },
-            match ($getSpace()) {
-                1 => 'space-y-1',
-                2 => 'space-y-2',
-                3 => 'space-y-3',
-                default => null,
-            },
-        ]) }}
+    {{
+        $attributes
+            ->merge($getExtraAttributes())
+            ->class([
+                'flex flex-col',
+                match ($getAlignment()) {
+                    'center' => 'items-center',
+                    'right' => 'items-end',
+                    default => 'items-start',
+                },
+                match ($getSpace()) {
+                    1 => 'space-y-1',
+                    2 => 'space-y-2',
+                    3 => 'space-y-3',
+                    default => null,
+                },
+            ])
+    }}
 >
     <x-tables::columns.layout
         :components="$getComponents()"
