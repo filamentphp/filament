61db2a517adb367dc9f9ed43fdb8ebc071063b20
danharrin <danharrin@users.noreply.github.com>
2022-09-26 10:50:44 +0000

    chore: fix code style

--- packages/admin/resources/lang/ja/layout.php
+++ packages/admin/resources/lang/ja/layout.php
@@ -9,10 +9,10 @@ return [
         'dark_mode' => [
             'label' => 'ダークモードに切り替える',
         ],
-        
+
         'database_notifications' => [
-			'label' => 'お知らせを確認',
-		],
+            'label' => 'お知らせを確認',
+        ],
 
         'light_mode' => [
             'label' => 'ライトモードに切り替える',
