95348285eb4eb5fd7c89bb275e02bf15a38ee91e
Nuhel <nuheljft@gmail.com>
2022-09-30 00:10:56 +0600

    enhancement:Bangla Language Added.

---
+++ packages/admin/resources/lang/bn/resources/pages/view-record.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'title' => ':label দেখুন',
+
+    'breadcrumb' => 'দেখুন',
+
+    'form' => [
+
+        'tab' => [
+            'label' => 'দেখুন',
+        ],
+
+    ],
+
+];
