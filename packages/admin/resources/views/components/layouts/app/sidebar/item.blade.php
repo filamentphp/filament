1efdbe4e683d1db2169de506a11803001b66cd1c
Wahyu Kristianto <w.kristories@gmail.com>
2022-09-30 23:07:38 +0700

    Update packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
    Co-authored-by: Zep Fietje <hey@zepfietje.com>

--- packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
+++ packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
@@ -7,7 +7,7 @@
     'url',
 ])
 
-<li @class(['filament-sidebar-item', 'filament-sidebar-item-active' => $active, 'overflow-hidden'])>
+<li @class(['filament-sidebar-item overflow-hidden', 'filament-sidebar-item-active' => $active])>
     <a
         href="{{ $url }}"
         {!! $shouldOpenUrlInNewTab ? 'target="_blank"' : '' !!}
