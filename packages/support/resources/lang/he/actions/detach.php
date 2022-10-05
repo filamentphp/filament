764935c10bb015071c604b042bcf903e68b61c52
RoeeM <roee.mayerowicz@gmail.com>
2022-10-05 14:54:36 +0300

    hebrew translation for support

---
+++ packages/support/resources/lang/he/actions/detach.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'הסר קישור',
+
+        'modal' => [
+
+            'heading' => 'הסר קישור של :label',
+
+            'actions' => [
+
+                'detach' => [
+                    'label' => 'הסר קישור',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'detached' => 'קישור הוסר',
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => 'הסר קישור של נבחרים',
+
+        'modal' => [
+
+            'heading' => 'הסר קישור נבחרים של :label',
+
+            'actions' => [
+
+                'detach' => [
+                    'label' => 'הסר קישור',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'detached' => 'קישורים הוסרו',
+        ],
+
+    ],
+
+];
