06756f19fe8cc5d2ab6e8dd7554e5d29c78129c4
Dan Harrin <git@danharrin.com>
2022-10-07 13:51:40 +0100

    Update hint.blade.php

--- packages/forms/resources/views/components/field-wrapper/hint.blade.php
+++ packages/forms/resources/views/components/field-wrapper/hint.blade.php
@@ -1,41 +1,33 @@
 @props([
-    'icon' => null,
     'color' => null,
+    'icon' => null,
 ])
 
-<div {{ $attributes->class(
-    array_merge(
-        [
-            'filament-forms-field-wrapper-hint flex space-x-2 rtl:space-x-reverse',
+<div {{ $attributes->class([
+    'filament-forms-field-wrapper-hint flex space-x-2 rtl:space-x-reverse',
+    match ($color) {
+        'danger' => [
+            'text-danger-500',
+            'dark:text-danger-300' => config('tables.dark_mode'),
         ],
-        match ($color) {
-            'danger' => [
-                'text-danger-700',
-                'dark:text-danger-500' => config('tables.dark_mode'),
-            ],
-            'secondary' => [
-                'text-gray-700',
-                'dark:text-gray-500' => config('tables.dark_mode'),
-            ],
-            'success' => [
-                'text-success-700',
-                'dark:text-success-500' => config('tables.dark_mode'),
-            ],
-            'warning' => [
-                'text-warning-700',
-                'dark:text-warning-500' => config('filament.dark_mode'),
-            ],
-            'primary' => [
-                'text-primary-700',
-                'dark:text-primary-500' => config('tables.dark_mode'),
-            ],
-            null => [
-                'text-gray-500',
-                'dark:text-gray-300' => config('tables.dark_mode'),
-            ],
-        },
-    )
-) }}>
+        'success' => [
+            'text-success-500',
+            'dark:text-success-300' => config('tables.dark_mode'),
+        ],
+        'warning' => [
+            'text-warning-500',
+            'dark:text-warning-300' => config('filament.dark_mode'),
+        ],
+        'primary' => [
+            'text-primary-500',
+            'dark:text-primary-300' => config('tables.dark_mode'),
+        ],
+        default => [
+            'text-gray-500',
+            'dark:text-gray-300' => config('tables.dark_mode'),
+        ],
+    },
+]) }}>
     @if ($slot->isNotEmpty())
         <span class="text-xs leading-tight">
             {{ $slot }}
