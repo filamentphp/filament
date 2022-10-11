71847deccca162540b47281d06bd6eb30822e594
Tommi Carleman <tommicarleman+git@gmail.com>
2022-10-11 09:28:13 +0200

    Update chart-widget.blade.php
    Adds the pulse animation to the select dropdown to indicate that data loading is happening.

--- packages/admin/resources/views/widgets/chart-widget.blade.php
+++ packages/admin/resources/views/widgets/chart-widget.blade.php
@@ -1,4 +1,4 @@
-<x-filament::widget class="filament-widgets-chart-widget">
+<x-filament::widget class="filament-widgets-chart-widget" x-data="{loading: false}">
     <x-filament::card>
         <div class="flex items-center justify-between gap-8">
             <x-filament::card.heading>
@@ -8,10 +8,12 @@
             @if ($filters = $this->getFilters())
                 <select
                     wire:model="filter"
+                    x-on:change="loading = true"
                     @class([
                         'text-gray-900 border-gray-300 block h-10 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500',
                         'dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 dark:focus:border-primary-500' => config('filament.dark_mode'),
                     ])
+                    :class="loading && 'animate-pulse'"
                 >
                     @foreach ($filters as $value => $label)
                         <option value="{{ $value }}">
@@ -40,6 +42,7 @@
                         $wire.on('filterChartData', async ({ data }) => {
                             chart.destroy()
                             chart = this.initChart(data)
+                            loading = false;
                         })
                     },
 
