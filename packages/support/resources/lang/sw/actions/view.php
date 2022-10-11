6e34b489316be92a9afb55aa88f6e5cef96d3224
Thomson Maguru <thomson@magurugroup.co.tz>
2022-10-11 12:28:58 +0300

    Swahili Translations

---
+++ packages/support/resources/lang/sw/actions/view.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Angalia',
+
+        'modal' => [
+
+            'heading' => 'Angalia :label',
+
+            'actions' => [
+
+                'close' => [
+                    'label' => 'Funga',
+                ],
+
+            ],
+
+        ],
+
+    ],
+
+];
---
+++ packages/support/resources/lang/sw/components/button.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'messages' => [
+        'uploading_file' => 'Inapakia faili...',
+    ],
+
+];
