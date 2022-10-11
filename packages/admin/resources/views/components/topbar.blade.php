45b2b9118462a4bd79b9c2ac22935c7ba286758c
Zep Fietje <hey@zepfietje.com>
2022-10-12 00:19:17 +0200

    Use Blade component for topbar menu icon

--- packages/admin/resources/views/components/topbar.blade.php
+++ packages/admin/resources/views/components/topbar.blade.php
@@ -17,9 +17,7 @@
                 'lg:hidden' => ! (config('filament.layout.sidebar.is_collapsible_on_desktop') && (config('filament.layout.sidebar.collapsed_width') === 0)),
             ])
         >
-            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
+            <x-heroicon-o-bars-3 class="w-6 h-6" />
         </button>
 
         <div class="flex items-center justify-between flex-1">
