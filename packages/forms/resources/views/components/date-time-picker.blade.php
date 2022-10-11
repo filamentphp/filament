29ab0e97df1abf84645cbcb8cb3f2aa61e5a71e8
Zep Fietje <hey@zepfietje.com>
2022-10-12 00:18:50 +0200

    Use Blade component for date time picker calendar icon

--- packages/forms/resources/views/components/date-time-picker.blade.php
+++ packages/forms/resources/views/components/date-time-picker.blade.php
@@ -67,12 +67,10 @@
             />
 
             <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none rtl:right-auto rtl:left-0 rtl:pl-2">
-                <svg @class([
+                <x-heroicon-m-calendar @class([
                     'w-5 h-5 text-gray-400',
                     'dark:text-gray-400' => config('forms.dark_mode'),
-                ]) xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>
+                ]) />
             </span>
         </button>
 
