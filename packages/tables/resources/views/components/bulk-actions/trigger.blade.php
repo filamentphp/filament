1d4d1c17cdf348e04ccd670a6d1b974ff8313b99
Dan Harrin <git@danharrin.com>
2022-10-13 11:00:03 +0100

    outined table trigger icons

--- packages/tables/resources/views/components/bulk-actions/trigger.blade.php
+++ packages/tables/resources/views/components/bulk-actions/trigger.blade.php
@@ -1,5 +1,5 @@
 <x-tables::icon-button
-    icon="heroicon-m-ellipsis-vertical"
+    icon="heroicon-o-ellipsis-vertical"
     :label="__('tables::table.buttons.open_actions.label')"
     x-cloak
     {{ $attributes->class(['filament-tables-bulk-actions-trigger']) }}
