5bb50ec8e3c4c13f9dea317bebbbfaf4c3c8b22f
Zep Fietje <hey@zepfietje.com>
2022-10-13 09:12:33 +0200

    Update packages/forms/resources/js/components/markdown-editor.js

--- packages/forms/resources/js/components/markdown-editor.js
+++ packages/forms/resources/js/components/markdown-editor.js
@@ -28,6 +28,7 @@ export default (Alpine) => {
                     if (value === null) {
                         this.state = ''
                     }
+
                     this.render()
                 })
             },
