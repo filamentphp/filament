7b2d8e86de4ee40e19bb7f41b1a9bd52dee271bf
Luis Andrés Arce C <luilliarcec@gmail.com>
2022-10-13 14:47:37 -0500

    Update packages/admin/resources/views/components/layouts/app/sidebar/subgroup.blade.php
    Co-authored-by: Zep Fietje <hey@zepfietje.com>

--- packages/admin/resources/views/components/layouts/app/sidebar/subgroup.blade.php
+++ packages/admin/resources/views/components/layouts/app/sidebar/subgroup.blade.php
@@ -45,7 +45,7 @@
     @endif
 
     <ul
-        x-show="! ($store.sidebar.groupIsCollapsed(label) && {{ config('filament.layout.sidebar.is_collapsible_on_desktop') ? '$store.sidebar.isOpen' : 'true' }})"
+        x-show="!($store.sidebar.groupIsCollapsed(label) && {{ config('filament.layout.sidebar.is_collapsible_on_desktop') ? '$store.sidebar.isOpen' : 'true' }})"
         x-collapse.duration.200ms
         @class([
             'text-sm space-y-1 mx-4',
