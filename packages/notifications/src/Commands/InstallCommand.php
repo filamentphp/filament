a3526e597c59f92d0be9a3269e03eb9fa8a88b9b
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2022-10-13 23:14:58 +0600

    chore: notifications package console ui improvement

--- packages/notifications/src/Commands/InstallCommand.php
+++ packages/notifications/src/Commands/InstallCommand.php
@@ -20,9 +20,9 @@ class InstallCommand extends Command
         $filesystem->delete(resource_path('js/bootstrap.js'));
         $filesystem->copyDirectory(__DIR__ . '/../../stubs/scaffolding', base_path());
 
-        $this->info('Scaffolding installed successfully.');
+        $this->components->info('Scaffolding installed successfully.');
 
-        $this->comment('Please run `npm install && npm run dev` to compile your new assets.');
+        $this->components->info('Please run `npm install && npm run dev` to compile your new assets.');
 
         return static::SUCCESS;
     }
