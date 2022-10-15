20a156ad3e0fcfe50eb32cc9bd41c3a14857898b
Thomas Vestergaard <thomas.vestergaard@raketech.com>
2022-10-15 15:33:41 +0300

    Danish translations for the Admin package

---
+++ packages/admin/resources/lang/da/global-search.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'field' => [
+        'label' => 'Global søgning',
+        'placeholder' => 'Søg',
+    ],
+
+    'no_results_message' => 'Ingen søgeresultater fundet.',
+
+];
---
+++ packages/admin/resources/lang/da/resources/pages/create-record.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'title' => 'Opret :label',
+
+    'breadcrumb' => 'Opret',
+
+    'form' => [
+
+        'actions' => [
+
+            'cancel' => [
+                'label' => 'Annuller',
+            ],
+
+            'create' => [
+                'label' => 'Opret',
+            ],
+
+            'create_another' => [
+                'label' => 'Opret & opret en mere',
+            ],
+
+        ],
+
+    ],
+
+    'messages' => [
+        'created' => 'Oprettet',
+    ],
+
+];
