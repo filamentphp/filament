ba6c049f7465edc777f67b649bcb40a2d21a171c
Zep Fietje <hey@zepfietje.com>
2022-10-16 10:42:09 +0200

    Only show chart widget divider if it has a header

--- packages/admin/resources/views/widgets/chart-widget.blade.php
+++ packages/admin/resources/views/widgets/chart-widget.blade.php
@@ -1,36 +1,36 @@
 @php
-    $filters = $this->getFilters();
     $heading = $this->getHeading();
+    $filters = $this->getFilters();
 @endphp
 
 <x-filament::widget class="filament-widgets-chart-widget">
     <x-filament::card>
-        <div class="flex items-center justify-between gap-8">
-            @if ($heading)
-                <x-filament::card.heading>
-                    {{ $heading }}
-                </x-filament::card.heading>
-            @endif
-
-            @if ($filters)
-                <select
-                    wire:model="filter"
-                    @class([
-                        'text-gray-900 border-gray-300 block h-10 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500',
-                        'dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 dark:focus:border-primary-500' => config('filament.dark_mode'),
-                    ])
-                    wire:loading.class="animate-pulse"
-                >
-                    @foreach ($filters as $value => $label)
-                        <option value="{{ $value }}">
-                            {{ $label }}
-                        </option>
-                    @endforeach
-                </select>
-            @endif
-        </div>
-
         @if ($heading || $filters)
+            <div class="flex items-center justify-between gap-8">
+                @if ($heading)
+                    <x-filament::card.heading>
+                        {{ $heading }}
+                    </x-filament::card.heading>
+                @endif
+
+                @if ($filters)
+                    <select
+                        wire:model="filter"
+                        @class([
+                            'text-gray-900 border-gray-300 block h-10 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500',
+                            'dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 dark:focus:border-primary-500' => config('filament.dark_mode'),
+                        ])
+                        wire:loading.class="animate-pulse"
+                    >
+                        @foreach ($filters as $value => $label)
+                            <option value="{{ $value }}">
+                                {{ $label }}
+                            </option>
+                        @endforeach
+                    </select>
+                @endif
+            </div>
+
             <x-filament::hr />
         @endif
 
