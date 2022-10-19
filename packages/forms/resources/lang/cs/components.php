743469f76e4413a403f97098b0aa809d92b8cc7f
Lukas Frey <mail@lukasfrey.cz>
2022-10-19 18:09:51 +0200

    lang(cs): added pluralization
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

--- packages/forms/resources/lang/cs/components.php
+++ packages/forms/resources/lang/cs/components.php
@@ -220,7 +220,7 @@ return [
 
         'loading_message' => 'Načítání...',
 
-        'max_items_message' => 'Lze vybrat pouze 1 položka.|Lze vybrat pouze :count položek.',
+        'max_items_message' => 'Lze vybrat pouze 1 položka.|Lze vybrat pouze :count položky.|Lze vybrat pouze :count položek.',
 
         'no_search_results_message' => 'Vašemu hledání neodpovídají žádné výsledky.',
 
