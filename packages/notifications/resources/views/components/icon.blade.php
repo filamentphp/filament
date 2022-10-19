9f3408408d5467c82ec52ec9f718f3ae096c46a0
Zep Fietje <hey@zepfietje.com>
2022-10-19 16:56:36 +0200

    Add color to notification icon

--- packages/notifications/resources/views/components/icon.blade.php
+++ packages/notifications/resources/views/components/icon.blade.php
@@ -6,15 +6,14 @@
 <x-filament-support::icon
     :name="$icon"
     alias="notifications::notification"
+    :color="match ($color) {
+        'danger' => 'text-danger-400',
+        'primary' => 'text-primary-400',
+        'secondary' => 'text-gray-400',
+        'success' => 'text-success-400',
+        'warning' => 'text-warning-400',
+        default => $color,
+    }"
     size="h-6 w-6"
-    :class="\Illuminate\Support\Arr::toCssClasses([
-        'filament-notifications-notification-icon',
-        match ($color) {
-            'success' => 'text-success-400',
-            'warning' => 'text-warning-400',
-            'danger' => 'text-danger-400',
-            'primary' => 'text-primary-400',
-            'secondary' => 'text-gray-400',
-        },
-    ])"
+    class="filament-notifications-notification-icon"
 />
