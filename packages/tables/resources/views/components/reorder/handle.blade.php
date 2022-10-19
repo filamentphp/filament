df8c2dc29919bea3101578c737caadac5a198e42
Zep Fietje <hey@zepfietje.com>
2022-10-19 16:58:27 +0200

    Clean up tables reorder handle

--- packages/tables/resources/views/components/reorder/handle.blade.php
+++ packages/tables/resources/views/components/reorder/handle.blade.php
@@ -1,14 +1,11 @@
 <button
     type="button"
-    {{ $attributes->class([
-        'filament-tables-reorder-handle text-gray-500 cursor-move transition group-hover:text-primary-500',
-        'dark:text-gray-400 dark:group-hover:text-primary-400' => config('tables.dark_mode'),
-    ]) }}
+    {{ $attributes->class(['filament-tables-reorder-handle text-gray-500 cursor-move transition group-hover:text-primary-500 dark:text-gray-400 dark:group-hover:text-primary-400']) }}
 >
     <x-filament-support::icon
         name="heroicon-o-bars-3"
-        size="h-4 w-4"
         alias="tables::reorder.handle"
+        size="h-4 w-4"
         class="block"
     />
 </button>
