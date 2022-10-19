21c0bef990130a6c251e7dae9413997f01f1f714
Zep Fietje <hey@zepfietje.com>
2022-10-19 16:54:54 +0200

    Add column and size to icon column icon

--- packages/tables/resources/views/columns/icon-column.blade.php
+++ packages/tables/resources/views/columns/icon-column.blade.php
@@ -18,24 +18,22 @@
         <x-filament-support::icon
             :name="$icon"
             alias="tables::columns.icon"
-            :class="\Illuminate\Support\Arr::toCssClasses([
-                match ($color) {
-                    'danger' => 'text-danger-500',
-                    'primary' => 'text-primary-500',
-                    'success' => 'text-success-500',
-                    'warning' => 'text-warning-500',
-                    null => \Illuminate\Support\Arr::toCssClasses(['text-gray-700', 'dark:text-gray-200' => config('tables.dark_mode'),]),
-                    default => $color,
-                },
-                match ($size) {
-                    'xs' => 'h-3 w-3',
-                    'sm' => 'h-4 w-4',
-                    'md' => 'h-5 w-5',
-                    'lg' => 'h-6 w-6',
-                    'xl' => 'h-7 w-7',
-                    default => $size,
-                },
-            ])"
+            :color="match ($color) {
+                'danger' => 'text-danger-500',
+                'primary' => 'text-primary-500',
+                'success' => 'text-success-500',
+                'warning' => 'text-warning-500',
+                null => 'text-gray-500',
+                default => $color,
+            }"
+            :size="match ($size) {
+                'xs' => 'h-3 w-3',
+                'sm' => 'h-4 w-4',
+                'md' => 'h-5 w-5',
+                'lg' => 'h-6 w-6',
+                'xl' => 'h-7 w-7',
+                default => $size,
+            }"
         />
     @endif
 </div>
