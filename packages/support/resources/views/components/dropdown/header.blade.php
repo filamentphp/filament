b7f8eadebe750c065ea66c0dc95bd1cf53745f40
Zep Fietje <hey@zepfietje.com>
2022-10-19 15:58:09 +0200

    Add size to dropdown header icon

--- packages/support/resources/views/components/dropdown/header.blade.php
+++ packages/support/resources/views/components/dropdown/header.blade.php
@@ -8,8 +8,10 @@
 @php
     $headerClasses = 'filament-dropdown-header flex w-full p-3 text-sm';
 
+    $iconSize = 'h-5 w-5';
+
     $iconClasses = \Illuminate\Support\Arr::toCssClasses([
-        'filament-dropdown-header-icon mr-2 h-5 w-5 rtl:ml-2 rtl:mr-0',
+        'filament-dropdown-header-icon mr-2 rtl:ml-2 rtl:mr-0',
         'text-primary-500' => $color === 'primary',
         'text-danger-500' => $color === 'danger',
         'text-gray-500' => $color === 'secondary',
@@ -26,6 +28,7 @@
             <x-filament-support::icon
                 :name="$icon"
                 alias="support::dropdown.header"
+                :size="$iconSize"
                 :class="$iconClasses"
             />
         @endif
@@ -40,6 +43,7 @@
             <x-filament-support::icon
                 :name="$icon"
                 alias="support::dropdown.header"
+                :size="$iconSize"
                 :class="$iconClasses"
             />
         @endif
