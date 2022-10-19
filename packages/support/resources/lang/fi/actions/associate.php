2f1c0af6b25378a2968cd8fb249b71dd32168f39
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2022-10-19 18:47:23 +0300

    Finnish translation

---
+++ packages/support/resources/lang/fi/actions/associate.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Yhdistä',
+
+        'modal' => [
+
+            'heading' => 'Yhdistä :label',
+
+            'fields' => [
+
+                'record_id' => [
+                    'label' => 'Tietue',
+                ],
+
+            ],
+
+            'actions' => [
+
+                'associate' => [
+                    'label' => 'Yhdistä',
+                ],
+
+                'associate_another' => [
+                    'label' => 'Yhdistä & yhdistä toinen',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'associated' => 'Yhdistetty',
+        ],
+
+    ],
+
+];
---
+++ packages/support/resources/lang/fi/actions/detach.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Irrota',
+
+        'modal' => [
+
+            'heading' => 'Irrota :label',
+
+            'actions' => [
+
+                'detach' => [
+                    'label' => 'Irrota',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'detached' => 'Irrotettu',
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => 'Irrota valitut',
+
+        'modal' => [
+
+            'heading' => 'Irrota valitut :label',
+
+            'actions' => [
+
+                'detach' => [
+                    'label' => 'Irrota valitut',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'detached' => 'Irrotettu',
+        ],
+
+    ],
+
+];
