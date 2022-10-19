0f6a951b183874b41f3aa77fd25a2fc6e06cc335
Zep Fietje <hey@zepfietje.com>
2022-10-19 16:04:49 +0200

    Refactor dropdown header

--- packages/support/resources/views/components/dropdown/header.blade.php
+++ packages/support/resources/views/components/dropdown/header.blade.php
@@ -1,55 +1,28 @@
 @props([
     'color' => 'primary',
-    'darkMode' => false,
     'icon' => null,
     'tag' => 'div',
 ])
 
-@php
-    $headerClasses = 'filament-dropdown-header flex w-full p-3 text-sm';
+<{{ $tag }} {{ $attributes->class(['filament-dropdown-header flex w-full p-3 text-sm']) }}>
+    @if ($icon)
+        <x-filament-support::icon
+            :name="$icon"
+            alias="support::dropdown.header"
+            :color="match ($color) {
+                'danger' => 'text-danger-500',
+                'primary' => 'text-primary-500',
+                'secondary' => 'text-gray-500',
+                'success' => 'text-success-500',
+                'warning' => 'text-warning-500',
+                default => $color,
+            }"
+            size="h-5 w-5"
+            class="filament-dropdown-header-icon mr-2 rtl:ml-2 rtl:mr-0"
+        />
+    @endif
 
-    $iconSize = 'h-5 w-5';
-
-    $iconClasses = \Illuminate\Support\Arr::toCssClasses([
-        'filament-dropdown-header-icon mr-2 rtl:ml-2 rtl:mr-0',
-        'text-primary-500' => $color === 'primary',
-        'text-danger-500' => $color === 'danger',
-        'text-gray-500' => $color === 'secondary',
-        'text-success-500' => $color === 'success',
-        'text-warning-500' => $color === 'warning',
-    ]);
-
-    $labelClasses = 'filament-dropdown-header-label';
-@endphp
-
-@if ($tag === 'div')
-    <div {{ $attributes->class([$headerClasses]) }}>
-        @if ($icon)
-            <x-filament-support::icon
-                :name="$icon"
-                alias="support::dropdown.header"
-                :size="$iconSize"
-                :class="$iconClasses"
-            />
-        @endif
-
-        <span class="{{ $labelClasses }}">
-            {{ $slot }}
-        </span>
-    </div>
-@elseif ($tag === 'a')
-    <a {{ $attributes->class([$headerClasses]) }}>
-        @if ($icon)
-            <x-filament-support::icon
-                :name="$icon"
-                alias="support::dropdown.header"
-                :size="$iconSize"
-                :class="$iconClasses"
-            />
-        @endif
-
-        <span class="{{ $labelClasses }}">
-            {{ $slot }}
-        </span>
-    </a>
-@endif
+    <span class="filament-dropdown-header-label">
+        {{ $slot }}
+    </span>
+</{{ $tag }}>
