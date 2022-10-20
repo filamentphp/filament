741c702a40227e409a41d11290baa80e3b585918
Zep Fietje <hey@zepfietje.com>
2022-10-20 23:45:16 +0200

    Refactor tab item

--- packages/app/resources/views/components/tabs/item.blade.php
+++ packages/app/resources/views/components/tabs/item.blade.php
@@ -4,23 +4,15 @@
     'type' => 'button',
 ])
 
-@php
-    $buttonClasses = \Illuminate\Support\Arr::toCssClasses([
+<{{ $tag }}
+    @if ($tag === 'button')
+        type="{{ $type }}"
+    @endif
+    {{ $attributes->class([
         'filament-tabs-item flex items-center h-8 px-5 font-medium rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-inset',
         'hover:text-gray-800 focus:text-primary-600 dark:text-gray-400 dark:hover:text-gray-300 dark:focus:text-primary-400' => ! $active,
         'text-primary-600 shadow bg-white' => $active,
-    ]);
-@endphp
-
-@if ($tag === 'button')
-    <button
-        type="{{ $type }}"
-        {{ $attributes->class([$buttonClasses]) }}
-    >
-        {{ $slot }}
-    </button>
-@elseif ($tag === 'a')
-    <a {{ $attributes->class([$buttonClasses]) }}>
-        {{ $slot }}
-    </a>
-@endif
+    ]) }}
+>
+    {{ $slot }}
+</{{ $tag }}>
