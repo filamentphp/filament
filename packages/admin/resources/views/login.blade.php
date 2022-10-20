c3bc378f967c8922912d86fc499f3c9657a2913f
Zep Fietje <hey@zepfietje.com>
2022-10-20 16:28:01 +0200

    Undo formatting change

--- packages/admin/resources/views/login.blade.php
+++ packages/admin/resources/views/login.blade.php
@@ -1,5 +1,6 @@
 <form wire:submit.prevent="authenticate" class="space-y-8">
     {{ $this->form }}
+
     <x-filament::button type="submit" form="authenticate" class="w-full">
         {{ __('filament::login.buttons.submit.label') }}
     </x-filament::button>
