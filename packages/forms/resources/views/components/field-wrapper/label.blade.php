724dd24b2a3881e1068abde98b4ea741df9aa32f
Zep Fietje <hey@zepfietje.com>
2022-10-21 16:50:39 +0200

    Remove required asterisk for disabled field

--- packages/forms/resources/views/components/field-wrapper/label.blade.php
+++ packages/forms/resources/views/components/field-wrapper/label.blade.php
@@ -1,5 +1,6 @@
 @props([
     'error' => false,
+    'isDisabled' => false,
     'isMarkedAsRequired' => true,
     'prefix' => null,
     'required' => false,
@@ -16,7 +17,7 @@
     ])>
         {{ $slot }}
 
-        @if ($required && $isMarkedAsRequired)
+        @if ($required && $isMarkedAsRequired && ! $isDisabled)
             <sup class="font-medium text-danger-700">*</sup>
         @endif
     </span>
