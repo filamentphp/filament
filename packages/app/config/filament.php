2fc5267881c22a67ab9b0e5d19bc5c4398e1b782
Zep Fietje <hey@zepfietje.com>
2022-10-21 11:28:40 +0200

    Fix config comment spacing

--- packages/app/config/filament.php
+++ packages/app/config/filament.php
@@ -160,14 +160,14 @@ return [
     ],
 
     /*
-     |--------------------------------------------------------------------------
-     | Dark mode
-     |--------------------------------------------------------------------------
-     |
-     | By enabling this feature, your users are able to select between a light
-     | and dark appearance for the admin panel, or let their system decide.
-     |
-     */
+    |--------------------------------------------------------------------------
+    | Dark mode
+    |--------------------------------------------------------------------------
+    |
+    | By enabling this feature, your users are able to select between a light
+    | and dark appearance for the admin panel, or let their system decide.
+    |
+    */
 
     'dark_mode' => true,
 
