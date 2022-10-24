76ea23925d1f74346a2489b817c834258001b0e9
Marcus Moore <contact@marcusmoore.io>
2022-10-24 16:18:00 -0700

    Use translation string for user menu

--- packages/admin/resources/views/components/layouts/app/topbar/user-menu.blade.php
+++ packages/admin/resources/views/components/layouts/app/topbar/user-menu.blade.php
@@ -12,7 +12,7 @@
 
 <x-filament::dropdown placement="bottom-end">
     <x-slot name="trigger" class="ml-4">
-        <button class="block" aria-label="User Menu">
+        <button class="block" aria-label="{{ __('filament::layout.buttons.user_menu.label') }}">
             <x-filament::user-avatar :user="$user" />
         </button>
     </x-slot>
