6f60ff2200a8819528559d67c67aad7cb2bc99f7
Dan Harrin <git@danharrin.com>
2022-10-26 14:52:59 +0100

    wip

---
+++ packages/app/src/ContextServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Filament;
+
+use Filament\Support\PluginServiceProvider;
+
+class ContextServiceProvider extends PluginServiceProvider
+{
+    public function configureContext(Context $context): void
+    {
+    }
+}
