768b8832ce08821fb417030aeba4d1b49d42983b
Beej <howdu@users.noreply.github.com>
2022-10-28 13:43:56 +0100

    Add wire target to login button

--- packages/admin/resources/views/login.blade.php
+++ packages/admin/resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <form wire:submit.prevent="authenticate" class="space-y-8">
     {{ $this->form }}
 
-    <x-filament::button type="submit" form="authenticate" class="w-full">
+    <x-filament::button type="submit" form="authenticate" class="w-full"  wire:target="authenticate">
         {{ __('filament::login.buttons.submit.label') }}
     </x-filament::button>
 </form>
