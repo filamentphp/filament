6e0283349438a5c366dd6f9b0772f3c0e737260c
awcodes <adam@adamwgd.com>
2022-10-31 08:47:21 -0400

    Remove record based identifier from wire key in mounted table actions form.

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -869,7 +869,7 @@
 
         <x-tables::modal
             :id="$this->id . '-table-action'"
-            :wire:key="$action ? $this->id . '.table' . ($getMountedActionRecordKey() ? '.records.' . $getMountedActionRecordKey() : null) . '.actions.' . $action->getName() . '.modal' : null"
+            :wire:key="$action ? $this->id . '.table.actions.' . $action->getName() . '.modal' : null"
             :visible="filled($action)"
             :width="$action?->getModalWidth()"
             :slide-over="$action?->isModalSlideOver()"
