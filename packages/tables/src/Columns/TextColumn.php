ae5f663c7e9b8afeadec2160ed52fa62a2b4ffd3
Alan Mosko <moskoweb@gmail.com>
2022-11-03 11:44:26 -0300

    Update TextColumn.php

--- packages/tables/src/Columns/TextColumn.php
+++ packages/tables/src/Columns/TextColumn.php
@@ -16,6 +16,53 @@ class TextColumn extends Column
     protected string $view = 'tables::columns.text-column';
 
     protected bool | Closure $canWrap = false;
+    
+    protected bool | Closure $isCopyable = false;
+
+    protected string | Closure | null $copyMessage = null;
+
+    protected int | Closure | null $copyMessageDuration = null;
+
+    public function copyable(bool | Closure $condition = true): static
+    {
+        $this->isCopyable = $condition;
+
+        return $this;
+    }
+
+    public function copyMessage(string | Closure | null $message): static
+    {
+        $this->copyMessage = $message;
+
+        return $this;
+    }
+
+    public function copyMessageDuration(int | Closure | null $duration): static
+    {
+        $this->copyMessageDuration = $duration;
+
+        return $this;
+    }
+
+    public function getCopyMessage(): string
+    {
+        return $this->evaluate($this->copyMessage) ?? __('tables::table.columns.text-column.messages.copied');
+    }
+
+    public function getCopyMessageDuration(): int
+    {
+        return $this->evaluate($this->copyMessageDuration) ?? 2000;
+    }
+
+    public function isCopyable(): bool
+    {
+        return $this->evaluate($this->isCopyable);
+    }
+
+    public function isClickDisabled(): bool
+    {
+        return parent::isClickDisabled() || $this->isCopyable();
+    }
 
     public function wrap(bool | Closure $condition = true): static
     {
