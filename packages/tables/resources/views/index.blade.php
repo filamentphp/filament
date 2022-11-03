7c3555ca874ab02b287aedb80f348320f1ac1fe5
Dan Harrin <git@danharrin.com>
2022-11-03 10:19:48 +0000

    Update index.blade.php

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -424,9 +424,9 @@
                                 <div
                                     @if ($hasCollapsibleColumnsLayout)
                                         x-data="{ isCollapsed: true }"
-                                        x-init="$dispatch('init-collapsible-row')"
-                                        x-on:expand-all-rows.window="isCollapsed = false"
-                                        x-on:collapse-all-rows.window="isCollapsed = true"
+                                        x-init="$dispatch('collapsible-table-row-initialized')"
+                                        x-on:expand-all-table-rows.window="isCollapsed = false"
+                                        x-on:collapse-all-table-rows.window="isCollapsed = true"
                                     @endif
                                     wire:key="{{ $this->id }}.table.records.{{ $recordKey }}"
                                     @if ($isReordering)
