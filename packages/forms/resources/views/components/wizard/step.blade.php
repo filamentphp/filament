526ecd3b3595064a951c18ce6b40dbbe9acac505
Dan Harrin <git@danharrin.com>
2022-11-12 16:24:56 +0000

    fix: Wizard submit validation

--- packages/forms/resources/views/components/wizard/step.blade.php
+++ packages/forms/resources/views/components/wizard/step.blade.php
@@ -4,11 +4,15 @@
     x-ref="step-{{ $getId() }}"
     role="tabpanel"
     tabindex="0"
-    x-bind:class="{ 'invisible h-0 overflow-y-hidden': step !== '{{ $getId() }}' }"
+    x-bind:class="{ 'invisible h-0 overflow-y-hidden': step !== @js($getId()) }"
     x-on:expand-concealing-component.window="
         error = $el.querySelector('[data-validation-error]')
 
         if (! error) {
+            return
+        }
+
+        if (! isStepAccessible(step, @js($getId()))) {
             return
         }
 
