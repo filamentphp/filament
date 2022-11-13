c45a568365ff563fbeb1eae9bf9b81ed1a8eb12c
HUSAM TAREQ AL-SHAIBANI <hu22am1@gmail.com>
2022-11-13 21:12:25 +0300

    add group to notifications

--- packages/notifications/resources/views/components/database/modal/index.blade.php
+++ packages/notifications/resources/views/components/database/modal/index.blade.php
@@ -1,44 +1,70 @@
-@props([
-    'notifications',
-    'unreadNotificationsCount',
-])
+@props(['notifications', 'unreadNotificationsCount'])
 
-<x-notifications::modal
-    id="database-notifications"
-    close-button
-    slide-over
-    width="md"
->
+<x-notifications::modal id="database-notifications" close-button slide-over width="md">
     @if ($notifications->count())
         <x-slot name="header">
-            <x-notifications::database.modal.heading
-                :unread-notifications-count="$unreadNotificationsCount"
-            />
+            <x-notifications::database.modal.heading :unread-notifications-count="$unreadNotificationsCount" />
 
-            <x-notifications::database.modal.actions
-                :notifications="$notifications"
-                :unread-notifications-count="$unreadNotificationsCount"
-            />
+            <x-notifications::database.modal.actions :notifications="$notifications" :unread-notifications-count="$unreadNotificationsCount" />
         </x-slot>
+        @foreach ($notifications as $key => $notificationGroup)
+            <div x-data="{ isOpen: true }" class="filament-sidebar-group">
+                @if ($key)
+                    <button class="flex items-center justify-between w-full" x-on:click.prevent="isOpen= !isOpen">
+                        <div @class([
+                            'flex items-center gap-4 text-gray-600',
+                            'dark:text-gray-300' => config('filament.dark_mode'),
+                        ])>
 
-        <div class="mt-[calc(-1rem-1px)]">
-            @foreach ($notifications as $notification)
-                <div @class([
-                    '-mx-6 border-b',
-                    'border-t' => $notification->unread(),
-                    'dark:border-gray-700' => (! $notification->unread()) && config('notifications.dark_mode'),
-                    'dark:border-gray-800' => $notification->unread() && config('notifications.dark_mode'),
-                ])>
-                    <div @class([
-                        'py-2 pl-4 pr-2',
-                        'bg-primary-50 -mb-px' => $notification->unread(),
-                        'dark:bg-gray-700' => $notification->unread() && config('notifications.dark_mode'),
-                    ])>
-                        {{ $this->getNotificationFromDatabaseRecord($notification)->inline() }}
+
+                            <p class="flex-1 text-xs font-bold tracking-wider uppercase">
+                                {{ $key }}
+                            </p>
+                        </div>
+
+
+                        <x-heroicon-o-chevron-down :class="\Illuminate\Support\Arr::toCssClasses([
+                            'w-3 h-3 text-gray-600 transition',
+                            'dark:text-gray-300' => config('filament.dark_mode'),
+                        ])" x-bind:class="isOpen || '-rotate-180'" x-cloak />
+
+                    </button>
+                @endif
+
+                    <div x-show="isOpen" x-collapse.duration.200ms @class(['text-sm space-y-1 -mx-3', 'mt-2' => $key])>
+
+
+                        @foreach ($notificationGroup as $notification)
+                            <div @class([
+                                'border  rounded-lg overflow-hidden ',
+                                'dark:border-gray-700' =>
+                                    !$notification->unread() && config('notifications.dark_mode'),
+                                'dark:border-gray-800' =>
+                                    $notification->unread() && config('notifications.dark_mode'),
+                            ])>
+                                <div @class([
+                                    'py-2 pl-4 pr-2',
+                                    'bg-primary-50' => $notification->unread(),
+                                    'dark:bg-gray-700' =>
+                                        $notification->unread() && config('notifications.dark_mode'),
+                                ])>
+                                    {{ $this->getNotificationFromDatabaseRecord($notification)->inline() }}
+                                </div>
+                            </div>
+                        @endforeach
+
                     </div>
-                </div>
-            @endforeach
-        </div>
+
+            </div>
+            @if (! $loop->last)
+
+            <div @class([
+                'border-t -mx-6',
+                'dark:border-gray-700' => config('filament.dark_mode'),
+            ])></div>
+
+    @endif
+        @endforeach
     @else
         <x-notifications::database.modal.empty-state />
     @endif
