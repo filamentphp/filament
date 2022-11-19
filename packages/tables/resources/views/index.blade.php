9fd7865ea79e97cd8e6312b6a7f04775befe63d1
Zep Fietje <hey@zepfietje.com>
2022-11-19 14:50:29 +0100

    Update packages/tables/resources/views/index.blade.php

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -980,7 +980,7 @@
                     <x-slot name="header">
                         @if ($modalHeading = $action->getModalHeading())
                             <x-tables::modal.heading>
-                                {{ $action->getModalHeading() }}
+                                {{ $modalHeading }}
                             </x-tables::modal.heading>
                         @endif
 
