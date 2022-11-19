8e2062ab65be7d6554e442369b3564ab86f53d22
Zep Fietje <hey@zepfietje.com>
2022-11-19 14:49:10 +0100

    Update packages/admin/resources/views/components/page.blade.php

--- packages/admin/resources/views/components/page.blade.php
+++ packages/admin/resources/views/components/page.blade.php
@@ -69,7 +69,7 @@
         >
             @if ($action)
                 @if ($action->isModalCentered())
-                    @if($modalHeading = $action->getModalHeading())
+                    @if ($modalHeading = $action->getModalHeading())
                         <x-tables::modal.heading>
                             {{ $action->getModalHeading() }}
                         </x-tables::modal.heading>
