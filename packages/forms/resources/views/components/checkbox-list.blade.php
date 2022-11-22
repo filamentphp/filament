2ff83b5f3ee05700b4ac6200d33a40321b6b85cc
John Carter <john@johncarter.co.uk>
2022-11-22 14:11:29 +0000

    Maybe fixed blade indentation

--- packages/forms/resources/views/components/checkbox-list.blade.php
+++ packages/forms/resources/views/components/checkbox-list.blade.php
@@ -1,123 +1,133 @@
 <x-dynamic-component
-  :component="$getFieldWrapperView()"
-  :id="$getId()"
-  :label="$getLabel()"
-  :label-sr-only="$isLabelHidden()"
-  :helper-text="$getHelperText()"
-  :hint="$getHint()"
-  :hint-action="$getHintAction()"
-  :hint-color="$getHintColor()"
-  :hint-icon="$getHintIcon()"
-  :required="$isRequired()"
-  :state-path="$getStatePath()">
+    :component="$getFieldWrapperView()"
+    :id="$getId()"
+    :label="$getLabel()"
+    :label-sr-only="$isLabelHidden()"
+    :helper-text="$getHelperText()"
+    :hint="$getHint()"
+    :hint-action="$getHintAction()"
+    :hint-color="$getHintColor()"
+    :hint-icon="$getHintIcon()"
+    :required="$isRequired()"
+    :state-path="$getStatePath()">
 
-  @if ($isBulkToggleable() || $isSearchable())
-    <div x-data="{
-        checkboxes: $root.querySelectorAll('input[type=checkbox]'),
+    @if ($isBulkToggleable() || $isSearchable())
+        <div x-data="{
+            checkboxes: $root.querySelectorAll('input[type=checkbox]'),
 
-        isAllSelected: false,
+            isAllSelected: false,
 
-        init: function() {
-            this.updateIsAllSelected()
-        },
+            init: function () {
+                this.updateIsAllSelected()
+            },
 
-        updateIsAllSelected: function() {
-            this.isAllSelected = this.checkboxes.length === this.$root.querySelectorAll('input[type=checkbox]:checked').length
-        },
+            updateIsAllSelected: function () {
+                this.isAllSelected = this.checkboxes.length === this.$root.querySelectorAll('input[type=checkbox]:checked').length
+            },
 
-        toggleAll: function() {
-            state = !this.isAllSelected
+            toggleAll: function () {
+                state = !this.isAllSelected
 
-            this.checkboxes.forEach((checkbox) => {
-                checkbox.checked = state
-                checkbox.dispatchEvent(new Event('change'))
-            })
-        },
+                this.checkboxes.forEach((checkbox) => {
+                    checkbox.checked = state
+                    checkbox.dispatchEvent(new Event('change'))
+                })
+            },
 
-        checkboxListItems: $root.querySelectorAll('label'),
+            checkboxListItems: $root.querySelectorAll('label'),
 
-        searchText: '',
+            searchText: '',
 
-        hiddenBySearchCount: 0,
-    }">
-  @endif
+            hiddenBySearchCount: 0,
+        }">
+    @endif
 
-  @if ($isBulkToggleable())
-    <div class="mb-2">
-      <x-forms::link
-        tag="button"
-        size="sm"
-        x-show="!isAllSelected"
-        x-on:click="toggleAll">
-        {{ __('forms::components.checkbox_list.buttons.select_all.label') }}
-      </x-forms::link>
+    @if ($isBulkToggleable())
+        <div class="mb-2">
+            <x-forms::link
+                tag="button"
+                size="sm"
+                x-show="!isAllSelected"
+                x-on:click="toggleAll"
+            >
+                {{ __('forms::components.checkbox_list.buttons.select_all.label') }}
+            </x-forms::link>
 
-      <x-forms::link
-        tag="button"
-        size="sm"
-        x-show="isAllSelected"
-        x-on:click="toggleAll">
-        {{ __('forms::components.checkbox_list.buttons.deselect_all.label') }}
-      </x-forms::link>
-    </div>
-  @endif
+            <x-forms::link
+                tag="button"
+                size="sm"
+                x-show="isAllSelected"
+                x-on:click="toggleAll"
+            >
+                {{ __('forms::components.checkbox_list.buttons.deselect_all.label') }}
+            </x-forms::link>
+        </div>
+    @endif
 
-  @if ($isSearchable())
-    <div class="mb-4">
-      <input
-        class="block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 border-gray-300"
-        type="search"
-        placeholder="{{ $getSearchPrompt() }}"
-        x-model.debounce.{{ $getSearchDebounce() }}="searchText">
-    </div>
-  @endif
+    @if ($isSearchable())
+        <div class="mb-4">
+            <input
+                class="block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 border-gray-300"
+                type="search"
+                placeholder="{{ $getSearchPrompt() }}"
+                x-model.debounce.{{ $getSearchDebounce() }}="searchText">
+        </div>
+    @endif
 
-  <x-filament-support::grid
-    :default="$getColumns('default')"
-    :sm="$getColumns('sm')"
-    :md="$getColumns('md')"
-    :lg="$getColumns('lg')"
-    :xl="$getColumns('xl')"
-    :two-xl="$getColumns('2xl')"
-    direction="column"
-    :attributes="$attributes->class(['filament-forms-checkbox-list-component gap-1 space-y-2'])">
-    @php
-      $isDisabled = $isDisabled();
-    @endphp
+    <x-filament-support::grid
+        :default="$getColumns('default')"
+        :sm="$getColumns('sm')"
+        :md="$getColumns('md')"
+        :lg="$getColumns('lg')"
+        :xl="$getColumns('xl')"
+        :two-xl="$getColumns('2xl')"
+        direction="column"
+        :attributes="$attributes->class(['filament-forms-checkbox-list-component gap-1 space-y-2'])">
+        @php
+            $isDisabled = $isDisabled();
+        @endphp
 
-    @foreach ($getOptions() as $optionValue => $optionLabel)
-      <label
-        class="flex items-center space-x-3 rtl:space-x-reverse"
-        @if ($isSearchable()) x-show="if( $el.querySelector('span').innerText.toLowerCase().includes(searchText.toLowerCase()) ) { return true } else {hiddenBySearchCount++; return false}" @endif>
-        <input
-          {!! $isDisabled ? 'disabled' : null !!}
-          wire:loading.attr="disabled"
-          type="checkbox"
-          value="{{ $optionValue }}"
-          dusk="filament.forms.{{ $getStatePath() }}"
-          @if ($isBulkToggleable()) x-on:change="updateIsAllSelected" @endif
-          {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
-          {{ $getExtraAttributeBag()->class(['text-primary-600 transition duration-75 rounded shadow-sm focus:border-primary-500 focus:ring-2 focus:ring-primary-500 disabled:opacity-70', 'dark:bg-gray-700 dark:checked:bg-primary-500' => config('forms.dark_mode'), 'border-gray-300' => !$errors->has($getStatePath()), 'dark:border-gray-600' => !$errors->has($getStatePath()) && config('forms.dark_mode'), 'border-danger-300 ring-danger-500' => $errors->has($getStatePath()), 'dark:border-danger-400 dark:ring-danger-400' => $errors->has($getStatePath()) && config('forms.dark_mode')]) }} />
+        @foreach ($getOptions() as $optionValue => $optionLabel)
+            <label
+                class="flex items-center space-x-3 rtl:space-x-reverse"
+                @if ($isSearchable()) x-show="if( $el.querySelector('span').innerText.toLowerCase().includes(searchText.toLowerCase()) ) { return true } else {hiddenBySearchCount++; return false}" @endif>
+                <input
+                    {!! $isDisabled ? 'disabled' : null !!}
+                    wire:loading.attr="disabled"
+                    type="checkbox"
+                    value="{{ $optionValue }}"
+                    dusk="filament.forms.{{ $getStatePath() }}"
+                    @if ($isBulkToggleable()) x-on:change="updateIsAllSelected" @endif
+                    {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
+                    {{ $getExtraAttributeBag()->class([
+                        'text-primary-600 transition duration-75 rounded shadow-sm focus:border-primary-500 focus:ring-2 focus:ring-primary-500 disabled:opacity-70',
+                        'dark:bg-gray-700 dark:checked:bg-primary-500' => config('forms.dark_mode'),
+                        'border-gray-300' => !$errors->has($getStatePath()),
+                        'dark:border-gray-600' => !$errors->has($getStatePath()) && config('forms.dark_mode'),
+                        'border-danger-300 ring-danger-500' => $errors->has($getStatePath()),
+                        'dark:border-danger-400 dark:ring-danger-400' => $errors->has($getStatePath()) && config('forms.dark_mode')
+                        ]) }}
+                />
 
-        <span @class([
-            'text-sm font-medium text-gray-700',
-            'dark:text-gray-200' => config('forms.dark_mode'),
-        ])>
-          {{ $optionLabel }}
-        </span>
-      </label>
-    @endforeach
+                <span @class([
+                    'text-sm font-medium text-gray-700',
+                    'dark:text-gray-200' => config('forms.dark_mode'),
+                ])>
+                    {{ $optionLabel }}
+                </span>
+            </label>
+        @endforeach
 
-  </x-filament-support::grid>
+    </x-filament-support::grid>
 
-  @if ($isSearchable())
-    <div
-      x-cloak
-      x-show="hiddenBySearchCount === checkboxListItems.length"
-      class="filament-forms-checkbox-list-component-no-results">{{ $getNoSearchResultsMessage() }}</div>
-  @endif
+    @if ($isSearchable())
+        <div
+            x-cloak
+            x-show="hiddenBySearchCount === checkboxListItems.length"
+            class="filament-forms-checkbox-list-component-no-results">{{ $getNoSearchResultsMessage() }}</div>
+    @endif
 
-  @if ($isBulkToggleable() || $isSearchable())
-    </div>
-  @endif
+    @if ($isBulkToggleable() || $isSearchable())
+        </div>
+    @endif
 </x-dynamic-component>
