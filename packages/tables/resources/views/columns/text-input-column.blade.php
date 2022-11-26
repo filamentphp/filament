454dadf8a6b01514950dff753989a4e1a9854383
Zep Fietje <hey@zepfietje.com>
2022-11-26 15:41:32 +0100

    Support text input column align start and end

--- packages/tables/resources/views/columns/text-input-column.blade.php
+++ packages/tables/resources/views/columns/text-input-column.blade.php
@@ -30,8 +30,10 @@
                     'ml-0.5 text-gray-900 inline-block transition duration-75 rounded-lg shadow-sm focus:ring-primary-500 focus:ring-1 focus:ring-inset focus:border-primary-500 disabled:opacity-70 dark:bg-gray-700 dark:text-white dark:focus:border-primary-500',
                     match ($getAlignment()) {
                         'center' => 'text-center',
+                        'end' => 'text-end',
+                        'left' => 'text-left',
                         'right' => 'text-right',
-                        default => 'text-left',
+                        'start', null => 'text-start',
                     },
                 ])
         }}
