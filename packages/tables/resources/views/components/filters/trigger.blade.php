04d1c118202206a79b503f0af5496dae990e2a5b
Adam Weston <adam@awcodes.com>
2022-11-26 09:40:39 -0500

    change icon color for table filters and toggleable

--- packages/tables/resources/views/components/filters/trigger.blade.php
+++ packages/tables/resources/views/components/filters/trigger.blade.php
@@ -4,6 +4,7 @@
 
 <x-filament::icon-button
     icon="heroicon-o-funnel"
+    color="gray"
     :label="__('filament-tables::table.buttons.filter.label')"
     :indicator="$indicatorsCount"
     {{ $attributes->class(['filament-tables-filters-trigger']) }}
--- packages/tables/resources/views/components/toggleable/trigger.blade.php
+++ packages/tables/resources/views/components/toggleable/trigger.blade.php
@@ -1,5 +1,6 @@
 <x-filament::icon-button
     icon="heroicon-o-view-columns"
+    color="gray"
     :label="__('filament-tables::table.buttons.toggle_columns.label')"
     {{ $attributes->class(['filament-tables-column-toggling-trigger']) }}
 />
