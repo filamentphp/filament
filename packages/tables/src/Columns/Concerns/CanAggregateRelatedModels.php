0ff92f3cfe6df3d1f1af803ec125a2fd2bfc3f06
Hugh Messenger <hugh.messenger@gmail.com>
2022-11-28 19:37:45 -0600

    Adding hasAggregate() function to CanAggregateRelatedModels

--- packages/tables/src/Columns/Concerns/CanAggregateRelatedModels.php
+++ packages/tables/src/Columns/Concerns/CanAggregateRelatedModels.php
@@ -121,4 +121,10 @@ trait CanAggregateRelatedModels
     {
         return $this->evaluate($this->relationshipToSum);
     }
+
+    public function hasAggregate(): bool
+    {
+        return $this->relationshipToCount || $this->relationshipToAvg ||
+            $this->relationshipToMax || $this->relationshipToMin || $this->relationshipToSum;
+    }
 }
