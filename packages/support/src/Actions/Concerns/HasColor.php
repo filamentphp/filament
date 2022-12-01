3676e8f5ac79a413cf6e578e07d41d01c7dc2ade
Dennis Koch <info@pixelarbeit.de>
2022-12-01 22:31:46 +0100

    revert #5132 additional color functions

--- packages/support/src/Actions/Concerns/HasColor.php
+++ packages/support/src/Actions/Concerns/HasColor.php
@@ -15,41 +15,6 @@ trait HasColor
         return $this;
     }
 
-    public function colorDanger(): static
-    {
-        $this->color('danger');
-
-        return $this;
-    }
-
-    public function colorPrimary(): static
-    {
-        $this->color('primary');
-
-        return $this;
-    }
-
-    public function colorSecondary(): static
-    {
-        $this->color('secondary');
-
-        return $this;
-    }
-
-    public function colorSuccess(): static
-    {
-        $this->color('success');
-
-        return $this;
-    }
-
-    public function colorWarning(): static
-    {
-        $this->color('warning');
-
-        return $this;
-    }
-
     public function getColor(): ?string
     {
         return $this->evaluate($this->color);
