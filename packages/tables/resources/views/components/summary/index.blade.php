f4a6e40d05278b64e95299434481428470bcd3d9
Dan Harrin <git@danharrin.com>
2022-12-02 11:05:57 +0000

    Update index.blade.php

--- packages/tables/resources/views/components/summary/index.blade.php
+++ packages/tables/resources/views/components/summary/index.blade.php
@@ -39,7 +39,7 @@
         @foreach ($columns as $column)
             @if ($placeholderColumns || $column->hasSummary())
                 <td {{ $column->getExtraHeaderAttributeBag()->class([
-                    'text-center px-4 py-2 font-medium text-sm text-gray-600 dark:text-gray-300',
+                    'px-4 py-2 font-medium text-sm text-gray-600 dark:text-gray-300',
                     'whitespace-nowrap' => ! $column->isHeaderWrapped(),
                     'whitespace-normal' => $column->isHeaderWrapped(),
                 ]) }}>
