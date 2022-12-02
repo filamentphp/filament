de65bbb301e3d32ea55534739db06a278a931587
Dan Harrin <git@danharrin.com>
2022-12-02 11:00:45 +0000

    Update packages/tables/resources/views/components/summary/index.blade.php
    Co-authored-by: Zep Fietje <hey@zepfietje.com>

--- packages/tables/resources/views/components/summary/index.blade.php
+++ packages/tables/resources/views/components/summary/index.blade.php
@@ -39,11 +39,10 @@
         @foreach ($columns as $column)
             @if ($placeholderColumns || $column->hasSummary())
                 <td @class([
-                        'px-4 py-2 font-medium text-sm text-gray-600 dark:text-gray-300',
-                        'whitespace-nowrap' => ! $column->isHeaderWrapped(),
-                        'whitespace-normal' => $column->isHeaderWrapped()
-                    ])
-                >
+                    'px-4 py-2 font-medium text-sm text-gray-600 dark:text-gray-300',
+                    'whitespace-nowrap' => ! $column->isHeaderWrapped(),
+                    'whitespace-normal' => $column->isHeaderWrapped()
+                ])>
                     @if ($loop->first && (! $extraHeadingColumn))
                         <span class="text-base">
                             {{ __('filament-tables::table.summary.heading', ['label' => $pluralModelLabel]) }}
