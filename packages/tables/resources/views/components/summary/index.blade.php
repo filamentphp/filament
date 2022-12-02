5830a82dd6ff8d201744ba68ee31e155c6ea0a77
Dan Harrin <git@danharrin.com>
2022-12-02 11:03:03 +0000

    Update index.blade.php

--- packages/tables/resources/views/components/summary/index.blade.php
+++ packages/tables/resources/views/components/summary/index.blade.php
@@ -42,8 +42,7 @@
                     'text-center px-4 py-2 font-medium text-sm text-gray-600 dark:text-gray-300',
                     'whitespace-nowrap' => ! $column->isHeaderWrapped(),
                     'whitespace-normal' => $column->isHeaderWrapped(),
-                ]) }}
-                >
+                ]) }}>
                     @if ($loop->first && (! $extraHeadingColumn))
                         <span class="text-base">
                             {{ __('filament-tables::table.summary.heading', ['label' => $pluralModelLabel]) }}
