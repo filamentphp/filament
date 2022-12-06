f7cb32405d1045b5a9ab8c7131d6b65ee8383113
Dan Harrin <git@danharrin.com>
2022-12-06 15:33:44 +0000

    Update MakeTableCommand.php

--- packages/tables/src/Commands/MakeTableCommand.php
+++ packages/tables/src/Commands/MakeTableCommand.php
@@ -55,7 +55,7 @@ class MakeTableCommand extends Command
         $viewPath = resource_path(
             (string) str($view)
                 ->replace('.', '/')
-                ->prepend('views')
+                ->prepend('views/')
                 ->append('.blade.php'),
         );
 
