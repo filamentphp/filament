500ad34589f9bce3f587514a6270fd183c89c8fe
stealeks <aleksandrs.stepanovs@eleving.com>
2022-12-22 12:05:43 +0200

    Tabs Component now have persistTabInQueryString(). Will store active tab browser's query string and will read browser's query string to open tab on page load. Same as in Wizard Component

--- packages/forms/src/Components/Tabs.php
+++ packages/forms/src/Components/Tabs.php
@@ -13,6 +13,8 @@ class Tabs extends Component
 
     public int | Closure $activeTab = 1;
 
+    protected string | Closure | null $tabQueryStringKey = null;
+
     final public function __construct(string $label)
     {
         $this->label($label);
@@ -42,6 +44,35 @@ class Tabs extends Component
 
     public function getActiveTab(): int
     {
+        if ($this->isTabPersistedInQueryString()) {
+            $queryStringStep = request()->query($this->getTabQueryStringKey());
+
+            foreach ($this->getChildComponents() as $index => $step) {
+                if ($step->getId() !== $queryStringStep) {
+                    continue;
+                }
+
+                return $index + 1;
+            }
+        }
+
         return $this->evaluate($this->activeTab);
+    }
+
+    public function getTabQueryStringKey(): ?string
+    {
+        return $this->evaluate($this->tabQueryStringKey);
+    }
+
+    public function isTabPersistedInQueryString(): bool
+    {
+        return filled($this->getTabQueryStringKey());
+    }
+
+    public function persistTabInQueryString(string | Closure | null $key = 'tab'): static
+    {
+        $this->tabQueryStringKey = $key;
+
+        return $this;
     }
 }
