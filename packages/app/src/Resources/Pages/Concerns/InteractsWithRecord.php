73197a7fe911ad6e01e434d3cf3ce012e3a8058f
Dan Harrin <git@danharrin.com>
2022-12-31 10:41:08 +0600

    Update InteractsWithRecord.php

--- packages/app/src/Resources/Pages/Concerns/InteractsWithRecord.php
+++ packages/app/src/Resources/Pages/Concerns/InteractsWithRecord.php
@@ -5,6 +5,7 @@ namespace Filament\Resources\Pages\Concerns;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Str;
+use Throwable;
 
 trait InteractsWithRecord
 {
@@ -53,11 +54,11 @@ trait InteractsWithRecord
         if ($record->exists && $resource::hasRecordTitle()) {
             if ($resource::hasPage('view') && $resource::canView($record)) {
                 $breadcrumbs[
-                $resource::getUrl('view', ['record' => $record])
+                    $resource::getUrl('view', ['record' => $record])
                 ] = $this->getRecordTitle();
             } elseif ($resource::hasPage('edit') && $resource::canEdit($record)) {
                 $breadcrumbs[
-                $resource::getUrl('edit', ['record' => $record])
+                    $resource::getUrl('edit', ['record' => $record])
                 ] = $this->getRecordTitle();
             } else {
                 $breadcrumbs[] = $this->getRecordTitle();
