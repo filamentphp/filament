cf831edccbce82a183b7361a9560bab585d57d90
Dan Harrin <git@danharrin.com>
2023-01-07 18:59:43 +0600

    wip

--- packages/actions/src/Concerns/CanReplicateRecords.php
+++ packages/actions/src/Concerns/CanReplicateRecords.php
@@ -11,12 +11,14 @@ trait CanReplicateRecords
 
     protected ?Closure $afterReplicaSavedCallback = null;
 
-    protected ?Closure $beforeReplicaSavedCallback = null;
+    protected ?Closure $beforeReplicaSaved = null;
 
     /**
      * @var array<string> | Closure | null
      */
     protected array | Closure | null $excludedAttributes = null;
+
+    protected ?Model $replica = null;
 
     public static function getDefaultName(): ?string
     {
@@ -39,13 +41,11 @@ trait CanReplicateRecords
 
         $this->action(function () {
             $result = $this->process(function (Model $record) {
-                $replica = $record->replicate($this->getExcludedAttributes());
+                $this->replica = $record->replicate($this->getExcludedAttributes());
 
-                $this->callBeforeReplicaSaved($replica);
+                $this->callBeforeReplicaSaved();
 
-                $replica->save();
-
-                return $this->callAfterReplicaSaved($replica);
+                $this->replica->save();
             });
 
             try {
@@ -58,14 +58,17 @@ trait CanReplicateRecords
 
     public function beforeReplicaSaved(Closure $callback): static
     {
-        $this->beforeReplicaSavedCallback = $callback;
+        $this->beforeReplicaSaved = $callback;
 
         return $this;
     }
 
+    /**
+     * @deprecated Use `after()` instead.
+     */
     public function afterReplicaSaved(Closure $callback): static
     {
-        $this->afterReplicaSavedCallback = $callback;
+        $this->after($callback);
 
         return $this;
     }
@@ -80,18 +83,9 @@ trait CanReplicateRecords
         return $this;
     }
 
-    public function callBeforeReplicaSaved(Model $replica): void
+    public function callBeforeReplicaSaved(): void
     {
-        $this->evaluate($this->beforeReplicaSavedCallback, [
-            'replica' => $replica,
-        ]);
-    }
-
-    public function callAfterReplicaSaved(Model $replica): mixed
-    {
-        return $this->evaluate($this->afterReplicaSavedCallback, [
-            'replica' => $replica,
-        ]);
+        $this->evaluate($this->beforeReplicaSaved);
     }
 
     /**
@@ -100,5 +94,20 @@ trait CanReplicateRecords
     public function getExcludedAttributes(): ?array
     {
         return $this->evaluate($this->excludedAttributes);
+    }
+
+    public function getReplica(): ?Model
+    {
+        return $this->replica;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    protected function getDefaultEvaluationParameters(): array
+    {
+        return array_merge(parent::getDefaultEvaluationParameters(), [
+            'replica' => $this->getReplica(),
+        ]);
     }
 }
