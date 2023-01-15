e396da1baa4a303bac7c6a2f91a20abe20d3188c
Beej <howdu@users.noreply.github.com>
2023-01-15 14:20:36 +0000

    Builder Collapse add click to header
    Builder header clickable so that it works the same as a collapsible Repeater

--- packages/forms/resources/views/components/builder.blade.php
+++ packages/forms/resources/views/components/builder.blade.php
@@ -94,13 +94,18 @@
                         ])
                     >
                         @if ((! $isItemMovementDisabled) || $hasBlockLabels || (! $isItemDeletionDisabled) || $isCollapsible || $isCloneable)
-                            <header @class([
-                                'flex items-center h-10 overflow-hidden border-b bg-gray-50 rounded-t-xl',
-                                'dark:bg-gray-800 dark:border-gray-700' => config('forms.dark_mode'),
-                            ])>
+                            <header
+                                @if ($isCollapsible) x-on:click.stop="isCollapsed = ! isCollapsed" @endif
+                                @class([
+                                    'flex items-center h-10 overflow-hidden border-b bg-gray-50 rounded-t-xl',
+                                    'dark:bg-gray-800 dark:border-gray-700' => config('forms.dark_mode'),
+                                    'cursor-pointer' => $isCollapsible,
+                                ])
+                            >
                                 @unless ($isItemMovementDisabled)
                                     <button
                                         title="{{ __('forms::components.builder.buttons.move_item.label') }}"
+                                        x-on:click.stop
                                         wire:sortable.handle
                                         wire:keydown.prevent.arrow-up="dispatchFormEvent('builder::moveItemUp', '{{ $getStatePath() }}', '{{ $uuid }}')"
                                         wire:keydown.prevent.arrow-down="dispatchFormEvent('builder::moveItemDown', '{{ $getStatePath() }}', '{{ $uuid }}')"
@@ -151,7 +156,7 @@
                                         <li>
                                             <button
                                                 title="{{ __('forms::components.builder.buttons.clone_item.label') }}"
-                                                wire:click="dispatchFormEvent('builder::cloneItem', '{{ $getStatePath() }}', '{{ $uuid }}')"
+                                                wire:click.stop="dispatchFormEvent('builder::cloneItem', '{{ $getStatePath() }}', '{{ $uuid }}')"
                                                 type="button"
                                                 @class([
                                                     'flex items-center justify-center flex-none w-10 h-10 text-gray-400 transition hover:text-gray-500',
@@ -171,7 +176,7 @@
                                         <li>
                                             <button
                                                 title="{{ __('forms::components.builder.buttons.delete_item.label') }}"
-                                                wire:click="dispatchFormEvent('builder::deleteItem', '{{ $getStatePath() }}', '{{ $uuid }}')"
+                                                wire:click.stop="dispatchFormEvent('builder::deleteItem', '{{ $getStatePath() }}', '{{ $uuid }}')"
                                                 type="button"
                                                 @class([
                                                     'flex items-center justify-center flex-none w-10 h-10 text-danger-600 transition hover:text-danger-500',
@@ -191,7 +196,7 @@
                                         <li>
                                             <button
                                                 x-bind:title="(! isCollapsed) ? '{{ __('forms::components.builder.buttons.collapse_item.label') }}' : '{{ __('forms::components.builder.buttons.expand_item.label') }}'"
-                                                x-on:click="isCollapsed = ! isCollapsed"
+                                                x-on:click.stop="isCollapsed = ! isCollapsed"
                                                 type="button"
                                                 class="flex items-center justify-center flex-none w-10 h-10 text-gray-400 transition hover:text-gray-500"
                                             >
