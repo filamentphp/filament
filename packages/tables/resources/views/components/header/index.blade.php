53bc91211a53793fb500e8dcb2a138052e0c270f
Zep Fietje <hey@zepfietje.com>
2023-01-23 11:44:12 +0100

    Fix showing table header children and spacing

--- packages/tables/resources/views/components/header/index.blade.php
+++ packages/tables/resources/views/components/header/index.blade.php
@@ -6,43 +6,45 @@
     'actions' => [],
     'actionsPosition' => ActionsPosition::End,
     'description' => null,
-    'heading',
+    'heading' => null,
 ])
 
 <div {{ $attributes->class(['filament-tables-header px-4 py-2']) }}>
-    <div class="flex flex-col gap-4 md:justify-between md:items-start md:flex-row">
-        <div>
-            @if ($heading)
-                <x-filament-tables::header.heading>
-                    {{ $heading }}
-                </x-filament-tables::header.heading>
-            @endif
+    <div class="flex flex-col gap-4 md:flex-row md:items-start">
+        @if ($heading || $description || $actionsPosition === ActionsPosition::Start)
+            <div>
+                @if ($heading)
+                    <x-filament-tables::header.heading>
+                        {{ $heading }}
+                    </x-filament-tables::header.heading>
+                @endif
 
-            @if ($description)
-                <x-filament-tables::header.description>
-                    {{ $description }}
-                </x-filament-tables::header.description>
-            @endif
+                @if ($description)
+                    <x-filament-tables::header.description>
+                        {{ $description }}
+                    </x-filament-tables::header.description>
+                @endif
 
-            @if ($actionsPosition === ActionsPosition::Start)
-                <x-filament-tables::actions
-                    :actions="$actions"
-                    alignment="start"
-                    wrap
-                    @class([
-                        'md:-ml-2' => ! ($heading || $description),
-                        'mt-2' => $heading || $description,
-                    ])
-                />
-            @endif
-        </div>
+                @if ($actionsPosition === ActionsPosition::Start)
+                    <x-filament-tables::actions
+                        :actions="$actions"
+                        alignment="start"
+                        wrap
+                        @class([
+                            'md:-ml-2' => ! ($heading || $description),
+                            'mt-2' => $heading || $description,
+                        ])
+                    />
+                @endif
+            </div>
+        @endif
 
         @if ($actionsPosition === ActionsPosition::End)
             <x-filament-tables::actions
                 :actions="$actions"
                 alignment="end"
                 wrap
-                class="shrink-0 md:-mr-2"
+                class="ml-auto shrink-0 md:-mr-2"
             />
         @endif
     </div>
