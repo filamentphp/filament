fcbb8860eb258b05a14ff120b007341edf68807e
danharrin <danharrin@users.noreply.github.com>
2023-01-26 12:42:43 +0000

    chore: fix code style

--- packages/tables/src/Filters/Concerns/HasRelationship.php
+++ packages/tables/src/Filters/Concerns/HasRelationship.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Str;
 
