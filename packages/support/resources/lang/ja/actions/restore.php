d5b70dba795680a691ba9de57177863c8edbb121
Hiroki Shibahara <shibomb@gmail.com>
2023-01-29 12:04:35 +0900

    Add missing Japanese translations for support

---
+++ packages/support/resources/lang/ja/actions/restore.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'リストア',
+
+        'modal' => [
+
+            'heading' => ':labelリストア',
+
+            'actions' => [
+
+                'restore' => [
+                    'label' => 'リストア',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'restored' => 'リストアしました',
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => '選択のリストア',
+
+        'modal' => [
+
+            'heading' => '選択した:labelをリストア',
+
+            'actions' => [
+
+                'restore' => [
+                    'label' => 'リストア',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'restored' => 'リストアしました',
+        ],
+
+    ],
+
+];
