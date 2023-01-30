4ec40786552369323b811d1caaf99d659a487fe8
Kevin McKee <mckee.kevin@gmail.com>
2023-01-30 14:04:48 -0600

    Add filter loading indicator

--- packages/widgets/resources/views/chart-widget.blade.php
+++ packages/widgets/resources/views/chart-widget.blade.php
@@ -23,17 +23,31 @@
                 </x-filament::card.header>
 
                 @if ($filters)
-                    <select
-                        class="text-gray-900 border-gray-300 block h-10 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 dark:focus:border-primary-500"
-                        wire:model="filter"
-                        wire:loading.class="animate-pulse"
-                    >
-                        @foreach ($filters as $value => $label)
-                            <option value="{{ $value }}">
-                                {{ $label }}
-                            </option>
-                        @endforeach
-                    </select>
+                    <div class="flex items-center space-x-3">
+                        @if ($showFilterLoadingIndicator)
+                            <svg
+                                wire:loading
+                                wire:target="filter"
+                                class="animate-spin h-8 w-8 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                        stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor"
+                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
+                        @endif
+                        <select
+                            class="text-gray-900 border-gray-300 block h-10 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 dark:focus:border-primary-500"
+                            wire:model="filter"
+                            wire:loading.class="animate-pulse"
+                        >
+                            @foreach ($filters as $value => $label)
+                                <option value="{{ $value }}">
+                                    {{ $label }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
                 @endif
             </div>
         @endif
