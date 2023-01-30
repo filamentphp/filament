dfd4dde63213bdea728a5d9a1ddad6b56e31dfed
Zep Fietje <hey@zepfietje.com>
2023-01-30 09:16:16 +0100

    Fix getting action key bindings

--- packages/actions/resources/views/components/action.blade.php
+++ packages/actions/resources/views/components/action.blade.php
@@ -19,7 +19,7 @@
     :target="($url && $action->shouldOpenUrlInNewTab()) ? '_blank' : null"
     :type="($action instanceof \Filament\Actions\Contracts\SubmitsForm && $action->canSubmitForm()) ? 'submit' : 'button'"
     :color="$action->getColor()"
-    :key-bindings="$action->getKeybindings()"
+    :key-bindings="$action->getKeyBindings()"
     :tooltip="$action->getTooltip()"
     :disabled="$isDisabled"
     :icon="$icon ?? $action->getIcon()"
