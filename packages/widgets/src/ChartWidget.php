9b74cf3195d3a8b811b849dfe0b2cc8aff84e3e5
Kevin McKee <mckee.kevin@gmail.com>
2023-01-30 13:59:04 -0600

    Add filter loading indicator

--- packages/widgets/src/ChartWidget.php
+++ packages/widgets/src/ChartWidget.php
@@ -14,6 +14,8 @@ abstract class ChartWidget extends Widget
     protected ?array $cachedData = null;
 
     public string $dataChecksum;
+    
+    public bool $showFilterLoadingIndicator = true;
 
     public ?string $filter = null;
 
