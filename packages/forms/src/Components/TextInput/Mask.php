5b9f241c5631282ce65e84bc511742255c9c2e74
Marga Rizaldi <margarizaldi@gmail.com>
2023-02-06 21:13:11 +0700

    Add decimal separator parameter

--- packages/forms/src/Components/TextInput/Mask.php
+++ packages/forms/src/Components/TextInput/Mask.php
@@ -141,13 +141,14 @@ class Mask implements Jsonable
         return $this;
     }
 
-    public function money(string $prefix = '$', string $thousandsSeparator = ',', int $decimalPlaces = 2, bool $isSigned = true): static
+    public function money(string $prefix = '$', string $thousandsSeparator = ',', string $decimalSeparator = '.', int $decimalPlaces = 2, bool $isSigned = true): static
     {
         $this
             ->patternBlocks([
                 'money' => fn (Mask $mask) => $mask
                     ->numeric()
                     ->thousandsSeparator($thousandsSeparator)
+                    ->decimalSeparator($decimalSeparator)
                     ->decimalPlaces($decimalPlaces)
                     ->signed($isSigned)
                     ->padFractionalZeros()
