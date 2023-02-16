0406e1e05e091f47aefef72ecea16aaabaf56b61
Zep Fietje <hey@zepfietje.com>
2023-02-16 09:40:44 +0100

    Remove outline-none from div

--- packages/forms/resources/views/components/tags-input.blade.php
+++ packages/forms/resources/views/components/tags-input.blade.php
@@ -26,7 +26,7 @@
         <div
             x-show="state.length || @js(! $isDisabled)"
             @class([
-                'block w-full transition duration-75 divide-y rounded-lg shadow-sm sm:text-sm border overflow-hidden outline-none focus-within:border-primary-500 focus-within:ring-1 focus-within:ring-primary-500 dark:divide-gray-600',
+                'block w-full transition duration-75 divide-y rounded-lg shadow-sm sm:text-sm border overflow-hidden focus-within:border-primary-500 focus-within:ring-1 focus-within:ring-primary-500 dark:divide-gray-600',
                 'border-gray-300 dark:border-gray-600' => ! $errors->has($statePath),
                 'border-danger-600 ring-1 ring-inset ring-danger-600 dark:border-danger-400 dark:ring-danger-400' => $errors->has($statePath),
             ])
