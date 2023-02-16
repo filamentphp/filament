0713cfefe5cd410aefeacb912d4f0e3cd8d57b25
Zep Fietje <hey@zepfietje.com>
2023-02-16 10:05:45 +0100

    Remove duplicate outline-none from checkbox column

--- packages/tables/resources/views/columns/checkbox-column.blade.php
+++ packages/tables/resources/views/columns/checkbox-column.blade.php
@@ -22,7 +22,7 @@
         {{
             $attributes
                 ->merge($getExtraInputAttributes(), escape: false)
-                ->class(['ml-4 text-primary-600 transition duration-75 rounded shadow-sm outline-none text-sm outline-none focus:border-primary-500 focus:ring-2 focus:ring-primary-500 disabled:opacity-70 dark:bg-gray-700 dark:checked:bg-primary-500'])
+                ->class(['ml-4 text-primary-600 transition duration-75 rounded shadow-sm text-sm outline-none focus:border-primary-500 focus:ring-2 focus:ring-primary-500 disabled:opacity-70 dark:bg-gray-700 dark:checked:bg-primary-500'])
         }}
         x-bind:class="{
             'opacity-70 pointer-events-none': isLoading,
