026ae5af42cd463d31f8df0d97dc2784f5698f24
Lukas Frey <mail@lukasfrey.cz>
2023-02-16 18:02:48 +0100

    chore(lang): fixed table pagination string
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

--- packages/tables/resources/lang/cs/table.php
+++ packages/tables/resources/lang/cs/table.php
@@ -35,7 +35,7 @@ return [
 
         'label' => 'Stránkování',
 
-        'overview' => 'Zobrazeno od :first do :last z :total záznamů',
+        'overview' => '{1} Zobrazuji 1 výsledek|[2,*] Zobrazuji :first až :last z :total výsledků',
 
         'fields' => [
 
