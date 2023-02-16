dc423cc646063bdeb4a63cf667e82f4cea90ee74
Dan Harrin <git@danharrin.com>
2023-02-16 14:09:40 +0000

    Update script.js

--- docs-assets/screenshots/script.js
+++ docs-assets/screenshots/script.js
@@ -38,7 +38,7 @@ for (const [file, options] of Object.entries(schema)) {
 configure()
 
 function configure(php = null) {
-    fs.writeFileSync('../app/Providers/Filament/configure.php', `
+    fs.writeFileSync('../app/app/Providers/Filament/configure.php', `
         <?php
 
         use Filament\\Context;
