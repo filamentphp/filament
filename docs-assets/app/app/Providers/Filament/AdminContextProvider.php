def708878c10d43a905afe2c2bc38ea6781abadf
Dan Harrin <git@danharrin.com>
2023-02-18 19:32:05 +0000

    Update AdminContextProvider.php

--- docs-assets/app/app/Providers/Filament/AdminContextProvider.php
+++ docs-assets/app/app/Providers/Filament/AdminContextProvider.php
@@ -48,8 +48,8 @@ class AdminContextProvider extends ContextProvider
                 ShareErrorsFromSession::class,
                 VerifyCsrfToken::class,
                 SubstituteBindings::class,
-                DispatchServingFilamentEvent::class,
                 DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
             ])
             ->authMiddleware([
                 Authenticate::class,
