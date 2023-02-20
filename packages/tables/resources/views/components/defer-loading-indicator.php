57e40f2b7e1dd960453f8e2fab316377241cb27b
Kenneth Sese <ksese@hope4tomorrow.net>
2023-02-20 08:04:30 -0600

    Update packages/tables/resources/views/components/defer-loading-indicator.php
    Co-authored-by: Zep Fietje <hey@zepfietje.com>

--- packages/tables/resources/views/components/defer-loading-indicator.php
+++ packages/tables/resources/views/components/defer-loading-indicator.php
@@ -6,8 +6,6 @@
         'flex items-center justify-center w-16 h-16 text-primary-500 rounded-full bg-primary-50',
         'dark:bg-gray-700' => config('tables.dark_mode'),
     ])>
-        <x-filament-support::loading-indicator
-            class="w-6 h-6"
-        />
+        <x-filament-support::loading-indicator class="w-6 h-6" />
     </div>                
 </div>
