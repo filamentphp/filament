426e03a5607b14d5515e97336790a375d377a346
Dan Harrin <git@danharrin.com>
2023-03-01 22:43:12 +0000

    fix: Nested repeater actions

--- packages/forms/src/Components/Concerns/Cloneable.php
+++ packages/forms/src/Components/Concerns/Cloneable.php
@@ -4,8 +4,18 @@ namespace Filament\Forms\Components\Concerns;
 
 trait Cloneable
 {
+    public function cloneChildComponents(): static
+    {
+        $components = $this->getChildComponentContainer()->getClone()->getComponents();
+
+        return $this->childComponents($components);
+    }
+
     public function getClone(): static
     {
-        return clone $this;
+        $clone = clone $this;
+        $clone->cloneChildComponents();
+
+        return $clone;
     }
 }
