1c453bcffa11f2fc36ea4ac564943361672db52e
Dan Harrin <git@danharrin.com>
2023-03-12 10:32:13 +0000

    Update sticky styling

--- packages/admin/resources/views/components/form/actions.blade.php
+++ packages/admin/resources/views/components/form/actions.blade.php
@@ -6,24 +6,22 @@
 <div
     @if (config('filament.layout.forms.actions.are_sticky'))
         x-data="{
-        
-            areSticky: false,
-            
+
+            isSticky: false,
+
             evaluatePageScrollPosition: function() {
                 let documentHeight = document.body.scrollHeight
                 let currentScroll = window.scrollY + window.innerHeight
-                
-                this.areSticky = (currentScroll + 200) <= documentHeight
+
+                this.isSticky = (currentScroll + window.innerHeight) <= documentHeight
             },
-            
+
         }"
         x-init="evaluatePageScrollPosition"
         x-on:scroll.window="evaluatePageScrollPosition"
-        class="transform"
         x-bind:class="{
-            'sticky left-0 bottom-0 mx-auto max-w-2xl -translate-y-4 rounded-md border border-gray-300 bg-white p-3 shadow-3xl transition-colors transition-shadow transition-transform': areSticky,
-            'dark:border-gray-700 dark:bg-gray-800 dark:shadow-none': @js(config('filament.dark_mode')) && areSticky,
-            'translate-y-0': ! areSticky
+            'filament-form-actions-sticky-panel sticky bottom-0 -mx-4 transform -translate-y-4 bg-white p-4 shadow-lg transition ring-1 ring-black/5 md:rounded-xl': isSticky,
+            'dark:bg-gray-800': @js(config('filament.dark_mode')) && isSticky,
         }"
     @endif
 >
