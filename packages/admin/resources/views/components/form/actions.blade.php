25f380563520757c83cfb6eed514248e41bcf553
Dan Harrin <git@danharrin.com>
2023-03-12 10:10:03 +0000

    Update packages/admin/resources/views/components/form/actions.blade.php

--- packages/admin/resources/views/components/form/actions.blade.php
+++ packages/admin/resources/views/components/form/actions.blade.php
@@ -6,25 +6,24 @@
 <div
     @if (config('filament.layout.forms.actions.are_sticky'))
         x-data="{
-            sticky: false,
-            scrollPage() {
-                let documentHeight = document.body.scrollHeight;
-                let currentScroll = window.scrollY + window.innerHeight;
-                let modifier = 200;
-                if (currentScroll + modifier > documentHeight) {
-                    this.sticky = false
-                } else {
-                    this.sticky = true
-                }
-            }
+        
+            areSticky: false,
+            
+            evaluatePageScrollPosition: function() {
+                let documentHeight = document.body.scrollHeight
+                let currentScroll = window.scrollY + window.innerHeight
+                
+                this.areSticky = (currentScroll + 200) <= documentHeight
+            },
+            
         }"
-        x-on:scroll.window="scrollPage"
-        x-init="scrollPage"
+        x-init="evaluatePageScrollPosition"
+        x-on:scroll.window="evaluatePageScrollPosition"
         class="transform"
         x-bind:class="{
-            'sticky left-0 bottom-0 mx-auto max-w-2xl -translate-y-4 rounded-md border border-gray-300 bg-white p-3 shadow-3xl transition-colors transition-shadow transition-transform': sticky,
-            'dark:border-gray-700 dark:bg-gray-800 dark:shadow-none': !!{{ config('filament.dark_mode') }} && sticky,
-            'translate-y-0': !sticky
+            'sticky left-0 bottom-0 mx-auto max-w-2xl -translate-y-4 rounded-md border border-gray-300 bg-white p-3 shadow-3xl transition-colors transition-shadow transition-transform': areSticky,
+            'dark:border-gray-700 dark:bg-gray-800 dark:shadow-none': @js(config('filament.dark_mode')) && areSticky,
+            'translate-y-0': ! areSticky
         }"
     @endif
 >
