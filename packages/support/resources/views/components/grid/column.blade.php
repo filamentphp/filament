e3373b2ea2d9e913ff387ba9fd8c73f301089144
Dan Harrin <git@danharrin.com>
2023-03-17 22:09:18 +0000

    Reduce class repetition

--- packages/support/resources/views/components/grid/column.blade.php
+++ packages/support/resources/views/components/grid/column.blade.php
@@ -14,194 +14,46 @@
     'hidden' => false,
 ])
 
-<div {{ $attributes->class([
-    'hidden' => $hidden,
-    ($default && (! $hidden)) ? match ($default) {
-        1 => 'col-span-1',
-        2 => 'col-span-2',
-        3 => 'col-span-3',
-        4 => 'col-span-4',
-        5 => 'col-span-5',
-        6 => 'col-span-6',
-        7 => 'col-span-7',
-        8 => 'col-span-8',
-        9 => 'col-span-9',
-        10 => 'col-span-10',
-        11 => 'col-span-11',
-        12 => 'col-span-12',
-        'full' => 'col-span-full',
-        default => $default,
-    } : null,
-    ($sm && (! $hidden)) ? match ($sm) {
-        1 => 'sm:col-span-1',
-        2 => 'sm:col-span-2',
-        3 => 'sm:col-span-3',
-        4 => 'sm:col-span-4',
-        5 => 'sm:col-span-5',
-        6 => 'sm:col-span-6',
-        7 => 'sm:col-span-7',
-        8 => 'sm:col-span-8',
-        9 => 'sm:col-span-9',
-        10 => 'sm:col-span-10',
-        11 => 'sm:col-span-11',
-        12 => 'sm:col-span-12',
-        'full' => 'sm:col-span-full',
-        default => $sm,
-    } : null,
-    ($md && (! $hidden)) ? match ($md) {
-        1 => 'md:col-span-1',
-        2 => 'md:col-span-2',
-        3 => 'md:col-span-3',
-        4 => 'md:col-span-4',
-        5 => 'md:col-span-5',
-        6 => 'md:col-span-6',
-        7 => 'md:col-span-7',
-        8 => 'md:col-span-8',
-        9 => 'md:col-span-9',
-        10 => 'md:col-span-10',
-        11 => 'md:col-span-11',
-        12 => 'md:col-span-12',
-        'full' => 'md:col-span-full',
-        default => $md,
-    } : null,
-    ($lg && (! $hidden)) ? match ($lg) {
-        1 => 'lg:col-span-1',
-        2 => 'lg:col-span-2',
-        3 => 'lg:col-span-3',
-        4 => 'lg:col-span-4',
-        5 => 'lg:col-span-5',
-        6 => 'lg:col-span-6',
-        7 => 'lg:col-span-7',
-        8 => 'lg:col-span-8',
-        9 => 'lg:col-span-9',
-        10 => 'lg:col-span-10',
-        11 => 'lg:col-span-11',
-        12 => 'lg:col-span-12',
-        'full' => 'lg:col-span-full',
-        default => $lg,
-    } : null,
-    ($xl && (! $hidden)) ? match ($xl) {
-        1 => 'xl:col-span-1',
-        2 => 'xl:col-span-2',
-        3 => 'xl:col-span-3',
-        4 => 'xl:col-span-4',
-        5 => 'xl:col-span-5',
-        6 => 'xl:col-span-6',
-        7 => 'xl:col-span-7',
-        8 => 'xl:col-span-8',
-        9 => 'xl:col-span-9',
-        10 => 'xl:col-span-10',
-        11 => 'xl:col-span-11',
-        12 => 'xl:col-span-12',
-        'full' => 'xl:col-span-full',
-        default => $xl,
-    } : null,
-    ($twoXl && (! $hidden)) ? match ($twoXl) {
-        1 => '2xl:col-span-1',
-        2 => '2xl:col-span-2',
-        3 => '2xl:col-span-3',
-        4 => '2xl:col-span-4',
-        5 => '2xl:col-span-5',
-        6 => '2xl:col-span-6',
-        7 => '2xl:col-span-7',
-        8 => '2xl:col-span-8',
-        9 => '2xl:col-span-9',
-        10 => '2xl:col-span-10',
-        11 => '2xl:col-span-11',
-        12 => '2xl:col-span-12',
-        'full' => '2xl:col-span-full',
-        default => $twoXl,
-    } : null,
-    ($defaultStart && (! $hidden)) ? match ($defaultStart) {
-        1 => 'col-start-1',
-        2 => 'col-start-2',
-        3 => 'col-start-3',
-        4 => 'col-start-4',
-        5 => 'col-start-5',
-        6 => 'col-start-6',
-        7 => 'col-start-7',
-        8 => 'col-start-8',
-        9 => 'col-start-9',
-        10 => 'col-start-10',
-        11 => 'col-start-11',
-        12 => 'col-start-12',
-        default => $defaultStart,
-    } : null,
-    ($smStart && (! $hidden)) ? match ($smStart) {
-        1 => 'sm:col-start-1',
-        2 => 'sm:col-start-2',
-        3 => 'sm:col-start-3',
-        4 => 'sm:col-start-4',
-        5 => 'sm:col-start-5',
-        6 => 'sm:col-start-6',
-        7 => 'sm:col-start-7',
-        8 => 'sm:col-start-8',
-        9 => 'sm:col-start-9',
-        10 => 'sm:col-start-10',
-        11 => 'sm:col-start-11',
-        12 => 'sm:col-start-12',
-        default => $smStart,
-    } : null,
-    ($mdStart && (! $hidden)) ? match ($mdStart) {
-        1 => 'md:col-start-1',
-        2 => 'md:col-start-2',
-        3 => 'md:col-start-3',
-        4 => 'md:col-start-4',
-        5 => 'md:col-start-5',
-        6 => 'md:col-start-6',
-        7 => 'md:col-start-7',
-        8 => 'md:col-start-8',
-        9 => 'md:col-start-9',
-        10 => 'md:col-start-10',
-        11 => 'md:col-start-11',
-        12 => 'md:col-start-12',
-        default => $mdStart,
-    } : null,
-    ($lgStart && (! $hidden)) ? match ($lgStart) {
-        1 => 'lg:col-start-1',
-        2 => 'lg:col-start-2',
-        3 => 'lg:col-start-3',
-        4 => 'lg:col-start-4',
-        5 => 'lg:col-start-5',
-        6 => 'lg:col-start-6',
-        7 => 'lg:col-start-7',
-        8 => 'lg:col-start-8',
-        9 => 'lg:col-start-9',
-        10 => 'lg:col-start-10',
-        11 => 'lg:col-start-11',
-        12 => 'lg:col-start-12',
-        default => $lgStart,
-    } : null,
-    ($xlStart && (! $hidden)) ? match ($xlStart) {
-        1 => 'xl:col-start-1',
-        2 => 'xl:col-start-2',
-        3 => 'xl:col-start-3',
-        4 => 'xl:col-start-4',
-        5 => 'xl:col-start-5',
-        6 => 'xl:col-start-6',
-        7 => 'xl:col-start-7',
-        8 => 'xl:col-start-8',
-        9 => 'xl:col-start-9',
-        10 => 'xl:col-start-10',
-        11 => 'xl:col-start-11',
-        12 => 'xl:col-start-12',
-        default => $xlStart,
-    } : null,
-    ($twoXlStart && (! $hidden)) ? match ($twoXlStart) {
-        1 => '2xl:col-start-1',
-        2 => '2xl:col-start-2',
-        3 => '2xl:col-start-3',
-        4 => '2xl:col-start-4',
-        5 => '2xl:col-start-5',
-        6 => '2xl:col-start-6',
-        7 => '2xl:col-start-7',
-        8 => '2xl:col-start-8',
-        9 => '2xl:col-start-9',
-        10 => '2xl:col-start-10',
-        11 => '2xl:col-start-11',
-        12 => '2xl:col-start-12',
-        default => $twoXlStart,
-    } : null,
-]) }}>
+@php
+    $getSpanValue = function ($span) {
+        if ($span === 'full') {
+            return '1 / -1';
+        }
+
+        return "span {$span} / span {$span}";
+    }
+@endphp
+
+<div
+    style="
+        @if ($default) --col-span-default: {{ $getSpanValue($default) }}; @endif
+        @if ($sm) --col-span-sm: {{ $getSpanValue($sm) }}; @endif
+        @if ($md) --col-span-md: {{ $getSpanValue($md) }}; @endif
+        @if ($lg) --col-span-lg: {{ $getSpanValue($lg) }}; @endif
+        @if ($xl) --col-span-xl: {{ $getSpanValue($xl) }}; @endif
+        @if ($twoXl) --col-span-2xl: {{ $getSpanValue($twoXl) }}; @endif
+        @if ($defaultStart) --col-start-default: {{ $defaultStart }}; @endif
+        @if ($smStart) --col-start-sm: {{ $smStart }}; @endif
+        @if ($mdStart) --col-start-md: {{ $mdStart }}; @endif
+        @if ($lgStart) --col-start-lg: {{ $lgStart }}; @endif
+        @if ($xlStart) --col-start-xl: {{ $xlStart }}; @endif
+        @if ($twoXlStart) --col-start-2xl: {{ $twoXlStart }}; @endif
+    "
+    {{ $attributes->class([
+        'hidden' => $hidden,
+        'col-[var(--col-span-default)]' => $default && (! $hidden),
+        'sm:col-[var(--col-span-sm)]' => $sm && (! $hidden),
+        'md:col-[var(--col-span-md)]' => $md && (! $hidden),
+        'lg:col-[var(--col-span-lg)]' => $lg && (! $hidden),
+        'xl:col-[var(--col-span-xl)]' => $xl && (! $hidden),
+        '2xl:col-[var(--col-span-2xl)]' => $twoXl && (! $hidden),
+        'col-start-[var(--col-start-default)]' => $defaultStart && (! $hidden),
+        'sm:col-start-[var(--col-start-sm)]' => $smStart && (! $hidden),
+        'md:col-start-[var(--col-start-md)]' => $mdStart && (! $hidden),
+        'lg:col-start-[var(--col-start-lg)]' => $lgStart && (! $hidden),
+        'xl:col-start-[var(--col-start-xl)]' => $xlStart && (! $hidden),
+        '2xl:col-start-[var(--col-start-2xl)]' => $twoXlStart && (! $hidden),
+    ]) }}
+>
     {{ $slot }}
 </div>
