f3ea76b3e0bba3292818776e6cd7dcaf69c1fd55
danharrin <danharrin@users.noreply.github.com>
2023-03-31 12:16:54 +0000

    chore: fix code style

--- packages/forms/src/Components/Concerns/HasState.php
+++ packages/forms/src/Components/Concerns/HasState.php
@@ -4,7 +4,6 @@ namespace Filament\Forms\Components\Concerns;
 
 use Closure;
 use Filament\Forms\Components\Component;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Get;
 use Filament\Forms\Set;
 use Illuminate\Contracts\Support\Arrayable;
