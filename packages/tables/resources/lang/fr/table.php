cb5a77116f0157781d12385c2d075c8f5b1b06e0
Brenier Arnaud <arnaud.brenier91@gmail.com>
2023-03-31 13:49:10 +0200

    Add french translation to reset column search button

--- packages/tables/resources/lang/fr/table.php
+++ packages/tables/resources/lang/fr/table.php
@@ -94,6 +94,14 @@ return [
 
     'empty' => [
         'heading' => 'Aucun élément trouvé',
+        
+        'buttons' => [
+
+            'reset_column_searches' => [
+                'label' => 'Effacer la recherche de colonne',
+            ],
+
+        ],
     ],
 
     'filters' => [
@@ -105,7 +113,7 @@ return [
             ],
 
             'remove_all' => [
-                'label' => 'Supprimer tous les filters',
+                'label' => 'Supprimer tous les filtres',
                 'tooltip' => 'Supprimer tous les filtres',
             ],
 
