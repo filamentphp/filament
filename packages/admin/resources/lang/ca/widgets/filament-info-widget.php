7548d6b7e57fc5b2efc8d8209444a5d8a19f2ece
Marc Garcia <garciatorrent96@gmail.com>
2023-04-05 12:02:28 +0200

    Catalan translation for Admin panel

---
+++ packages/admin/resources/lang/ca/widgets/filament-info-widget.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'buttons' => [
+
+        'visit_documentation' => [
+            'label' => 'Documentació',
+        ],
+
+        'visit_github' => [
+            'label' => 'GitHub',
+        ],
+
+    ],
+
+];
