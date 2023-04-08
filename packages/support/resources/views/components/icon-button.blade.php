576d8d8bcd67f6671a8b5c650ac578f6c39f64fe
Ben Johnson <howdu@users.noreply.github.com>
2023-04-08 10:27:38 +0100

    Apply wire target change to buttons

--- packages/support/resources/views/components/icon-button.blade.php
+++ packages/support/resources/views/components/icon-button.blade.php
@@ -45,10 +45,12 @@
         'bg-warning-500/10' => $color === 'warning',
     ]);
 
-    $hasLoadingIndicator = filled($attributes->get('wire:target')) || filled($attributes->get('wire:click')) || (($type === 'submit') && filled($form));
+    $wireTarget = $attributes->whereStartsWith(['wire:target', 'wire:click'])->first();
+
+    $hasLoadingIndicator = filled($wireTarget) || ($type === 'submit' && filled($form));
 
     if ($hasLoadingIndicator) {
-        $loadingIndicatorTarget = html_entity_decode($attributes->get('wire:target', $attributes->get('wire:click', $form)), ENT_QUOTES);
+        $loadingIndicatorTarget = html_entity_decode($wireTarget ?: $form), ENT_QUOTES);
     }
 @endphp
 
