cee041b4971c366e1b016afb1212a75cb465e4bf
Levan Velijanashvili <stichoza@gmail.com>
2023-04-09 18:31:05 +0400

    Update Georgian translations

--- packages/tables/resources/lang/ka/table.php
+++ packages/tables/resources/lang/ka/table.php
@@ -2,7 +2,27 @@
 
 return [
 
+    'columns' => [
+
+        'tags' => [
+            'more' => 'და კიდევ :count',
+        ],
+
+        'messages' => [
+            'copied' => 'დაკოპირებულია',
+        ],
+
+    ],
+
     'fields' => [
+
+        'bulk_select_page' => [
+            'label' => 'Select/deselect all items for bulk actions.',
+        ],
+
+        'bulk_select_record' => [
+            'label' => 'Select/deselect item :key for bulk actions.',
+        ],
 
         'search_query' => [
             'label' => 'ძიება',
@@ -13,14 +33,20 @@ return [
 
     'pagination' => [
 
-        'label' => 'გვერდების ნავიგაცია',
+        'label' => 'გვერდებზე ნავიგაცია',
 
-        'overview' => 'Showing :first to :last of :total results',
+        'overview' => '{1} ნაჩვენებია ერთი შედეგი|[2,*] ნაჩვენებია :first-დან :last-მდე :total-დან',
 
         'fields' => [
 
             'records_per_page' => [
+
                 'label' => 'ჩანაწერი თითო გვერდზე',
+
+                'options' => [
+                    'all' => 'ყველა',
+                ],
+
             ],
 
         ],
@@ -28,7 +54,7 @@ return [
         'buttons' => [
 
             'go_to_page' => [
-                'label' => ':page-ე გვერდზე გადასვლა',
+                'label' => '{1} :page-ელ გვერდზე გადასვლა|[2,*] მე-:page გვერდზე გადასვლა',
             ],
 
             'next' => [
@@ -45,26 +71,122 @@ return [
 
     'buttons' => [
 
+        'disable_reordering' => [
+            'label' => 'ჩანაწერების გადანაცვლების დასრულება',
+        ],
+
+        'enable_reordering' => [
+            'label' => 'ჩანაწერების გადანაცვლება',
+        ],
+
         'filter' => [
             'label' => 'ფილტრი',
         ],
 
         'open_actions' => [
-            'label' => 'Open actions',
+            'label' => 'ქმედებები',
+        ],
+
+        'toggle_columns' => [
+            'label' => 'სვეტების დამალვა/გამოჩენა',
         ],
 
     ],
 
     'empty' => [
-        'heading' => 'ჩანაწერები არ არსებობს.',
+
+        'heading' => 'ჩანაწერები ვერ მოიძებნა',
+
+        'buttons' => [
+
+            'reset_column_searches' => [
+                'label' => 'სვეტის ძიების წაშლა',
+            ],
+
+        ],
+
     ],
 
+    'filters' => [
+
+        'buttons' => [
+
+            'remove' => [
+                'label' => 'ფილტრის მოხსნა',
+            ],
+
+            'remove_all' => [
+                'label' => 'ყველა ფილტრის მოხსნა',
+                'tooltip' => 'ყველა ფილტრის მოხსნა',
+            ],
+
+            'reset' => [
+                'label' => 'ფილტრების გაუქმება',
+            ],
+
+        ],
+
+        'indicator' => 'აქტიური ფილტრები',
+
+        'multi_select' => [
+            'placeholder' => 'ყველა',
+        ],
+
+        'select' => [
+            'placeholder' => 'ყველა',
+        ],
+
+        'trashed' => [
+
+            'label' => 'წაშლილი ჩანაწერები',
+
+            'only_trashed' => 'მხოლოდ წაშლილი ჩანაწერები',
+
+            'with_trashed' => 'წაშლილი ჩანაწერებიანა',
+
+            'without_trashed' => 'წაშლილი ჩანაწერების გარეშე',
+
+        ],
+
+    ],
+
+    'reorder_indicator' => 'გადაადგილე ჩანაწერები.',
+
     'selection_indicator' => [
+
+        'selected_count' => 'მონიშნულია :count ჩანაწერი.',
 
         'buttons' => [
 
             'select_all' => [
-                'label' => 'Select all :count',
+                'label' => '{1} მონიშნე ყველა|[2,*] მონიშნე :count-ივე',
+            ],
+
+            'deselect_all' => [
+                'label' => 'მონიშვნების მოხსნა',
+            ],
+
+        ],
+
+    ],
+
+    'sorting' => [
+
+        'fields' => [
+
+            'column' => [
+                'label' => 'სორტირება',
+            ],
+
+            'direction' => [
+
+                'label' => 'სორტირების მიმართულება',
+
+                'options' => [
+                    'asc' => 'ზრდადობით',
+                    'desc' => 'კლებადობით',
+                ],
+
             ],
 
         ],
