30ef388c2a1899a2ed1ece0042fa5c70635cb495
Ben Johnson <howdu@users.noreply.github.com>
2023-04-14 13:01:07 +0100

    Move to list item

--- packages/forms/resources/views/components/repeater.blade.php
+++ packages/forms/resources/views/components/repeater.blade.php
@@ -88,7 +88,7 @@
                                 setTimeout(() => $el.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' }), 200)
                             "
                             @class([
-                                'bg-white border border-gray-300 shadow-sm rounded-xl relative',
+                                'filament-forms-repeater-item bg-white border border-gray-300 shadow-sm rounded-xl relative',
                                 'dark:bg-gray-800 dark:border-gray-600' => config('forms.dark_mode'),
                             ])
                         >
@@ -294,7 +294,7 @@
                                 </header>
                             @endif
 
-                            <div class="filament-forms-repeater-item p-6" x-show="! isCollapsed">
+                            <div class="p-6" x-show="! isCollapsed">
                                 {{ $item }}
                             </div>
 
