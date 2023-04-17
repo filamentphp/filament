47f769a0975dc17afa3157f58db746422bd83364
Zep Fietje <hey@zepfietje.com>
2023-04-17 12:12:05 +0200

    Fix type tpos

--- packages/support/src/Color.php
+++ packages/support/src/Color.php
@@ -324,7 +324,7 @@ class Color
     }
 
     /**
-     * @return array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, string, 950: string}
+     * @return array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string}
      */
     public static function rgb(string $color): array
     {
@@ -332,7 +332,7 @@ class Color
     }
 
     /**
-     * @return array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, string, 950: string}
+     * @return array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string}
      */
     protected static function generateShades(Rgb $color): array
     {
