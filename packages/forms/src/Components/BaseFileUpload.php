2271ff8b7669de2ac9cdf0ae6a3079783b34bdfc
danharrin <danharrin@users.noreply.github.com>
2023-04-17 13:01:38 +0000

    chore: fix code style

--- packages/forms/src/Components/BaseFileUpload.php
+++ packages/forms/src/Components/BaseFileUpload.php
@@ -105,7 +105,7 @@ class BaseFileUpload extends Field
             $component->saveUploadedFiles();
         });
 
-        $this->dehydrateStateUsing(static function (BaseFileUpload $component, ?array $state): string | array | null | TemporaryUploadedFile  {
+        $this->dehydrateStateUsing(static function (BaseFileUpload $component, ?array $state): string | array | null | TemporaryUploadedFile {
             $files = array_values($state ?? []);
 
             if ($component->isMultiple()) {
