5837cf24f35138b50cef24ae26e16401a0909ec5
Zep Fietje <hey@zepfietje.com>
2023-04-17 09:57:37 +0200

    Update packages/admin/resources/views/components/user-avatar.blade.php

--- packages/admin/resources/views/components/user-avatar.blade.php
+++ packages/admin/resources/views/components/user-avatar.blade.php
@@ -3,7 +3,7 @@
 ])
 
 <div
- l   {{ $attributes->class([
+    {{ $attributes->class([
         'w-10 h-10 rounded-full bg-gray-200 bg-cover bg-center',
         'dark:bg-gray-900' => config('filament.dark_mode'),
     ]) }}
