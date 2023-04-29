eb4703efb4fb1f0e3882e17aa7106e8bd0fb6058
Ben Johnson <howdu@users.noreply.github.com>
2023-04-29 11:59:57 +0100

    Update code styling

--- packages/tables/resources/views/components/cell.blade.php
+++ packages/tables/resources/views/components/cell.blade.php
@@ -3,10 +3,14 @@
 ])
 
 <td
-    {{ $attributes->merge($extraAttributes)->class([
-        'filament-tables-cell',
-        'dark:text-white' => config('tables.dark_mode'),
-    ]) }}
+    {{
+        $attributes
+            ->merge($extraAttributes)
+            ->class([
+                'filament-tables-cell',
+                'dark:text-white' => config('tables.dark_mode'),
+            ])
+    }}
 >
     {{ $slot }}
 </td>
