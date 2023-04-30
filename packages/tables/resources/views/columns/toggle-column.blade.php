1f36d3f49b97a989592aea2d8f5346e056d23ef3
Dan Harrin <git@danharrin.com>
2023-04-30 14:04:16 +0100

    Update toggle-column.blade.php

--- packages/tables/resources/views/columns/toggle-column.blade.php
+++ packages/tables/resources/views/columns/toggle-column.blade.php
@@ -9,38 +9,11 @@
             state: @js((bool) $state),
             isLoading: false,
         }"
-        x-init="
-            $watch('state', () => $refs.button?.dispatchEvent(new Event('change')))
-
-            Livewire.hook('message.processed', (component) => {
-                if (component.component.id !== @js($this->id)) {
-                    return
-                }
-
-                if (! $refs.newState) {
-                    return
-                }
-
-                let newState = $refs.newState.value === '1' ? true : false
-
-                if (state === newState) {
-                    return
-                }
-
-                state = newState
-            })
-        "
         wire:ignore
         {{ $attributes->merge($getExtraAttributes(), escape: false)->class([
             'filament-tables-toggle-column',
         ]) }}
     >
-        <input
-            type="hidden"
-            value="{{ $state ? 1 : 0 }}"
-            x-ref="newState"
-        />
-
         @php
             $offColor = $getOffColor();
             $onColor = $getOnColor();
@@ -50,12 +23,21 @@
             role="switch"
             aria-checked="false"
             x-bind:aria-checked="state.toString()"
-            x-on:click="! isLoading && (state = ! state)"
-            x-ref="button"
-            x-on:change="
+            x-on:click="
+                if (isLoading) {
+                    return
+                }
+
+                state = ! state
+
                 isLoading = true
                 response = await $wire.updateTableColumnState(@js($getName()), @js($recordKey), state)
                 error = response?.error ?? undefined
+
+                if (error) {
+                    state = ! state
+                }
+
                 isLoading = false
             "
             x-tooltip="error"
@@ -84,102 +66,58 @@
             type="button"
             class="relative inline-flex shrink-0 ms-4 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 outline-none disabled:opacity-70 disabled:pointer-events-none"
         >
-            <button
-                role="switch"
-                aria-checked="false"
-                x-bind:aria-checked="state.toString()"
-                x-on:click="
-                    if (isLoading) {
-                        return
-                    }
-
-                    state = ! state
-
-                    isLoading = true
-                    response = await $wire.updateTableColumnState(@js($getName()), @js($recordKey), state)
-                    error = response?.error ?? undefined
-
-                    if (error) {
-                        state = ! state
-                    }
-
-                    isLoading = false
-                "
-                x-tooltip="error"
+            <span
+                class="pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 ease-in-out transition duration-200"
                 x-bind:class="{
-                    'opacity-70 pointer-events-none': isLoading,
-                    '{{ match ($getOnColor()) {
-                        'danger' => 'bg-danger-500',
-                        'secondary' => 'bg-gray-500',
-                        'success' => 'bg-success-500',
-                        'warning' => 'bg-warning-500',
-                        default => 'bg-primary-600',
-                    } }}': state,
-                    '{{ match ($getOffColor()) {
-                        'danger' => 'bg-danger-500',
-                        'primary' => 'bg-primary-500',
-                        'success' => 'bg-success-500',
-                        'warning' => 'bg-warning-500',
-                        default => 'bg-gray-200',
-                    } }} @if (config('forms.dark_mode')) dark:bg-white/10 @endif': ! state,
+                    'translate-x-5 rtl:-translate-x-5': state,
+                    'translate-x-0': ! state,
                 }"
-                {!! $isDisabled() ? 'disabled' : null !!}
-                type="button"
-                class="relative inline-flex shrink-0 ml-4 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 outline-none focus:ring-1 focus:ring-offset-1 focus:ring-primary-500 disabled:opacity-70 disabled:cursor-not-allowed disabled:pointer-events-none"
             >
-                <span
-                    class="pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 ease-in-out transition duration-200"
-                    x-bind:class="{
-                        'translate-x-5 rtl:-translate-x-5': state,
-                        'translate-x-0': ! state,
-                    }"
-                >
-                    @if ($hasOffIcon())
-                        <x-filament::icon
-                            :name="$getOffIcon()"
-                            alias="filament-tables::columns.toggle.off"
-                            :color="match ($offColor) {
-                                'danger' => 'text-danger-600',
-                                'gray' => 'text-gray-600',
-                                'primary' => 'text-primary-600',
-                                'secondary' => 'text-secondary-600',
-                                'success' => 'text-success-600',
-                                'warning' => 'text-warning-600',
-                                null => 'text-gray-400 dark:text-gray-700',
-                                default => $offColor,
-                            }"
-                            size="h-3 w-3"
-                        />
-                    @endif
-                </span>
+                @if ($hasOffIcon())
+                    <x-filament::icon
+                        :name="$getOffIcon()"
+                        alias="filament-tables::columns.toggle.off"
+                        :color="match ($offColor) {
+                            'danger' => 'text-danger-600',
+                            'gray' => 'text-gray-600',
+                            'primary' => 'text-primary-600',
+                            'secondary' => 'text-secondary-600',
+                            'success' => 'text-success-600',
+                            'warning' => 'text-warning-600',
+                            null => 'text-gray-400 dark:text-gray-700',
+                            default => $offColor,
+                        }"
+                        size="h-3 w-3"
+                    />
+                @endif
+            </span>
 
-                <span
-                    class="absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
-                    aria-hidden="true"
-                    x-bind:class="{
-                        'opacity-100 ease-in duration-200': state,
-                        'opacity-0 ease-out duration-100': ! state,
-                    }"
-                >
-                    @if ($hasOnIcon())
-                        <x-filament::icon
-                            :name="$getOnIcon()"
-                            alias="filament-tables::columns.toggle.on"
-                            :color="match ($onColor) {
-                                'danger' => 'text-danger-600',
-                                'gray' => 'text-gray-600',
-                                'primary', null => 'text-primary-600',
-                                'secondary' => 'text-secondary-600',
-                                'success' => 'text-success-600',
-                                'warning' => 'text-warning-600',
-                                default => $onColor,
-                            }"
-                            size="h-3 w-3"
-                            x-cloak="x-cloak"
-                        />
-                    @endif
-                </span>
-            </button>
-        </div>
+            <span
+                class="absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+                aria-hidden="true"
+                x-bind:class="{
+                    'opacity-100 ease-in duration-200': state,
+                    'opacity-0 ease-out duration-100': ! state,
+                }"
+            >
+                @if ($hasOnIcon())
+                    <x-filament::icon
+                        :name="$getOnIcon()"
+                        alias="filament-tables::columns.toggle.on"
+                        :color="match ($onColor) {
+                            'danger' => 'text-danger-600',
+                            'gray' => 'text-gray-600',
+                            'primary', null => 'text-primary-600',
+                            'secondary' => 'text-secondary-600',
+                            'success' => 'text-success-600',
+                            'warning' => 'text-warning-600',
+                            default => $onColor,
+                        }"
+                        size="h-3 w-3"
+                        x-cloak="x-cloak"
+                    />
+                @endif
+            </span>
+        </button>
     </div>
 </div>
