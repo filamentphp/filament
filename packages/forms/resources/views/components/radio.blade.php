daab2d8656866442e16901559de5043fa4ad6137
Dan Harrin <git@danharrin.com>
2023-04-30 13:28:21 +0100

    fix: Consistent error styling

--- packages/forms/resources/views/components/radio.blade.php
+++ packages/forms/resources/views/components/radio.blade.php
@@ -46,9 +46,9 @@
                                 @disabled($shouldOptionBeDisabled)
                                 {{ $applyStateBindingModifiers('wire:model') }}="{{ $statePath }}"
                                 {{ $getExtraInputAttributeBag()->class([
-                                    'focus:ring-primary-500 h-4 w-4 text-primary-600 outline-none disabled:opacity-70 dark:bg-gray-700 dark:checked:bg-primary-500',
-                                    'border-gray-300 dark:border-gray-500' => ! $errors->has($statePath),
-                                    'border-danger-600 ring-1 ring-inset ring-danger-600 dark:border-danger-400 dark:ring-danger-400' => $errors->has($statePath),
+                                    'h-4 w-4 text-primary-600 outline-none ring-1 ring-inset disabled:opacity-70 dark:bg-gray-700 dark:checked:bg-primary-500',
+                                    'border-gray-300 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:focus:border-primary-500' => ! $errors->has($statePath),
+                                    'border-danger-600 ring-danger-600 dark:border-danger-400 dark:ring-danger-400' => $errors->has($statePath),
                                 ]) }}
                                 wire:loading.attr="disabled"
                             />
