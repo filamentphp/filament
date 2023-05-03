ec4b661839db1d0f9598527a3a42b705c5457ab1
Dennis Koch <info@pixelarbeit.de>
2023-05-03 11:37:22 +0200

    German auth translation

---
+++ packages/app/resources/lang/de/pages/auth/password-reset/request-password-reset.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'title' => 'Passwort zurücksetzen',
+
+    'heading' => 'Passwort vergessen?',
+
+    'buttons' => [
+
+        'login' => [
+            'label' => 'zurück zum Login',
+        ],
+
+        'request' => [
+            'label' => 'E-Mail zusenden',
+        ],
+
+    ],
+
+    'fields' => [
+
+        'email' => [
+            'label' => 'E-Mail-Adresse',
+        ],
+
+    ],
+
+    'messages' => [
+        'throttled' => 'Zu viele Versuche. Versuchen Sie es bitte in :seconds Sekunden nochmal.',
+    ],
+
+];
