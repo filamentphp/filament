4daacb6f40e0357e02bbfac8f018893e5d79d14f
Zep Fietje <hey@zepfietje.com>
2023-05-03 12:37:43 +0200

    Add Dutch auth translations

---
+++ packages/app/resources/lang/nl/pages/auth/register.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    'title' => 'Registreren',
+
+    'heading' => 'Registreren',
+
+    'buttons' => [
+
+        'login' => [
+            'before' => 'of',
+            'label' => 'inloggen op je account',
+        ],
+
+        'register' => [
+            'label' => 'Registreren',
+        ],
+
+    ],
+
+    'fields' => [
+
+        'email' => [
+            'label' => 'E-mailadres',
+        ],
+
+        'name' => [
+            'label' => 'Naam',
+        ],
+
+        'password' => [
+            'label' => 'Wachtwoord',
+            'validation_attribute' => 'wachtwoord',
+        ],
+
+        'passwordConfirmation' => [
+            'label' => 'Wachtwoord bevestigen',
+        ],
+
+    ],
+
+    'messages' => [
+        'throttled' => 'Te veel pogingen. Probeer het opnieuw over :seconds seconden.',
+    ],
+
+];
