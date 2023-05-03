1f8a262be9cbf1315e74fc6c4311e0bacd9fbd59
michaelpritchard <6510941-michaelpritchard@users.noreply.gitlab.com>
2023-05-03 13:21:46 +0100

    Update Welsh form translations to v2.12.10 standard

--- packages/forms/resources/lang/cy/components.php
+++ packages/forms/resources/lang/cy/components.php
@@ -1,19 +1,23 @@
 <?php
 
-declare(strict_types=1);
-
 return [
 
     'builder' => [
 
+        'collapsed' => 'Cynnwys wedi Cwympo',
+
         'buttons' => [
+
+            'clone_item' => [
+                'label' => 'Clôn',
+            ],
 
             'create_item' => [
                 'label' => 'Ychwanegu at :label',
             ],
 
             'create_item_between' => [
-                'label' => 'Ychwanegu rhwng',
+                'label' => 'Mewnosod',
             ],
 
             'delete_item' => [
@@ -32,6 +36,38 @@ return [
                 'label' => 'Symud i fyny',
             ],
 
+            'collapse_item' => [
+                'label' => 'Cwympo',
+            ],
+
+            'expand_item' => [
+                'label' => 'Ymledu',
+            ],
+
+            'collapse_all' => [
+                'label' => 'Cwympo popeth',
+            ],
+
+            'expand_all' => [
+                'label' => 'Ymledu popeth',
+            ],
+
+        ],
+
+    ],
+
+    'checkbox_list' => [
+
+        'buttons' => [
+
+            'deselect_all' => [
+                'label' => 'Dad ddewis popeth',
+            ],
+
+            'select_all' => [
+                'label' => 'Dewis popeth',
+            ],
+
         ],
 
     ],
@@ -46,6 +82,10 @@ return [
 
             'delete' => [
                 'label' => 'Dileu rhes',
+            ],
+
+            'reorder' => [
+                'label' => 'Rhes recordiwr',
             ],
 
         ],
@@ -68,35 +108,39 @@ return [
 
         'toolbar_buttons' => [
             'attach_files' => 'Atodi ffeiliau',
-            'bold' => 'Trwm',
-            'bullet_list' => 'Rhestr bwled',
-            'code_block' => 'Bloc côd',
-            'edit' => 'Golygu',
-            'italic' => 'Italig',
-            'link' => 'Dolen',
+            'bold'         => 'Trwm',
+            'bullet_list'  => 'Rhestr bwled',
+            'code_block'   => 'Bloc côd',
+            'edit'         => 'Golygu',
+            'italic'       => 'Italig',
+            'link'         => 'Dolen',
             'ordered_list' => 'Rhestr wedi i rhifo',
-            'preview' => 'Rhagolwg',
-            'strike' => 'Llinell drwodd',
+            'preview'      => 'Rhagolwg',
+            'strike'       => 'Llinell drwodd',
         ],
 
     ],
 
-    'multi_select' => [
-        'no_search_results_message' => 'Nid oes unrhyw opsiynau yn cyfateb i`ch chwiliad.',
-        'placeholder' => 'Dewis opsiwn',
-        'search_prompt' => 'Dechrau teipio i chwilio...',
-    ],
-
     'repeater' => [
+
+        'collapsed' => 'Cynnwys wedi Cwympo',
 
         'buttons' => [
 
             'create_item' => [
-                'label' => 'Ychwanegu at: label',
+                'label' => 'Ychwanegu at :label',
             ],
 
             'delete_item' => [
                 'label' => 'Dileu',
+            ],
+
+            'clone_item' => [
+                'label' => 'Clôn',
+            ],
+
+            'move_item' => [
+                'label' => 'Symud',
             ],
 
             'move_item_down' => [
@@ -105,6 +149,22 @@ return [
 
             'move_item_up' => [
                 'label' => 'Symud i fyny',
+            ],
+
+            'collapse_item' => [
+                'label' => 'Cwympo',
+            ],
+
+            'expand_item' => [
+                'label' => 'Ymledu',
+            ],
+
+            'collapse_all' => [
+                'label' => 'Cwympo popeth',
+            ],
+
+            'expand_all' => [
+                'label' => 'Ymledu popeth',
             ],
 
         ],
@@ -118,7 +178,7 @@ return [
             'link' => [
 
                 'buttons' => [
-                    'link' => 'Dolen',
+                    'link'   => 'Dolen',
                     'unlink' => 'Datgysylltu',
                 ],
 
@@ -132,31 +192,84 @@ return [
 
         'toolbar_buttons' => [
             'attach_files' => 'Atodi ffeiliau',
-            'blockquote' => 'Dyfynnod Bloc',
-            'bold' => 'Trwm',
-            'bullet_list' => 'Rhestr bwled',
-            'code_block' => 'Bloc côd',
-            'h1' => 'Teitl',
-            'h2' => 'Pennawd',
-            'h3' => 'Is-bennawd',
-            'italic' => 'Italig',
-            'link' => 'Dolen',
+            'blockquote'   => 'Dyfynnod Bloc',
+            'bold'         => 'Trwm',
+            'bullet_list'  => 'Rhestr bwled',
+            'code_block'   => 'Bloc côd',
+            'h1'           => 'Teitl',
+            'h2'           => 'Pennawd',
+            'h3'           => 'Is-bennawd',
+            'italic'       => 'Italig',
+            'link'         => 'Dolen',
             'ordered_list' => 'Rhestr wedi`i rhifo',
-            'redo' => 'Ail-wneud',
-            'strike' => 'Llinell drwodd',
-            'undo' => 'Dadwneud',
+            'redo'         => 'Ail-wneud',
+            'strike'       => 'Llinell drwodd',
+            'undo'         => 'Dadwneud',
         ],
 
     ],
 
     'select' => [
+
+        'actions' => [
+
+            'create_option' => [
+
+                'modal' => [
+
+                    'heading' => 'Creu',
+
+                    'actions' => [
+
+                        'create' => [
+                            'label' => 'Creu',
+                        ],
+
+                    ],
+
+                ],
+
+            ],
+
+        ],
+
+        'boolean' => [
+            'true'  => 'Ie',
+            'false' => 'Na',
+        ],
+
+        'loading_message' => 'Llwytho...',
+
+        'max_items_message' => 'Gall dim on :count gael eu dewis.',
+
         'no_search_results_message' => 'Nid oes unrhyw opsiynau yn cyfateb i`ch chwiliad.',
+
         'placeholder' => 'Dewis opsiwn',
+
+        'searching_message' => 'Chwilio...',
+
         'search_prompt' => 'Dechrau teipio i chwilio...',
+
     ],
 
     'tags_input' => [
         'placeholder' => 'Tag newydd',
+    ],
+
+    'wizard' => [
+
+        'buttons' => [
+
+            'previous_step' => [
+                'label' => 'Yn ôl',
+            ],
+
+            'next_step' => [
+                'label' => 'Nesaf',
+            ],
+
+        ],
+
     ],
 
 ];
