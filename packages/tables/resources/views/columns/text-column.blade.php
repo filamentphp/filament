f99b1a0f827a2b6b51adb701f04e49ad034256f1
Dan Harrin <git@danharrin.com>
2023-05-04 23:21:01 +0100

    Update text-column.blade.php

--- packages/tables/resources/views/columns/text-column.blade.php
+++ packages/tables/resources/views/columns/text-column.blade.php
@@ -56,7 +56,7 @@
     @if (filled($descriptionAbove))
         <div @class([
             'text-sm text-gray-500',
-            'dark:text-gray-400' => config('tables.dark_mode')
+            'dark:text-gray-400' => config('tables.dark_mode'),
         ])>
             {{ $descriptionAbove instanceof \Illuminate\Support\HtmlString ? $descriptionAbove : \Illuminate\Support\Str::of($descriptionAbove)->markdown()->sanitizeHtml()->toHtmlString() }}
         </div>
@@ -89,7 +89,7 @@
     @if (filled($descriptionBelow))
         <div @class([
             'text-sm text-gray-500',
-            'dark:text-gray-400' => config('tables.dark_mode')
+            'dark:text-gray-400' => config('tables.dark_mode'),
         ])>
             {{ $descriptionBelow instanceof \Illuminate\Support\HtmlString ? $descriptionBelow : \Illuminate\Support\Str::of($descriptionBelow)->markdown()->sanitizeHtml()->toHtmlString() }}
         </div>
