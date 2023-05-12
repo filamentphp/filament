6c12f11b26580cac266783604f700d28c0bd706e
Zep Fietje <hey@zepfietje.com>
2023-05-12 13:12:21 +0200

    Update packages/forms/resources/views/components/date-time-picker.blade.php

--- packages/forms/resources/views/components/date-time-picker.blade.php
+++ packages/forms/resources/views/components/date-time-picker.blade.php
@@ -82,28 +82,13 @@
 
             @if ($icon !== false)
                 <span class="absolute inset-y-0 right-0 flex items-center pr-2 cursor-pointer pointer-events-none rtl:right-auto rtl:left-0 rtl:pl-2">
-                    @if ($icon === null)
-                        <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            @class([
-                                'w-5 h-5 text-gray-400 hover:text-gray-800 focus:text-primary-600',
-                                'dark:text-gray-400 dark:hover:text-gray-200 dark:focus:text-primary-600' => config('forms.dark_mode'),
-                            ])
-                        >
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                        </svg>
-                    @else
-                        <x-dynamic-component
-                                :component="$icon"
-                            @class([
-                                'w-5 h-5 text-gray-400 hover:text-gray-800 focus:text-primary-600',
-                                'dark:text-gray-400 dark:hover:text-gray-200 dark:focus:text-primary-600' => config('forms.dark_mode'),
-                            ])
-                        />
-                    @endif
+                    <x-dynamic-component
+                        :component="$icon ?? 'heroicon-s-calendar'"
+                        @class([
+                            'w-5 h-5 text-gray-400 hover:text-gray-800 focus:text-primary-600',
+                            'dark:text-gray-400 dark:hover:text-gray-200 dark:focus:text-primary-600' => config('forms.dark_mode'),
+                        ])
+                    />
                 </span>
             @endif
         </button>
