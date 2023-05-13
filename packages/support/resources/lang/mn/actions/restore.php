77df90b67be1158f2c6c54ffb94e8065d0503628
Tuvshu <tuvshinjargal.b@gmail.com>
2023-05-13 12:18:35 +0800

    Translate Support

---
+++ packages/support/resources/lang/mn/actions/restore.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Сэргээх',
+
+        'modal' => [
+
+            'heading' => 'Сэргээх :label',
+
+            'actions' => [
+
+                'restore' => [
+                    'label' => 'Сэргээх',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'restored' => 'Сэргээв',
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => 'Сонгосонг сэргээх',
+
+        'modal' => [
+
+            'heading' => 'Сэргээх :label',
+
+            'actions' => [
+
+                'restore' => [
+                    'label' => 'Сэргээх',
+                ],
+
+            ],
+
+        ],
+
+        'messages' => [
+            'restored' => 'Сэргээв',
+        ],
+
+    ],
+
+];
