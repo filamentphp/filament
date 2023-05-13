236a47a7636257ef15238c5b38be475a631cb67d
Tuvshu <tuvshinjargal.b@gmail.com>
2023-05-13 11:41:00 +0800

    Translate admin resource

---
+++ packages/admin/resources/lang/mn/layout.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    'direction' => 'ltr',
+
+    'buttons' => [
+
+        'dark_mode' => [
+            'label' => 'Гэрэлгүй горимд шилжих',
+        ],
+
+        'database_notifications' => [
+            'label' => 'Сонордуулга нээх',
+        ],
+
+        'light_mode' => [
+            'label' => 'Гэрэлтэй горимд шилжих',
+        ],
+
+        'logout' => [
+            'label' => 'Гарах',
+        ],
+
+        'sidebar' => [
+
+            'collapse' => [
+                'label' => 'Хажуугийн хавтсыг хаах',
+            ],
+
+            'expand' => [
+                'label' => 'Хажуугийн хавтсыг нээх',
+            ],
+
+        ],
+
+        'user_menu' => [
+            'label' => 'Хэрэглэгчийн цэс',
+        ],
+
+    ],
+
+];
