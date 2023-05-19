4f9f45319bf0a878db7874b080c5c745b4ede783
Zep Fietje <hey@zepfietje.com>
2023-05-19 16:31:20 +0200

    Fix indentation

--- packages/support/resources/views/components/button.blade.php
+++ packages/support/resources/views/components/button.blade.php
@@ -21,7 +21,7 @@
     $buttonClasses = [
         ...[
             'filament-button grid-flow-col items-center justify-center rounded-lg border font-medium relative outline-none transition-colors focus:ring-2 disabled:pointer-events-none disabled:opacity-70',
-             match ($size) {
+            match ($size) {
                 'xs' => 'filament-button-size-xs gap-1.5 py-[calc(theme(spacing.2)-1px)] px-[calc(theme(spacing.3)-1px)] text-xs',
                 'sm' => 'filament-button-size-sm gap-1.5 py-[calc(theme(spacing.2)-1px)] px-[calc(theme(spacing.[3.5])-1px)] text-sm',
                 'md' => 'filament-button-size-md gap-2 py-[calc(theme(spacing[2.5])-1px)] px-[calc(theme(spacing.4)-1px)] text-sm',
