289ae14a7aef7865fa700f46d93933a4438ddbd3
Dan Harrin <git@danharrin.com>
2023-05-21 20:11:19 +0200

    Update index.js

--- packages/forms/resources/js/index.js
+++ packages/forms/resources/js/index.js
@@ -2,13 +2,12 @@ import '../css/components/color-picker.css'
 
 import '../css/components/date-time-picker.css'
 
+import 'cropperjs/dist/cropper.min.css'
 import 'filepond/dist/filepond.min.css'
 import 'filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css'
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
 import 'filepond-plugin-media-preview/dist/filepond-plugin-media-preview.css'
 import '../css/components/file-upload.css'
-
-import 'cropperjs/dist/cropper.min.css'
 
 import 'easymde/dist/easymde.min.css'
 import '../css/components/markdown-editor.css'
