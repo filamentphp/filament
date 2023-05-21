f7c4ca64efec46e28576f15265d9dca406595a70
Kenneth Sese <ksese@hope4tomorrow.net>
2023-05-20 18:22:30 -0600

    Update tags-input.blade.php

--- packages/forms/resources/views/components/tags-input.blade.php
+++ packages/forms/resources/views/components/tags-input.blade.php
@@ -24,13 +24,11 @@
         }}
     >
         <div x-show="state.length || @js(! $isDisabled)">
-            <div 
-                @class([
-                    'block w-full transition duration-75 rounded-lg shadow-sm sm:text-sm border overflow-hidden focus-within:ring-1',
-                    'border-gray-300 focus-within:border-primary-500 focus-within:ring-primary-500 dark:border-gray-600 dark:focus-within:border-primary-500' => ! $errors->has($statePath),
-                    'border-danger-600 ring-danger-600 dark:border-danger-400 dark:ring-danger-400' => $errors->has($statePath),
-                ])
-            >
+            <div @class([
+                'block w-full transition duration-75 rounded-lg shadow-sm sm:text-sm border overflow-hidden focus-within:ring-1',
+                'border-gray-300 focus-within:border-primary-500 focus-within:ring-primary-500 dark:border-gray-600 dark:focus-within:border-primary-500' => ! $errors->has($statePath),
+                'border-danger-600 ring-danger-600 dark:border-danger-400 dark:ring-danger-400' => $errors->has($statePath),
+            ])>
                 @unless ($isDisabled)
                     <div>
                         <input
