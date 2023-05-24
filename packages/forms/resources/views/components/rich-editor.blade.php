d12cbb64d34f300e6332b404930371d7ec3cac1a
Sebastiaan Kloos <sebastiaan@codecycler.com>
2023-05-24 12:37:05 +0200

    fixed issue

--- packages/forms/resources/views/components/rich-editor.blade.php
+++ packages/forms/resources/views/components/rich-editor.blade.php
@@ -100,7 +100,7 @@
                                 @endif
 
                                 @if ($hasToolbarButton('underline'))
-                                    <x-forms::rich-editor.toolbar-button
+                                    <x-filament-forms::rich-editor.toolbar-button
                                         data-trix-attribute="underline"
                                         title="{{ __('forms::components.rich_editor.toolbar_buttons.underline') }}"
                                         tabindex="-1"
@@ -109,7 +109,7 @@
                                             'h-4',
                                             'dark:fill-current' => config('forms.dark_mode'),
                                         ]) aria-hidden="true" focusable="false" data-prefix="fas" data-icon="underline" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 64c0-17.7 14.3-32 32-32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32h-16v128c0 53 43 96 96 96s96-43 96-96V96h-16c-17.7 0-32-14.3-32-32s14.3-32 32-32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32h-16v128c0 88.4-71.6 160-160 160S64 312.4 64 224V96H48c-17.7 0-32-14.3-32-32zM0 448c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32z"/></svg>
-                                    </x-forms::rich-editor.toolbar-button>
+                                    </x-filament-forms::rich-editor.toolbar-button>
                                 @endif
 
                                 @if ($hasToolbarButton('strike'))
