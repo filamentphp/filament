e45f7f30c05e0517b66a6ac60fc89f6dfab05e3a
bnzo <17174973+bnzo@users.noreply.github.com>
2023-06-07 16:16:25 +0200

    Removed span

--- packages/forms/resources/views/components/field-wrapper/label.blade.php
+++ packages/forms/resources/views/components/field-wrapper/label.blade.php
@@ -20,17 +20,13 @@
         ])
     >
         {{-- Deliberately poor formatting to ensure that the asterisk sticks to the final word in the label. --}}
-        {{ $slot }}@if ($required)
-            <span class="whitespace-nowrap">
-                <sup
+        {{ $slot }}@if ($required)<sup
                     @class([
                         'text-danger-700 font-medium',
                         'dark:text-danger-400' => config('forms.dark_mode'),
-                    ])
-                >
-                    *
-                </sup>
-            </span>
+                    ])>
+                *
+            </sup>
         @endif
     </span>
 
