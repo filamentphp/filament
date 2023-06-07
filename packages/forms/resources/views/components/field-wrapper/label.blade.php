deeb3b08b5a82d62a8d544b92c6500f49324e245
bnzo <17174973+bnzo@users.noreply.github.com>
2023-06-07 20:20:58 +0200

    Update label.blade.php

--- packages/forms/resources/views/components/field-wrapper/label.blade.php
+++ packages/forms/resources/views/components/field-wrapper/label.blade.php
@@ -14,7 +14,7 @@
         @class([
             'text-sm font-medium leading-4',
             'text-gray-700' => ! $error,
-            'dark:text-gray-300' => ! $error && config('forms.dark_mode'),
+            'dark:text-gray-300' => (! $error) && config('forms.dark_mode'),
             'text-danger-700' => $error,
             'dark:text-danger-400' => $error && config('forms.dark_mode'),
         ])
