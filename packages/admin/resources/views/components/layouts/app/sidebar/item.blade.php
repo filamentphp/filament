5745d5b39a01722585e735312754ef905fe27b98
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2023-06-09 20:16:50 +0100

    Ref : Remove formatting changes.

--- packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
+++ packages/admin/resources/views/components/layouts/app/sidebar/item.blade.php
@@ -20,7 +20,12 @@
     ]);
 @endphp
 
-<li @class(['filament-sidebar-item overflow-hidden', 'filament-sidebar-item-active' => $active])>
+<li
+    @class([
+        'filament-sidebar-item overflow-hidden',
+        'filament-sidebar-item-active' => $active,
+    ])
+>
     <a
         href="{{ $url }}"
         {!! $shouldOpenUrlInNewTab ? 'target="_blank"' : '' !!}
