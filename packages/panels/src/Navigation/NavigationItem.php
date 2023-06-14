84bfabd7d60fb8d95be2c377a1dc4ee1f772e406
Zep Fietje <hey@zepfietje.com>
2023-06-14 22:17:17 +0200

    Fix type

--- packages/panels/src/Navigation/NavigationItem.php
+++ packages/panels/src/Navigation/NavigationItem.php
@@ -17,7 +17,7 @@ class NavigationItem
     /**
      * @var string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
      */
-    protected ?string $iconColor = null;
+    protected string | array | null $iconColor = null;
 
     protected string $label;
 
