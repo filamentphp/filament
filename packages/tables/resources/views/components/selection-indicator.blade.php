dc4bdc7bcfd790d75e407d9700d1ef97bf670ef0
Ben <howdu@users.noreply.github.com>
2023-06-19 12:05:37 +0100

    Add slot to selection-indicator

--- packages/tables/resources/views/components/selection-indicator.blade.php
+++ packages/tables/resources/views/components/selection-indicator.blade.php
@@ -7,36 +7,40 @@
 <div
     wire:key="{{ $this->id }}.table.selection.indicator"
     x-cloak
-    {{ $attributes->class(['filament-tables-selection-indicator whitespace-nowrap bg-primary-500/10 px-4 py-2 text-sm']) }}
+    {{ $attributes->class(['filament-tables-selection-indicator flex flex-wrap items-center gap-1 whitespace-nowrap bg-primary-500/10 px-4 py-2 text-sm']) }}
 >
-    <x-filament-support::loading-indicator
-        x-show="isLoading"
-        class="mr-3 inline-block h-4 w-4 text-primary-500 rtl:ml-3 rtl:mr-0"
-    />
+    <div class="flex-1">
+        <x-filament-support::loading-indicator
+            x-show="isLoading"
+            class="mr-3 inline-block h-4 w-4 text-primary-500 rtl:ml-3 rtl:mr-0"
+        />
 
-    <span
-        @class(['dark:text-white' => config('tables.dark_mode')])
-        x-text="window.pluralize(@js(__('tables::table.selection_indicator.selected_count')), selectedRecords.length, { count: selectedRecords.length })"
-    ></span>
+        <span
+            @class(['dark:text-white' => config('tables.dark_mode')])
+            x-text="window.pluralize(@js(__('tables::table.selection_indicator.selected_count')), selectedRecords.length, { count: selectedRecords.length })"
+        ></span>
 
-    <span
-        id="{{ $this->id }}.table.selection.indicator.record-count.{{ $allSelectableRecordsCount }}"
-        x-show="{{ $allSelectableRecordsCount }} !== selectedRecords.length"
-    >
-        <button
-            x-on:click="selectAllRecords"
-            class="text-sm font-medium text-primary-600"
+        <span
+            id="{{ $this->id }}.table.selection.indicator.record-count.{{ $allSelectableRecordsCount }}"
+            x-show="{{ $allSelectableRecordsCount }} !== selectedRecords.length"
         >
-            {{ trans_choice('tables::table.selection_indicator.buttons.select_all.label', $allSelectableRecordsCount) }}.
-        </button>
-    </span>
+            <button
+                x-on:click="selectAllRecords"
+                class="text-sm font-medium text-primary-600"
+            >
+                {{ trans_choice('tables::table.selection_indicator.buttons.select_all.label', $allSelectableRecordsCount) }}.
+            </button>
+        </span>
 
-    <span>
-        <button
-            x-on:click="deselectAllRecords"
-            class="text-sm font-medium text-primary-600"
-        >
-            {{ __('tables::table.selection_indicator.buttons.deselect_all.label') }}.
-        </button>
-    </span>
+        <span>
+            <button
+                x-on:click="deselectAllRecords"
+                class="text-sm font-medium text-primary-600"
+            >
+                {{ __('tables::table.selection_indicator.buttons.deselect_all.label') }}.
+            </button>
+        </span>
+    </div>
+    
+    {{ $slot }}
 </div>
