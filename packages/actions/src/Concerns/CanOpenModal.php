fd2dbbe0f6c68eab79921e5e1c237d903f490241
danharrin <danharrin@users.noreply.github.com>
2023-06-20 10:17:50 +0000

    chore: fix code style

--- packages/actions/src/Concerns/CanOpenModal.php
+++ packages/actions/src/Concerns/CanOpenModal.php
@@ -90,7 +90,7 @@ trait CanOpenModal
     }
 
     /**
-     * @param string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | Closure | null $color
+     * @param  string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | Closure | null  $color
      */
     public function modalIconColor(string | array | Closure | null $color = null): static
     {
