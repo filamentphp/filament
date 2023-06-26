5edc66895bf26d7851e1b308dd64616869ba4997
Morry <web@in-mente.it>
2023-06-26 13:09:48 +0200

    fix: style format

--- packages/tables/src/Concerns/CanReorderRecords.php
+++ packages/tables/src/Concerns/CanReorderRecords.php
@@ -7,6 +7,7 @@ use Filament\Tables\Contracts\HasRelationshipTable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+
 trait CanReorderRecords
 {
     public bool $isTableReordering = false;
