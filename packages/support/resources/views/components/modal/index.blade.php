97d0e975e12b0a6cac761b6985dcb12b6747cf12
Ben <howdu@users.noreply.github.com>
2023-07-02 19:54:37 +0100

    Revert "Add scrollContent to modal"
    This reverts commit c67fc0205f8570264613c27b3ed511f135850b11.

--- packages/support/resources/views/components/modal/index.blade.php
+++ packages/support/resources/views/components/modal/index.blade.php
@@ -13,7 +13,6 @@
     'hrComponent' => 'filament-support::hr',
     'id' => null,
     'openEventName' => 'open-modal',
-    'scrollContent' => false,
     'slideOver' => false,
     'subheading' => null,
     'subheadingComponent' => 'filament-support::modal.subheading',
@@ -91,7 +90,6 @@
                 $attributes->class([
                     'pointer-events-none relative w-full cursor-pointer transition',
                     'my-auto p-4' => ! $slideOver,
-                    'flex shrink max-h-screen ' => $scrollContent,
                 ])
             }}
         >
@@ -169,7 +167,7 @@
 
                 <div
                     @class([
-                        'flex h-full flex-col' => ($width === 'screen') || $slideOver || $scrollContent,
+                        'flex h-full flex-col' => ($width === 'screen') || $slideOver,
                     ])
                 >
                     <div class="space-y-2">
@@ -190,7 +188,7 @@
                     <div
                         @class([
                             'filament-modal-content space-y-2 p-2',
-                            'flex-1 overflow-y-auto' => ($width === 'screen') || $slideOver || $scrollContent,
+                            'flex-1 overflow-y-auto' => ($width === 'screen') || $slideOver,
                         ])
                     >
                         @if ($heading || $subheading)
