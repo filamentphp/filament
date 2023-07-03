70e21972dc0c5cb0f40778d3d5cd106ebd99709e
Dan Harrin <git@danharrin.com>
2023-07-03 09:42:33 +0100

    Revert "Merge branch '2.x' of https://github.com/filamentphp/filament into 2.x"
    This reverts commit 416a75fcd49ef597c3e1faeed7488377c60a229a, reversing
    changes made to a3753e402ff4720e953110b5704bad115c6ac9fa.

--- packages/admin/src/Resources/Resource.php
+++ packages/admin/src/Resources/Resource.php
@@ -135,10 +135,6 @@ class Resource
             return true;
         }
 
-        if (method_exists($policy, 'before') && is_bool($condition = $policy->before($user, $action))) {
-            return $condition;
-        }
-
         if (! method_exists($policy, $action)) {
             return true;
         }
