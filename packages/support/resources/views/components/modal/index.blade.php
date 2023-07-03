5aeedeadfa328e0d98f627e2c4d2cb68f84d23ee
Dan Harrin <git@danharrin.com>
2023-07-03 09:05:39 +0100

    Update index.blade.php

--- packages/support/resources/views/components/modal/index.blade.php
+++ packages/support/resources/views/components/modal/index.blade.php
@@ -13,7 +13,7 @@
     'hrComponent' => 'filament-support::hr',
     'id' => null,
     'openEventName' => 'open-modal',
-    'scrollContent' => false,
+    'scrollableContent' => false,
     'slideOver' => false,
     'subheading' => null,
     'subheadingComponent' => 'filament-support::modal.subheading',
@@ -91,7 +91,7 @@
                 $attributes->class([
                     'pointer-events-none relative w-full cursor-pointer transition',
                     'my-auto p-4' => ! $slideOver,
-                    'flex max-h-screen shrink' => $scrollContent,
+                    'flex max-h-screen shrink' => $scrollableContent,
                 ])
             }}
         >
@@ -169,7 +169,7 @@
 
                 <div
                     @class([
-                        'flex h-full flex-col' => ($width === 'screen') || $slideOver || $scrollContent,
+                        'flex h-full flex-col' => ($width === 'screen') || $slideOver || $scrollableContent,
                     ])
                 >
                     <div class="space-y-2">
@@ -190,7 +190,7 @@
                     <div
                         @class([
                             'filament-modal-content space-y-2 p-2',
-                            'flex-1 overflow-y-auto' => ($width === 'screen') || $slideOver || $scrollContent,
+                            'flex-1 overflow-y-auto' => ($width === 'screen') || $slideOver || $scrollableContent,
                         ])
                     >
                         @if ($heading || $subheading)
