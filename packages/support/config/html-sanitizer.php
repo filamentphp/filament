77a5bc71bf6b47382f7f16672a3eda35d5d395a5
Adam Weston <adam@awcodes.com>
2023-07-06 19:05:31 -0400

    Feat: Allow style attributes in html sanitizer config

--- packages/support/config/html-sanitizer.php
+++ packages/support/config/html-sanitizer.php
@@ -21,11 +21,11 @@ return [
      */
     'tags' => [
         'abbr' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'a' => [
 
-            'allowed_attributes' => ['href', 'title', 'class'],
+            'allowed_attributes' => ['href', 'title', 'class', 'style'],
 
             /*
              * If an array is provided, links targeting other hosts than one in this array
@@ -51,68 +51,68 @@ return [
 
         ],
         'blockquote' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'br' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'caption' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'code' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'dd' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'del' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'details' => [
-            'allowed_attributes' => ['open'],
+            'allowed_attributes' => ['open', 'style'],
         ],
         'div' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'dl' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'dt' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'em' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'figcaption' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'figure' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'h1' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'h2' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'h3' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'h4' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'h5' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'h6' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'hr' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'iframe' => [
 
-            'allowed_attributes' => ['src', 'width', 'height', 'frameborder', 'title', 'allow', 'allowfullscreen', 'class'],
+            'allowed_attributes' => ['src', 'width', 'height', 'frameborder', 'title', 'allow', 'allowfullscreen', 'class', 'style'],
 
             /*
              * If an array is provided, iframes relying on other hosts than one in this array
@@ -133,7 +133,7 @@ return [
         ],
         'img' => [
 
-            'allowed_attributes' => ['src', 'alt', 'title', 'class'],
+            'allowed_attributes' => ['src', 'alt', 'title', 'class', 'style'],
 
             /*
              * If an array is provided, images relying on other hosts than one in this array
@@ -158,76 +158,76 @@ return [
 
         ],
         'i' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'li' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'ol' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'pre' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'p' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'q' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'rp' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'rt' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'ruby' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'small' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'span' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'strong' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'sub' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'summary' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'sup' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'table' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'tbody' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'td' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'tfoot' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'thead' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'th' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'tr' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'u' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
         'ul' => [
-            'allowed_attributes' => ['class'],
+            'allowed_attributes' => ['class', 'style'],
         ],
     ],
 
