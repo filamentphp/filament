8da52759a82a3f468eee59cf04eaafaf7cdbaa44
Zep Fietje <hey@zepfietje.com>
2023-07-07 11:57:44 +0200

    wip

--- packages/forms/resources/views/components/builder/block.blade.php
+++ packages/forms/resources/views/components/builder/block.blade.php
@@ -1,5 +1,9 @@
 <div
-    {{ $attributes->merge($getExtraAttributes(), escape: false)->class(['filament-forms-builder-component-block py-8']) }}
+    {{
+        $attributes
+            ->merge($getExtraAttributes(), escape: false)
+            ->class(['filament-forms-builder-component-block py-8'])
+    }}
 >
     {{ $getChildComponentContainer() }}
 </div>
