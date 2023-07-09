c27a959e906cea9731543bd5e8ada568c1e4e937
Zep Fietje <hey@zepfietje.com>
2023-07-09 17:25:40 +0200

    checkbox

--- packages/forms/resources/views/components/checkbox.blade.php
+++ packages/forms/resources/views/components/checkbox.blade.php
@@ -19,9 +19,9 @@
                     ->merge($getExtraAttributes(), escape: false)
                     ->merge($getExtraInputAttributes(), escape: false)
                     ->class([
-                        'filament-forms-checkbox-component rounded text-primary-600 shadow-sm transition duration-75 focus:ring-2 disabled:opacity-70 dark:bg-gray-700 dark:checked:bg-primary-500',
-                        'border-gray-300 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:focus:border-primary-500' => ! $errors->has($statePath),
-                        'border-danger-600 ring-danger-600 dark:border-danger-400 dark:ring-danger-400' => $errors->has($statePath),
+                        'filament-forms-checkbox-component rounded border-none bg-white shadow-sm ring-1 transition duration-75 checked:ring-0 focus:ring-2 focus:ring-offset-0 disabled:bg-gray-50 disabled:text-gray-50 disabled:checked:bg-current disabled:checked:text-gray-400 dark:bg-gray-900 dark:disabled:bg-gray-950 dark:disabled:checked:bg-gray-600',
+                        'text-primary-600 ring-gray-950/10 focus:ring-primary-600 checked:focus:ring-primary-500/50 dark:ring-white/20 dark:checked:bg-primary-600 dark:focus:ring-primary-600 dark:checked:focus:ring-primary-500/50' => ! $errors->has($statePath),
+                        'text-danger-600 ring-danger-600 focus:ring-danger-600 checked:focus:ring-danger-500/50 dark:ring-danger-400 dark:checked:bg-danger-600 dark:focus:ring-danger-600 dark:checked:focus:ring-danger-500/50' => $errors->has($statePath),
                     ])
             }}
         />
