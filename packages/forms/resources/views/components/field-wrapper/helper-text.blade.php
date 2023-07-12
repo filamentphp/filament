9703ff64e24e8aab23e4d416f16b04e7bc88860c
Zep Fietje <hey@zepfietje.com>
2023-07-12 10:57:00 +0200

    wip

--- packages/forms/resources/views/components/field-wrapper/helper-text.blade.php
+++ packages/forms/resources/views/components/field-wrapper/helper-text.blade.php
@@ -1,5 +1,5 @@
 <div
-    {{ $attributes->class(['filament-forms-field-wrapper-helper-text text-sm text-gray-600 dark:text-gray-300']) }}
+    {{ $attributes->class(['filament-forms-field-wrapper-helper-text text-sm text-gray-500']) }}
 >
     {{ $slot }}
 </div>
