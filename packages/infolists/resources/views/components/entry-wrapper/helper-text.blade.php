22e2168ab955a66c0132b1b6b148b1996c72f7b0
Zep Fietje <hey@zepfietje.com>
2023-07-12 11:00:35 +0200

    wip

--- packages/infolists/resources/views/components/entry-wrapper/helper-text.blade.php
+++ packages/infolists/resources/views/components/entry-wrapper/helper-text.blade.php
@@ -1,5 +1,5 @@
 <div
-    {{ $attributes->class(['filament-infolists-entry-wrapper-helper-text text-sm text-gray-600 dark:text-gray-300']) }}
+    {{ $attributes->class(['filament-infolists-entry-wrapper-helper-text text-sm text-gray-500']) }}
 >
     {{ $slot }}
 </div>
