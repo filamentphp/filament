87f1009f855e0c9a8b5f69d634dbe87c621256fe
Zep Fietje <hey@zepfietje.com>
2023-07-13 19:56:10 +0200

    wip

--- packages/panels/resources/views/components/avatar/index.blade.php
+++ packages/panels/resources/views/components/avatar/index.blade.php
@@ -4,9 +4,12 @@
 
 <div
     {{
-        $attributes->class([
-            'h-9 w-9 rounded-full bg-cover bg-center ring-1 ring-inset ring-gray-950/10 dark:ring-white/20',
-        ])
+        $attributes
+            ->class([
+                'h-9 w-9 rounded-full bg-cover bg-center ring-1 ring-inset ring-gray-950/10 dark:ring-white/20',
+            ])
+            ->style([
+                "background-image: url('{$src}')",
+            ])
     }}
-    style="background-image: url('{{ $src }}')"
 ></div>
