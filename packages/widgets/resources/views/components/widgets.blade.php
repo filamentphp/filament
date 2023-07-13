faffadc0035442b91b2ee64c3cab3d7b86651a02
Dan Harrin <git@danharrin.com>
2023-07-13 11:13:36 +0100

    Update widgets.blade.php

--- packages/widgets/resources/views/components/widgets.blade.php
+++ packages/widgets/resources/views/components/widgets.blade.php
@@ -13,7 +13,7 @@
     :lg="$columns['lg'] ?? ($columns ? (is_array($columns) ? null : $columns) : 2)"
     :xl="$columns['xl'] ?? null"
     :two-xl="$columns['2xl'] ?? null"
-    class="fi-widgets mb-6 gap-4 lg:gap-8"
+    class="fi-wi mb-6 gap-4 lg:gap-8"
 >
     @foreach ($widgets as $key => $widget)
         @if ($widget::canView())
