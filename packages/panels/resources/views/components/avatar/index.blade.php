3aaebfd2206bf2ba8bcc447ef1a65f31e8d9abb2
Zep Fietje <hey@zepfietje.com>
2023-07-13 19:55:14 +0200

    avatar

--- packages/panels/resources/views/components/avatar/index.blade.php
+++ packages/panels/resources/views/components/avatar/index.blade.php
@@ -1,19 +1,11 @@
 @props([
-    'size' => 'md',
     'src',
 ])
 
 <div
     {{
         $attributes->class([
-            'rounded-full bg-gray-200 bg-cover bg-center dark:bg-gray-900',
-            match ($size) {
-                'xs' => 'h-6 w-6',
-                'sm' => 'h-8 w-8',
-                'md' => 'h-10 w-10',
-                'lg' => 'h-12 w-12',
-                default => $size,
-            },
+            'h-9 w-9 rounded-full bg-cover bg-center ring-1 ring-inset ring-gray-950/10 dark:ring-white/20',
         ])
     }}
     style="background-image: url('{{ $src }}')"
