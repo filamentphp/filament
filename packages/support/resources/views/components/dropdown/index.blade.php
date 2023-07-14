110fc869e6c6335834c69883993c7282c3c61d0c
Zep Fietje <hey@zepfietje.com>
2023-07-14 13:57:24 +0200

    wip

--- packages/support/resources/views/components/dropdown/index.blade.php
+++ packages/support/resources/views/components/dropdown/index.blade.php
@@ -13,9 +13,11 @@
         toggle: function (event) {
             $refs.panel.toggle(event)
         },
+
         open: function (event) {
             $refs.panel.open(event)
         },
+
         close: function (event) {
             $refs.panel.close(event)
         },
@@ -30,20 +32,17 @@
     </div>
 
     <div
-        x-ref="panel"
-        x-float{{ $placement ? ".placement.{$placement}" : '' }}.flip{{ $shift ? '.shift' : '' }}{{ $teleport ? '.teleport' : '' }}{{ $offset ? '.offset' : '' }}="{ offset: {{ $offset }} }"
         x-cloak
-        x-transition:enter-start="scale-95 opacity-0"
-        x-transition:leave-end="scale-95 opacity-0"
+        x-float{{ $placement ? ".placement.{$placement}" : '' }}.flip{{ $shift ? '.shift' : '' }}{{ $teleport ? '.teleport' : '' }}{{ $offset ? '.offset' : '' }}="{ offset: {{ $offset }} }"
+        x-ref="panel"
+        x-transition:enter-start="opacity-0"
+        x-transition:leave-end="opacity-0"
         @if ($attributes->has('wire:key'))
             wire:ignore.self
             wire:key="{{ $attributes->get('wire:key') }}.panel"
         @endif
-        @if ($maxHeight)
-            style="max-height: {{ $maxHeight }}"
-        @endif
         @class([
-            'fi-dropdown-panel absolute z-10 w-screen divide-y divide-gray-100 rounded-lg bg-white shadow-lg ring-1 ring-gray-950/5 transition dark:divide-gray-800 dark:bg-gray-900 dark:ring-white/10',
+            'fi-dropdown-panel absolute z-10 w-screen divide-y divide-gray-100 rounded-lg bg-white shadow-lg ring-1 ring-gray-950/5 transition dark:divide-white/10 dark:bg-gray-900 dark:ring-white/10',
             match ($width) {
                 'xs' => 'max-w-xs',
                 'sm' => 'max-w-sm',
@@ -60,6 +59,9 @@
                 default => $width,
             },
             'overflow-y-auto' => $maxHeight,
+        ])
+        @style([
+            "max-height: {$maxHeight}" => $maxHeight,
         ])
     >
         {{ $slot }}
