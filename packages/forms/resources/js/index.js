033e15056ad8907a634ef55c1538100400ce7394
Dan Harrin <git@danharrin.com>
2023-07-16 07:15:55 +0100

    wip

--- packages/forms/resources/js/index.js
+++ packages/forms/resources/js/index.js
@@ -1,22 +1,10 @@
 import mask from '@alpinejs/mask'
 
 import '../css/components/date-time-picker.css'
-
-import 'cropperjs/dist/cropper.min.css'
-import 'filepond/dist/filepond.min.css'
-import 'filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css'
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
-import 'filepond-plugin-media-preview/dist/filepond-plugin-media-preview.css'
 import '../css/components/file-upload.css'
-
-import 'easymde/dist/easymde.min.css'
 import '../css/components/markdown-editor.css'
-
-import 'trix/dist/trix.css'
 import '../css/components/rich-editor.css'
-
 import '../css/components/select.css'
-
 import '../css/components/tags-input.css'
 
 document.addEventListener('alpine:init', () => {
