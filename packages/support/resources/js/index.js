13553e0c17f54062134f122a87eae87cdd23a744
Dan Harrin <git@danharrin.com>
2023-07-17 18:45:17 +0100

    Remove forms index.css

--- packages/support/resources/js/index.js
+++ packages/support/resources/js/index.js
@@ -3,6 +3,9 @@ import AlpineLazyLoadAssets from 'alpine-lazy-load-assets'
 import Sortable from './sortable'
 import Tooltip from '@ryangjchandler/alpine-tooltip'
 
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
+
 document.addEventListener('alpine:init', () => {
     window.Alpine.plugin(AlpineFloatingUI)
     window.Alpine.plugin(AlpineLazyLoadAssets)
