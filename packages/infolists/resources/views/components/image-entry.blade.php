684d056898af50fe0801e2451319a405b44bc296
Zep Fietje <hey@zepfietje.com>
2023-07-17 13:24:00 +0200

    wip

--- packages/infolists/resources/views/components/image-entry.blade.php
+++ packages/infolists/resources/views/components/image-entry.blade.php
@@ -1,47 +1,51 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
     @php
+        $height = $getHeight();
         $isCircular = $isCircular();
         $isSquare = $isSquare();
-        $height = $getHeight();
-        $width = $getWidth() ?? ($isCircular || $isSquare ? $height : null);
+        $path = $getImagePath();
+        $width = $getWidth() ?? (($isCircular || $isSquare) ? $height : null);
     @endphp
 
     <div
         {{
-            $attributes->merge($getExtraAttributes(), escape: false)->class([
-                'fi-in-image flex',
-                match ($getAlignment()) {
-                    'center' => 'justify-center',
-                    'end' => 'justify-end',
-                    'left' => 'justify-left',
-                    'right' => 'justify-right',
-                    'start', null => 'justify-start',
-                },
-            ])
+            $attributes
+                ->merge($getExtraAttributes(), escape: false)
+                ->class([
+                    'fi-in-image flex',
+                    match ($getAlignment()) {
+                        'center' => 'justify-center',
+                        'end' => 'justify-end',
+                        'left' => 'justify-left',
+                        'right' => 'justify-right',
+                        'start', null => 'justify-start',
+                    },
+                ])
         }}
     >
         <div
-            style="
-                @if ($height) height: {{ $height }}; @endif
-                @if ($width) width: {{ $width }}; @endif
-            "
             @class([
                 'inline-block',
                 'overflow-hidden' => $isCircular || $isSquare,
                 'rounded-full' => $isCircular,
             ])
+            @style([
+                "height: {$height}" => $height,
+                "width: {$width}" => $width,
+            ])
         >
-            @if ($path = $getImagePath())
+            @if ($path)
                 <img
                     src="{{ $path }}"
-                    style="
-                        @if ($height) height: {{ $height }}; @endif
-                        @if ($width) width: {{ $width }}; @endif
-                    "
                     {{
-                        $getExtraImgAttributeBag()->class([
-                            'object-cover object-center' => $isCircular || $isSquare,
-                        ])
+                        $getExtraImgAttributeBag()
+                            ->class([
+                                'object-cover object-center' => $isCircular || $isSquare,
+                            ])
+                            ->style([
+                                "height: {$height}" => $height,
+                                "width: {$width}" => $width,
+                            ])
                     }}
                 />
             @endif
