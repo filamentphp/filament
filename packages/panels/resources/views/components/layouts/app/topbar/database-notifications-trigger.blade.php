e478d09beacc000938527f72064f92b6a7200b79
Zep Fietje <hey@zepfietje.com>
2023-07-19 16:50:04 +0200

    wip

--- packages/panels/resources/views/components/layouts/app/topbar/database-notifications-trigger.blade.php
+++ packages/panels/resources/views/components/layouts/app/topbar/database-notifications-trigger.blade.php
@@ -1,8 +1,8 @@
 <x-filament::icon-button
     :badge="$unreadNotificationsCount"
     color="gray"
+    icon="heroicon-o-bell"
     icon-alias="panels::topbar.open-database-notifications-button"
     icon-size="lg"
-    icon="heroicon-o-bell"
     :label="__('filament::layout.actions.open_database_notifications.label')"
 />
