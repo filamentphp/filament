ac0c7de5d832bdcba6126ba1365b07bd45b6992d
awcodes <awcodes@users.noreply.github.com>
2023-07-22 00:40:36 +0000

    chore: fix code style

--- packages/forms/resources/views/components/rich-editor.blade.php
+++ packages/forms/resources/views/components/rich-editor.blade.php
@@ -474,7 +474,7 @@
                     wire:ignore
                     {{
                         $getExtraInputAttributeBag()->class([
-                            'prose min-h-[theme(spacing.48)] max-w-none !border-none  focus:outline-none focus-visible:outline-none px-3 py-1.5 text-base text-gray-950 dark:prose-invert dark:text-white sm:text-sm sm:leading-6',
+                            'prose min-h-[theme(spacing.48)] max-w-none !border-none px-3 py-1.5 text-base text-gray-950 dark:prose-invert focus:outline-none focus-visible:outline-none dark:text-white sm:text-sm sm:leading-6',
                         ])
                     }}
                 ></trix-editor>
