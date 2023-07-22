b3bb51904fd35c6e5bff821b3901deeeccb54d3e
Sumardi Shukor <me@sumardi.net>
2023-07-22 14:10:57 +0800

    Fix missing malay translations

--- packages/notifications/resources/lang/ms/database.php
+++ packages/notifications/resources/lang/ms/database.php
@@ -23,6 +23,24 @@ return [
             'description' => 'Sila semak semula kemudian',
         ],
 
+        'pagination' => [
+
+            'label' => 'Navigasi penomboran',
+
+            'actions' => [
+
+                'next' => [
+                    'label' => 'Seterusnya',
+                ],
+
+                'previous' => [
+                    'label' => 'Sebelumnya',
+                ],
+
+            ],
+
+        ],
+
     ],
 
 ];
