6a9a09ca8df858fd18988d6df4ccf6b6f2a2bd1a
Zep Fietje <hey@zepfietje.com>
2023-07-27 12:11:55 +0200

    fix

--- packages/panels/src/FontProviders/Contracts/FontProvider.php
+++ packages/panels/src/FontProviders/Contracts/FontProvider.php
@@ -6,5 +6,8 @@ use Illuminate\Contracts\Support\Htmlable;
 
 interface FontProvider
 {
+    /**
+     * @param  array<string, int>  $weights
+     */
     public function getHtml(string $family, array $weights, ?string $url = null): Htmlable;
 }
