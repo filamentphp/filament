c5fb240e387cd14daf0e85a82c83611751396b10
sumardi <sumardi@users.noreply.github.com>
2023-07-29 19:04:39 +0000

    chore: fix code style

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -371,7 +371,7 @@
                 wire:poll.{{ $pollingInterval }}
             @endif
             @class([
-                'fi-ta-content divide-y divide-gray-200 overflow-x-auto relative dark:divide-white/10',
+                'fi-ta-content relative divide-y divide-gray-200 overflow-x-auto dark:divide-white/10',
                 '!border-t-0' => ! $hasHeader,
             ])
         >
