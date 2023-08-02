efba8e13798abe91cbe5f1f83835d1db1eb060df
Sebastiaan Kloos <sebastiaan@codecycler.com>
2023-08-02 11:43:02 +0200

    Added the option to add an image as icon

--- packages/support/resources/views/components/icon.blade.php
+++ packages/support/resources/views/components/icon.blade.php
@@ -8,8 +8,10 @@
     $icon = ($alias ? \Filament\Support\Facades\FilamentIcon::resolve($alias) : null) ?: $icon;
 @endphp
 
-@if (is_string($icon))
+@if (is_string($icon) && !str_contains($icon, '/'))
     @svg($icon, $class, array_filter($attributes->getAttributes()))
+@elseif(is_string($icon) && str_contains($icon, '/'))
+    <img {{ $attributes->class($class) }} src="{{ $icon }}"   />
 @else
     <div {{ $attributes->class($class) }}>
         {{ $icon ?? $slot }}
