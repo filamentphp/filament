81fc4a6a88c9a5d2d71c411bdbbb09e22f480ec5
wallo <andrewdwallo@gmail.com>
2023-08-02 16:26:05 -0400

    Improve default state handling for boolean values

--- packages/forms/src/Components/Concerns/HasState.php
+++ packages/forms/src/Components/Concerns/HasState.php
@@ -85,6 +85,10 @@ trait HasState
 
     public function default(mixed $state): static
     {
+        if (is_bool($state)) {
+            $state = $state ? '1' : '0';
+        }
+
         $this->defaultState = $state;
         $this->hasDefaultState = true;
 
