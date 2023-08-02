a21ebf227931f23d33b737f30eee81dee2d99ff9
Sebastiaan Kloos <sebastiaan@codecycler.com>
2023-08-02 11:43:37 +0200

    Update icon.blade.php

--- packages/support/resources/views/components/icon.blade.php
+++ packages/support/resources/views/components/icon.blade.php
@@ -11,7 +11,7 @@
 @if (is_string($icon) && !str_contains($icon, '/'))
     @svg($icon, $class, array_filter($attributes->getAttributes()))
 @elseif(is_string($icon) && str_contains($icon, '/'))
-    <img {{ $attributes->class($class) }} src="{{ $icon }}"   />
+    <img {{ $attributes->class($class) }} src="{{ $icon }}" />
 @else
     <div {{ $attributes->class($class) }}>
         {{ $icon ?? $slot }}
