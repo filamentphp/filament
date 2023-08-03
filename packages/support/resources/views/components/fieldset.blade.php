b345f7accb719d688607689bc00fa29aa943dfec
Falko Woudstra <falko@coddin.nl>
2023-08-03 21:06:48 +0200

    Update fieldset styling to remove background color and add padding to legend tag for better visibility.

--- packages/support/resources/views/components/fieldset.blade.php
+++ packages/support/resources/views/components/fieldset.blade.php
@@ -5,12 +5,12 @@
 <fieldset
     {{
         $attributes->class([
-            'fi-fieldset rounded-xl bg-white p-6 shadow-sm ring-1 ring-gray-950/5 dark:bg-white/5 dark:ring-white/10',
+            'fi-fieldset rounded-xl p-6 shadow-sm ring-1 ring-gray-950/5 dark:ring-white/10',
         ])
     }}
 >
     @if (filled($label))
-        <legend class="text-sm font-medium leading-6">
+        <legend class="text-sm font-medium leading-6 bg-gray-50 dark:bg-gray-950 px-2 -mx-2">
             {{ $label }}
         </legend>
     @endif
