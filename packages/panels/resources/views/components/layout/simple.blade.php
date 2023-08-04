244170cb204c7097fb2d610b8eafec02ee3e00a3
Ash Monsh <wh7r.com@gmail.com>
2023-08-04 14:25:30 +0300

    clean up the clases

--- packages/panels/resources/views/components/layout/simple.blade.php
+++ packages/panels/resources/views/components/layout/simple.blade.php
@@ -5,10 +5,10 @@
         'subheading' => null,
     ])
 
-    <div class="fi-simple-layout flex min-h-screen items-center bord">
+    <div class="fi-simple-layout flex flex-col min-h-screen items-center">
         @if (filament()->auth()->check())
             <div
-                class="absolute end-0 top-0 flex h-16 items-center gap-x-4 pe-4 md:pe-6 lg:pe-8 bord"
+                class="absolute end-0 top-0 flex h-16 items-center gap-x-4 pe-4 md:pe-6 lg:pe-8"
             >
                 @if (filament()->hasDatabaseNotifications())
                     @livewire(Filament\Livewire\DatabaseNotifications::class, ['lazy' => true])
@@ -18,14 +18,14 @@
             </div>
         @endif
 
-        <div class="fi-simple-main-ctn w-full flex items-center flex-col bord">
+        <div class="fi-simple-main-ctn w-full flex items-center flex-grow">
             <main
-                class="bord fi-simple-main mx-auto my-16 w-full bg-white px-6 py-12 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 sm:max-w-lg sm:rounded-xl sm:px-12"
+                class="fi-simple-main mx-auto my-16 w-full bg-white px-6 py-12 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 sm:max-w-lg sm:rounded-xl sm:px-12"
             >
                 {{ $slot }}
             </main>
-
-            {{ \Filament\Support\Facades\FilamentView::renderHook('panels::footer') }}
         </div>
+
+        {{ \Filament\Support\Facades\FilamentView::renderHook('panels::footer') }}
     </div>
 </x-filament-panels::layout.base>
