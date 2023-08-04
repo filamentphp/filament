4be7380db7048a87c8547aa6f85887efd32f2ca8
Falko Woudstra <falko@coddin.nl>
2023-08-04 17:02:16 +0200

    Update fieldset styling to use border instead of ring for native border removal behind legend.

--- packages/support/resources/views/components/fieldset.blade.php
+++ packages/support/resources/views/components/fieldset.blade.php
@@ -5,12 +5,12 @@
 <fieldset
     {{
         $attributes->class([
-            'fi-fieldset rounded-xl p-6 shadow-sm ring-1 ring-gray-950/5 dark:ring-white/10',
+            'fi-fieldset rounded-xl p-6 shadow-sm border border-gray-950/5 dark:border-white/10',
         ])
     }}
 >
     @if (filled($label))
-        <legend class="text-sm font-medium leading-6 bg-gray-50 dark:bg-gray-950 px-2 -mx-2">
+        <legend class="text-sm font-medium leading-6 px-1 -mx-1">
             {{ $label }}
         </legend>
     @endif
