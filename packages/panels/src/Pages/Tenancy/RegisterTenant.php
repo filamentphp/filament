dfd68f6eaaaf951c06898fba0c5a4db27e8ca547
Iliyas M <im.smiliyas@gmail.com>
2023-08-04 15:57:21 +0530

    add authorization check for tenant registration

--- packages/panels/src/Pages/Tenancy/RegisterTenant.php
+++ packages/panels/src/Pages/Tenancy/RegisterTenant.php
@@ -11,9 +11,11 @@ use Filament\Pages\Concerns\InteractsWithFormActions;
 use Filament\Pages\SimplePage;
 use Filament\Panel;
 use Filament\Support\Exceptions\Halt;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Route;
+use function Filament\authorize;
 
 /**
  * @property Form $form
@@ -59,6 +61,8 @@ abstract class RegisterTenant extends SimplePage
 
     public function mount(): void
     {
+        abort_unless(static::canView(), 404);
+
         $this->form->fill();
     }
 
@@ -165,5 +169,14 @@ abstract class RegisterTenant extends SimplePage
     protected function hasFullWidthFormActions(): bool
     {
         return true;
+    }
+
+    public static function canView(): bool
+    {
+        try {
+            return authorize('create', Filament::getTenantModel())->allowed();
+        } catch (AuthorizationException $exception) {
+            return $exception->toResponse()->allowed();
+        }
     }
 }
