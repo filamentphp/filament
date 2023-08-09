28b8f4442873a891f6269a7374d39a5308c0557e
filacare <hansan@xincloud.com>
2023-08-09 17:07:37 +0800

    Fix fileUpload button tooltip, Fix translate

--- packages/forms/resources/lang/zh_CN/components.php
+++ packages/forms/resources/lang/zh_CN/components.php
@@ -6,24 +6,36 @@ return [
 
         'actions' => [
 
+            'clone' => [
+                'label' => '克隆',
+            ],
+
             'add' => [
-                'label' => '添加 :label',
+                'label' => '添加到 :label',
+            ],
+
+            'add_between' => [
+                'label' => '在块之间插入',
             ],
 
             'delete' => [
                 'label' => '删除',
             ],
 
+            'reorder' => [
+                'label' => '移动',
+            ],
+
             'move_down' => [
-                'label' => '下移',
+                'label' => '向下移动',
             ],
 
             'move_up' => [
-                'label' => '上移',
+                'label' => '向上移动',
             ],
 
             'collapse' => [
-                'label' => '收起',
+                'label' => '折叠',
             ],
 
             'expand' => [
@@ -31,11 +43,146 @@ return [
             ],
 
             'collapse_all' => [
-                'label' => '全部收起',
+                'label' => '全部折叠',
             ],
 
             'expand_all' => [
                 'label' => '全部展开',
+            ],
+
+        ],
+
+    ],
+
+    'checkbox_list' => [
+
+        'actions' => [
+
+            'deselect_all' => [
+                'label' => '取消全选',
+            ],
+
+            'select_all' => [
+                'label' => '全选',
+            ],
+
+        ],
+
+    ],
+
+    'file_upload' => [
+
+        'editor' => [
+
+            'actions' => [
+
+                'cancel' => [
+                    'label' => '取消',
+                ],
+
+                'drag_crop' => [
+                    'label' => '拖动模式“裁剪”',
+                ],
+
+                'drag_move' => [
+                    'label' => '拖动模式“移动”',
+                ],
+
+                'flip_horizontal' => [
+                    'label' => '水平翻转图像',
+                ],
+
+                'flip_vertical' => [
+                    'label' => '垂直翻转图像',
+                ],
+
+                'move_down' => [
+                    'label' => '向下移动图像',
+                ],
+
+                'move_left' => [
+                    'label' => '将图像移动到左侧',
+                ],
+
+                'move_right' => [
+                    'label' => '将图像移动到右侧',
+                ],
+
+                'move_up' => [
+                    'label' => '向上移动图像',
+                ],
+
+                'reset' => [
+                    'label' => '重置',
+                ],
+
+                'rotate_left' => [
+                    'label' => '将图像向左旋转',
+                ],
+
+                'rotate_right' => [
+                    'label' => '将图像向右旋转',
+                ],
+
+                'set_aspect_ratio' => [
+                    'label' => '将纵横比设置为 :ratio',
+                ],
+
+                'save' => [
+                    'label' => '保存',
+                ],
+
+                'zoom_100' => [
+                    'label' => '将图像缩放到100%',
+                ],
+
+                'zoom_in' => [
+                    'label' => '放大',
+                ],
+
+                'zoom_out' => [
+                    'label' => '缩小',
+                ],
+
+            ],
+
+            'fields' => [
+
+                'height' => [
+                    'label' => '高度',
+                    'unit' => '像素',
+                ],
+
+                'rotation' => [
+                    'label' => '旋转',
+                    'unit' => '度',
+                ],
+
+                'width' => [
+                    'label' => '宽度',
+                    'unit' => '像素',
+                ],
+
+                'x_position' => [
+                    'label' => 'X',
+                    'unit' => '像素',
+                ],
+
+                'y_position' => [
+                    'label' => 'Y',
+                    'unit' => '像素',
+                ],
+
+            ],
+
+            'aspect_ratios' => [
+
+                'label' => '纵横比',
+
+                'no_fixed' => [
+                    'label' => '自由',
+                ],
+
             ],
 
         ],
@@ -54,12 +201,16 @@ return [
                 'label' => '删除行',
             ],
 
+            'reorder' => [
+                'label' => '重新排序行',
+            ],
+
         ],
 
         'fields' => [
 
             'key' => [
-                'label' => '键名',
+                'label' => '键',
             ],
 
             'value' => [
@@ -73,16 +224,19 @@ return [
     'markdown_editor' => [
 
         'toolbar_buttons' => [
-            'attach_files' => '附件',
+            'attach_files' => '附加文件',
+            'blockquote' => '引用',
             'bold' => '加粗',
-            'bullet_list' => '普通列表',
-            'code_block' => '代码',
-            'edit' => '编辑',
+            'bullet_list' => '无序列表',
+            'code_block' => '代码块',
+            'heading' => '标题',
             'italic' => '斜体',
             'link' => '链接',
-            'ordered_list' => '数字列表',
-            'preview' => '预览',
-            'strike' => '文字中线',
+            'ordered_list' => '有序列表',
+            'redo' => '重做',
+            'strike' => '删除线',
+            'table' => '表格',
+            'undo' => '撤销',
         ],
 
     ],
@@ -92,23 +246,31 @@ return [
         'actions' => [
 
             'add' => [
-                'label' => '添加 :label',
+                'label' => '添加到 :label',
             ],
 
             'delete' => [
                 'label' => '删除',
             ],
 
+            'clone' => [
+                'label' => '克隆',
+            ],
+
+            'reorder' => [
+                'label' => '移动',
+            ],
+
             'move_down' => [
-                'label' => '下移',
+                'label' => '向下移动',
             ],
 
             'move_up' => [
-                'label' => '上移',
+                'label' => '向上移动',
             ],
 
             'collapse' => [
-                'label' => '收起',
+                'label' => '折叠',
             ],
 
             'expand' => [
@@ -116,7 +278,7 @@ return [
             ],
 
             'collapse_all' => [
-                'label' => '全部收起',
+                'label' => '全部折叠',
             ],
 
             'expand_all' => [
@@ -147,20 +309,21 @@ return [
         ],
 
         'toolbar_buttons' => [
-            'attach_files' => '附件',
+            'attach_files' => '附加文件',
             'blockquote' => '引用',
             'bold' => '加粗',
-            'bullet_list' => '普通列表',
-            'code_block' => '代码',
+            'bullet_list' => '无序列表',
+            'code_block' => '代码块',
             'h1' => '标题',
             'h2' => '标题',
             'h3' => '副标题',
             'italic' => '斜体',
             'link' => '链接',
-            'ordered_list' => '数字列表',
-            'redo' => '下一步',
-            'strike' => '文字中线',
-            'undo' => '上一步',
+            'ordered_list' => '有序列表',
+            'redo' => '重做',
+            'strike' => '删除线',
+            'underline' => '下划线',
+            'undo' => '撤销',
         ],
 
     ],
@@ -181,6 +344,28 @@ return [
                             'label' => '创建',
                         ],
 
+                        'create_another' => [
+                            'label' => '创建并继续创建',
+                        ],
+
+                    ],
+
+                ],
+
+            ],
+
+            'edit_option' => [
+
+                'modal' => [
+
+                    'heading' => '编辑',
+
+                    'actions' => [
+
+                        'save' => [
+                            'label' => '保存',
+                        ],
+
                     ],
 
                 ],
@@ -194,15 +379,17 @@ return [
             'false' => '否',
         ],
 
-        'loading_message' => '加载中...',
+        'loading_message' => '正在加载...',
 
-        'no_search_results_message' => '没有选项匹配您的搜索',
+        'max_items_message' => '只能选择 :count 个。',
 
-        'placeholder' => '选择选项',
+        'no_search_results_message' => '没有匹配您搜索的选项。',
 
-        'searching_message' => '搜索中...',
+        'placeholder' => '选择一个选项',
 
-        'search_prompt' => '输入内容以搜索...',
+        'searching_message' => '正在搜索...',
+
+        'search_prompt' => '开始输入以搜索...',
 
     ],
 
@@ -215,7 +402,7 @@ return [
         'actions' => [
 
             'previous_step' => [
-                'label' => '上一步',
+                'label' => '返回',
             ],
 
             'next_step' => [
--- packages/forms/resources/views/components/file-upload.blade.php
+++ packages/forms/resources/views/components/file-upload.blade.php
@@ -226,7 +226,7 @@
                                                         @foreach ($groupedActions as $action)
                                                             <x-filament::button
                                                                 x-tooltip="{
-                                                                    content: @js($action['label']),
+                                                                    content: {{ Illuminate\Support\Js::from($action['label']) }},
                                                                     theme: $store.theme,
                                                                 }"
                                                                 x-on:click.stop.prevent="{{ $action['alpineClickHandler'] }}"
@@ -261,7 +261,7 @@
                                                             @foreach ($ratiosChunk as $label => $ratio)
                                                                 <x-filament::button
                                                                     x-tooltip="{
-                                                                        content: @js(__('filament-forms::components.file_upload.editor.actions.set_aspect_ratio.label', ['ratio' => $label])),
+                                                                        content: {{ Illuminate\Support\Js::from(__('filament-forms::components.file_upload.editor.actions.set_aspect_ratio.label', ['ratio' => $label])) }},
                                                                         theme: $store.theme,
                                                                     }"
                                                                     x-on:click.stop.prevent="currentRatio = '{{ $label }}'; editor.setAspectRatio({{ $ratio }})"
