972a457a2695e498519518424b76e566706224ea
Hugh Messenger <hugh.messenger@gmail.com>
2023-08-13 18:40:00 -0500

    Added check for pivot table qualifier in prepareQueryForNoConstraints

--- packages/support/src/Services/RelationshipJoiner.php
+++ packages/support/src/Services/RelationshipJoiner.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Kirschbaum\PowerJoins\PowerJoins;
 
 class RelationshipJoiner
@@ -45,8 +47,6 @@ class RelationshipJoiner
 
     public function prepareQueryForNoConstraints(Relation $relationship): Builder
     {
-        ray($relationship);
-
         $relationshipQuery = $relationship->getQuery();
 
         // By default, `BelongsToMany` relationships use an inner join to scope the results to only
@@ -59,17 +59,23 @@ class RelationshipJoiner
             if ($firstRelationshipJoinClause) {
                 $firstRelationshipJoinClause->type = 'left';
 
-                // We need to move any where clauses (which will be from pivotWhere et al) in to the join's where clauses
+                // We need to move any where clauses added by pivotWhere() et al in to the join's where clauses
                 // so they are only filtering the join itself, not the entire result set, and the final query looks like ...
                 //   SELECT table.* FROM table LEFT JOIN pivot_table ON table.id = pivot.parent_id AND [pivot clauses] WHERE [query clauses]
                 // ... instead of ...
                 //   SELECT table.* FROM table LEFT JOIN pivot_table ON table.id = pivot.parent_id WHERE [pivot clauses] AND [query clauses]
-
-                $relationshipQueryWheres = $relationshipQuery->getQuery()->wheres;
+                // As far as I can tell, all clauses added with wherePivot and orWherePivot, and variants thereof, are always
+                // "Basic" with a column.  Anything added to a relationship with where(), rather than wherePivot(), will be a
+                // nested where (so no 'column').  So we don't have to check anything that doesn't have a 'column' attribute.
+                $relationshipQueryWheres = Arr::where(
+                    $relationshipQuery->getQuery()->wheres,
+                    fn (array $where) => array_key_exists('column', $where)
+                        && Str::startsWith($where['column'], $relationship->getTable() . '.')
+                );
 
                 if (filled($relationshipQueryWheres)) {
-                    $firstRelationshipJoinClause->wheres = array_merge($firstRelationshipJoinClause->wheres, $relationshipQueryWheres);
-                    $relationshipQuery->getQuery()->wheres = [];
+                    $firstRelationshipJoinClause->wheres   = array_merge($firstRelationshipJoinClause->wheres, $relationshipQueryWheres);
+                    $relationshipQuery->getQuery()->wheres = Arr::except($relationshipQuery->getQuery()->wheres, array_keys($relationshipQueryWheres));
                 }
             }
 
