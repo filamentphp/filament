767be4898ee5a9004c1fd361142f2e75944194d0
Guilherme Saade <saade@outlook.com.br>
2023-08-14 11:38:48 -0300

    add closure customization to `transformOptionsForJs`

--- packages/forms/src/Components/Select.php
+++ packages/forms/src/Components/Select.php
@@ -84,6 +84,8 @@ class Select extends Field implements Contracts\HasAffixActions, Contracts\HasNe
 
     protected ?Closure $getSelectedRecordUsing = null;
 
+    protected ?Closure $transformOptionsForJsUsing = null;
+
     /**
      * @var array<string> | null
      */
@@ -167,6 +169,15 @@ class Select extends Field implements Contracts\HasAffixActions, Contracts\HasNe
             }
 
             return $labels;
+        });
+
+        $this->transformOptionsForJsUsing(static function (array $options): array {
+            return collect($options)
+                ->map(fn ($label, $value): array => is_array($label)
+                    ? ['label' => $value, 'choices' => $this->transformOptionsForJs($label)]
+                    : ['label' => $label, 'value' => strval($value)])
+                ->values()
+                ->all();
         });
 
         $this->placeholder(fn (Select $component): ?string => $component->isDisabled() ? null : __('filament-forms::components.select.placeholder'));
@@ -502,6 +513,13 @@ class Select extends Field implements Contracts\HasAffixActions, Contracts\HasNe
         return $this;
     }
 
+    public function transformOptionsForJsUsing(?Closure $callback): static
+    {
+        $this->transformOptionsForJsUsing = $callback;
+
+        return $this;
+    }
+
     /**
      * @param  bool | array<string> | Closure  $condition
      */
@@ -647,12 +665,19 @@ class Select extends Field implements Contracts\HasAffixActions, Contracts\HasNe
      */
     protected function transformOptionsForJs(array $options): array
     {
-        return collect($options)
-            ->map(fn ($label, $value): array => is_array($label)
-                ? ['label' => $value, 'choices' => $this->transformOptionsForJs($label)]
-                : ['label' => $label, 'value' => strval($value)])
-            ->values()
-            ->all();
+        if(! $options) {
+            return [];
+        }
+
+        $transformedOptions = $this->evaluate($this->transformOptionsForJsUsing, [
+            'options' => $options,
+        ]);
+
+        if ($transformedOptions instanceof Arrayable) {
+            return $transformedOptions->toArray();
+        }
+
+        return $transformedOptions;
     }
 
     public function isMultiple(): bool
