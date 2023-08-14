f040ba92b76af3042067f677eaf87265e1eff813
Dan Harrin <git@danharrin.com>
2023-08-14 15:25:05 +0100

    wip

--- tests/database/factories/TeamFactory.php
+++ tests/database/factories/TeamFactory.php
@@ -3,9 +3,7 @@
 namespace Filament\Tests\Database\Factories;
 
 use Filament\Tests\Models\Team;
-use Filament\Tests\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class TeamFactory extends Factory
 {
