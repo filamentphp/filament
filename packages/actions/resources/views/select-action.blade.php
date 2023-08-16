c8a02991f0d9fabdb8d88f63fcc267af26fc6739
Lukas Frey <mail@lukasfrey.cz>
2023-08-16 19:02:13 +0200

    fix: PR changes
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

--- packages/actions/resources/views/select-action.blade.php
+++ packages/actions/resources/views/select-action.blade.php
@@ -1,6 +1,7 @@
 @php
     $id = $getId();
     $isDisabled = $isDisabled();
+    $attributes = $getExtraAttributeBag();
 @endphp
 
 <div class="fi-ac-select-action">
@@ -8,21 +9,23 @@
         {{ $getLabel() }}
     </label>
 
-    <x-filament::input.wrapper :disabled="$isDisabled">
-        <x-filament::input.select
-            :disabled="$isDisabled"
-            :id="$id"
-            :attributes="$getExtraAttributeBag()"
-        >
-            @if (($placeholder = $getPlaceholder()) !== null)
-                <option value="">{{ $placeholder }}</option>
-            @endif
+    <x-filament::input.select
+        :disabled="$isDisabled"
+        :id="$id"
+        :attributes="
+                \Filament\Support\prepare_inherited_attributes(
+                    $attributes
+                )
+            "
+    >
+        @if (($placeholder = $getPlaceholder()) !== null)
+            <option value="">{{ $placeholder }}</option>
+        @endif
 
-            @foreach ($getOptions() as $value => $label)
-                <option value="{{ $value }}">
-                    {{ $label }}
-                </option>
-            @endforeach
-        </x-filament::input.select>
-    </x-filament::input.wrapper>
+        @foreach ($getOptions() as $value => $label)
+            <option value="{{ $value }}">
+                {{ $label }}
+            </option>
+        @endforeach
+    </x-filament::input.select>
 </div>
