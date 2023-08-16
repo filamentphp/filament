b75df67f7ec2567a8aa5102c73b78652a401a818
AlanD20 <aland20@pm.me>
2023-08-16 21:53:20 +0200

    Delete ku translation

--- packages/support/resources/lang/ku/components/button.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'messages' => [
-        'uploading_file' => 'بارکردنی فایل...',
-    ],
-
-];
--- packages/support/resources/lang/ku/components/copyable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'messages' => [
-        'copied' => 'لەبەرگیرا',
-    ],
-
-];
