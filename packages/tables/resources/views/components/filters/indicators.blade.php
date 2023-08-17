4231afe58e3dd48048fa7e1e6ba1572c1fe51475
Kenneth Sese <ksese@hope4tomorrow.net>
2023-08-17 04:27:07 -0600

    Update indicators.blade.php

--- packages/tables/resources/views/components/filters/indicators.blade.php
+++ packages/tables/resources/views/components/filters/indicators.blade.php
@@ -5,9 +5,9 @@
 <div
     {{ $attributes->class(['fi-ta-filter-indicators flex items-start justify-between gap-x-3 bg-gray-50 px-3 py-1.5 dark:bg-white/5 sm:px-6']) }}
 >
-    <div class="flex flex-col gap-x-3 gap-y-1 sm:flex-row sm:items-center">
+    <div class="flex flex-col gap-x-3 gap-y-1 sm:flex-row">
         <span
-            class="text-sm font-medium leading-6 text-gray-700 dark:text-gray-200"
+            class="text-sm font-medium leading-6 text-gray-700 whitespace-nowrap dark:text-gray-200"
         >
             {{ __('filament-tables::table.filters.indicator') }}
         </span>
