bd35b98925fedcbb89b9463e65bcd82e00171b5b
Dan Harrin <git@danharrin.com>
2023-08-17 09:12:58 +0100

    Remove union type usage

--- packages/tables/src/Concerns/HasActions.php
+++ packages/tables/src/Concerns/HasActions.php
@@ -25,7 +25,10 @@ trait HasActions
      */
     public ?array $mountedTableActionsData = [];
 
-    public int | string | null $mountedTableActionRecord = null;
+    /**
+     * @var int | string | null
+     */
+    public $mountedTableActionRecord = null;
 
     protected ?Model $cachedMountedTableActionRecord = null;
 
