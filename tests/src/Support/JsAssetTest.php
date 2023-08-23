ce830d15bcea86bd19be18dfdb0675c877d98f6b
Dan Harrin <git@danharrin.com>
2023-08-23 13:05:53 +0300

    Update JsAssetTest.php

--- tests/src/Support/JsAssetTest.php
+++ tests/src/Support/JsAssetTest.php
@@ -8,14 +8,16 @@ uses(TestCase::class);
 
 it('registers script tag with type of module', function () {
     FilamentAsset::register([
-        Js::make('test-js', 'test.js')
+        Js::make('test-js', 'test.js'),
     ]);
 
-    expect(FilamentAsset::renderScripts())->not->toContain('type="module"');
+    expect(FilamentAsset::renderScripts())
+        ->not->toContain('type="module"');
 
     FilamentAsset::register([
-        Js::make('test-js-with-module', 'test.js')->module()
+        Js::make('test-js-with-module', 'test.js')->module(),
     ]);
 
-    expect(FilamentAsset::renderScripts())->toContain('type="module"');
+    expect(FilamentAsset::renderScripts())
+        ->toContain('type="module"');
 });
