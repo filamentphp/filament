494ebfa27fbc23cf0ba1213b9797009c932040aa
Lambert Traccard <lambert.traccard@gmail.com>
2023-08-26 09:39:12 +0200

    Update CreateRecord.php

--- packages/panels/src/Resources/Pages/CreateRecord.php
+++ packages/panels/src/Resources/Pages/CreateRecord.php
@@ -283,14 +283,14 @@ class CreateRecord extends Page
     {
         $resource = static::getResource();
 
-        if ($resource::hasPage('edit') && $resource::canEdit($this->getRecord())) {
-            return $resource::getUrl('edit', ['record' => $this->getRecord(), ...$this->getRedirectUrlParameters()]);
-        }
-
         if ($resource::hasPage('view') && $resource::canView($this->getRecord())) {
             return $resource::getUrl('view', ['record' => $this->getRecord(), ...$this->getRedirectUrlParameters()]);
         }
 
+        if ($resource::hasPage('edit') && $resource::canEdit($this->getRecord())) {
+            return $resource::getUrl('edit', ['record' => $this->getRecord(), ...$this->getRedirectUrlParameters()]);
+        }
+        
         return $resource::getUrl('index');
     }
 
