ab2ed4662d91c1fcd8c32cfa1096e6f2bfde2050
zepfietje <zepfietje@users.noreply.github.com>
2023-08-28 07:53:10 +0000

    chore: fix code style

--- packages/panels/resources/views/components/sidebar/index.blade.php
+++ packages/panels/resources/views/components/sidebar/index.blade.php
@@ -83,7 +83,9 @@
         @endif
     </header>
 
-    <nav class="fi-sidebar-nav flex flex-col gap-y-7 overflow-y-auto overflow-x-hidden px-6 py-8">
+    <nav
+        class="fi-sidebar-nav flex flex-col gap-y-7 overflow-y-auto overflow-x-hidden px-6 py-8"
+    >
         {{ \Filament\Support\Facades\FilamentView::renderHook('panels::sidebar.nav.start') }}
 
         @if (filament()->hasTenancy())
