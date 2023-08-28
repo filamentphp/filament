86110d0c18d8c71f7b3d9d2dd47bac8b6e3bed1f
François M <francoism90@users.noreply.github.com>
2023-08-28 15:29:20 +0200

    Expand broadcasting config

--- packages/support/config/filament.php
+++ packages/support/config/filament.php
@@ -16,12 +16,17 @@ return [
 
     'broadcasting' => [
 
-        // 'echo' => [
-        //     'broadcaster' => 'pusher',
-        //     'key' => env('VITE_PUSHER_APP_KEY'),
-        //     'cluster' => env('VITE_PUSHER_APP_CLUSTER'),
-        //     'forceTLS' => true,
-        // ],
+        'echo' => [
+            'broadcaster' => 'pusher',
+            'key' => env('VITE_PUSHER_APP_KEY'),
+            'cluster' => env('VITE_PUSHER_APP_CLUSTER'),
+            'wsHost' => env('VITE_PUSHER_HOST'),
+            'wsPort' => env('VITE_PUSHER_PORT'),
+            'wssPort' => env('VITE_PUSHER_PORT'),
+            // 'authEndpoint' => '/broadcasting/auth',
+            'disableStats' => true,
+            'encrypted' => true,
+        ],
 
     ],
 
