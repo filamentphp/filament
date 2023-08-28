ab4ebe7feeb1467edc12f28dca1eb0c845da8793
François M <francoism90@users.noreply.github.com>
2023-08-28 15:42:23 +0200

    Comment code

--- packages/support/config/filament.php
+++ packages/support/config/filament.php
@@ -17,16 +17,16 @@ return [
     'broadcasting' => [
 
         'echo' => [
-            'broadcaster' => 'pusher',
-            'key' => env('VITE_PUSHER_APP_KEY'),
-            'cluster' => env('VITE_PUSHER_APP_CLUSTER'),
-            'wsHost' => env('VITE_PUSHER_HOST'),
-            'wsPort' => env('VITE_PUSHER_PORT'),
-            'wssPort' => env('VITE_PUSHER_PORT'),
-            // 'authEndpoint' => '/broadcasting/auth',
-            'disableStats' => true,
-            'encrypted' => true,
-        ],
+        //     'broadcaster' => 'pusher',
+        //     'key' => env('VITE_PUSHER_APP_KEY'),
+        //     'cluster' => env('VITE_PUSHER_APP_CLUSTER'),
+        //     'wsHost' => env('VITE_PUSHER_HOST'),
+        //     'wsPort' => env('VITE_PUSHER_PORT'),
+        //     'wssPort' => env('VITE_PUSHER_PORT'),
+        //     'authEndpoint' => '/api/v1/broadcasting/auth',
+        //     'disableStats' => true,
+        //     'encrypted' => true,
+        // ],
 
     ],
 
