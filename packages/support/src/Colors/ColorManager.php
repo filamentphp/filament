2111c780280e0f4aeff2f73d161e5961364f3b73
Zep Fietje <hey@zepfietje.com>
2023-08-29 08:33:56 +0200

    wip

--- packages/support/src/Colors/ColorManager.php
+++ packages/support/src/Colors/ColorManager.php
@@ -13,6 +13,10 @@ class ColorManager
 
     protected array $shades = [];
 
+    protected array $shadesToRemove = [];
+
+    protected array $shadesToAdd = [];
+
     /**
      * @param  array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string>  $colors
      */
