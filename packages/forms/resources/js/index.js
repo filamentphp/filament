72d941eaea1fd3c80e62bf7229e2d7644c46e145
Dan Harrin <git@danharrin.com>
2023-09-09 15:41:41 +0100

    wip

--- packages/forms/resources/js/index.js
+++ packages/forms/resources/js/index.js
@@ -1,14 +1,6 @@
-import mask from '@alpinejs/mask'
-import intersect from '@alpinejs/intersect'
-
 import '../css/components/date-time-picker.css'
 import '../css/components/file-upload.css'
 import '../css/components/markdown-editor.css'
 import '../css/components/rich-editor.css'
 import '../css/components/select.css'
 import '../css/components/tags-input.css'
-
-document.addEventListener('alpine:init', () => {
-    window.Alpine.plugin(mask)
-    window.Alpine.plugin(intersect)
-})
