3904d1286f842a4f4a73c6de5a4b9fe0edad176e
Dan Harrin <git@danharrin.com>
2023-09-09 23:03:47 +0100

    Update repeatable-entry.blade.php

--- packages/infolists/resources/views/components/repeatable-entry.blade.php
+++ packages/infolists/resources/views/components/repeatable-entry.blade.php
@@ -30,7 +30,7 @@
                     @foreach ($childComponentContainers as $container)
                         <li
                             @class([
-                                'block',
+                                'fi-in-repeatable-item block',
                                 'rounded-xl bg-white p-4 shadow-sm ring-1 ring-gray-950/5 dark:bg-white/5 dark:ring-white/10' => $isContained,
                             ])
                         >
