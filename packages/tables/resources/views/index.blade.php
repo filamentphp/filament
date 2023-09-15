49553b7e26dcc9623dbf0bc80e68a0916b8c253e
Linas Pašviestis <linas.pasviestis@gmail.com>
2023-09-15 13:38:41 +0300

    Update index.blade.php

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -167,7 +167,7 @@
                     <x-filament-tables::filters
                         :form="$getFiltersForm()"
                         x-show="areFiltersOpen"
-                        style="display: none;"
+                        x-cloak
                         @class([
                             'py-1 sm:py-3' => $hasFiltersAboveContentCollapsible,
                         ])
