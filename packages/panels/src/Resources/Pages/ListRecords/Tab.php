54d02ade8c31ef38776830fb4104db16345b80be
Tally Schmeits <tally@schmeits.com>
2023-09-18 09:33:16 +0200

    Update Tab.php
    Forgot to remove the closure :( sorry ;)

--- packages/panels/src/Resources/Pages/ListRecords/Tab.php
+++ packages/panels/src/Resources/Pages/ListRecords/Tab.php
@@ -86,7 +86,7 @@ class Tab extends Component
     /**
      * @return string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
      */
-    public function getBadgeColor(): string | array | Closure | null
+    public function getBadgeColor(): string | array | null
     {
         return $this->evaluate($this->badgeColor);
     }    
