237a371406bc73b8c428c34b393a0bcc2233d505
danharrin <danharrin@users.noreply.github.com>
2023-09-18 08:53:52 +0000

    chore: fix code style

--- packages/widgets/resources/views/table-widget.blade.php
+++ packages/widgets/resources/views/table-widget.blade.php
@@ -1,6 +1,6 @@
 <x-filament-widgets::widget class="fi-wi-table">
-    {{ \Filament\Support\Facades\FilamentView::renderHook('widgets::table-widget.start', scopes: static::class) }}    
-    
+    {{ \Filament\Support\Facades\FilamentView::renderHook('widgets::table-widget.start', scopes: static::class) }}
+
     {{ $this->table }}
 
     {{ \Filament\Support\Facades\FilamentView::renderHook('widgets::table-widget.end', scopes: static::class) }}
