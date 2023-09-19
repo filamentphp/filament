1d41a26488623c3951b3622a94cf080f4871c101
Barış AKSU <baris@barisaksu.com>
2023-09-19 05:43:51 +0300

    update packages/actions

--- packages/actions/resources/lang/tr/create.php
+++ packages/actions/resources/lang/tr/create.php
@@ -4,7 +4,7 @@ return [
 
     'single' => [
 
-        'label' => ':label Oluştur',
+        'label' => ':Label Oluştur',
 
         'modal' => [
 
@@ -17,7 +17,7 @@ return [
                 ],
 
                 'create_another' => [
-                    'label' => 'Oluştur ve başka bir taneye başla',
+                    'label' => 'Oluştur ve başka bir tane oluştur',
                 ],
 
             ],
