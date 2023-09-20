14cceef3ee06704ddb88d23073a6fb3aadb0f5c8
Kenneth Sese <ksese@hope4tomorrow.net>
2023-09-19 21:32:14 -0600

    Update index.blade.php

--- packages/support/resources/views/components/pagination/index.blade.php
+++ packages/support/resources/views/components/pagination/index.blade.php
@@ -12,7 +12,7 @@
 <nav
     aria-label="{{ __('filament::components/pagination.label') }}"
     role="navigation"
-    {{ $attributes->class(['fi-pagination grid grid-col-3 grid-flow-col items-center gap-3']) }}
+    {{ $attributes->class(['fi-pagination grid grid-cols-3 grid-flow-col items-center gap-3']) }}
 >
     @if (! $paginator->onFirstPage())
         <x-filament::button
