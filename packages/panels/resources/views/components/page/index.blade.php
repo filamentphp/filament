73cba9940dd051e9733b083391b241d54b4aee8b
Dan Harrin <git@danharrin.com>
2023-09-21 19:35:45 +0100

    Add mobile navigation

--- packages/panels/resources/views/components/page/index.blade.php
+++ packages/panels/resources/views/components/page/index.blade.php
@@ -24,9 +24,50 @@
             ])
         >
             @if ($subNavigation)
-                <div class="col-span-1">
+                <div wire:ignore class="col-span-1">
+                    <x-filament::input.wrapper class="md:hidden">
+                        <x-filament::input.select
+                            x-data="{}"
+                            x-on:change="window.location = $event.target.value"
+                        >
+                            @foreach ($subNavigation as $subNavigationGroup)
+                                @php
+                                    $subNavigationGroupLabel = $subNavigationGroup->getLabel();
+                                @endphp
+
+                                @if (filled($subNavigationGroupLabel))
+                                    <optgroup
+                                        label="{{ $subNavigationGroupLabel }}"
+                                    >
+                                        @foreach ($subNavigationGroup->getItems() as $subNavigationItem)
+                                            <option
+                                                @if ($subNavigationItem->isActive())
+                                                    selected
+                                                @endif
+                                                value="{{ $subNavigationItem->getUrl() }}"
+                                            >
+                                                {{ $subNavigationItem->getLabel() }}
+                                            </option>
+                                        @endforeach
+                                    </optgroup>
+                                @else
+                                    @foreach ($subNavigationGroup->getItems() as $subNavigationItem)
+                                        <option
+                                            @if ($subNavigationItem->isActive())
+                                                selected
+                                            @endif
+                                            value="{{ $subNavigationItem->getUrl() }}"
+                                        >
+                                            {{ $subNavigationItem->getLabel() }}
+                                        </option>
+                                    @endforeach
+                                @endif
+                            @endforeach
+                        </x-filament::input.select>
+                    </x-filament::input.wrapper>
+
                     <div
-                        class="rounded-xl bg-white p-2 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"
+                        class="hidden rounded-xl bg-white p-2 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 md:block"
                     >
                         @foreach ($subNavigation as $subNavigationGroup)
                             <x-filament-panels::sidebar.group
