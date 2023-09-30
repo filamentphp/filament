d1090306ba55bedeec431aa02213a3f8600c9fa0
SHIBAHARA Hiroki <shibomb@gmail.com>
2023-10-01 02:35:37 +0900

    Translation ja: panels

---
+++ packages/panels/resources/lang/ja/pages/auth/edit-profile.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    'label' => 'プロフィール',
+
+    'form' => [
+
+        'email' => [
+            'label' => 'メールアドレス',
+        ],
+
+        'name' => [
+            'label' => '名前',
+        ],
+
+        'password' => [
+            'label' => '新しいパスワード',
+        ],
+
+        'password_confirmation' => [
+            'label' => '新しいパスワードの確認',
+        ],
+
+        'actions' => [
+
+            'save' => [
+                'label' => '変更を保存',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'saved' => [
+            'title' => '保存しました',
+        ],
+
+    ],
+
+    'actions' => [
+
+        'cancel' => [
+            'label' => 'キャンセル',
+        ],
+
+    ],
+
+];
