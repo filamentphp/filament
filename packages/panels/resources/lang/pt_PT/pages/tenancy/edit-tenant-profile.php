06564385ea22a99d380f9ab3396fc40135c0e12c
Deusmar Júnior <deusmar.junior@hotmail.com>
2023-10-03 17:39:43 +0100

    Adds missing Portuguese (PT/BR) translations

---
+++ packages/panels/resources/lang/pt_PT/pages/tenancy/edit-tenant-profile.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'form' => [
+
+        'actions' => [
+
+            'save' => [
+                'label' => 'Guardar mudanças',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'saved' => [
+            'title' => 'Guardado',
+        ],
+
+    ],
+
+];
+
