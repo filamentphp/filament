8c49d1f398b8c1a1cb2040e173c208e3329ded07
Zep Fietje <hey@zepfietje.com>
2023-10-04 14:04:39 +0200

    Sort

--- packages/tables/resources/views/components/group/header.blade.php
+++ packages/tables/resources/views/components/group/header.blade.php
@@ -38,13 +38,13 @@
     @if ($collapsible)
         <x-filament::icon-button
             color="gray"
-            icon-alias="tables::grouping.collapse-button"
             icon="heroicon-m-chevron-up"
-            size="sm"
+            icon-alias="tables::grouping.collapse-button"
             :label="filled($label) ? ($label . ': ' . $title) : $title"
+            size="sm"
             :x-bind:aria-expanded="'! isGroupCollapsed(' . \Illuminate\Support\Js::from($title) . ')'"
-            :x-bind:class="'isGroupCollapsed(' . \Illuminate\Support\Js::from($title) . ') && \'rotate-180\''"
             class="-m-1.5"
+            :x-bind:class="'isGroupCollapsed(' . \Illuminate\Support\Js::from($title) . ') && \'rotate-180\''"
         />
     @endif
 </div>
