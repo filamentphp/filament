0faff7df32ccb7d7db9a2d8b2aa8ae669fd616bc
Juan Antonio López Seguí <31510870+jals-es@users.noreply.github.com>
2023-10-05 14:20:19 +0200

    Force value attribute in Hidden Form Component

--- packages/forms/resources/views/components/hidden.blade.php
+++ packages/forms/resources/views/components/hidden.blade.php
@@ -5,6 +5,7 @@
                 'id' => $getId(),
                 'type' => 'hidden',
                 $applyStateBindingModifiers('wire:model') => $getStatePath(),
+                'value' => $getState()
             ], escape: false)
             ->merge($getExtraAttributes(), escape: false)
             ->class(['fi-fo-hidden'])
