3505aa8c877c3dbe44d2debc9532e419dc74fcf1
Lloric Mayuga Garcia <lloricode@gmail.com>
2023-10-14 22:09:09 +0800

    Allow \Closure on favicon

--- packages/panels/src/Panel/Concerns/HasFavicon.php
+++ packages/panels/src/Panel/Concerns/HasFavicon.php
@@ -2,19 +2,24 @@
 
 namespace Filament\Panel\Concerns;
 
+use Closure;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\HtmlString;
+
 trait HasFavicon
 {
-    protected ?string $favicon = null;
+    protected string | HtmlString | Closure | null $favicon = null;
 
-    public function favicon(?string $url): static
+    public function favicon(string | Htmlable | Closure | null $url): static
     {
         $this->favicon = $url;
 
         return $this;
     }
 
-    public function getFavicon(): ?string
+    public function getFavicon():  string | Htmlable | null
     {
-        return $this->favicon;
+        return $this->evaluate($this->favicon);
     }
 }
+
