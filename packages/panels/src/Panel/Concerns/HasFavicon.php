bafa6ae9bbe9a8af9adfd28e4e58f418cdaf7792
Dan Harrin <git@danharrin.com>
2023-10-15 08:23:50 +0100

    Update HasFavicon.php

--- packages/panels/src/Panel/Concerns/HasFavicon.php
+++ packages/panels/src/Panel/Concerns/HasFavicon.php
@@ -4,11 +4,10 @@ namespace Filament\Panel\Concerns;
 
 use Closure;
 use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\HtmlString;
 
 trait HasFavicon
 {
-    protected string | HtmlString | Closure | null $favicon = null;
+    protected string | Htmlable | Closure | null $favicon = null;
 
     public function favicon(string | Htmlable | Closure | null $url): static
     {
