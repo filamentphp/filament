8dad75f79a3d1f110105ef103d8678bdc59a25f9
Ryan Chandler <ryangjchandler@gmail.com>
2023-10-17 10:53:04 +0100

    feature: add new Markdown helper class

---
+++ packages/support/src/Markdown.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Filament\Support;
+
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Str;
+use Stringable;
+
+class Markdown implements Htmlable, Stringable
+{
+    public function __construct(
+        protected string $text,
+    ) {}
+
+    public function toHtml()
+    {
+        return Str::markdown($this->text);
+    }
+
+    public function __toString(): string
+    {
+        return $this->toHtml();
+    }
+}
---
+++ tests/src/Support/MarkdownTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Filament\Support\Markdown;
+use Filament\Tests\TestCase;
+
+uses(TestCase::class);
+
+it('can convert markdown to a string of HTML', function () {
+    $markdown = new Markdown('This is a **snippet** of _example_ Markdown.');
+
+    expect($markdown->toHtml())
+        ->toBe("<p>This is a <strong>snippet</strong> of <em>example</em> Markdown.</p>\n");
+});
