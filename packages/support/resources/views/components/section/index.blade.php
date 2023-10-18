076f021fad88c4729da2bbcd198ff98194355ba5
Zep Fietje <hey@zepfietje.com>
2023-10-18 12:20:43 +0200

    Update packages/support/resources/views/components/section/index.blade.php

--- packages/support/resources/views/components/section/index.blade.php
+++ packages/support/resources/views/components/section/index.blade.php
@@ -26,7 +26,7 @@
     $hasDescription = filled((string) $description);
     $hasHeading = filled($heading);
     $hasIcon = filled($icon);
-    $hasHeader = $hasActions || $hasIcon || $hasHeading || $hasDescription || $collapsible || filled((string) $headerEnd);
+    $hasHeader =  $hasIcon || $hasHeading || $hasDescription || $collapsible || $hasActions || filled((string) $headerEnd);
 @endphp
 
 <section
