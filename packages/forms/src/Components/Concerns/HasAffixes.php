d33ed283b851270a938e60044078a82ac7120b40
Zep Fietje <hey@zepfietje.com>
2023-10-18 13:53:00 +0200

    Order

--- packages/forms/src/Components/Concerns/HasAffixes.php
+++ packages/forms/src/Components/Concerns/HasAffixes.php
@@ -35,12 +35,12 @@ trait HasAffixes
 
     protected string | Closure | null $prefixIcon = null;
 
-    protected string | Closure | null $suffixIcon = null;
-
     /**
      * @var string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | Closure | null
      */
     protected string | array | Closure | null $prefixIconColor = null;
+
+    protected string | Closure | null $suffixIcon = null;
 
     /**
      * @var string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | Closure | null
