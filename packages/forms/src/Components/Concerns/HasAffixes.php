20c385f7b25843205292392802d2a6cf7b6f14bf
Zep Fietje <hey@zepfietje.com>
2023-10-18 14:26:00 +0200

    Fix annotation

--- packages/forms/src/Components/Concerns/HasAffixes.php
+++ packages/forms/src/Components/Concerns/HasAffixes.php
@@ -155,7 +155,7 @@ trait HasAffixes
     }
 
     /**
-     * @param  string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | Closure | null  $iconColor
+     * @param  string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | Closure | null  $color
      */
     public function suffixIconColor(string | array | Closure | null $color = null): static
     {
