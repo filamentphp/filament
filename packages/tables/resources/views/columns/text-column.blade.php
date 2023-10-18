2df821569b436ec934b3804cf059955d89d9d351
wychoong <wychoong@users.noreply.github.com>
2023-10-18 15:14:33 +0000

    chore: fix code style

--- packages/tables/resources/views/columns/text-column.blade.php
+++ packages/tables/resources/views/columns/text-column.blade.php
@@ -45,7 +45,7 @@
         $attributes
             ->merge($getExtraAttributes(), escape: false)
             ->class([
-                'fi-ta-text grid gap-y-1 w-full',
+                'fi-ta-text grid w-full gap-y-1',
                 'px-3 py-4' => ! $isInline(),
             ])
     }}
