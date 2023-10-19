25f47c39ac99011304faead946cd40f855a60c5e
Dan Harrin <git@danharrin.com>
2023-10-19 09:48:40 +0100

    Update packages/tables/src/Columns/Concerns/CanFormatState.php

--- packages/tables/src/Columns/Concerns/CanFormatState.php
+++ packages/tables/src/Columns/Concerns/CanFormatState.php
@@ -85,7 +85,7 @@ trait CanFormatState
 
     public function since(?string $timezone = null): static
     {
-        $this->isDate = true;
+        $this->isDateTime = true;
         
         $this->formatStateUsing(static function (TextColumn $column, $state) use ($timezone): ?string {
             if (blank($state)) {
