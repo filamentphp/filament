b112b3d4c74ca20658fe85d155151bb3dbababae
Lalhmangaihenga <valpuia@lomatechnology.com>
2023-10-26 12:42:22 +0700

    actions

---
+++ packages/actions/resources/lang/km/replicate.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'ចម្លង',
+
+        'modal' => [
+
+            'heading' => 'ចម្លង :label',
+
+            'actions' => [
+
+                'replicate' => [
+                    'label' => 'ចម្លង',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'replicated' => [
+                'title' => 'ចម្លង',
+            ],
+
+        ],
+
+    ],
+
+];
