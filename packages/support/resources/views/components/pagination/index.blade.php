435b17a581d61461732d72b04f952d31dbf18c42
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2023-10-30 15:01:16 +0100

    Hide pagination dropdown if nr. of records is smaller than

--- packages/support/resources/views/components/pagination/index.blade.php
+++ packages/support/resources/views/components/pagination/index.blade.php
@@ -7,6 +7,11 @@
 @php
     $isRtl = __('filament-panels::layout.direction') === 'rtl';
     $isSimple = ! $paginator instanceof \Illuminate\Pagination\LengthAwarePaginator;
+	
+    $smallestPageOption = collect($pageOptions)
+        ->filter(fn (int $option) => $option > 0)
+        ->sort()
+        ->first();
 @endphp
 
 <nav
@@ -49,7 +54,7 @@
         </span>
     @endif
 
-    @if (count($pageOptions) > 1)
+    @if (count($pageOptions) > 1 && ($isSimple || $paginator->total() > $smallestPageOption))
         <div class="col-start-2 justify-self-center">
             <label class="fi-pagination-records-per-page-select fi-compact">
                 <x-filament::input.wrapper>
