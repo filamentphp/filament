c839708b52873a3c6b2aa11b62c7ec611b06214c
sumardi <sumardi@users.noreply.github.com>
2023-11-03 07:54:48 +0000

    chore: fix code style

--- packages/forms/src/Components/Select.php
+++ packages/forms/src/Components/Select.php
@@ -8,8 +8,6 @@ use Filament\Forms\ComponentContainer;
 use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Form;
 use Filament\Support\Concerns\HasExtraAlpineAttributes;
-use function Filament\Support\generate_search_column_expression;
-use function Filament\Support\generate_search_term_expression;
 use Filament\Support\Services\RelationshipJoiner;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Htmlable;
@@ -25,6 +23,9 @@ use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Exists;
 use Livewire\Component as LivewireComponent;
+
+use function Filament\Support\generate_search_column_expression;
+use function Filament\Support\generate_search_term_expression;
 
 class Select extends Field implements Contracts\HasAffixActions, Contracts\HasNestedRecursiveValidationRules
 {
