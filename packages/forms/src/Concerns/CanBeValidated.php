974385ee764eb4ce1d39662097ab6c691a2608c8
wychoong <67364036+wychoong@users.noreply.github.com>
2023-11-08 19:38:34 +0800

    Update CanBeValidated.php

--- packages/forms/src/Concerns/CanBeValidated.php
+++ packages/forms/src/Concerns/CanBeValidated.php
@@ -34,7 +34,7 @@ trait CanBeValidated
     }
 
     /**
-     * @return array<string, string>>
+     * @return array<string, string>
      */
     public function getValidationMessages(): array
     {
