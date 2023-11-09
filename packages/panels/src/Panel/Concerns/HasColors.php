c1840c5baf17d5f758ad043c4d135cd9048057b0
Sam Maosa <coolsam726@users.noreply.github.com>
2023-11-09 11:54:05 +0300

    Fixes: ColorManager was missing the EvaluatesClosure, Removed Generics from the $color php docs

--- packages/panels/src/Panel/Concerns/HasColors.php
+++ packages/panels/src/Panel/Concerns/HasColors.php
@@ -7,12 +7,12 @@ use Closure;
 trait HasColors
 {
     /**
-     * @var array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure<array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string>>
+     * @var array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure>
      */
     protected array $colors = [];
 
     /**
-     * @param  array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure<array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string>>  $colors
+     * @param  array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure>  $colors
      */
     public function colors(array $colors): static
     {
--- packages/support/src/Colors/ColorManager.php
+++ packages/support/src/Colors/ColorManager.php
@@ -3,10 +3,12 @@
 namespace Filament\Support\Colors;
 
 use Closure;
+use Filament\Support\Concerns\EvaluatesClosures;
 use Spatie\Color\Hex;
 
 class ColorManager
 {
+    use EvaluatesClosures;
     /**
      * @var array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure<array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string>>
      */
@@ -28,7 +30,7 @@ class ColorManager
     protected array $removedShades = [];
 
     /**
-     * @param  array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure<array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string>>  $colors
+     * @param  array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure>  $colors
      */
     public function register(array $colors): static
     {
