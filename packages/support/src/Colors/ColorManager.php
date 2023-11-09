42fcda6f2c544addd77442747dbdafe4594f29d7
Sam Maosa <coolsam726@users.noreply.github.com>
2023-11-09 12:05:20 +0300

    Fixes: Removed more Generic closures in ColorManager

--- packages/support/src/Colors/ColorManager.php
+++ packages/support/src/Colors/ColorManager.php
@@ -10,7 +10,7 @@ class ColorManager
 {
     use EvaluatesClosures;
     /**
-     * @var array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure<array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string>>
+     * @var array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure>
      */
     protected array $colors = [];
 
