62e8cff52b1e0ace461bb222c9eb871a1e5cb7f5
Sam Maosa <maosa.sam@gmail.com>
2023-11-09 12:53:15 +0300

    Bug Fix: Missing Closure import in FilamentColor

--- packages/support/src/Facades/FilamentColor.php
+++ packages/support/src/Facades/FilamentColor.php
@@ -4,6 +4,7 @@ namespace Filament\Support\Facades;
 
 use Filament\Support\Colors\ColorManager;
 use Illuminate\Support\Facades\Facade;
+use Closure;
 
 /**
  * @method static array<int> addShades(string $alias, array<int> $shades)
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static array<int> | null getOverridingShades(string $alias)
  * @method static array<int> | null getRemovedShades(string $alias)
  * @method static array<int> overrideShades(string $alias, array<int> $shades)
- * @method static array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string}> | string processColor(array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string $color)
+ * @method static array<string, array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string}> | string processColor(array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string | Closure $color)
  * @method static array<int> removeShades(string $alias, array<int> $shades)
  *
  * @see ColorManager
