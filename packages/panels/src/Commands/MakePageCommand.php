30f4e28c62cace199d350f174f535c4781d5d1df
Zep Fietje <hey@zepfietje.com>
2023-11-13 09:48:36 +0100

    Update packages/panels/src/Commands/MakePageCommand.php

--- packages/panels/src/Commands/MakePageCommand.php
+++ packages/panels/src/Commands/MakePageCommand.php
@@ -45,7 +45,7 @@ class MakePageCommand extends Command
         $resourcePage = null;
 
         $resourceInput = $this->option('resource') ?? text(
-            label: 'Which resource would you like to create this page in?',
+            label: 'What is the resource you would like to create this in?',
             placeholder: '[Optional] UserResource',
         );
 
