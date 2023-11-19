5adcfe6b9363e99a16b69f75cb05a87b32dda918
danharrin <danharrin@users.noreply.github.com>
2023-11-19 18:26:36 +0000

    chore: fix code style

--- packages/panels/resources/views/components/sidebar/group.blade.php
+++ packages/panels/resources/views/components/sidebar/group.blade.php
@@ -17,10 +17,12 @@
 <li
     x-data="{ label: @js($label) }"
     data-group-label="{{ $label }}"
-    {{ $attributes->class([
-        'fi-sidebar-group flex flex-col gap-y-1',
-        'fi-active' => $active,
-    ]) }}
+    {{
+        $attributes->class([
+            'fi-sidebar-group flex flex-col gap-y-1',
+            'fi-active' => $active,
+        ])
+    }}
 >
     @if ($label)
         <div
