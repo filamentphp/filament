cd0c7b7aaf82d56d549ec6271a33d0c258033a73
Marius Oppolzer <marius.oppolzer@PC-AMP-0003.fritz.box>
2023-11-24 10:53:42 +0100

    adds properties and their boolean checks

--- packages/forms/src/Components/Concerns/CanBeAccepted.php
+++ packages/forms/src/Components/Concerns/CanBeAccepted.php
@@ -6,8 +6,14 @@ use Closure;
 
 trait CanBeAccepted
 {
+    protected bool | Closure $mustBeAccepted = false;
+
+    protected bool | Closure $mustBeDeclined = false;
+
     public function accepted(bool | Closure $condition = true): static
     {
+        $this->mustBeAccepted = $condition;
+
         $this->rule('accepted', $condition);
 
         return $this;
@@ -15,8 +21,20 @@ trait CanBeAccepted
 
     public function declined(bool | Closure $condition = true): static
     {
+        $this->mustBeDeclined = $condition;
+
         $this->rule('declined', $condition);
 
         return $this;
+    }
+
+    public function mustBeAccepted(): bool
+    {
+        return (bool) $this->evaluate($this->mustBeAccepted);
+    }
+
+    public function mustBeDeclined(): bool
+    {
+        return (bool) $this->evaluate($this->mustBeDeclined);
     }
 }
