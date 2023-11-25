ce990555054cfafdaa31bc6ce4489dbdd6be1570
Glenn Jacobs <glenn@neondigital.co.uk>
2023-11-25 12:29:31 +0000

    Update index.blade.php

--- packages/panels/resources/views/components/page/index.blade.php
+++ packages/panels/resources/views/components/page/index.blade.php
@@ -84,7 +84,7 @@
                     </x-filament::input.wrapper>
 
                     <div
-                        class="hidden rounded-xl bg-white p-2 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 md:block"
+                        class="hidden md:block"
                     >
                         @foreach ($subNavigation as $subNavigationGroup)
                             <x-filament-panels::sidebar.group
