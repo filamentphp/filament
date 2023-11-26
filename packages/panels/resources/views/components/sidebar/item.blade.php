5654f7babdfae97b5685fe245ea6c0191773fe3b
Zep Fietje <hey@zepfietje.com>
2023-11-26 12:23:59 +0000

    Clean up

--- packages/panels/resources/views/components/sidebar/item.blade.php
+++ packages/panels/resources/views/components/sidebar/item.blade.php
@@ -12,6 +12,10 @@
     'url',
 ])
 
+@php
+    $sidebarCollapsible = $sidebarCollapsible && filament()->isSidebarCollapsibleOnDesktop();
+@endphp
+
 <li
     @class([
         'fi-sidebar-item',
@@ -22,7 +26,7 @@
     <a
         {{ \Filament\Support\generate_href_html($url, $shouldOpenUrlInNewTab) }}
         x-on:click="window.matchMedia(`(max-width: 1024px)`).matches && $store.sidebar.close()"
-        @if ($sidebarCollapsible && filament()->isSidebarCollapsibleOnDesktop())
+        @if ($sidebarCollapsible)
             x-data="{ tooltip: false }"
             x-effect="
                 tooltip = $store.sidebar.isOpen
@@ -76,7 +80,7 @@
         @endif
 
         <span
-            @if ($sidebarCollapsible && filament()->isSidebarCollapsibleOnDesktop())
+            @if ($sidebarCollapsible)
                 x-show="$store.sidebar.isOpen"
                 x-transition:enter="lg:transition lg:delay-100"
                 x-transition:enter-start="opacity-0"
@@ -95,7 +99,7 @@
 
         @if (filled($badge))
             <span
-                @if ($sidebarCollapsible && filament()->isSidebarCollapsibleOnDesktop())
+                @if ($sidebarCollapsible)
                     x-show="$store.sidebar.isOpen"
                     x-transition:enter="lg:transition lg:delay-100"
                     x-transition:enter-start="opacity-0"
