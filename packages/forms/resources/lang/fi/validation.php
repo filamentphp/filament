dd89a0973984222db8318bf21d8bff33cb2111b0
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2023-11-29 22:33:02 +0200

    Update fi translation

---
+++ packages/forms/resources/lang/fi/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'distinct' => [
+        'must_be_selected' => 'Vähintään yksi :attribute kenttä tulee olla valittuna.',
+        'only_one_must_be_selected' => 'Vain yksi :attribute kenttä tulee olla valittuna.',
+    ],
+
+];
--- packages/panels/resources/lang/fi/pages/dashboard.php
+++ packages/panels/resources/lang/fi/pages/dashboard.php
@@ -4,4 +4,30 @@ return [
 
     'title' => 'Ohjausnäkymä',
 
+    'actions' => [
+
+        'filter' => [
+
+            'label' => 'Suodatin',
+
+            'modal' => [
+
+                'heading' => 'Suodatin',
+
+                'actions' => [
+
+                    'apply' => [
+
+                        'label' => 'Käytä',
+
+                    ],
+
+                ],
+
+            ],
+
+        ],
+
+    ],
+
 ];
