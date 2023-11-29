42d82b2cece0cc8546c89a75df08a619ceb7ef7f
Zep Fietje <hey@zepfietje.com>
2023-11-29 09:54:19 +0000

    wip

--- packages/panels/resources/views/components/page/sub-navigation/select.blade.php
+++ packages/panels/resources/views/components/page/sub-navigation/select.blade.php
@@ -14,31 +14,28 @@
         x-on:change="window.location = $event.target.value"
     >
         @foreach ($navigation as $navigationGroup)
-            @php
-                $navigationGroupLabel = $navigationGroup->getLabel();
-            @endphp
-
-            @if (filled($navigationGroupLabel))
-                <optgroup
-                    label="{{ $navigationGroupLabel }}"
-                >
-            @endif
-
-            @foreach ($navigationGroup->getItems() as $navigationItem)
-                @foreach ([$navigationItem, ...$navigationItem->getChildItems()] as $navigationItemChild)
-                    <option
-                        @if ($navigationItem->isActive())
-                            selected
-                        @endif
-                        value="{{ $navigationItem->getUrl() }}"
-                    >
-                        @if ($loop->index)&ensp;&ensp;@endif{{ $navigationItem->getLabel() }}
-                    </option>
+            @capture($options)
+                @foreach ($navigationGroup->getItems() as $navigationItem)
+                    @foreach ([$navigationItem, ...$navigationItem->getChildItems()] as $navigationItemChild)
+                        <option
+                            @if ($navigationItem->isActive())
+                                selected
+                            @endif
+                            value="{{ $navigationItem->getUrl() }}"
+                        >
+                            {{ $loop->index ? '&ensp;&ensp;' : null }}
+                            {{ $navigationItem->getLabel() }}
+                        </option>
+                    @endforeach
                 @endforeach
-            @endforeach
+            @endcapture
 
-            @if (filled($navigationGroupLabel))
+            @if (filled($navigationGroupLabel = $navigationGroup->getLabel()))
+                <optgroup label="{{ $navigationGroupLabel }}">
+                    {{ $options() }}
                 </optgroup>
+            @else
+                {{ $options() }}
             @endif
         @endforeach
     </x-filament::input.select>
