c70ba7216e606a53f336a2a096caf5ef495026ad
Luis Parrado <luisprmat@gmail.com>
2023-11-30 00:17:56 -0500

    Add Spanish (es) translations for v3.1.0

--- packages/tables/resources/lang/es/table.php
+++ packages/tables/resources/lang/es/table.php
@@ -11,7 +11,14 @@ return [
     'columns' => [
 
         'text' => [
+
+            'actions' => [
+                'collapse_list' => 'Mostrar :count menos',
+                'expand_list' => 'Mostrar :count más',
+            ],
+
             'more_list_items' => 'y :count más',
+
         ],
 
     ],
