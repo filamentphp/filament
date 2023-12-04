e953ecbd9ed877acc59271205dd63ab393d417b4
Zep Fietje <hey@zepfietje.com>
2023-12-04 14:20:12 +0000

    Fix

--- packages/widgets/resources/views/chart-widget.blade.php
+++ packages/widgets/resources/views/chart-widget.blade.php
@@ -14,7 +14,7 @@
                 <x-filament::input.wrapper
                     inline-prefix
                     wire:target="filter"
-                    class="mt-3 sm:-my-2"
+                    class="w-max sm:-my-2"
                 >
                     <x-filament::input.select
                         inline-prefix
