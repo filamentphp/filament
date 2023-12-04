ccfef318d2cad3fa41a27b47bbec1225d815b8a2
Zep Fietje <hey@zepfietje.com>
2023-12-04 11:28:22 +0000

    Run prettier

--- packages/panels/resources/views/components/form/actions.blade.php
+++ packages/panels/resources/views/components/form/actions.blade.php
@@ -7,12 +7,12 @@
 @if (count($actions))
     <div
         @if ($this->areFormActionsSticky())
-        x-data="{
+            x-data="{
                 isSticky: false,
                 isPinned: false,
                 evaluatePageScrollPosition: function () {
-                    this.isSticky = (this.isPinned == false && window.scrollY >= 0);
-                    this.isPinned = $el.getBoundingClientRect().bottom <= window.innerHeight;
+                    this.isSticky = this.isPinned == false && window.scrollY >= 0
+                    this.isPinned = $el.getBoundingClientRect().bottom <= window.innerHeight
                 },
             }"
             x-init="evaluatePageScrollPosition"
@@ -21,8 +21,7 @@
             x-bind:class="{
                 'transform bg-white shadow-lg ring-1 ring-gray-950/5 transition dark:bg-gray-900 dark:ring-white/10 md:rounded-t-xl -mx-4 md:mx-0':
                     isSticky,
-                'fi-sticky sticky p-4':
-                    true,
+                'fi-sticky sticky p-4': true,
             }"
             x-bind:style="{
                 bottom: '-1px',
