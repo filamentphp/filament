521e9b2b53de83168a2fa9869fdede45bf792e0c
Dan Harrin <git@danharrin.com>
2023-12-11 11:56:03 +0000

    fix: Disabled navigation

--- packages/panels/resources/views/components/layout/index.blade.php
+++ packages/panels/resources/views/components/layout/index.blade.php
@@ -6,16 +6,18 @@
 
 <x-filament-panels::layout.base :livewire="$livewire">
     <div class="fi-layout flex min-h-screen w-full overflow-x-clip">
-        <div
-            x-cloak
-            x-data="{}"
-            x-on:click="$store.sidebar.close()"
-            x-show="$store.sidebar.isOpen"
-            x-transition.opacity.300ms
-            class="fi-sidebar-close-overlay fixed inset-0 z-30 bg-gray-950/50 transition duration-500 dark:bg-gray-950/75 lg:hidden"
-        ></div>
+        @if (filament()->hasNavigation())
+            <div
+                x-cloak
+                x-data="{}"
+                x-on:click="$store.sidebar.close()"
+                x-show="$store.sidebar.isOpen"
+                x-transition.opacity.300ms
+                class="fi-sidebar-close-overlay fixed inset-0 z-30 bg-gray-950/50 transition duration-500 dark:bg-gray-950/75 lg:hidden"
+            ></div>
 
-        <x-filament-panels::sidebar :navigation="$navigation" />
+            <x-filament-panels::sidebar :navigation="$navigation" />
+        @endif
 
         <div
             @if (filament()->isSidebarCollapsibleOnDesktop())
@@ -30,15 +32,15 @@
                     'fi-main-ctn-sidebar-open': $store.sidebar.isOpen,
                 }"
                 x-bind:style="'display: flex; opacity:1;'" {{-- Mimics `x-cloak`, as using `x-cloak` causes visual issues with chart widgets --}}
-            @elseif (! (filament()->isSidebarCollapsibleOnDesktop() || filament()->isSidebarFullyCollapsibleOnDesktop() || filament()->hasTopNavigation()))
+            @elseif (! (filament()->isSidebarCollapsibleOnDesktop() || filament()->isSidebarFullyCollapsibleOnDesktop() || filament()->hasTopNavigation() || (! filament()->hasNavigation())))
                 x-data="{}"
                 x-bind:style="'display: flex; opacity:1;'" {{-- Mimics `x-cloak`, as using `x-cloak` causes visual issues with chart widgets --}}
             @endif
             @class([
                 'fi-main-ctn w-screen flex-1 flex-col',
                 'h-full opacity-0 transition-all' => filament()->isSidebarCollapsibleOnDesktop() || filament()->isSidebarFullyCollapsibleOnDesktop(),
-                'opacity-0' => ! (filament()->isSidebarCollapsibleOnDesktop() || filament()->isSidebarFullyCollapsibleOnDesktop() || filament()->hasTopNavigation()),
-                'flex' => filament()->hasTopNavigation(),
+                'opacity-0' => ! (filament()->isSidebarCollapsibleOnDesktop() || filament()->isSidebarFullyCollapsibleOnDesktop() || filament()->hasTopNavigation() || (! filament()->hasNavigation())),
+                'flex' => filament()->hasTopNavigation() || (! filament()->hasNavigation()),
             ])
         >
             <x-filament-panels::topbar :navigation="$navigation" />
