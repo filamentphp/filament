2784e1952be46ad107fd259af9ab7fd9042be308
Zep Fietje <hey@zepfietje.com>
2023-12-12 19:06:58 +0100

    Add `fi-invalid` CSS class to textarea

--- packages/forms/resources/views/components/textarea.blade.php
+++ packages/forms/resources/views/components/textarea.blade.php
@@ -37,7 +37,6 @@
             x-intersect.once="render()"
             x-on:input="render()"
             x-on:resize.window="render()"
-            style="height: {{ $initialHeight }}rem"
             {{ $getExtraAlpineAttributeBag() }}
         @endif
         {{
@@ -59,10 +58,13 @@
                 ->merge($getExtraAttributes(), escape: false)
                 ->merge($getExtraInputAttributes(), escape: false)
                 ->class([
-                    'fi-fo-textarea block w-full rounded-lg border-none bg-white px-3 py-1.5 text-base text-gray-950 shadow-sm outline-none ring-1 transition duration-75 placeholder:text-gray-400 focus-visible:ring-2 disabled:bg-gray-50 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:bg-white/5 dark:text-white dark:placeholder:text-gray-500 dark:disabled:bg-transparent dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6',
+                    'fi-fo-textarea block w-full rounded-lg border-none bg-white px-3 py-1.5 text-base text-gray-950 shadow-sm ring-1 transition duration-75 placeholder:text-gray-400 focus-visible:ring-2 disabled:bg-gray-50 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:bg-white/5 dark:text-white dark:placeholder:text-gray-500 dark:disabled:bg-transparent dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6',
                     'resize-none' => $shouldAutosize,
                     'ring-gray-950/10 focus-visible:ring-primary-600 dark:ring-white/20 dark:focus-visible:ring-primary-500 dark:disabled:ring-white/10' => ! $errors->has($statePath),
-                    'ring-danger-600 focus-visible:ring-danger-600 dark:ring-danger-500 dark:focus-visible:ring-danger-500' => $errors->has($statePath),
+                    'fi-invalid ring-danger-600 focus-visible:ring-danger-600 dark:ring-danger-500 dark:focus-visible:ring-danger-500' => $errors->has($statePath),
+                ])
+                ->style([
+                    "height: {$initialHeight}rem" => $shouldAutosize,
                 ])
         }}
     ></textarea>
