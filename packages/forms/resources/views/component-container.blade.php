b31bc2de29819e6f385b6e27c5fa7b16762ecfd7
Dan Harrin <git@danharrin.com>
2023-12-14 17:59:33 +0000

    update

--- packages/forms/resources/views/component-container.blade.php
+++ packages/forms/resources/views/component-container.blade.php
@@ -9,33 +9,33 @@
     :x-data="$isRoot ? '{}' : null"
     :x-on:form-validation-error.window="
         $isRoot ? ('if ($event.detail.livewireId !== ' . Js::from($this->getId()) . ') {
-            return
-        }
-
-        $nextTick(() => {
-            error = $el.querySelector(\'[data-validation-error]\')
-
-            if (! error) {
                 return
             }
 
-            elementToExpand = error
+            $nextTick(() => {
+                error = $el.querySelector(\'[data-validation-error]\')
 
-            while (elementToExpand) {
-                elementToExpand.dispatchEvent(new CustomEvent(\'expand\'))
+                if (! error) {
+                    return
+                }
 
-                elementToExpand = elementToExpand.parentNode
-            }
+                elementToExpand = error
 
-            setTimeout(
-                () =>
-                    error.closest(\'[data-field-wrapper]\').scrollIntoView({
-                        behavior: \'smooth\',
-                        block: \'start\',
-                        inline: \'start\',
-                    }),
-                200,
-            )
+                while (elementToExpand) {
+                    elementToExpand.dispatchEvent(new CustomEvent(\'expand\'))
+
+                    elementToExpand = elementToExpand.parentNode
+                }
+
+                setTimeout(
+                    () =>
+                        error.closest(\'[data-field-wrapper]\').scrollIntoView({
+                            behavior: \'smooth\',
+                            block: \'start\',
+                            inline: \'start\',
+                        }),
+                    200,
+                )
         })') : null
     "
     :default="$getColumns('default')"
