9ff0db6d1fbb81daef78033e79f0538078629ae0
Zep Fietje <hey@zepfietje.com>
2023-12-22 14:45:36 +0100

    Extract radio input

--- packages/forms/resources/views/components/radio.blade.php
+++ packages/forms/resources/views/components/radio.blade.php
@@ -27,32 +27,26 @@
         "
     >
         @foreach ($getOptions() as $value => $label)
-            @php
-                $shouldOptionBeDisabled = $isDisabled || $isOptionDisabled($value, $label);
-            @endphp
-
             <div
                 @class([
                     'break-inside-avoid pt-4' => (! $isInline) && $gridDirection === 'column',
                 ])
             >
                 <label class="flex gap-x-3">
-                    <input
-                        @disabled($shouldOptionBeDisabled)
-                        id="{{ $id }}-{{ $value }}"
-                        name="{{ $id }}"
-                        type="radio"
-                        value="{{ $value }}"
-                        wire:loading.attr="disabled"
-                        {{ $applyStateBindingModifiers('wire:model') }}="{{ $statePath }}"
-                        {{
-                            $getExtraInputAttributeBag()
-                                ->class([
-                                    'mt-1 border-none bg-white shadow-sm ring-1 transition duration-75 checked:ring-0 focus:ring-2 focus:ring-offset-0 disabled:bg-gray-50 disabled:text-gray-50 disabled:checked:bg-current disabled:checked:text-gray-400 dark:bg-white/5 dark:disabled:bg-transparent dark:disabled:checked:bg-gray-600',
-                                    'text-primary-600 ring-gray-950/10 focus:ring-primary-600 checked:focus:ring-primary-500/50 dark:text-primary-500 dark:ring-white/20 dark:checked:bg-primary-500 dark:focus:ring-primary-500 dark:checked:focus:ring-primary-400/50 dark:disabled:ring-white/10' => ! $errors->has($statePath),
-                                    'text-danger-600 ring-danger-600 focus:ring-danger-600 checked:focus:ring-danger-500/50 dark:text-danger-500 dark:ring-danger-500 dark:checked:bg-danger-500 dark:focus:ring-danger-500 dark:checked:focus:ring-danger-400/50' => $errors->has($statePath),
-                                ])
-                        }}
+                    <x-filament::input.radio
+                        :valid="! $errors->has($statePath)"
+                        :attributes="
+                            \Filament\Support\prepare_inherited_attributes($getExtraInputAttributeBag())
+                                ->merge([
+                                    'disabled' => $isDisabled || $isOptionDisabled($value, $label),
+                                    'id' => $id . '-' . $value,
+                                    'name' => $id,
+                                    'value' => $value,
+                                    'wire:loading.attr' => 'disabled',
+                                    $applyStateBindingModifiers('wire:model') => $statePath,
+                                ], escape: false)
+                                ->class(['mt-1'])
+                        "
                     />
 
                     <div class="grid text-sm leading-6">
