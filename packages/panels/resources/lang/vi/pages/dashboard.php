a5b5a69af582cbbd8357de97d7917cf6fe10dcdc
Ngo Quoc Dat <datlechin@gmail.com>
2023-12-25 09:58:56 -0800

    Update Vietnamese translations

--- packages/panels/resources/lang/vi/pages/dashboard.php
+++ packages/panels/resources/lang/vi/pages/dashboard.php
@@ -4,4 +4,29 @@ return [
 
     'title' => 'Bảng điều khiển',
 
+    'actions' => [
+
+        'filter' => [
+
+            'label' => 'Bộ lọc',
+
+            'modal' => [
+
+                'heading' => 'Bộ lọc',
+
+                'actions' => [
+
+                    'apply' => [
+
+                        'label' => 'Áp dụng',
+
+                    ],
+
+                ],
+
+            ],
+
+        ],
+
+    ],
 ];
