7ca574ff9ca4a6cb31bdae0a20af32d847cd0a31
Dan Harrin <git@danharrin.com>
2023-12-26 12:21:10 +0000

    wip

--- packages/actions/src/Concerns/CanRedirect.php
+++ packages/actions/src/Concerns/CanRedirect.php
@@ -4,6 +4,7 @@ namespace Filament\Actions\Concerns;
 
 use Closure;
 use Filament\Support\Facades\FilamentView;
+use function Filament\Support\is_app_url;
 
 trait CanRedirect
 {
@@ -35,7 +36,9 @@ trait CanRedirect
 
     public function redirect(string | Closure $url): void
     {
-        $this->getLivewire()->redirect($this->evaluate($url), navigate: FilamentView::hasSpaMode());
+        $url = $this->evaluate($url);
+
+        $this->getLivewire()->redirect($url, navigate: FilamentView::hasSpaMode() && is_app_url($url));
     }
 
     public function failureRedirectUrl(string | Closure | null $url): static
--- packages/panels/src/Resources/Pages/CreateRecord.php
+++ packages/panels/src/Resources/Pages/CreateRecord.php
@@ -107,11 +107,7 @@ class CreateRecord extends Page
 
         $redirectUrl = $this->getRedirectUrl();
 
-        if (FilamentView::hasSpaMode()) {
-            $this->redirect($redirectUrl, navigate: is_app_url($redirectUrl));
-        } else {
-            $this->redirect($redirectUrl);
-        }
+        $this->redirect($redirectUrl, navigate: FilamentView::hasSpaMode() && is_app_url($redirectUrl));
     }
 
     protected function getCreatedNotification(): ?Notification
