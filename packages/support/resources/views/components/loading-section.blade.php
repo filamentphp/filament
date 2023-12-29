9b6378b353fa36bf4b76b14d309e6c6b7967c1c3
Zep Fietje <hey@zepfietje.com>
2023-12-29 16:45:36 +0100

    Add CSS hook class for loading section

--- packages/support/resources/views/components/loading-section.blade.php
+++ packages/support/resources/views/components/loading-section.blade.php
@@ -25,6 +25,7 @@
     :lgStart="$columnStart['lg'] ?? null"
     :xlStart="$columnStart['xl'] ?? null"
     :twoXlStart="$columnStart['2xl'] ?? null"
+    class="fi-loading-section"
 >
     <x-filament::section class="h-32 animate-pulse" />
 </x-filament::grid.column>
