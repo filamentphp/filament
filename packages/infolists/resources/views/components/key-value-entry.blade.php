d99bc1923215aaebb182e8cb69171e73944ae8fa
juliomotol <juliomotol@users.noreply.github.com>
2023-12-29 15:05:12 +0000

    chore: fix code style

--- packages/infolists/resources/views/components/key-value-entry.blade.php
+++ packages/infolists/resources/views/components/key-value-entry.blade.php
@@ -1,7 +1,4 @@
-<x-dynamic-component
-    :component="$getEntryWrapperView()"
-    :entry="$entry"
->
+<x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
     @php
         $state = $getState();
     @endphp
@@ -10,21 +7,23 @@
         {{
             $attributes
                 ->merge($getExtraAttributes(), escape: false)
-                ->class(['fi-key-value w-full rounded-xl shadow-sm bg-white ring-1 ring-gray-950/5 dark:bg-white/5 dark:ring-white/10'])
+                ->class(['fi-key-value w-full rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-white/5 dark:ring-white/10'])
         }}
     >
-        <table class="w-full divide-y divide-gray-200 table-auto dark:divide-white/5">
+        <table
+            class="w-full table-auto divide-y divide-gray-200 dark:divide-white/5"
+        >
             <thead>
                 <tr>
                     <th
-                        class="px-3 py-2 text-sm font-medium text-gray-700 text-start dark:text-gray-200"
+                        class="px-3 py-2 text-start text-sm font-medium text-gray-700 dark:text-gray-200"
                         scope="col"
                     >
                         {{ $getKeyLabel() }}
                     </th>
 
                     <th
-                        class="px-3 py-2 text-sm font-medium text-gray-700 text-start dark:text-gray-200"
+                        class="px-3 py-2 text-start text-sm font-medium text-gray-700 dark:text-gray-200"
                         scope="col"
                     >
                         {{ $getValueLabel() }}
@@ -32,14 +31,18 @@
                 </tr>
             </thead>
 
-            <tbody class="font-mono text-base divide-y divide-gray-200 sm:leading-6 sm:text-sm dark:divide-white/5">
+            <tbody
+                class="divide-y divide-gray-200 font-mono text-base sm:text-sm sm:leading-6 dark:divide-white/5"
+            >
                 @foreach ($state as $key => $value)
-                    <tr class="divide-x divide-gray-200 rtl:divide-x-reverse dark:divide-white/5">
-                        <td class="w-1/2 py-1.5 px-3">
+                    <tr
+                        class="divide-x divide-gray-200 rtl:divide-x-reverse dark:divide-white/5"
+                    >
+                        <td class="w-1/2 px-3 py-1.5">
                             {{ $key }}
                         </td>
 
-                        <td class="w-1/2 py-1.5 px-3">
+                        <td class="w-1/2 px-3 py-1.5">
                             {{ $value }}
                         </td>
                     </tr>
