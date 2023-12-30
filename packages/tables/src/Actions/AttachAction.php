8923801850a928292dd8671d7a02c840258be0a3
Dennis van Dalen <denemio@gmail.com>
2023-12-30 16:30:27 +0800

    Update AttachAction.php

--- packages/tables/src/Actions/AttachAction.php
+++ packages/tables/src/Actions/AttachAction.php
@@ -230,12 +230,12 @@ class AttachAction extends Action
             $relationCountHash = $relationship->getRelationCountHash(false);
             $relationshipQuery
                 ->when(
-                    !$table->allowsDuplicates(),
-                    fn(Builder $query): Builder => $query->whereDoesntHave(
+                    ! $table->allowsDuplicates(),
+                    fn (Builder $query): Builder => $query->whereDoesntHave(
                         $table->getInverseRelationship(),
 
-                        fn(Builder $query): Builder => $query->where(
-                        // https://github.com/filamentphp/filament/issues/8067
+                        fn (Builder $query): Builder => $query->where(
+                            // https://github.com/filamentphp/filament/issues/8067
                             $relationship->getParent()->getTable() === $relationship->getRelated()->getTable() ?
                                 ($relationCountHash.'.'.$relationship->getParent()->getKeyName()) :
                                 $relationship->getParent()->getQualifiedKeyName(),
