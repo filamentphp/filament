bc8b4e183b09905351f89d37f7adbd0630b33b15
Dennis van Dalen <denemio@gmail.com>
2023-12-30 16:44:18 +0800

    run CS on packages/tables/src/Actions/AssociateAction.php

--- packages/tables/src/Actions/AssociateAction.php
+++ packages/tables/src/Actions/AssociateAction.php
@@ -241,7 +241,7 @@ class AssociateAction extends Action
                     return $query->where(
                         // https://github.com/filamentphp/filament/issues/8067
                         $relationship->getParent()->getTable() === $relationship->getRelated()->getTable() ?
-                            ($relationCountHash.'.'.$relationship->getParent()->getKeyName()) :
+                            ($relationCountHash . '.' . $relationship->getParent()->getKeyName()) :
                             $relationship->getParent()->getQualifiedKeyName(),
                         $relationship->getParent()->getKey(),
                     );
