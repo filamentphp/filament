e2db18ea94b30d7a1280bedde29f57dc59a65ee7
Lambert TRACCARD <lambert.traccard@gmail.com>
2024-01-09 23:25:28 +0100

    Add filament register event

---
+++ packages/panels/src/Events/Auth/Registered.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Filament\Events\Auth;
+
+use Illuminate\Queue\SerializesModels;
+
+class Registered
+{
+    use SerializesModels;
+
+    /**
+     * The authenticated user.
+     *
+     * @var \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+}
