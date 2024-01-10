e039ee8bb04bce54aa30a4b10aec33bdbe241b17
Dan Harrin <git@danharrin.com>
2024-01-10 15:35:13 +0000

    Update Registered.php

--- packages/panels/src/Events/Auth/Registered.php
+++ packages/panels/src/Events/Auth/Registered.php
@@ -2,27 +2,14 @@
 
 namespace Filament\Events\Auth;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Queue\SerializesModels;
 
 class Registered
 {
     use SerializesModels;
 
-    /**
-     * The authenticated user.
-     *
-     * @var \Illuminate\Contracts\Auth\Authenticatable
-     */
-    public $user;
-
-    /**
-     * Create a new event instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
-     * @return void
-     */
-    public function __construct($user)
-    {
-        $this->user = $user;
-    }
+    public function __construct(
+        readonly public Authenticatable $user,
+    ) {}
 }
