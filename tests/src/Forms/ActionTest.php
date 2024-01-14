f973fa73ae7eef5df70a5071d489fec4ff75e54e
Dan Harrin <git@danharrin.com>
2024-01-14 14:24:10 +0000

    feature: Form component action testing

--- tests/src/Forms/ActionTest.php
+++ tests/src/Forms/ActionTest.php
@@ -2,8 +2,11 @@
 
 use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\TextInput;
+use Filament\Tests\Forms\Fixtures\Actions;
 use Filament\Tests\TestCase;
 use Illuminate\Support\Str;
+
+use function Filament\Tests\livewire;
 
 uses(TestCase::class);
 
@@ -29,4 +32,91 @@ it('can auto-register actions from affixes', function () {
     expect($component)
         ->getAction($prefixActionName)->toBe($prefixAction)
         ->getAction($suffixActionName)->toBe($suffixAction);
+});
+
+it('can call an action with data', function () {
+    livewire(Actions::class)
+        ->callFormComponentAction('textInput', 'setValue', data: [
+            'value' => $value = Str::random(),
+        ])
+        ->assertFormSet(['textInput' => $value]);
+});
+
+it('can validate an action\'s data', function () {
+    livewire(Actions::class)
+        ->callFormComponentAction('textInput', 'setValue', data: [
+            'value' => null,
+        ])
+        ->assertHasFormComponentActionErrors(['value' => ['required']])
+        ->assertFormSet(['textInput' => null]);
+});
+
+it('can set default action data when mounted', function () {
+    livewire(Actions::class)
+        ->mountFormComponentAction('textInput', 'setValue')
+        ->assertFormComponentActionDataSet([
+            'value' => 'foo',
+        ]);
+});
+
+it('can call an action with arguments', function () {
+    livewire(Actions::class)
+        ->callFormComponentAction('textInput', 'setValueFromArguments', arguments: [
+            'value' => $value = Str::random(),
+        ])
+        ->assertFormSet(['textInput' => $value]);
+});
+
+it('can call an action and halt', function () {
+    livewire(Actions::class)
+        ->callFormComponentAction('textInput', 'halt')
+        ->assertFormComponentActionHalted('textInput', 'halt');
+});
+
+it('can hide an action', function () {
+    livewire(Actions::class)
+        ->assertFormComponentActionVisible('textInput', 'visible')
+        ->assertFormComponentActionHidden('textInput', 'hidden');
+});
+
+it('can disable an action', function () {
+    livewire(Actions::class)
+        ->assertFormComponentActionEnabled('textInput', 'enabled')
+        ->assertFormComponentActionDisabled('textInput', 'disabled');
+});
+
+it('can have an icon', function () {
+    livewire(Actions::class)
+        ->assertFormComponentActionHasIcon('textInput', 'hasIcon', 'heroicon-m-pencil-square')
+        ->assertFormComponentActionDoesNotHaveIcon('textInput', 'hasIcon', 'heroicon-m-trash');
+});
+
+it('can have a label', function () {
+    livewire(Actions::class)
+        ->assertFormComponentActionHasLabel('textInput', 'hasLabel', 'My Action')
+        ->assertFormComponentActionDoesNotHaveLabel('textInput', 'hasLabel', 'My Other Action');
+});
+
+it('can have a color', function () {
+    livewire(Actions::class)
+        ->assertFormComponentActionHasColor('textInput', 'hasColor', 'primary')
+        ->assertFormComponentActionDoesNotHaveColor('textInput', 'hasColor', 'gray');
+});
+
+it('can have a URL', function () {
+    livewire(Actions::class)
+        ->assertFormComponentActionHasUrl('textInput', 'url', 'https://filamentphp.com')
+        ->assertFormComponentActionDoesNotHaveUrl('textInput', 'url', 'https://google.com');
+});
+
+it('can open a URL in a new tab', function () {
+    livewire(Actions::class)
+        ->assertFormComponentActionShouldOpenUrlInNewTab('textInput', 'urlInNewTab')
+        ->assertFormComponentActionShouldNotOpenUrlInNewTab('textInput', 'urlNotInNewTab');
+});
+
+it('can state whether a form component action exists', function () {
+    livewire(Actions::class)
+        ->assertFormComponentActionExists('textInput', 'exists')
+        ->assertFormComponentActionDoesNotExist('textInput', 'doesNotExist');
 });
