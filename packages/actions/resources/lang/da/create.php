08bff0e473055239f77a2ae32ec4a098fa9675ea
Mads Møller <mm@developdiverse.com>
2024-01-19 08:22:20 +0100

    i18n: actions in danish

---
+++ packages/actions/resources/lang/da/create.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Opret :label',
+
+        'modal' => [
+
+            'heading' => 'Opret :label',
+
+            'actions' => [
+
+                'create' => [
+                    'label' => 'Opret',
+                ],
+
+                'create_another' => [
+                    'label' => 'Opret & opret en mere',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'created' => [
+                'title' => 'Oprettet',
+            ],
+
+        ],
+
+    ],
+
+];
---
+++ packages/actions/resources/lang/da/dissociate.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Dissocier',
+
+        'modal' => [
+
+            'heading' => 'Dissocier :label',
+
+            'actions' => [
+
+                'dissociate' => [
+                    'label' => 'Dissocier',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'dissociated' => [
+                'title' => 'Dissocieret',
+            ],
+
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => 'Dissocier valgte',
+
+        'modal' => [
+
+            'heading' => 'Dissocier valgte :label',
+
+            'actions' => [
+
+                'dissociate' => [
+                    'label' => 'Dissocier',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'dissociated' => [
+                'title' => 'Dissocieret',
+            ],
+
+        ],
+
+    ],
+
+];
