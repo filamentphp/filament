2a314b88ac05cde7269e1e920e09a4a5192a1707
tpheaktra168 <pheaktratuy99@gmail.com>
2024-01-20 11:54:00 +0700

    add translations

---
+++ packages/forms/resources/lang/km/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'distinct' => [
+        'must_be_selected' => 'យ៉ាងហោចណាស់​មួយ :attribute ត្រូវតែត្រូវបានជ្រើសរើស។',
+        'only_one_must_be_selected' => 'តែមួយគត់ :attribute ត្រូវតែត្រូវបានជ្រើសរើស។',
+    ],
+
+];
