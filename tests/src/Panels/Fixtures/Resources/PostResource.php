aa20aaaa2f4c87b8412ba32a7ca08abc1563b07e
ArielMejiaDev <arielmejiadev@gmail.com>
2024-01-22 07:10:30 -0600

    remove type changes from UserResource

--- tests/src/Panels/Fixtures/Resources/PostResource.php
+++ tests/src/Panels/Fixtures/Resources/PostResource.php
@@ -5,7 +5,6 @@ namespace Filament\Tests\Panels\Fixtures\Resources;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Support\Contracts\Iconizable;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Tests\Models\Post;
@@ -18,7 +17,7 @@ class PostResource extends Resource
 
     protected static ?string $navigationGroup = 'Blog';
 
-    protected static string | Iconizable | null $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-user';
 
     protected static ?string $recordTitleAttribute = 'title';
 
