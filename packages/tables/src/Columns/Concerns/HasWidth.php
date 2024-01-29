552583cc1a80fc3da95adadb4fa9546dcb3fa360
Heiko Klingele <mail@heikoklingele.de>
2024-01-29 22:02:08 +0100

    int to string with px

--- packages/tables/src/Columns/Concerns/HasWidth.php
+++ packages/tables/src/Columns/Concerns/HasWidth.php
@@ -4,8 +4,12 @@ namespace Filament\Tables\Columns\Concerns;
 
 trait HasWidth
 {
-    public function columnWidth(int|string $width): static
+    public function columnWidth(int | string $width): static
     {
+        if (is_int($width)) {
+            $width = "{$width}px";
+        }
+
         $this->extraHeaderAttributes[] = ['style' => 'width: ' . $width];
 
         return $this;
