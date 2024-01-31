b4da6036afe2cf52e26f8b8dd39259ab520b454e
Dan Harrin <git@danharrin.com>
2024-01-31 11:10:33 +0000

    Add method to all forms

--- packages/panels/resources/views/components/form/index.blade.php
+++ packages/panels/resources/views/components/form/index.blade.php
@@ -1,8 +1,13 @@
+@props([
+    'method' => 'post',
+])
+
 <form
     x-data="{ isProcessing: false }"
     x-on:submit="if (isProcessing) $event.preventDefault()"
     x-on:form-processing-started="isProcessing = true"
     x-on:form-processing-finished="isProcessing = false"
+    method="{{ $method }}"
     {{ $attributes->class(['fi-form grid gap-y-6']) }}
 >
     {{ $slot }}
