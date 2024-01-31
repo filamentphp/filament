928ae22e0eeb1e24ea5f63b4d01a6fd8fa86b567
Dan Harrin <git@danharrin.com>
2024-01-31 20:15:11 +0000

    feature: Customize import/export auth

--- packages/actions/src/Exports/Http/Controllers/DownloadExport.php
+++ packages/actions/src/Exports/Http/Controllers/DownloadExport.php
@@ -5,13 +5,20 @@ namespace Filament\Actions\Exports\Http\Controllers;
 use Filament\Actions\Exports\Enums\ExportFormat;
 use Filament\Actions\Exports\Models\Export;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+
+use function Filament\authorize;
 
 class DownloadExport
 {
     public function __invoke(Request $request, Export $export): StreamedResponse
     {
-        abort_unless($export->user->is(auth()->user()), 403);
+        if (filled(Gate::getPolicyFor($export::class))) {
+            authorize('view', $export);
+        } else {
+            abort_unless($export->user()->is(auth()->user()), 403);
+        }
 
         $format = ExportFormat::tryFrom($request->query('format'));
 
