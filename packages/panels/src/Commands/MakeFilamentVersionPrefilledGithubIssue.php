5b0c62766385f235548ffe3c8d38a5f05b7b0412
M. D. <dissto@users.noreply.github.com>
2024-02-09 20:47:26 +0100

    Initial proposal

---
+++ packages/panels/src/Commands/MakeFilamentVersionPrefilledGithubIssue.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeFilamentVersionPrefilledGithubIssue extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:filament-issue';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $filament = \Composer\InstalledVersions::getPrettyVersion('filament/filament');
+        $laravel = \Composer\InstalledVersions::getPrettyVersion('laravel/framework');
+        $livewire = \Composer\InstalledVersions::getPrettyVersion('livewire/livewire');
+        $php = \PHP_VERSION;
+
+        $issueTemplate = 'bug_report.yml';
+        $baseGithubUrl = 'https://github.com/filamentphp/filament/issues/new';
+
+        $queryParams = [
+            'template' => $issueTemplate,
+            'package-version' => $filament,
+            'laravel-version' => $laravel,
+            'livewire-version' => $livewire,
+            'php-version' => $php,
+        ];
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = $baseGithubUrl . '?' . $queryString;
+
+        $this->info('Please fill out the issue template at ' . $fullUrl);
+    }
+}
