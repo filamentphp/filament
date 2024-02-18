cd0a9966d72be80ab3942bd00bed3fb4343d4220
Alexis <287688+AlexisSerneels@users.noreply.github.com>
2024-02-18 12:29:58 +0100

    Use the guards defined in panel configuration

--- packages/actions/routes/web.php
+++ packages/actions/routes/web.php
@@ -2,12 +2,16 @@
 
 use Filament\Actions\Exports\Http\Controllers\DownloadExport;
 use Filament\Actions\Imports\Http\Controllers\DownloadImportFailureCsv;
+use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/filament/exports/{export}/download', DownloadExport::class)
-    ->name('filament.exports.download')
-    ->middleware(['web', 'auth']);
+foreach (Filament::getPanels() as $panel) {
+    /** @var \Filament\Panel $panel */
+    Route::get('/filament/exports/{export}/download', DownloadExport::class)
+        ->name('filament.exports.download')
+        ->middleware(array_merge($panel->getMiddleware(), $panel->getAuthMiddleware()));
 
-Route::get('/filament/imports/{import}/failed-rows/download', DownloadImportFailureCsv::class)
-    ->name('filament.imports.failed-rows.download')
-    ->middleware(['web', 'auth']);
+    Route::get('/filament/imports/{import}/failed-rows/download', DownloadImportFailureCsv::class)
+        ->name('filament.imports.failed-rows.download')
+        ->middleware(array_merge($panel->getMiddleware(), $panel->getAuthMiddleware()));
+}
