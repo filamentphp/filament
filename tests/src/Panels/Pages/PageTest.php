14e8bcb9f7336e7a9706a78df8afd024eded765f
Dan Harrin <git@danharrin.com>
2024-02-20 09:42:10 +0000

    Update PageTest.php

--- tests/src/Panels/Pages/PageTest.php
+++ tests/src/Panels/Pages/PageTest.php
@@ -1,11 +1,9 @@
 <?php
 
-use Filament\Facades\Filament;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
 use Filament\Tests\Panels\Fixtures\Pages\Settings;
 use Filament\Tests\Panels\Pages\TestCase;
-use Illuminate\Support\HtmlString;
 use Illuminate\Validation\ValidationException;
 
 use function Filament\Tests\livewire;
@@ -36,14 +34,4 @@ it('can report validation errors', function () {
         ->call('save')
         ->assertHasErrors(['name' => ['required']])
         ->assertNotified();
-});
-
-it('strips html tags from brand name in title tag', function () {
-    $htmlBrandName = '<span>Filament <i>HTML Title</i></span>';
-
-    Filament::getCurrentPanel()->brandName(new HtmlString($htmlBrandName));
-
-    $this->get(Settings::getUrl())
-        ->assertSee('Filament HTML Title')
-        ->assertDontSee($htmlBrandName);
 });
