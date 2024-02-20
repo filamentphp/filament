41dc0e850c023a5e92942859cf5703c2e10fcfb7
Maarten Paauw <maartenpaauw@gmail.com>
2024-02-20 17:33:30 +0100

    fix(actions): download exports and failed row imports as non-default auth guards
    This code gathers all Filament panels, retrieves their associated auth guards, and passes them as arguments to the auth middleware. This change will enable users authenticated with non-default auth guards to download exports and handle failed row imports.

--- packages/actions/routes/web.php
+++ packages/actions/routes/web.php
@@ -2,12 +2,20 @@
 
 use Filament\Actions\Exports\Http\Controllers\DownloadExport;
 use Filament\Actions\Imports\Http\Controllers\DownloadImportFailureCsv;
+use Filament\Facades\Filament;
+use Filament\Panel;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
+
+$guards = Collection::make(Filament::getPanels())
+    ->map(static fn (Panel $panel): string => $panel->getAuthGuard())
+    ->unique()
+    ->implode(',');
 
 Route::get('/filament/exports/{export}/download', DownloadExport::class)
     ->name('filament.exports.download')
-    ->middleware(['web', 'auth']);
+    ->middleware(['web', "auth:$guards"]);
 
 Route::get('/filament/imports/{import}/failed-rows/download', DownloadImportFailureCsv::class)
     ->name('filament.imports.failed-rows.download')
-    ->middleware(['web', 'auth']);
+    ->middleware(['web', "auth:$guards"]);
