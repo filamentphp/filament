719967fbe00a2a48f77443ea85613d4a154a8bd7
Zep Fietje <hey@zepfietje.com>
2024-02-23 14:17:38 +0100

    Update packages/tables/resources/views/components/actions/cell.blade.php

--- packages/tables/resources/views/components/actions/cell.blade.php
+++ packages/tables/resources/views/components/actions/cell.blade.php
@@ -1,7 +1,7 @@
 <x-filament-tables::cell
     :attributes="\Filament\Support\prepare_inherited_attributes($attributes)"
 >
-    <div class="fi-ta-action-cell whitespace-nowrap px-3 py-4">
+    <div class="fi-ta-actions-cell whitespace-nowrap px-3 py-4">
         {{ $slot }}
     </div>
 </x-filament-tables::cell>
