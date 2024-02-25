7263708c62433dc19db427be16d87bc78b9d44f6
Zep Fietje <hey@zepfietje.com>
2024-02-25 15:40:10 +0100

    Fix code style

--- packages/panels/routes/web.php
+++ packages/panels/routes/web.php
@@ -165,7 +165,6 @@ Route::name('filament.')
                                     }
                                 });
                         }
-
                     });
             }
         }
