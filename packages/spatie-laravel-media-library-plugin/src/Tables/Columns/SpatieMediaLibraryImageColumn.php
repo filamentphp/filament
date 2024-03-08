319ddf3389628d911ab8c90d4d841eeaeec008f2
Dan Harrin <git@danharrin.com>
2024-03-08 11:16:09 +0000

    Revert "ignore phpstan error"
    This reverts commit 176fc78686dd77e3c1aa1874210b856844b01326.

--- packages/spatie-laravel-media-library-plugin/src/Tables/Columns/SpatieMediaLibraryImageColumn.php
+++ packages/spatie-laravel-media-library-plugin/src/Tables/Columns/SpatieMediaLibraryImageColumn.php
@@ -57,8 +57,8 @@ class SpatieMediaLibraryImageColumn extends ImageColumn
             /** @var ?Media $media */
             $media = $record->getRelationValue('media')->first(fn (Media $media): bool => $media->uuid === $state);
 
-            if (!$media) {
-                return $record->getFirstMediaUrl($this->getCollection()); // @phpstan-ignore-line
+            if (! $media) {
+                return $record->getFallbackMediaUrl($this->getCollection());
             }
 
             $conversion = $this->getConversion();
