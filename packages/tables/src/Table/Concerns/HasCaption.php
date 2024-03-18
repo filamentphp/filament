1e559b9b8bf6070bcf5a4177efa154ff9c1f88dd
Achyut Neupane <achyutkneupane@gmail.com>
2024-03-19 02:12:47 +0545

    HasCaption Concern defined

---
+++ packages/tables/src/Table/Concerns/HasCaption.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Filament\Tables\Table\Concerns;
+
+use Closure;
+use Illuminate\Contracts\Support\Htmlable;
+
+trait HasCaption
+{
+    protected string | Htmlable | Closure | null $caption = null;
+
+    protected bool $shouldTranslateCaption = false;
+
+    public function caption(string | Htmlable | Closure | null $caption): static
+    {
+        $this->caption = $caption;
+
+        return $this;
+    }
+
+    public function translateCaption(bool $shouldTranslateCaption = true): static
+    {
+        $this->shouldTranslateCaption = $shouldTranslateCaption;
+
+        return $this;
+    }
+
+    public function getCaption(): string | Htmlable
+    {
+        $caption = $this->evaluate($this->caption);
+
+        return $this->shouldTranslateCaption ? __($caption) : $caption;
+    }
+}
