2e487e3bb99365a18935ca93df55d14e8c71467e
corean <corean@corean.biz>
2024-03-24 17:42:44 +0900

    - Update Korean translations

--- packages/panels/resources/lang/ko/pages/dashboard.php
+++ packages/panels/resources/lang/ko/pages/dashboard.php
@@ -4,4 +4,30 @@ return [
 
     'title' => '대시보드',
 
+    'actions' => [
+
+        'filter' => [
+
+            'label' => '필터',
+
+            'modal' => [
+
+                'heading' => '필터',
+
+                'actions' => [
+
+                    'apply' => [
+
+                        'label' => '적용',
+
+                    ],
+
+                ],
+
+            ],
+
+        ],
+
+    ],
+
 ];
