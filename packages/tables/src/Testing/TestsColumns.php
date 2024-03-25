748e7a17e6eff162c96e8455018b8376039bd588
Hugh Messenger <hugh.messenger@gmail.com>
2024-03-25 16:12:27 -0500

    Handle array states and values in assertTableColumnStateSet

--- packages/tables/src/Testing/TestsColumns.php
+++ packages/tables/src/Testing/TestsColumns.php
@@ -146,8 +146,18 @@ class TestsColumns
 
             $livewireClass = $this->instance()::class;
 
+            $state = $column->getState();
+
+            if (is_array($state)) {
+                $state = serialize($state);
+            }
+
+            if (is_array($value)) {
+                $value = serialize($value);
+            }
+
             Assert::assertTrue(
-                $column->getState() == $value,
+                $state == $value,
                 message: "Failed asserting that a table column with name [{$name}] has value of [{$value}] for record [{$record->getKey()}] on the [{$livewireClass}] component.",
             );
 
@@ -171,8 +181,18 @@ class TestsColumns
 
             $livewireClass = $this->instance()::class;
 
+            $state = $column->getState();
+
+            if (is_array($state)) {
+                $state = serialize($state);
+            }
+
+            if (is_array($value)) {
+                $value = serialize($value);
+            }
+
             Assert::assertFalse(
-                $column->getState() == $value,
+                $state == $value,
                 message: "Failed asserting that a table column with name [{$name}] does not have a value of [{$value}] for record [{$record->getKey()}] on the [{$livewireClass}] component.",
             );
 
