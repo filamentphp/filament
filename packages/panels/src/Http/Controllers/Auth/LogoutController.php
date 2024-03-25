5f655c3d7ca9f5c0718f3b286022e6a020bd2823
Burak Faruk ŞAHİN <buhacksahin@gmail.com>
2024-03-25 07:42:06 +0300

    Update LogoutController.php
    fix for multi panel logout 
    were logs out from all panel issue.

--- packages/panels/src/Http/Controllers/Auth/LogoutController.php
+++ packages/panels/src/Http/Controllers/Auth/LogoutController.php
@@ -9,11 +9,25 @@ class LogoutController
 {
     public function __invoke(): LogoutResponse
     {
-        Filament::auth()->logout();
+        $session = session()->all(); // We will get old session values.
+        Filament::getCurrentPanel()->auth()->logout(); // We will log out only from current panel.
 
-        session()->invalidate();
-        session()->regenerateToken();
+        session()->invalidate(); // it will invalidate and destory our session data.
+        session()->regenerateToken(); // it will regenerate session token.
+        session($this->removeGuardVariables($session)); // it will remove guard values such as password_hash and login from old session data.
+        
+        return app(LogoutResponse::class); // it will send user to logout response.
+    }
 
-        return app(LogoutResponse::class);
+    public function removeGuardVariables(array $session): array
+    {
+        $guard = Filament::getCurrentPanel()->getAuthGuard(); // intended way to get guard name of the panel you've been logged in.
+        foreach ($session as $sessionKey => $sessionValue) {
+            if (str_starts_with($sessionKey, 'password_hash_' . $guard)
+                || str_starts_with($sessionKey, 'login_' . $guard)) {
+                unset($session[$sessionKey]); // if string starts with password_hash or login just remove it.
+            }
+        }
+        return $session; // return it for freshly new created session.
     }
 }
