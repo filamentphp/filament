90d6c23cb01057e0f102a4e65d08802a86d7a20f
notscottsmith <51853082+notscottsmith@users.noreply.github.com>
2024-03-26 15:49:27 +0800

    Adding application/vnd.ms-excel to accepted file types for importing

--- packages/actions/src/Concerns/CanImportRecords.php
+++ packages/actions/src/Concerns/CanImportRecords.php
@@ -75,7 +75,7 @@ trait CanImportRecords
             FileUpload::make('file')
                 ->label(__('filament-actions::import.modal.form.file.label'))
                 ->placeholder(__('filament-actions::import.modal.form.file.placeholder'))
-                ->acceptedFileTypes(['text/csv', 'text/x-csv', 'application/csv', 'application/x-csv', 'text/comma-separated-values', 'text/x-comma-separated-values', 'text/plain'])
+                ->acceptedFileTypes(['application/vnd.ms-excel', 'text/csv', 'text/x-csv', 'application/csv', 'application/x-csv', 'text/comma-separated-values', 'text/x-comma-separated-values', 'text/plain'])
                 ->afterStateUpdated(function (Forms\Set $set, ?TemporaryUploadedFile $state) use ($action) {
                     if (! $state instanceof TemporaryUploadedFile) {
                         return;
