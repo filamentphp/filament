936a8ef5b819ec8526741f8154ba82c4926a925f
devhoussam1998 <121255432+devhoussam1998@users.noreply.github.com>
2024-04-06 19:57:50 +0000

    Update header.blade.php

--- packages/tables/resources/views/components/header.blade.php
+++ packages/tables/resources/views/components/header.blade.php
@@ -7,11 +7,11 @@
 @props([
     'actions' => [],
     'actionsPosition',
+    'description' => null,
+    'heading' => null,
     'icon' => null,
     'iconColor' => 'gray',
     'iconSize' => IconSize::Large,
-    'heading' => null,
-    'description' => null,
 ])
 
 <div
@@ -21,26 +21,34 @@
     ]) }}>
 
     @if ($icon)
-        <x-filament::icon :icon="$icon" @class([
-            'fi-ta-header-icon self-start',
-            match ($iconColor) {
-                'gray' => 'text-gray-400 dark:text-gray-500',
-                default => 'fi-color-custom text-custom-500 dark:text-custom-400',
-            },
-            is_string($iconColor) ? "fi-color-{$iconColor}" : null,
-            match ($iconSize) {
-                IconSize::Small, 'sm' => 'h-4 w-4 mt-1',
-                IconSize::Medium, 'md' => 'h-5 w-5 mt-0.5',
-                IconSize::Large, 'lg' => 'h-6 w-6',
-                default => $iconSize,
-            },
-        ]) @style([
-            \Filament\Support\get_color_css_variables($iconColor, shades: [400, 500], alias: 'table.header.icon') => $iconColor !== 'gray',
-        ]) />
+        <x-filament::icon 
+            :icon="$icon" 
+            @class([
+                'fi-ta-header-icon self-start',
+                match ($iconColor) {
+                    'gray' => 'text-gray-400 dark:text-gray-500',
+                    default => 'fi-color-custom text-custom-500 dark:text-custom-400',
+                },
+                is_string($iconColor) ? "fi-color-{$iconColor}" : null,
+                match ($iconSize) {
+                    IconSize::Small, 'sm' => 'h-4 w-4 mt-1',
+                    IconSize::Medium, 'md' => 'h-5 w-5 mt-0.5',
+                    IconSize::Large, 'lg' => 'h-6 w-6',
+                    default => $iconSize,
+                },
+            ])
+            @style([
+                \Filament\Support\get_color_css_variables(
+                    $iconColor,
+                    shades: [400, 500],
+                    alias: 'table.header.icon',
+                ) => $iconColor !== 'gray',
+            ]) 
+        />
     @endif
 
     @if ($heading || $description)
-        <div class="grid flex-1 gap-y-1">
+        <div class="grid gap-y-1">
             @if ($heading)
                 <h3 class="text-base font-semibold leading-6 fi-ta-header-heading text-gray-950 dark:text-white">
                     {{ $heading }}
