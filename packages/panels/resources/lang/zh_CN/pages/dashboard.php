8964fe2f9e8d7977e615c0111efb85186c1a29c9
Lalhmangaihenga <valpuia604@gmail.com>
2024-04-08 14:38:21 +0700

    revert dashboard.php

--- packages/panels/resources/lang/zh_CN/pages/dashboard.php
+++ packages/panels/resources/lang/zh_CN/pages/dashboard.php
@@ -4,30 +4,4 @@ return [
 
     'title' => '仪表板',
 
-    'actions' => [
-
-        'filter' => [
-
-            'label' => '筛选',
-
-            'modal' => [
-
-                'heading' => '筛选',
-
-                'actions' => [
-
-                    'apply' => [
-
-                        'label' => '申请',
-
-                    ],
-
-                ],
-
-            ],
-
-        ],
-
-    ],
-
 ];
