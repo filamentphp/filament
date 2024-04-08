07dcc39dbe06300b3b683bf14969806daaf89f18
Lalhmangaihenga <valpuia604@gmail.com>
2024-04-08 14:37:12 +0700

    Update zh_CN dashboard.php

--- packages/panels/resources/lang/zh_CN/pages/dashboard.php
+++ packages/panels/resources/lang/zh_CN/pages/dashboard.php
@@ -4,4 +4,30 @@ return [
 
     'title' => '仪表板',
 
+    'actions' => [
+
+        'filter' => [
+
+            'label' => '筛选',
+
+            'modal' => [
+
+                'heading' => '筛选',
+
+                'actions' => [
+
+                    'apply' => [
+
+                        'label' => '申请',
+
+                    ],
+
+                ],
+
+            ],
+
+        ],
+
+    ],
+
 ];
