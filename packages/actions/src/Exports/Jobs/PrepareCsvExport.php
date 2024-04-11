dbb7095a0cf1bad0ff30dbd714df9e64d357ed62
Dan Harrin <git@danharrin.com>
2024-04-11 08:28:59 +0100

    Update PrepareCsvExport.php

--- packages/actions/src/Exports/Jobs/PrepareCsvExport.php
+++ packages/actions/src/Exports/Jobs/PrepareCsvExport.php
@@ -9,6 +9,7 @@ use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Connection;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
