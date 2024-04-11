8ded135d02b91ae12a071463bcf7a7d4fd50ffef
Dan Harrin <git@danharrin.com>
2024-04-11 08:38:51 +0100

    cs

--- packages/actions/src/Exports/Jobs/PrepareCsvExport.php
+++ packages/actions/src/Exports/Jobs/PrepareCsvExport.php
@@ -73,7 +73,7 @@ class PrepareCsvExport implements ShouldQueue
                 ->unique('column');
 
             $query->reorder($qualifiedKeyName);
-            
+
             foreach ($originalOrders as $order) {
                 $query->orderBy($order['column'], $order['direction']);
             }
