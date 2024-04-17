87e5bbbd2f952f410b353bef0eefa183d2c5d0f9
Dima Udod <udod.dmitry@gmail.com>
2024-04-17 11:18:43 +0300

    Fixed repeater with translation bug #8328

--- packages/spatie-laravel-translatable-plugin/src/Resources/Pages/EditRecord/Concerns/Translatable.php
+++ packages/spatie-laravel-translatable-plugin/src/Resources/Pages/EditRecord/Concerns/Translatable.php
@@ -91,10 +91,10 @@ trait Translatable
 
         $this->otherLocaleData[$this->oldActiveLocale] = Arr::only($this->data, $translatableAttributes);
 
-        $this->data = [
+        $this->form->fill([
             ...Arr::except($this->data, $translatableAttributes),
             ...$this->otherLocaleData[$this->activeLocale] ?? [],
-        ];
+        ]);
 
         unset($this->otherLocaleData[$this->activeLocale]);
     }
