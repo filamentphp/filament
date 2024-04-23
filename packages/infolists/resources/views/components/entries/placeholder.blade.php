a0841b0acbf6ebd83db94d720957ee3174e878f6
Zep Fietje <hey@zepfietje.com>
2024-04-23 09:45:09 +0200

    Fix placeholder line height

--- packages/infolists/resources/views/components/entries/placeholder.blade.php
+++ packages/infolists/resources/views/components/entries/placeholder.blade.php
@@ -1,3 +1,5 @@
-<div class="fi-in-placeholder text-sm text-gray-400 dark:text-gray-500">
+<div
+    class="fi-in-placeholder text-sm leading-6 text-gray-400 dark:text-gray-500"
+>
     {{ $slot }}
 </div>
