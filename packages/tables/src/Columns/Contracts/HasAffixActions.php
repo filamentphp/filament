38ee49983dbfa4a00dc417eb15696c8b6ab76eb8
devhoussam1998 <121255432+devhoussam1998@users.noreply.github.com>
2024-05-07 23:19:24 +0100

    Create HasAffixActions.php

---
+++ packages/tables/src/Columns/Contracts/HasAffixActions.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Filament\Tables\Columns\Contracts;
+
+use Filament\Tables\Actions\Action;
+
+interface HasAffixActions
+{
+    /**
+     * @return array<Action>
+     */
+    public function getPrefixActions(): array;
+
+    /**
+     * @return array<Action>
+     */
+    public function getSuffixActions(): array;
+}
