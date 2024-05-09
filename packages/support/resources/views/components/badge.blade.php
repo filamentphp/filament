119f08eaed94b093d16c126f0639a4af66713118
Dan Harrin <git@danharrin.com>
2024-05-09 09:17:00 +0100

    add other usages

--- packages/support/resources/views/components/badge.blade.php
+++ packages/support/resources/views/components/badge.blade.php
@@ -81,7 +81,8 @@
         x-data="{}"
     @endif
     @if ($keyBindings)
-        x-mousetrap.global.{{ collect($keyBindings)->map(fn (string $keyBinding): string => str_replace('+', '-', $keyBinding))->implode('.') }}
+        x-bind:id="$id('key-bindings')"
+        x-mousetrap.global.{{ collect($keyBindings)->map(fn (string $keyBinding): string => str_replace('+', '-', $keyBinding))->implode('.') }}="document.getElementById($el.id).click()"
     @endif
     @if ($hasTooltip)
         x-tooltip="{
