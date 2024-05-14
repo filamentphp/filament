313922787461f8a855223e88b4432969b3d8476c
Ash Monsh <wh7r.com@gmail.com>
2024-05-14 16:39:29 +0300

    add HasExtraFieldWrapperAttributes trait

---
+++ packages/forms/src/Components/Concerns/HasExtraFieldWrapperAttributes.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Filament\Forms\Components\Concerns;
+
+use Closure;
+use Illuminate\View\ComponentAttributeBag;
+
+trait HasExtraFieldWrapperAttributes
+{
+    /**
+     * @var array<array<mixed> | Closure>
+     */
+    protected array $extraFieldWrapperAttributes = [];
+
+    /**
+     * @param  array<mixed> | Closure  $attributes
+     */
+    public function extraFieldWrapperAttributes(array | Closure $attributes, bool $merge = false): static
+    {
+        if ($merge) {
+            $this->extraFieldWrapperAttributes[] = $attributes;
+        } else {
+            $this->extraFieldWrapperAttributes = [$attributes];
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getExtraFieldWrapperAttributes(): array
+    {
+        $temporaryAttributeBag = new ComponentAttributeBag();
+
+        foreach ($this->extraFieldWrapperAttributes as $extraFieldWrapperAttributes) {
+            $temporaryAttributeBag = $temporaryAttributeBag->merge($this->evaluate($extraFieldWrapperAttributes));
+        }
+
+        return $temporaryAttributeBag->getAttributes();
+    }
+
+    public function getExtraFieldWrapperAttributesBag(): ComponentAttributeBag
+    {
+        return new ComponentAttributeBag($this->getExtraFieldWrapperAttributes());
+    }
+}
