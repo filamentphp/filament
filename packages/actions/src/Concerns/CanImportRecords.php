2d910bb94c8d6adc5344b4e2fad84ee5a09b82b1
Robert Lyons <r.lyons@thecpc.ac.uk>
2024-05-20 12:19:12 +0100

    remove old rule

--- packages/actions/src/Concerns/CanImportRecords.php
+++ packages/actions/src/Concerns/CanImportRecords.php
@@ -81,7 +81,6 @@ trait CanImportRecords
                 ->label(__('filament-actions::import.modal.form.file.label'))
                 ->placeholder(__('filament-actions::import.modal.form.file.placeholder'))
                 ->acceptedFileTypes(['text/csv', 'text/x-csv', 'application/csv', 'application/x-csv', 'text/comma-separated-values', 'text/x-comma-separated-values', 'text/plain', 'application/vnd.ms-excel'])
-                ->rule('extensions:csv,txt')
                 ->rules([
                     'extensions:csv,txt',
                     File::types(['csv', 'txt'])->rules([
