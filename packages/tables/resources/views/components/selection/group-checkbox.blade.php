5caeb34879ed2375edbd0014b4302ed05b812081
Maximilian Oswald <maxi.osw@outlook.com>
2024-05-20 14:56:48 +0200

    Updating group-checkbox livewire key to use page instead of random.

--- packages/tables/resources/views/components/selection/group-checkbox.blade.php
+++ packages/tables/resources/views/components/selection/group-checkbox.blade.php
@@ -1,11 +1,12 @@
 @props([
     'key',
     'title',
+    'page' => null
 ])
 
 {{-- format-ignore-start --}}
 <x-filament-tables::selection.checkbox
-    :wire:key="$this->getId() . 'table.bulk_select_group.checkbox' . \Illuminate\Support\Str::random()"
+    :wire:key="$this->getId() . 'table.bulk_select_group.checkbox.' . $page"
     :label="__('filament-tables::table.fields.bulk_select_group.label', ['title' => $title])"
     :x-bind:checked="'
         const recordsInGroup = getRecordsInGroupOnPage(' . \Illuminate\Support\Js::from($key) . ')
