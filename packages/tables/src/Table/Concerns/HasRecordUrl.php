5d195a15af991dbf24a93d78247b8c0c9b61ea3c
dmason30 <danmason@kirschbaumdevelopment.com>
2024-05-23 00:07:00 +0100

    Allow table row record url to open in new tab

--- packages/tables/src/Table/Concerns/HasRecordUrl.php
+++ packages/tables/src/Table/Concerns/HasRecordUrl.php
@@ -7,10 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasRecordUrl
 {
+    protected bool | Closure $shouldOpenRecordUrlInNewTab = false;
+
     protected string | Closure | null $recordUrl = null;
 
-    public function recordUrl(string | Closure | null $url): static
+    public function openRecordUrlInNewTab(bool | Closure $condition = true): static
     {
+        $this->shouldOpenRecordUrlInNewTab = $condition;
+
+        return $this;
+    }
+
+    public function recordUrl(string | Closure | null $url, bool | Closure $shouldOpenInNewTab = false): static
+    {
+        $this->openRecordUrlInNewTab($shouldOpenInNewTab);
         $this->recordUrl = $url;
 
         return $this;
@@ -20,6 +30,20 @@ trait HasRecordUrl
     {
         return $this->evaluate(
             $this->recordUrl,
+            namedInjections: [
+                'record' => $record,
+            ],
+            typedInjections: [
+                Model::class => $record,
+                $record::class => $record,
+            ],
+        );
+    }
+
+    public function shouldOpenRecordUrlInNewTab(Model $record): bool
+    {
+        return (bool) $this->evaluate(
+            $this->shouldOpenRecordUrlInNewTab,
             namedInjections: [
                 'record' => $record,
             ],
