77828b463cf4808c5c275e6ce3f30c35b202e5b2
Zep Fietje <hey@zepfietje.com>
2024-05-29 14:12:47 +0200

    Add CSS hook classes for split

--- packages/tables/resources/views/columns/layout/split.blade.php
+++ packages/tables/resources/views/columns/layout/split.blade.php
@@ -3,7 +3,7 @@
         $attributes
             ->merge($getExtraAttributes(), escape: false)
             ->class([
-                'flex',
+                'fi-ta-split flex',
                 match ($getFromBreakpoint()) {
                     'sm' => 'flex-col gap-2 sm:flex-row sm:items-center sm:gap-3',
                     'md' => 'flex-col gap-2 md:flex-row md:items-center md:gap-3',
