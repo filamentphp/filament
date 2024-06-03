006e20c013c017299dfc2285213c8c5cfb489b83
jlaperaltab <jperalta@enovus.cl>
2024-06-03 16:41:09 -0400

    run composer cs

--- packages/actions/src/Imports/ImportColumn.php
+++ packages/actions/src/Imports/ImportColumn.php
@@ -332,7 +332,7 @@ class ImportColumn extends Component
                     return;
                 }
 
-                $fail(__('validation.exists', ['attribute' =>  Str::lcfirst($this->getLabel())]));
+                $fail(__('validation.exists', ['attribute' => Str::lcfirst($this->getLabel())]));
             };
         }
 
