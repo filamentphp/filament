2a2ecbbb4e1b774f84aa8fdda87ce4f53b0ec6a1
Dan Harrin <git@danharrin.com>
2024-06-12 13:28:16 +0100

    Update packages/forms/resources/js/components/textarea.js

--- packages/forms/resources/js/components/textarea.js
+++ packages/forms/resources/js/components/textarea.js
@@ -17,6 +17,7 @@ export default function textareaFormComponent({ initialHeight }) {
             if (this.$el.scrollHeight > 0) {
                 // Only set the height if it differs from the current height
                 const newHeight = this.$el.scrollHeight + 'px';
+                
                 if (this.height !== newHeight) {
                     this.height = newHeight;
                     this.$el.style.height = this.height;
