8e074d28c0fad34c47a425518f7ac44942e19e5c
bnzo <17174973+bnzo@users.noreply.github.com>
2024-06-13 10:45:45 +0200

    Update date-time-picker.js to use advancedFormat plugin for more flexible date formatting

--- packages/forms/resources/js/components/date-time-picker.js
+++ packages/forms/resources/js/components/date-time-picker.js
@@ -1,10 +1,12 @@
 import dayjs from 'dayjs/esm'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
 import localeData from 'dayjs/plugin/localeData'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(customParseFormat)
+dayjs.extend(advancedFormat)
 dayjs.extend(localeData)
 dayjs.extend(timezone)
 dayjs.extend(utc)
