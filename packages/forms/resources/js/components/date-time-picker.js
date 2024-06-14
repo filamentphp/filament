88e4f7044dca93fe0f428de27881449074653379
Dan Harrin <git@danharrin.com>
2024-06-14 08:17:39 +0100

    Update date-time-picker.js

--- packages/forms/resources/js/components/date-time-picker.js
+++ packages/forms/resources/js/components/date-time-picker.js
@@ -1,12 +1,12 @@
 import dayjs from 'dayjs/esm'
-import customParseFormat from 'dayjs/plugin/customParseFormat'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 import localeData from 'dayjs/plugin/localeData'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 
-dayjs.extend(customParseFormat)
 dayjs.extend(advancedFormat)
+dayjs.extend(customParseFormat)
 dayjs.extend(localeData)
 dayjs.extend(timezone)
 dayjs.extend(utc)
