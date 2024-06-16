87ddb33fe201ed361eab6dd10d8da061a27bc7e9
Edu <edu@rdo.blog.br>
2024-06-16 00:09:15 -0300

    TDD expected behavior

---
+++ tests/src/Panels/Resources/Pages/CreateRecordUsingWizardTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Filament\Tests\Panels\Fixtures\Resources\PostResource\Pages\CreatePostUsingWizard;
+use Filament\Tests\Panels\Resources\TestCase;
+
+use function Filament\Tests\livewire;
+
+uses(TestCase::class);
+
+it('moves to next wizard step', function () {
+    livewire(CreatePostUsingWizard::class)
+        ->fillForm(['title' => null])
+        ->nextFormWizardStep()
+        ->assertHasErrors(['data.title'])
+        ->assertHasNoErrors(['data.content'])
+        ->nextFormWizardStep(currentStep: 1)
+        ->assertHasErrors(['data.content'])
+        ->assertHasNoErrors(['data.title']);
+});
+
+
+
