ec498ddf10858bcb370caa513cc8cd049e87bf5e
ralphjsmit <ralphjsmit@users.noreply.github.com>
2024-06-19 16:54:38 +0000

    chore: fix code style

--- packages/panels/resources/views/components/page/simple.blade.php
+++ packages/panels/resources/views/components/page/simple.blade.php
@@ -5,7 +5,7 @@
 
 <div {{ $attributes->class(['fi-simple-page']) }}>
     {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_START, scopes: $this->getRenderHookScopes()) }}
-    
+
     <section class="grid auto-cols-fr gap-y-6">
         <x-filament-panels::header.simple
             :heading="$heading ??= $this->getHeading()"
@@ -19,6 +19,6 @@
     @if (! $this instanceof \Filament\Tables\Contracts\HasTable)
         <x-filament-actions::modals />
     @endif
-    
+
     {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_END, scopes: $this->getRenderHookScopes()) }}
 </div>
