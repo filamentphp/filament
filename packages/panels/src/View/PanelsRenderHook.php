0e7ddc03a292bf651588551dd77d201ac832fdf0
ralphjsmit <ralphjsmit@users.noreply.github.com>
2024-06-20 09:05:15 +0000

    chore: fix code style

--- packages/panels/src/View/PanelsRenderHook.php
+++ packages/panels/src/View/PanelsRenderHook.php
@@ -87,10 +87,10 @@ class PanelsRenderHook
     const SIDEBAR_NAV_START = 'panels::sidebar.nav.start';
 
     const SIDEBAR_FOOTER = 'panels::sidebar.footer';
-	
-	const SIMPLE_PAGE_END = 'panels::simple-page.end';
-	
-	const SIMPLE_PAGE_START = 'panels::simple-page.start';
+
+    const SIMPLE_PAGE_END = 'panels::simple-page.end';
+
+    const SIMPLE_PAGE_START = 'panels::simple-page.start';
 
     const STYLES_AFTER = 'panels::styles.after';
 
