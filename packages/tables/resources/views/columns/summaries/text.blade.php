aeab489214b3dc0c8b58b49cc74e14c6f1510591
D-Jordy <kaaljordy@gmail.com>
2024-06-20 10:06:08 +0200

    Composer cs

--- packages/tables/resources/views/columns/summaries/text.blade.php
+++ packages/tables/resources/views/columns/summaries/text.blade.php
@@ -1,19 +1,19 @@
-@if($isVisible())
-<div
-    {{
-        $attributes
-            ->merge($getExtraAttributes(), escape: false)
-            ->class(['fi-ta-text-summary grid gap-y-1 px-3 py-4'])
-    }}
->
-    @if (filled($label = $getLabel()))
-        <span class="text-sm font-medium text-gray-950 dark:text-white">
-            {{ $label }}
-        </span>
-    @endif
+@if ($isVisible())
+    <div
+        {{
+            $attributes
+                ->merge($getExtraAttributes(), escape: false)
+                ->class(['fi-ta-text-summary grid gap-y-1 px-3 py-4'])
+        }}
+    >
+        @if (filled($label = $getLabel()))
+            <span class="text-sm font-medium text-gray-950 dark:text-white">
+                {{ $label }}
+            </span>
+        @endif
 
-    <span class="text-sm text-gray-500 dark:text-gray-400">
-        {{ $formatState($getState()) }}
-    </span>
-</div>
+        <span class="text-sm text-gray-500 dark:text-gray-400">
+            {{ $formatState($getState()) }}
+        </span>
+    </div>
 @endif
