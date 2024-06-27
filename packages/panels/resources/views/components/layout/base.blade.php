b1850dc1de89e63c23af5a8f181dc149d3605330
Tony Lea <tnylea@gmail.com>
2024-06-27 18:21:25 -0400

    Adding update to create dynamic class for a route

--- packages/panels/resources/views/components/layout/base.blade.php
+++ packages/panels/resources/views/components/layout/base.blade.php
@@ -103,6 +103,7 @@
                 ->class([
                     'fi-body',
                     'fi-panel-' . filament()->getId(),
+                    'fi-url-' . Str::slug(str_replace('/', '-', Request::path())),
                     'min-h-screen bg-gray-50 font-normal text-gray-950 antialiased dark:bg-gray-950 dark:text-white',
                 ]) }}
     >
