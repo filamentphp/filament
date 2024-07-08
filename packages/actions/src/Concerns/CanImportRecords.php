cc3f6232bfe35729abbcce2f1446f23516992ee9
Iliyas M <im.smiliyas@gmail.com>
2024-07-08 15:29:19 +0530

    use `Livewire`'s built-in `readStream()` method

--- packages/actions/src/Concerns/CanImportRecords.php
+++ packages/actions/src/Concerns/CanImportRecords.php
@@ -377,23 +377,7 @@ trait CanImportRecords
      */
     public function getUploadedFileStream(TemporaryUploadedFile $file)
     {
-        $filePath = $file->getRealPath();
-
-        if (config('filesystems.disks.' . config('filament.default_filesystem_disk') . '.driver') !== 's3') {
-            $resource = fopen($filePath, mode: 'r');
-        } else {
-            /** @var AwsS3V3Adapter $s3Adapter */
-            $s3Adapter = Storage::disk('s3')->getAdapter();
-
-            invade($s3Adapter)->client->registerStreamWrapper(); /** @phpstan-ignore-line */
-            $fileS3Path = 's3://' . config('filesystems.disks.s3.bucket') . '/' . $filePath;
-
-            $resource = fopen($fileS3Path, mode: 'r', context: stream_context_create([
-                's3' => [
-                    'seekable' => true,
-                ],
-            ]));
-        }
+        $resource = $file->readStream();
 
         $inputEncoding = $this->detectCsvEncoding($resource);
         $outputEncoding = 'UTF-8';
