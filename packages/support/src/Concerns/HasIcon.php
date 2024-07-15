6bf185bf9a69ccf89cbb1f2b9edb1eb60256d956
Dan Harrin <git@danharrin.com>
2024-07-15 15:41:53 +0100

    Update HasIcon.php

--- packages/support/src/Concerns/HasIcon.php
+++ packages/support/src/Concerns/HasIcon.php
@@ -40,14 +40,14 @@ trait HasIcon
 
     public function getIcon(): string | Htmlable | null
     {
-        $icon = $this->evaluate($icon);
+        $icon = $this->evaluate($this->icon);
 
         // https://github.com/filamentphp/filament/pull/13512
-        if ($this->icon instanceof Renderable) {
-            return new HtmlString($this->icon->render());
+        if ($icon instanceof Renderable) {
+            return new HtmlString($icon->render());
         }
 
-        return $this->icon;
+        return $icon;
     }
 
     public function getIconPosition(): IconPosition | string
