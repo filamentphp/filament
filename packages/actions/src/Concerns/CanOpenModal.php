94e40b4eaf573c5d5cc4de7ad7b83e5ab8b1f768
Tina Hammar <tinahammar@gmail.com>
2024-07-18 11:02:30 +0200

    keep defined record, if set

--- packages/actions/src/Concerns/CanOpenModal.php
+++ packages/actions/src/Concerns/CanOpenModal.php
@@ -434,7 +434,8 @@ trait CanOpenModal
 
         if (
             ($this instanceof HasRecord) &&
-            ($action instanceof HasRecord)
+            ($action instanceof HasRecord) &&
+            invade($action)->record === null
         ) {
             $action->record($this->getRecord());
         }
