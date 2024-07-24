5684281b8cb4f0a7108c861f158e5c7b319f69d0
M. D. <dissto@users.noreply.github.com>
2024-07-24 13:28:24 +0200

    Update OptimizeCommand.php

--- packages/support/src/Commands/OptimizeCommand.php
+++ packages/support/src/Commands/OptimizeCommand.php
@@ -1,39 +1,39 @@
 <?php
 
-namespace Filament\Support\Commands;
+    namespace Filament\Support\Commands;
 
-use Filament\Support\Concerns\CanCacheComponents;
-use Illuminate\Console\Command;
-use Symfony\Component\Console\Attribute\AsCommand;
+    use Filament\Support\Concerns\CanCacheComponents;
+    use Illuminate\Console\Command;
+    use Symfony\Component\Console\Attribute\AsCommand;
 
-#[AsCommand(name: 'filament:optimize')]
-class OptimizeCommand extends Command
-{
-    use CanCacheComponents;
-
-    protected $signature = 'filament:optimize';
-
-    protected $description = 'Cache components and Blade icons to improve performance.';
-
-    public function handle(): int
+    #[AsCommand(name: 'filament:optimize')]
+    class OptimizeCommand extends Command
     {
-        $this->components->info('Caching components and Blade icons...');
+        use CanCacheComponents;
 
-        $tasks = collect([
-            'Caching Blade icons' => fn (): bool => $this->callSilent('icons:cache') === static::SUCCESS,
-        ]);
+        protected $signature = 'filament:optimize';
 
-        if ($this->canCacheComponents()) {
-            $tasks->put(
-                'Caching components',
-                fn (): bool => $this->callSilent('filament:cache-components') === static::SUCCESS
-            );
+        protected $description = 'Cache components and Blade icons to improve performance.';
+
+        public function handle(): int
+        {
+            $this->components->info('Caching components and Blade icons.');
+
+            $tasks = collect([
+                'Caching Blade icons' => fn(): bool => $this->callSilent('icons:cache') === static::SUCCESS,
+            ]);
+
+            if ($this->canCacheComponents()) {
+                $tasks->put(
+                    'Caching components',
+                    fn(): bool => $this->callSilent('filament:cache-components') === static::SUCCESS
+                );
+            }
+
+            $tasks->each(fn($task, $description) => $this->components->task($description, $task));
+
+            $this->newLine();
+
+            return static::SUCCESS;
         }
-
-        $tasks->each(fn ($task, $description) => $this->components->task($description, $task));
-
-        $this->newLine();
-
-        return static::SUCCESS;
     }
-}
