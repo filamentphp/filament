3a4271d5e69011c482bc0bd5d28845500efd251e
ericmp <ericmorenopou@gmail.com>
2024-07-25 15:41:57 +0200

    fix: action - styling when isIconButton

--- packages/actions/resources/views/components/action.blade.php
+++ packages/actions/resources/views/components/action.blade.php
@@ -9,9 +9,10 @@
     $url = $action->getUrl();
     $tooltip = $action->getTooltip();
     $tooltipIsNotEmpty = str($tooltip)->isNotEmpty();
+    $icon = $icon ?? $action->getIcon();
 @endphp
 
-@if ($tooltipIsNotEmpty) <div x-data x-tooltip.raw="{{ $tooltip }}" class="flex"> @endif
+@if ($tooltipIsNotEmpty) <div x-data x-tooltip.raw="{{ $tooltip }}" @class(['flex', 'items-center justify-center -m-2 h-9 w-9' => str($icon)->isNotEmpty() && $action->isIconButton()])> @endif
     <x-dynamic-component
         :color="$action->getColor()"
         :component="$dynamicComponent"
@@ -19,7 +20,7 @@
         :form="$action->getFormToSubmit()"
         :form-id="$action->getFormId()"
         :href="$isDisabled ? null : $url"
-        :icon="$icon ?? $action->getIcon()"
+        :icon="$icon"
         :icon-size="$action->getIconSize()"
         :key-bindings="$action->getKeyBindings()"
         :label-sr-only="$action->isLabelHidden()"
@@ -32,7 +33,7 @@
         :attributes="
             \Filament\Support\prepare_inherited_attributes($attributes)
                 ->merge($action->getExtraAttributes(), escape: false)
-                ->class(['fi-ac-action flex-1'])
+                ->class(['fi-ac-action', 'flex-1' => $tooltipIsNotEmpty])
         "
     >
         {{ $slot }}
