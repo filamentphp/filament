3456f30b1cd42d6f24ebe9a487a824df0f0216e5
Luis Parrado <luisprmat@gmail.com>
2024-07-26 13:26:45 -0500

    Update Spanish translations (import, actions modals)

--- packages/forms/resources/lang/es/components.php
+++ packages/forms/resources/lang/es/components.php
@@ -11,15 +11,67 @@ return [
             ],
 
             'add' => [
+
                 'label' => 'Añadir a :label',
+
+                'modal' => [
+
+                    'heading' => 'Añadir a :label',
+
+                    'actions' => [
+
+                        'add' => [
+                            'label' => 'Añadir',
+                        ],
+
+                    ],
+
+                ],
+
             ],
 
             'add_between' => [
-                'label' => 'Insertar',
+
+                'label' => 'Insertar entre bloques',
+
+                'modal' => [
+
+                    'heading' => 'Añadir a :label',
+
+                    'actions' => [
+
+                        'add' => [
+                            'label' => 'Añadir',
+                        ],
+
+                    ],
+
+                ],
+
             ],
 
             'delete' => [
                 'label' => 'Borrar',
+            ],
+
+            'edit' => [
+
+                'label' => 'Editar',
+
+                'modal' => [
+
+                    'heading' => 'Editar bloque',
+
+                    'actions' => [
+
+                        'save' => [
+                            'label' => 'Guardar cambios',
+                        ],
+
+                    ],
+
+                ],
+
             ],
 
             'reorder' => [
