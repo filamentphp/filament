552bd94a5f9b78e7d8684e6821b6bedc05c1a18f
Dan Harrin <git@danharrin.com>
2024-07-30 14:10:45 +0100

    Update and rename HasUrls.php to CanGenerateResourceUrls.php

--- packages/panels/src/Panel/Concerns/CanGenerateResourceUrls.php
+++ packages/panels/src/Panel/Concerns/CanGenerateResourceUrls.php
@@ -6,26 +6,22 @@ use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Throwable;
 
-trait HasUrls
+trait CanGenerateResourceUrls
 {
     /**
      * @param  array<mixed>  $parameters
-     *
-     * @throws Throwable
      */
     public function getResourceUrl(string | Model $model, string $name = 'index', array $parameters = [], bool $isAbsolute = true, ?Model $tenant = null): string
     {
         $modelClass = is_string($model) ? $model : $model::class;
 
-        $resource = $this->getModelResource($modelClass);
+        $resource = $this->getModelResource($modelClass) ?? throw new Exception("No Filament resource found for model [{$modelClass}].");
 
-        if (blank($resource)) {
-            throw new Exception("No Filament resource found for model {$modelClass}");
-        }
-
-        // If the model is an instance of Model and the name is "edit" or "view" pass the record as a parameter.
-        if ($model instanceof Model && in_array($name, ['edit', 'view']) && blank($parameters)) {
-            $parameters = ['record' => $model];
+        if (
+            ($model instanceof Model) &&
+            in_array($name, ['edit', 'view'])
+        ) {
+            $parameters['record'] ??= $model;
         }
 
         return $resource::getUrl($name, $parameters, $isAbsolute, $this->getId(), $tenant);
