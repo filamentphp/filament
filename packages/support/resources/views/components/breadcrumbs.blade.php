d074d8c2ce711cf51a30d41de69244d4278f18d3
Dan Harrin <git@danharrin.com>
2024-08-07 07:09:23 +0300

    Update breadcrumbs.blade.php

--- packages/support/resources/views/components/breadcrumbs.blade.php
+++ packages/support/resources/views/components/breadcrumbs.blade.php
@@ -4,6 +4,7 @@
 
 @php
     $iconClasses = 'fi-breadcrumbs-item-separator flex h-5 w-5 text-gray-400 dark:text-gray-500';
+    $itemLabelClasses = 'fi-breadcrumbs-item-label text-sm font-medium text-gray-500 dark:text-gray-400';
 @endphp
 
 <nav {{ $attributes->class(['fi-breadcrumbs']) }}>
@@ -32,17 +33,15 @@
                 @endif
 
                 @if (is_int($url))
-                    <span
-                        class="fi-breadcrumbs-item-label text-sm font-medium text-gray-500 transition duration-75 dark:text-gray-400"
-                    >
+                    <span class="{{ $itemLabelClasses }}">
                         {{ $label }}
                     </span>
                 @else
                     <a
                         {{ \Filament\Support\generate_href_html(is_int($url) ? '#' : $url) }}
-                        class="fi-breadcrumbs-item-label text-sm font-medium text-gray-500 transition duration-75 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+                        class="{{ $itemLabelClasses }} transition duration-75 hover:text-gray-700 dark:hover:text-gray-200"
                     >
-                    {{ $label }}
+                        {{ $label }}
                     </a>
                 @endif
             </li>
