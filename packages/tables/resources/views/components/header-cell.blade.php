d9fb4a9d6fea0cb29372bf9d2384c15abf3d7026
Paul <paul@shopapps.co.uk>
2024-08-29 11:02:25 +0100

    Update header-cell.blade.php
    fix for aria-label showing html when column heading is htmlable

--- packages/tables/resources/views/components/header-cell.blade.php
+++ packages/tables/resources/views/components/header-cell.blade.php
@@ -25,7 +25,7 @@
 >
     <{{ $sortable ? 'button' : 'span' }}
         @if ($sortable)
-            aria-label="{{ $slot }}"
+            aria-label="{{ strip_tags($slot) }}"
             type="button"
             wire:click="sortTable('{{ $name }}')"
         @endif
