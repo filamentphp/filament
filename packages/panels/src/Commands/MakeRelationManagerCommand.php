b6449c998596e219a95e8ca8f1fb765f5864c654
wychoong <67364036+wychoong@users.noreply.github.com>
2024-09-09 23:02:51 +0800

    Update MakeRelationManagerCommand.php

--- packages/panels/src/Commands/MakeRelationManagerCommand.php
+++ packages/panels/src/Commands/MakeRelationManagerCommand.php
@@ -79,18 +79,24 @@ class MakeRelationManagerCommand extends Command
             )] : Arr::first($panels);
         }
 
-        $resourceDirectories = $panel->getResourceDirectories();
-        $resourceNamespaces = $panel->getResourceNamespaces();
+        $resourceDirectories = [
+            app_path('Filament/Resources/'),
+            ...$panel->getResourceDirectories(),
+        ];
+        $resourceNamespaces = [
+            'App\\Filament\\Resources',
+            ...$panel->getResourceNamespaces(),
+        ];
 
         $resourceNamespace = (count($resourceNamespaces) > 1) ?
             select(
                 label: 'Which namespace would you like to create this in?',
                 options: $resourceNamespaces
             ) :
-            (Arr::first($resourceNamespaces) ?? 'App\\Filament\\Resources');
+            Arr::first($resourceNamespaces);
         $resourcePath = (count($resourceDirectories) > 1) ?
             $resourceDirectories[array_search($resourceNamespace, $resourceNamespaces)] :
-            (Arr::first($resourceDirectories) ?? app_path('Filament/Resources/'));
+            Arr::first($resourceDirectories);
 
         $path = (string) str($managerClass)
             ->prepend("{$resourcePath}/{$resource}/RelationManagers/")
