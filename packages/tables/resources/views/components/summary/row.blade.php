a627b314d275abeec92eff8269bbb9e22606ea67
Dan Harrin <git@danharrin.com>
2024-10-07 13:36:36 +0100

    Update row.blade.php

--- packages/tables/resources/views/components/summary/row.blade.php
+++ packages/tables/resources/views/components/summary/row.blade.php
@@ -44,7 +44,7 @@
         <x-filament-tables::cell
             class="text-sm font-medium text-gray-950 dark:text-white"
         >
-            <span class="px-3 py-4">
+            <span class="fi-ta-summary-row-heading px-3 py-4">
                 {{ $heading }}
             </span>
         </x-filament-tables::cell>
