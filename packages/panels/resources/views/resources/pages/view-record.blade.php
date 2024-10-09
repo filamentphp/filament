f36b70e2f36c0b63b37fb38d98861510393c1bc6
Krzysztof <krzysztof@engineer.com>
2024-10-09 18:03:17 +0200

    fix formatting

--- packages/panels/resources/views/resources/pages/view-record.blade.php
+++ packages/panels/resources/views/resources/pages/view-record.blade.php
@@ -1,44 +1,37 @@
-<x-filament-panels::page
-    @class([ 'fi-resource-view-record-page' , 'fi-resource-' . str_replace('/', '-' , $this->getResource()::getSlug()),
+<x-filament-panels::page @class([
+    'fi-resource-view-record-page',
+    'fi-resource-' . str_replace('/', '-', $this->getResource()::getSlug()),
     'fi-resource-record-' . $record->getKey(),
-    ])
-    >
+])>
     @php
-    $relationManagers = $this->getRelationManagers();
-    $hasCombinedRelationManagerTabsWithContent = $this->hasCombinedRelationManagerTabsWithContent();
+        $relationManagers = $this->getRelationManagers();
+        $hasCombinedRelationManagerTabsWithContent = $this->hasCombinedRelationManagerTabsWithContent();
     @endphp
 
-    @if ((! $hasCombinedRelationManagerTabsWithContent) || (! count($relationManagers)))
-    @if ($this->hasInfolist())
-    {{ $this->infolist }}
-    @else
-    <div
-        wire:key="{{ $this->getId() }}.forms.{{ $this->getFormStatePath() }}">
-        {{ $this->form }}
-    </div>
-    @endif
+    @if (!$hasCombinedRelationManagerTabsWithContent || !count($relationManagers))
+        @if ($this->hasInfolist())
+            {{ $this->infolist }}
+        @else
+            <div wire:key="{{ $this->getId() }}.forms.{{ $this->getFormStatePath() }}">
+                {{ $this->form }}
+            </div>
+        @endif
     @endif
 
     @if (count($relationManagers))
-    <x-filament-panels::resources.relation-managers
-        :active-locale="isset($activeLocale) ? $activeLocale : null"
-        :active-manager="$this->activeRelationManager ?? ($hasCombinedRelationManagerTabsWithContent ? null : array_key_first($relationManagers))"
-        :content-tab-label="$this->getContentTabLabel()"
-        :content-tab-icon="$this->getContentTabIcon()"
-        :content-tab-position="$this->getContentTabPosition()"
-        :managers="$relationManagers"
-        :owner-record="$record"
-        :page-class="static::class">
-        @if ($hasCombinedRelationManagerTabsWithContent)
-        <x-slot name="content">
-            @if ($this->hasInfolist())
-            {{ $this->infolist }}
-            @else
-            {{ $this->form }}
+        <x-filament-panels::resources.relation-managers :active-locale="isset($activeLocale) ? $activeLocale : null" :active-manager="$this->activeRelationManager ??
+            ($hasCombinedRelationManagerTabsWithContent ? null : array_key_first($relationManagers))" :content-tab-label="$this->getContentTabLabel()"
+            :content-tab-icon="$this->getContentTabIcon()" :content-tab-position="$this->getContentTabPosition()" :managers="$relationManagers" :owner-record="$record" :page-class="static::class">
+            @if ($hasCombinedRelationManagerTabsWithContent)
+                <x-slot name="content">
+                    @if ($this->hasInfolist())
+                        {{ $this->infolist }}
+                    @else
+                        {{ $this->form }}
+                    @endif
+                </x-slot>
             @endif
-        </x-slot>
-        @endif
-    </x-filament-panels::resources.relation-managers>
+        </x-filament-panels::resources.relation-managers>
     @endif
 
     {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::RESOURCE_PAGES_VIEW_RECORD_END) }}
