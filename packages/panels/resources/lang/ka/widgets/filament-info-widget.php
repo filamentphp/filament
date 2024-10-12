34fcb029ece4c36361898c2f604ffe69459d28a4
Irakli Kirkitadze <kirkita205@gmail.com>
2024-10-12 15:25:13 +0400

    add Georgian translation for panels package

---
+++ packages/panels/resources/lang/ka/widgets/filament-info-widget.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'actions' => [
+
+        'open_documentation' => [
+            'label' => 'დოკუმენტაცია',
+        ],
+
+        'open_github' => [
+            'label' => 'GitHub',
+        ],
+
+    ],
+
+];
