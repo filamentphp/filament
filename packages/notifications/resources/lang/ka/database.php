4cb678c898e63cb6f54420f7f6f54877ef68ba64
Irakli Kirkitadze <kirkita205@gmail.com>
2024-10-12 16:28:23 +0400

    add Georgian translations folder for package notifications

---
+++ packages/notifications/resources/lang/ka/database.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'modal' => [
+
+        'heading' => 'Notifications',
+
+        'actions' => [
+
+            'clear' => [
+                'label' => 'Clear',
+            ],
+
+            'mark_all_as_read' => [
+                'label' => 'Mark all as read',
+            ],
+
+        ],
+
+        'empty' => [
+            'heading' => 'No notifications',
+            'description' => 'Please check again later.',
+        ],
+
+    ],
+
+];
