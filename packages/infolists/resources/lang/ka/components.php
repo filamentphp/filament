c6e0a536f20395072f19ea5d25486e355f4b9a1b
Irakli Kirkitadze <kirkita205@gmail.com>
2024-10-12 16:25:23 +0400

    add Georgian tranlation folder for package infolists

---
+++ packages/infolists/resources/lang/ka/components.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    'entries' => [
+
+        'text' => [
+
+            'actions' => [
+                'collapse_list' => 'Show :count less',
+                'expand_list' => 'Show :count more',
+            ],
+
+            'more_list_items' => 'and :count more',
+
+        ],
+
+        'key_value' => [
+
+            'columns' => [
+
+                'key' => [
+                    'label' => 'Key',
+                ],
+
+                'value' => [
+                    'label' => 'Value',
+                ],
+
+            ],
+
+            'placeholder' => 'No entries',
+
+        ],
+
+    ],
+
+];
