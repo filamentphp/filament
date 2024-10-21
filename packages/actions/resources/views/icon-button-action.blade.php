8d448ebf255469157641849a1f46f54c49a04f1e
Dan Harrin <git@danharrin.com>
2024-10-21 11:57:26 +0100

    Refactor select action & remove remaining action views

--- packages/actions/resources/views/icon-button-action.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament-actions::action
-    :action="$action"
-    :badge="$getBadge()"
-    :badge-color="$getBadgeColor()"
-    dynamic-component="filament::icon-button"
-    :label="$getLabel()"
-    :size="$getSize()"
-    class="fi-ac-icon-btn-action"
-/>
--- packages/actions/src/SelectAction.php
+++ packages/actions/src/SelectAction.php
@@ -2,14 +2,122 @@
 
 namespace Filament\Actions;
 
-class SelectAction extends Action
+use Filament\Actions\Concerns\HasId;
+use Filament\Schema\Components\Component;
+use Filament\Support\Components\Contracts\HasEmbeddedView;
+use Filament\Support\Contracts\HasLabel as LabelInterface;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\View\ComponentAttributeBag;
+
+class SelectAction extends Action implements HasEmbeddedView
 {
-    use Concerns\HasSelect;
+    use HasId;
 
-    protected function setUp(): void
+    /**
+     * @var array<string> | Arrayable | string | Closure
+     */
+    protected array | Arrayable | string | Closure $options = [];
+
+    protected ?string $placeholder = null;
+
+    /**
+     * @param  array<string> | Arrayable | string | Closure  $options
+     */
+    public function options(array | Arrayable | string | Closure $options): static
     {
-        parent::setUp();
+        $this->options = $options;
 
-        $this->view('filament-actions::select-action');
+        return $this;
+    }
+
+    public function placeholder(string $placeholder): static
+    {
+        $this->placeholder = $placeholder;
+
+        return $this;
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getOptions(): array
+    {
+        $options = $this->evaluate($this->options) ?? [];
+
+        if (
+            is_string($options) &&
+            enum_exists($enum = $options)
+        ) {
+            if (is_a($enum, LabelInterface::class, allow_string: true)) {
+                return array_reduce($enum::cases(), function (array $carry, LabelInterface & UnitEnum $case): array {
+                    $carry[$case?->value ?? $case->name] = $case->getLabel() ?? $case->name;
+
+                    return $carry;
+                }, []);
+            }
+
+            return array_reduce($enum::cases(), function (array $carry, UnitEnum $case): array {
+                $carry[$case?->value ?? $case->name] = $case->name;
+
+                return $carry;
+            }, []);
+        }
+
+        if ($options instanceof Arrayable) {
+            $options = $options->toArray();
+        }
+
+        return $options;
+    }
+
+    public function getPlaceholder(): ?string
+    {
+        return $this->placeholder;
+    }
+
+    public function toEmbeddedHtml(): string
+    {
+        $id = $this->getId();
+        $isDisabled = $this->isDisabled();
+
+        $inputWrapperAttributes = (new ComponentAttributeBag())
+            ->class([
+                'fi-input-wrp',
+                'fi-disabled' => $isDisabled,
+            ]);
+
+        $inputAttributes = (new ComponentAttributeBag())
+            ->merge([
+                'disabled' => $isDisabled,
+                'id' => $id,
+                'wire:model.live' => $this->getName(),
+            ], escape: false)
+            ->class([
+                'fi-select-input',
+            ]);
+
+        ob_start(); ?>
+
+        <div class="fi-ac-select-action">
+            <label for="<?= $id ?>" class="sr-only">
+                <?= e($this->getLabel()) ?>
+            </label>
+
+            <div <?= $inputWrapperAttributes->toHtml() ?>>
+                <select <?= $inputAttributes->toHtml() ?>>
+                    <?php if (($placeholder = $this->getPlaceholder()) !== null) { ?>
+                        <option value=""><?= e($placeholder) ?></option>
+                    <?php } ?>
+
+                    <?php foreach ($this->getOptions() as $value => $label) { ?>
+                        <option value="<?= e($value) ?>">
+                            <?= e($label) ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
+
+        <?php return ob_get_clean();
     }
 }
