00484f2fc0203ee31e62fe6651beea7ff820985a
Dan Harrin <git@danharrin.com>
2024-10-24 10:30:36 +0100

    fix weird issue with lazy placeholder crashing livewire if the outer element is a <section>

--- packages/support/resources/views/components/loading-section.blade.php
+++ packages/support/resources/views/components/loading-section.blade.php
@@ -4,13 +4,11 @@
     'height' => null,
 ])
 
-<x-filament::section
-    :attributes="
-        \Filament\Support\prepare_inherited_attributes(
-            ($attributes ?? new \Illuminate\View\ComponentAttributeBag)
-                ->gridColumn($columnSpan, $columnStart)
-                ->class(['fi-loading-section'])
-                ->style(['height: ' . ($height ?? '8rem')])
-        )
-    "
-/>
+<div
+    {{
+        ($attributes ?? new \Illuminate\View\ComponentAttributeBag)
+            ->gridColumn($columnSpan, $columnStart)
+            ->class(['fi-section fi-loading-section'])
+            ->style(['height: ' . ($height ?? '8rem')])
+    }}
+></div>
