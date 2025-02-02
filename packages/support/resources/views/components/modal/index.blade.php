fbc9bfb2195249ed550d5d46e90aad3722a066a0
Dan Harrin <git@danharrin.com>
2025-02-02 10:28:45 +0000

    Refactor

--- packages/support/resources/views/components/modal/index.blade.php
+++ packages/support/resources/views/components/modal/index.blade.php
@@ -56,180 +56,180 @@
     $closeEventHandler = filled($id) ? '$dispatch(' . \Illuminate\Support\Js::from($closeEventName) . ', { id: ' . \Illuminate\Support\Js::from($id) . ' })' : 'close()';
 @endphp
 
-<div
+@if ($trigger)
+    <div
+        x-on:click="$el.nextElementSibling.dispatchEvent(new CustomEvent(@js($openEventName)))"
+        {{ $trigger->attributes->class(['fi-modal-trigger']) }}
+    >
+        {{ $trigger }}
+    </div>
+@endif
+
+<dialog
+    x-data="filamentModal({
+        id: @js($id),
+    })"
+    @if ($id)
+        x-on:{{ $closeEventName }}.window="if (($event.detail.id === @js($id)) && isOpen) close()"
+        x-on:{{ $closeQuietlyEventName }}.window="if (($event.detail.id === @js($id)) && isOpen) closeQuietly()"
+        x-on:{{ $openEventName }}.window="if (($event.detail.id === @js($id)) && (! isOpen)) open()"
+    @else
+        x-on:{{ $closeEventName }}.stop="if (isOpen) close()"
+        x-on:{{ $closeQuietlyEventName }}.stop="if (isOpen) closeQuietly()"
+        x-on:{{ $openEventName }}.stop="if (! isOpen) open()"
+    @endif
+    x-on:cancel.prevent {{-- Prevents the dialog from closing when the escape key is pressed, we do that ourselves to properly transition it. --}}
+    id="{{ $id }}"
+    x-ref="dialog"
+    wire:ignore.self
+    x-trap.noscroll{{ $autofocus ? '' : '.noautofocus' }}="isOpen"
+    x-bind:class="{
+        'fi-modal-open': isOpen,
+    }"
     @if ($ariaLabelledby)
         aria-labelledby="{{ $ariaLabelledby }}"
     @elseif ($heading)
         aria-labelledby="{{ "{$id}.heading" }}"
     @endif
-    aria-modal="true"
-    id="{{ $id }}"
-    role="dialog"
-    x-data="filamentModal({
-                id: @js($id),
-            })"
-    @if ($id)
-        x-on:{{ $closeEventName }}.window="if (($event.detail.id === @js($id)) && isOpen) close()"
-        x-on:{{ $closeQuietlyEventName }}.window="if (($event.detail.id === @js($id)) && isOpen) closeQuietly()"
-        x-on:{{ $openEventName }}.window="if (($event.detail.id === @js($id)) && (! isOpen)) open()"
-    @endif
-    x-bind:class="{
-        'fi-modal-open': isOpen,
-    }"
     @class([
-        'fi-modal',
-        'fi-modal-slide-over' => $slideOver,
-        'fi-width-screen' => $width === Width::Screen,
-    ])
+            'fi-modal',
+            'fi-modal-slide-over' => $slideOver,
+            'fi-width-screen' => $width === Width::Screen,
+        ])
 >
-    @if ($trigger)
-        <div
-            x-on:click="open"
-            {{ $trigger->attributes->class(['fi-modal-trigger']) }}
-        >
-            {{ $trigger }}
-        </div>
-    @endif
+    <div
+        aria-hidden="true"
+        x-show="isModalWindowVisible"
+        x-transition.duration.300ms.opacity
+        class="fi-modal-close-overlay"
+    ></div>
 
-    <dialog
-        x-ref="dialog"
-        wire:ignore.self
-        x-trap.noscroll{{ $autofocus ? '' : '.noautofocus' }}="isOpen"
+    <div
+        @class([
+            'fi-modal-overlay',
+            'fi-clickable' => $closeByClickingAway,
+        ])
     >
         <div
-            aria-hidden="true"
-            x-show="isModalWindowVisible"
-            x-transition.duration.300ms.opacity
-            class="fi-modal-close-overlay"
-        ></div>
-
-        <div
-            @class([
-                'fi-modal-overlay',
-                'fi-clickable' => $closeByClickingAway,
-            ])
+            x-ref="modalContainer"
+            @if ($closeByClickingAway)
+                {{-- Ensure that the click element is not triggered from a user selecting text inside an input. --}}
+                x-on:click.self="
+                    document.activeElement.selectionStart === undefined &&
+                        document.activeElement.selectionEnd === undefined &&
+                        {{ $closeEventHandler }}
+                "
+            @endif
+            {{ $attributes->class(['fi-modal-window-ctn']) }}
         >
             <div
-                x-ref="modalContainer"
-                @if ($closeByClickingAway)
-                    {{-- Ensure that the click element is not triggered from a user selecting text inside an input. --}}
-                    x-on:click.self="
-                        document.activeElement.selectionStart === undefined &&
-                            document.activeElement.selectionEnd === undefined &&
-                            {{ $closeEventHandler }}
-                    "
+                x-show="isModalWindowVisible"
+                x-ref="window"
+                @if ($closeByEscaping)
+                    x-on:keydown.window.escape="{{ $closeEventHandler }}"
                 @endif
-                {{ $attributes->class(['fi-modal-window-ctn']) }}
+                @if ($width !== Width::Screen)
+                    x-transition:enter-start="fi-transition-enter-start"
+                    x-transition:enter-end="fi-transition-enter-end"
+                    x-transition:leave-start="fi-transition-leave-start"
+                    x-transition:leave-end="fi-transition-leave-end"
+                @endif
+                {{
+                    ($extraModalWindowAttributeBag ?? new \Illuminate\View\ComponentAttributeBag)->class([
+                        'fi-modal-window',
+                        'fi-modal-window-has-close-button' => $closeButton,
+                        'fi-modal-window-has-content' => $hasContent,
+                        'fi-modal-window-has-footer' => $hasFooter,
+                        'fi-modal-window-has-icon' => $hasIcon,
+                        'fi-modal-window-has-sticky-header' => $stickyHeader,
+                        'fi-hidden' => ! $visible,
+                        ($alignment instanceof Alignment) ? "fi-align-{$alignment->value}" : null,
+                        ($width instanceof Width) ? "fi-width-{$width->value}" : (is_string($width) ? $width : null),
+                    ])
+                }}
             >
-                <div
-                    x-show="isModalWindowVisible"
-                    x-ref="window"
-                    @if ($closeByEscaping)
-                        x-on:keydown.window.escape="{{ $closeEventHandler }}"
-                    @endif
-                    @if ($width !== Width::Screen)
-                        x-transition:enter-start="fi-transition-enter-start"
-                        x-transition:enter-end="fi-transition-enter-end"
-                        x-transition:leave-start="fi-transition-leave-start"
-                        x-transition:leave-end="fi-transition-leave-end"
-                    @endif
-                    {{
-                        ($extraModalWindowAttributeBag ?? new \Illuminate\View\ComponentAttributeBag)->class([
-                            'fi-modal-window',
-                            'fi-modal-window-has-close-button' => $closeButton,
-                            'fi-modal-window-has-content' => $hasContent,
-                            'fi-modal-window-has-footer' => $hasFooter,
-                            'fi-modal-window-has-icon' => $hasIcon,
-                            'fi-modal-window-has-sticky-header' => $stickyHeader,
-                            'fi-hidden' => ! $visible,
-                            ($alignment instanceof Alignment) ? "fi-align-{$alignment->value}" : null,
-                            ($width instanceof Width) ? "fi-width-{$width->value}" : (is_string($width) ? $width : null),
+                @if ($heading || $header)
+                    <div
+                        @class([
+                            'fi-modal-header',
+                            'fi-sticky' => $stickyHeader,
+                            'fi-vertical-align-center' => $hasIcon && $hasHeading && (! $hasDescription) && in_array($alignment, [Alignment::Start, Alignment::Left]),
                         ])
-                    }}
-                >
-                    @if ($heading || $header)
-                        <div
-                            @class([
-                                'fi-modal-header',
-                                'fi-sticky' => $stickyHeader,
-                                'fi-vertical-align-center' => $hasIcon && $hasHeading && (! $hasDescription) && in_array($alignment, [Alignment::Start, Alignment::Left]),
-                            ])
-                        >
-                            @if ($closeButton)
-                                <x-filament::icon-button
-                                    color="gray"
-                                    :icon="\Filament\Support\Icons\Heroicon::OutlinedXMark"
-                                    icon-alias="modal.close-button"
-                                    icon-size="lg"
-                                    :label="__('filament::components/modal.actions.close.label')"
-                                    tabindex="-1"
-                                    :x-on:click="$closeEventHandler"
-                                    class="fi-modal-close-btn"
-                                />
-                            @endif
+                    >
+                        @if ($closeButton)
+                            <x-filament::icon-button
+                                color="gray"
+                                :icon="\Filament\Support\Icons\Heroicon::OutlinedXMark"
+                                icon-alias="modal.close-button"
+                                icon-size="lg"
+                                :label="__('filament::components/modal.actions.close.label')"
+                                tabindex="-1"
+                                :x-on:click="$closeEventHandler"
+                                class="fi-modal-close-btn"
+                            />
+                        @endif
 
-                            @if ($header)
-                                {{ $header }}
-                            @else
-                                @if ($hasIcon)
-                                    <div class="fi-modal-icon-wrp-ctn">
-                                        <div
-                                            @class([
-                                                'fi-modal-icon-wrp',
-                                                ...\Filament\Support\get_component_color_classes(Icon::class, $iconColor),
-                                            ])
-                                        >
-                                            {{ \Filament\Support\generate_icon_html($icon, $iconAlias, size: \Filament\Support\Enums\IconSize::Large) }}
-                                        </div>
+                        @if ($header)
+                            {{ $header }}
+                        @else
+                            @if ($hasIcon)
+                                <div class="fi-modal-icon-wrp-ctn">
+                                    <div
+                                        @class([
+                                            'fi-modal-icon-wrp',
+                                            ...\Filament\Support\get_component_color_classes(Icon::class, $iconColor),
+                                        ])
+                                    >
+                                        {{ \Filament\Support\generate_icon_html($icon, $iconAlias, size: \Filament\Support\Enums\IconSize::Large) }}
                                     </div>
+                                </div>
+                            @endif
+
+                            <div>
+                                <h2 class="fi-modal-heading">
+                                    {{ $heading }}
+                                </h2>
+
+                                @if ($hasDescription)
+                                    <p class="fi-modal-description">
+                                        {{ $description }}
+                                    </p>
                                 @endif
+                            </div>
+                        @endif
+                    </div>
+                @endif
 
-                                <div>
-                                    <h2 class="fi-modal-heading">
-                                        {{ $heading }}
-                                    </h2>
+                @if ($hasContent)
+                    <div class="fi-modal-content">
+                        {{ $slot }}
+                    </div>
+                @endif
 
-                                    @if ($hasDescription)
-                                        <p class="fi-modal-description">
-                                            {{ $description }}
-                                        </p>
-                                    @endif
-                                </div>
-                            @endif
-                        </div>
-                    @endif
-
-                    @if ($hasContent)
-                        <div class="fi-modal-content">
-                            {{ $slot }}
-                        </div>
-                    @endif
-
-                    @if ($hasFooter)
-                        <div
-                            @class([
-                                'fi-modal-footer',
-                                'fi-sticky' => $stickyFooter,
-                                ($footerActionsAlignment instanceof Alignment) ? "fi-align-{$footerActionsAlignment->value}" : null,
-                            ])
-                        >
-                            @if (! \Filament\Support\is_slot_empty($footer))
-                                {{ $footer }}
-                            @else
-                                <div class="fi-modal-footer-actions">
-                                    @if (is_array($footerActions))
-                                        @foreach ($footerActions as $action)
-                                            {{ $action }}
-                                        @endforeach
-                                    @else
-                                        {{ $footerActions }}
-                                    @endif
-                                </div>
-                            @endif
-                        </div>
-                    @endif
-                </div>
+                @if ($hasFooter)
+                    <div
+                        @class([
+                            'fi-modal-footer',
+                            'fi-sticky' => $stickyFooter,
+                            ($footerActionsAlignment instanceof Alignment) ? "fi-align-{$footerActionsAlignment->value}" : null,
+                        ])
+                    >
+                        @if (! \Filament\Support\is_slot_empty($footer))
+                            {{ $footer }}
+                        @else
+                            <div class="fi-modal-footer-actions">
+                                @if (is_array($footerActions))
+                                    @foreach ($footerActions as $action)
+                                        {{ $action }}
+                                    @endforeach
+                                @else
+                                    {{ $footerActions }}
+                                @endif
+                            </div>
+                        @endif
+                    </div>
+                @endif
             </div>
         </div>
-    </dialog>
-</div>
+    </div>
+</dialog>
