c326fa663e9213557f297d12beea010726aeb348
Saif Kamal <kamal.saifkamal534@gmail.com>
2025-03-20 06:09:42 -0700

    Resolve merge conflict for WidgetCommandLivewireTest.php

---
+++ tests/src/Commands/WidgetCommandLivewireTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Filament\Tests\Commands;
+
+use Filament\Tests\TestCase;
+use Illuminate\Support\Facades\File;
+
+use function Pest\Laravel\artisan;
+
+uses(TestCase::class);
+
+test('can create a table widget in livewire directory', function () {
+    $widgetPath = $this->app->basePath('app/Livewire/TestWidget.php');
+    
+    if (file_exists($widgetPath)) {
+        unlink($widgetPath);
+    }
+
+    expect(file_exists($widgetPath))->toBeFalse();
+
+    artisan('make:filament-widget', [
+        'name' => 'TestWidget',
+        '--force' => true,
+    ])
+        ->expectsQuestion('What type of widget do you want to create?', 'Table')
+        ->expectsQuestion('What is the resource you would like to create this in?', '')
+        ->expectsQuestion('Where would you like to create this?', 'App\\Livewire alongside other Livewire components')
+        ->assertSuccessful();
+
+    expect(file_exists($widgetPath))->toBeTrue();
+});
