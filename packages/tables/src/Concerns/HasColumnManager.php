53fea733163409758022f31c79fa92db66be0f94
Marcus <marcus.venghaus@inklammern.de>
2025-06-18 07:15:20 +0200

    fix(table-columns): sort table columns when a new column is added

--- packages/tables/src/Concerns/HasColumnManager.php
+++ packages/tables/src/Concerns/HasColumnManager.php
@@ -6,6 +6,7 @@ use Filament\Schemas\Schema;
 use Filament\Support\Components\Component;
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\ColumnGroup;
+use Illuminate\Support\Collection;
 
 /**
  * @property-read Schema $toggleTableColumnForm
@@ -214,12 +215,18 @@ trait HasColumnManager
     protected function syncTableColumnManagerWithDefaultState(): void
     {
         $defaultState = $this->getDefaultTableColumnManagerState();
+        $newTableColumnManagerItems = $this->getNewTableColumnManagerItems($defaultState);
 
         $this->tableColumns = collect($this->tableColumns)
             ->map(fn (array $item) => $this->syncTableColumnManagerItemWithDefaultState($item, $defaultState))
             ->filter()
             ->values()
-            ->merge($this->getNewTableColumnManagerItems($defaultState))
+            ->when(
+                count($newTableColumnManagerItems),
+                fn (Collection $items) => $items
+                    ->merge($newTableColumnManagerItems)
+                    ->sortBy(fn (array $item): int => (int) array_search($item['name'], array_column($defaultState, 'name')))
+            )
             ->all();
     }
 
