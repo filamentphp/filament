8174f146b76c6fa8534988290c9c0b5a66aee45d
bambamboole <manuel.christlieb@xentral.com>
2025-06-18 19:08:21 +0200

    Cropperjs css is built into the web components

--- packages/forms/resources/js/index.js
+++ packages/forms/resources/js/index.js
@@ -1,5 +1,4 @@
 import 'easymde/dist/easymde.min.css'
-import 'cropperjs/dist/cropper.min.css'
 import 'filepond/dist/filepond.min.css'
 import 'filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css'
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
