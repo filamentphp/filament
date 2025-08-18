269c1592984b42d4e5e67dc2b7a3323c2dc74c45
molbal <molbal@outlook.com>
2025-08-18 22:35:45 +0200

    Add array parameter type option for allow setting custom data via $table->records()

--- packages/panels/src/Resources/Pages/ListRecords.php
+++ packages/panels/src/Resources/Pages/ListRecords.php
@@ -126,7 +126,7 @@ class ListRecords extends Page implements Tables\Contracts\HasTable
             ->modifyQueryUsing($this->modifyQueryWithActiveTab(...))
             ->when($this->getModelLabel(), fn (Table $table, string $modelLabel): Table => $table->modelLabel($modelLabel))
             ->when($this->getPluralModelLabel(), fn (Table $table, string $pluralModelLabel): Table => $table->pluralModelLabel($pluralModelLabel))
-            ->recordAction(function (Model $record, Table $table): ?string {
+            ->recordAction(function (Model|array $record, Table $table): ?string {
                 foreach (['view', 'edit'] as $action) {
                     $action = $table->getAction($action);
 
@@ -150,7 +150,7 @@ class ListRecords extends Page implements Tables\Contracts\HasTable
 
                 return null;
             })
-            ->recordUrl(function (Model $record, Table $table): ?string {
+            ->recordUrl(function (Model|array $record, Table $table): ?string {
                 foreach (['view', 'edit'] as $action) {
                     $action = $table->getAction($action);
 
