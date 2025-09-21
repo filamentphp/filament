a88ab4ba9d47ecf4c8b20ffbb03f78deaa1bbbd7
Dan Harrin <git@danharrin.com>
2025-09-21 15:36:43 +0100

    refactor

--- packages/forms/resources/js/components/rich-editor/extension-text-color.js
+++ packages/forms/resources/js/components/rich-editor/extension-text-color.js
@@ -19,26 +19,23 @@ export default Mark.create({
     },
 
     renderHTML({ HTMLAttributes }) {
+        const attrs = { ...HTMLAttributes }
+        const existingClass = HTMLAttributes.class
+        attrs.class = ['color', existingClass].filter(Boolean).join(' ')
+
         const colorName = HTMLAttributes['data-color']
-        const config = this.options.textColors?.[colorName]
+        const colors = this.options.textColors || {}
+        const config = colors[colorName]
 
-        // Build CSS variables string from provided textColors map
-        let variables = ''
         if (config && (config.color || config.darkColor)) {
-            const cssParts = []
-            if (config.color) cssParts.push(`--color: ${config.color}`)
-            if (config.darkColor) cssParts.push(`--dark-color: ${config.darkColor}`)
-            variables = cssParts.join('; ')
-        }
+            const parts = []
+            if (config.color) parts.push(`--color: ${config.color}`)
+            if (config.darkColor) parts.push(`--dark-color: ${config.darkColor}`)
+            const cssVars = parts.join('; ')
 
-        // Merge with any existing style (string). If object is provided, ignore to keep minimal complexity
-        let style = HTMLAttributes.style || ''
-        if (variables) {
-            style = [variables, style].filter(Boolean).join('; ')
+            const existingStyle = typeof HTMLAttributes.style === 'string' ? HTMLAttributes.style : ''
+            attrs.style = existingStyle ? `${cssVars}; ${existingStyle}` : cssVars
         }
-
-        const attrs = { ...HTMLAttributes, class: ['color', HTMLAttributes.class].filter(Boolean).join(' ') }
-        if (style) attrs.style = style
 
         return ['span', attrs, 0]
     },
