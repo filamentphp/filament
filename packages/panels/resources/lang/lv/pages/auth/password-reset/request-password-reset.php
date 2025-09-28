efc5c5f9730f30e589a56834274d0e20fde0173d
Janis Eglitis <janiseglitis@users.noreply.github.com>
2025-09-28 17:09:40 +0300

    Update request-password-reset.php
    Signed-off-by: Janis Eglitis <janiseglitis@users.noreply.github.com>

--- packages/panels/resources/lang/lv/pages/auth/password-reset/request-password-reset.php
+++ packages/panels/resources/lang/lv/pages/auth/password-reset/request-password-reset.php
@@ -32,6 +32,10 @@ return [
 
     'notifications' => [
 
+         'sent' => [
+            'body' => 'Jūs saņemsiet e-pastu, ja Jūsu konts eksistē.',
+        ],
+
         'throttled' => [
             'title' => 'Pārāk daudz mēģinājumu',
             'body' => 'Lūdzu, mēģiniet vēlreiz pēc :seconds sekundēm.',
