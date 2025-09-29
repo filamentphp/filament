2828ca18e916bcd64961868701b4a7e1f4bb5b2f
Raj Siva-Rajah <raj@zapzap.cloud>
2025-09-29 11:00:58 +0000

    Fix SPA mode key bindings in Blade views

--- packages/support/resources/views/components/link.blade.php
+++ packages/support/resources/views/components/link.blade.php
@@ -76,6 +76,12 @@
     @endif
     @if ($keyBindings)
         x-bind:id="$id('key-bindings')"
+        x-init="
+            const bindings = @js($keyBindings);
+            document.addEventListener('livewire:navigating', () => {
+                bindings.forEach(b => Mousetrap.unbind(b));
+            }, { once: true });
+        "
         x-mousetrap.global.{{ collect($keyBindings)->map(fn (string $keyBinding): string => str_replace('+', '-', $keyBinding))->implode('.') }}="document.getElementById($el.id).click()"
     @endif
     @if ($hasTooltip)
