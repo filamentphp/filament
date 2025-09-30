c232f80b25910b8c714f7a2789d0c41404ff9cd8
Ben <howdu@users.noreply.github.com>
2025-09-30 13:55:00 +0100

    Fix spa cmd click to open in new window

--- packages/support/src/helpers.php
+++ packages/support/src/helpers.php
@@ -135,7 +135,7 @@ if (! function_exists('Filament\Support\generate_href_html')) {
             if (FilamentView::hasSpaPrefetching()) {
                 $html .= ' wire:navigate.hover';
             } elseif ($hasNestedClickEventHandler) {
-                $html .= ' x-on:click.prevent="Alpine.navigate(' . "'{$url}'" . ')"';
+                $html .= ' x-on:click="if (!(($event.which === 1) && !$event.altKey && !$event.ctrlKey && !$event.metaKey && !$event.shiftKey)) return; $event.preventDefault(); Alpine.navigate(' . "'{$url}'" . ')"';
             } else {
                 $html .= ' wire:navigate';
             }
