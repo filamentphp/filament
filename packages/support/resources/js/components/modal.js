d031855e51db97febd43f25c65880ecdd85b4d8d
klemen <klemen@MacBook-Pro-klemen.local>
2025-10-01 14:05:51 +0200

    fix modal behaviour when closing it

--- packages/support/resources/js/components/modal.js
+++ packages/support/resources/js/components/modal.js
@@ -9,8 +9,48 @@ export default ({ id }) => ({
         this.$nextTick(() => {
             this.isWindowVisible = this.isOpen
 
+            setupModalClickGuard({
+                modalSelector: '.fi-modal-window',
+                backdropSelector: '.fi-modal-close-overlay',
+                pageSelector: '.fi-page',
+            })
+
             this.$watch('isOpen', () => (this.isWindowVisible = this.isOpen))
         })
+
+        function setupModalClickGuard({ modalSelector, backdropSelector, pageSelector, clickThreshold = 75 }) {
+            let mouseDownOnModal = false
+            let mouseDownTime = 0
+
+            const preventClick = (event) => {
+                event.stopPropagation()
+                event.preventDefault()
+                document.removeEventListener('click', preventClick, true)
+            }
+
+            const isBackdropClick = (e) => {
+                return !e.target.closest(modalSelector) &&
+                    (e.target.closest(backdropSelector) || e.target.closest(pageSelector))
+            }
+
+            document.addEventListener('mousedown', (e) => {
+                mouseDownTime = Date.now()
+                mouseDownOnModal = !!e.target.closest(modalSelector)
+            }, true)
+
+            document.addEventListener('mouseup', (e) => {
+                console.log('hello')
+                const isClick = Date.now() - mouseDownTime < clickThreshold
+
+                if (mouseDownOnModal && isBackdropClick(e) && !isClick) {
+                    document.addEventListener('click', preventClick, true)
+                } else {
+                    document.removeEventListener('click', preventClick, true)
+                }
+
+                mouseDownOnModal = false
+            }, true)
+        }
     },
 
     close() {
