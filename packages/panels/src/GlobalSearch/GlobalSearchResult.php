7d93996d22dee9c06e8c133543bf621c2a2bc8ae
John Edmerson Pizarra <edmer@rxtraclinical.com>
2025-10-02 14:09:12 +0800

    chore: run composer cs

--- packages/panels/src/GlobalSearch/GlobalSearchResult.php
+++ packages/panels/src/GlobalSearch/GlobalSearchResult.php
@@ -12,9 +12,9 @@ class GlobalSearchResult
      * @param  array<Action>  $actions
      */
     public function __construct(
-        readonly public string | Htmlable $title,
-        readonly public string $url,
-        readonly public array $details = [],
-        readonly public array $actions = [],
+        public readonly string | Htmlable $title,
+        public readonly string $url,
+        public readonly array $details = [],
+        public readonly array $actions = [],
     ) {}
 }
