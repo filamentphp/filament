5bba52a38c37c5aa451905bcda45e48f40b0b8fb
Dan Harrin <git@danharrin.com>
2025-10-04 13:28:42 +0100

    remove old code

--- packages/support/resources/js/utilities/select.js
+++ packages/support/resources/js/utilities/select.js
@@ -1097,46 +1097,6 @@ export class Select {
 
     // Handle keyboard events for the select button
     handleSelectButtonKeydown(event) {
-        // If the select is disabled, don't handle keyboard events
-        if (this.isDisabled) {
-            return
-        }
-
-        // Handle printable characters: when searchable, open dropdown and use search input
-        if (
-            !this.isOpen &&
-            event.key.length === 1 &&
-            !event.ctrlKey &&
-            !event.altKey &&
-            !event.metaKey
-        ) {
-            event.preventDefault()
-
-            if (this.isSearchable) {
-                // Open the dropdown and funnel the typed character into the search input
-                const char = event.key
-                Promise.resolve(this.openDropdown()).then(() => {
-                    if (!this.searchInput) return
-
-                    // Set the typed character as the initial search value
-                    this.searchInput.value = char
-                    this.searchQuery = char.toLowerCase()
-
-                    // Trigger the existing search logic
-                    this.handleSearch({ target: this.searchInput })
-
-                    // Focus the search input for continued typing
-                    this.searchInput.focus()
-
-                    try {
-                        const end = this.searchInput.value.length
-                        this.searchInput.setSelectionRange(end, end)
-                    } catch (_) {}
-                })
-                return
-            }
-        }
-
         switch (event.key) {
             case 'ArrowDown':
                 event.preventDefault()
@@ -1215,30 +1175,6 @@ export class Select {
 
     // Handle keyboard events within the dropdown
     handleDropdownKeydown(event) {
-        // If it's a printable character and the select is searchable, redirect typing to the search input
-        if (
-            this.isSearchable &&
-            event.key.length === 1 &&
-            !event.ctrlKey &&
-            !event.altKey &&
-            !event.metaKey
-        ) {
-            event.preventDefault()
-            if (this.searchInput) {
-                if (document.activeElement !== this.searchInput) {
-                    this.searchInput.focus()
-                }
-                const newVal = this.searchInput.value + event.key
-                this.searchInput.value = newVal
-                this.searchQuery = newVal.trim().toLowerCase()
-                this.handleSearch({ target: this.searchInput })
-                try {
-                    const end = this.searchInput.value.length
-                    this.searchInput.setSelectionRange(end, end)
-                } catch (_) {}
-            }
-            return
-        }
         switch (event.key) {
             case 'ArrowDown':
                 event.preventDefault()
