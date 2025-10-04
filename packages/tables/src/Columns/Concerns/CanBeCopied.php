6b8950156655f94811c565836117ad1b36f82050
Dan Harrin <git@danharrin.com>
2025-10-04 16:38:21 +0100

    Update CanBeCopied.php

--- packages/tables/src/Columns/Concerns/CanBeCopied.php
+++ packages/tables/src/Columns/Concerns/CanBeCopied.php
@@ -7,7 +7,7 @@ use Filament\Support\Concerns\CanBeCopied as BaseTrait;
 /**
  * @deprecated Use `Filament\Support\Concerns\CanBeCopied` instead.
  */
-trait CanBeCopied
+trait CanBeCopied /** @phpstan-ignore trait.unused */
 {
     use BaseTrait;
 }
