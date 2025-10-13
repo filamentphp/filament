63a9d959df1640027eeff1e8380cc8b6a265322c
Dennis Koch <info@pixelarbeit.de>
2025-10-13 12:31:06 +0200

    Mounted action modal test helpers

--- packages/actions/src/Testing/TestsActions.php
+++ packages/actions/src/Testing/TestsActions.php
@@ -515,6 +515,88 @@ class TestsActions
         };
     }
 
+    /**
+     * Helper method to get the modal content from partials.
+     * @internal
+     */
+    public function getMountedActionModalHtml(): Closure
+    {
+        return function (): string {
+            $partialName = 'action-modals';
+            $partials = data_get($this->lastState->getEffects(), 'partials', []);
+
+            if (! array_key_exists($partialName, $partials)) {
+                Assert::fail('No mounted action modal data found inside partials.');
+            }
+
+            return $partials[$partialName];
+        };
+    }
+
+    public function assertMountedActionModalSee(): Closure
+    {
+        return function ($values, $escape = true) {
+            $html = $this->getMountedActionModalHtml();
+
+            foreach (Arr::wrap($values) as $value) {
+                Assert::assertStringContainsString(
+                    $escape ? e($value) : $value,
+                    $html
+                );
+            }
+
+            return $this;
+        };
+    }
+
+    public function assertMountedActionModalDontSee(): Closure
+    {
+        return function ($values, $escape = true) {
+            $html = $this->getMountedActionModalHtml();
+
+            foreach (Arr::wrap($values) as $value) {
+                Assert::assertStringNotContainsString(
+                    $escape ? e($value) : $value,
+                    $html
+                );
+            }
+
+            return $this;
+        };
+    }
+
+    public function assertMountedActionModalSeeHtml(): Closure
+    {
+        return function ($values) {
+            $html = $this->getMountedActionModalHtml();
+
+            foreach (Arr::wrap($values) as $value) {
+                Assert::assertStringContainsString(
+                    $value,
+                    $html
+                );
+            }
+
+            return $this;
+        };
+    }
+
+    public function assertMountedActionModalDontSeeHtml(): Closure
+    {
+        return function ($values) {
+            $html = $this->getMountedActionModalHtml();
+
+            foreach (Arr::wrap($values) as $value) {
+                Assert::assertStringNotContainsString(
+                    $value,
+                    $html
+                );
+            }
+
+            return $this;
+        };
+    }
+
     public function assertActionHalted(): Closure
     {
         return $this->assertActionMounted();
