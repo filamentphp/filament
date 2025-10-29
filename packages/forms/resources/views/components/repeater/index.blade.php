ea00586467220ed87276d70651c18ed66eb713b5
TheSkybreaker <simoni.itaer@gmail.com>
2025-10-29 17:07:55 +0100

    Fix Repeater persist collapsed

--- packages/forms/resources/views/components/repeater/index.blade.php
+++ packages/forms/resources/views/components/repeater/index.blade.php
@@ -111,7 +111,7 @@
                         wire:ignore.self
                         wire:key="{{ $item->getLivewireKey() }}.item"
                         x-data="{
-                            isCollapsed: @js($isCollapsed($item)),
+                            isCollapsed: @if ($persistCollapsed) $persist(@js($isCollapsed($item))).as(`repeater-${@js($key)}-${@js($itemKey)}-isCollapsed`) @else @js($isCollapsed($item)) @endif,
                         }"
                         x-on:repeater-expand.window="$event.detail === '{{ $statePath }}' && (isCollapsed = false)"
                         x-on:repeater-collapse.window="$event.detail === '{{ $statePath }}' && (isCollapsed = true)"
