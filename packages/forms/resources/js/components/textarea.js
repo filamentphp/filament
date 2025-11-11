d2c82e5b18a27d026e47ccbefec850cc9361628e
Mark Hartman <72379653+Saracaen@users.noreply.github.com>
2025-11-11 22:35:18 +0100

    Prevent setting the initial height on every resize
    This causes complete relayout + scroll anchoring issues in Firefox when the Textarea is in a flex container and another flex container is above it with scrollable content. The scroll anchoring will break and it will scroll up by the same amount the textarea grew whenever you are at the bottom.
    
    Signed-off-by: Mark Hartman <72379653+Saracaen@users.noreply.github.com>

--- packages/forms/resources/js/components/textarea.js
+++ packages/forms/resources/js/components/textarea.js
@@ -31,8 +31,6 @@ export default function textareaFormComponent({
         },
 
         resize() {
-            this.setInitialHeight()
-
             if (this.$el.scrollHeight <= 0) {
                 return
             }
