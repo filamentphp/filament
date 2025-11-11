5c39a098bb73bb2a3f03821e53a145fa2cb523ba
DirtyRacer1337 <DirtyRacer@ya.ru>
2025-11-12 05:02:15 +0700

    fix group sort

--- packages/tables/src/Grouping/Group.php
+++ packages/tables/src/Grouping/Group.php
@@ -322,7 +322,7 @@ class Group extends Component
         }
 
         if ($relationshipName = $this->getRelationshipName()) {
-            return $query->orderByPowerJoins("{$relationshipName}.{$this->getRelationshipAttribute()}", $direction, joinType: 'leftJoin'); /** @phpstan-ignore method.notFound */
+            return $query->orderByPowerJoins("{$relationshipName}.{$this->getRelationshipAttribute()}", $direction, joinType: 'leftJoin', aliases: 'group'); /** @phpstan-ignore method.notFound */
         }
 
         return $query->orderBy($this->getRelationshipAttribute(), $direction);
