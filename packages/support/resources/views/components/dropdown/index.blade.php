91172bc1dea0c7a6cb223d9eabf7dbbed09db650
Justin Obara <obara.justin@gmail.com>
2025-11-12 09:11:18 -0500

    fix: user menu drop down cannot be expanded with the keyboard

--- packages/support/resources/views/components/dropdown/index.blade.php
+++ packages/support/resources/views/components/dropdown/index.blade.php
@@ -32,7 +32,7 @@
     {{ $attributes->class(['fi-dropdown']) }}
 >
     <div
-        x-on:mousedown="if ($event.button === 0) toggle($event)"
+        x-on:click="toggle($event)"
         {{ $trigger->attributes->class(['fi-dropdown-trigger']) }}
     >
         {{ $trigger }}
