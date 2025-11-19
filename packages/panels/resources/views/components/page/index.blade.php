df5cd729123a9667dd25cb2953a8ea986f438863
Stefan Zweifel <stefan@stefanzweifel.dev>
2025-11-19 07:42:24 +0100

    Use unsaved-changes-alert-setup component in page.index

--- packages/panels/resources/views/components/page/index.blade.php
+++ packages/panels/resources/views/components/page/index.blade.php
@@ -147,25 +147,7 @@
 
     {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_END, scopes: $this->getRenderHookScopes()) }}
 
-    @if (method_exists($this, 'hasUnsavedDataChangesAlert') && $this->hasUnsavedDataChangesAlert())
-        @if (\Filament\Support\Facades\FilamentView::hasSpaMode())
-            @script
-                <script>
-                    setUpSpaModeUnsavedDataChangesAlert({
-                        body: @js(__('filament-panels::unsaved-changes-alert.body')),
-                        resolveLivewireComponentUsing: () => @this,
-                        $wire,
-                    })
-                </script>
-            @endscript
-        @else
-            @script
-                <script>
-                    setUpUnsavedDataChangesAlert({ $wire })
-                </script>
-            @endscript
-        @endif
-    @endif
+    <x-filament-panels::unsaved-changes-alert-setup />
 
     @if ((! app()->hasDebugModeEnabled()) && $this->hasErrorNotifications())
         @script
