58de16ef79014916429854675d679b44a1500dd5
Dan Harrin <git@danharrin.com>
2025-12-04 10:03:38 +0000

    Revert "chore: fix code style"
    This reverts commit 3acc6fe781328b8a6f02e1d76e4361c9c92a365b.

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -1615,33 +1615,10 @@
 
                                     @if (count($records))
                                         @php
-                                                $recordAction = $getRecordAction($record);
-                                                $recordKey = $getRecordKey($record);
-                                                $recordUrl = $getRecordUrl($record);
-                                                $openRecordUrlInNewTab = $shouldOpenRecordUrlInNewTab($record);
-                                                $recordGroupKey = $group?->getStringKey($record);
-                                                $recordGroupTitle = $group?->getTitle($record);
-
-                                                $recordActions = array_reduce(
-                                                    $defaultRecordActions,
-                                                    function (array $carry, $action) use ($record): array {
-                                                        $action = $action->getClone();
-
-                                                        if (! $action instanceof \Filament\Actions\BulkAction) {
-                                                            $action->record($record);
-                                                        }
-
-                                                        if ($action->isHidden()) {
-                                                            return $carry;
-                                                        }
-
-                                                        $carry[] = $action;
-
-                                                        return $carry;
-                                                    },
-                                                    initial: [],
-                                                );
-                                            <BHTd2qEUTbdfDDMB></BHTd2qEUTbdfDDMB>
+                                            $isRecordRowStriped = false;
+                                            $previousRecord = null;
+                                            $previousRecordGroupKey = null;
+                                            $previousRecordGroupTitle = null;
                                         @endphp
 
                                         @foreach ($records as $record)
