3234794cca0e254effff48e6426022aba6956440
Dan Harrin <git@danharrin.com>
2025-12-29 06:46:41 +0000

    fix: Text column gray color

--- packages/infolists/src/View/Components/TextEntryComponent/ItemComponent.php
+++ packages/infolists/src/View/Components/TextEntryComponent/ItemComponent.php
@@ -5,9 +5,8 @@ namespace Filament\Infolists\View\Components\TextEntryComponent;
 use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentColor;
 use Filament\Support\View\Components\Contracts\HasColor;
-use Filament\Support\View\Components\Contracts\HasDefaultGrayColor;
 
-class ItemComponent implements HasColor, HasDefaultGrayColor
+class ItemComponent implements HasColor
 {
     /**
      * @param  array<int, string>  $color
