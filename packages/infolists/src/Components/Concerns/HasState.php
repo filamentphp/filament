6f2457d498c039b42069e0296c29685e6ef1d28a
Dan Harrin <git@danharrin.com>
2026-01-01 16:19:00 +0000

    Update HasState.php

--- packages/infolists/src/Components/Concerns/HasState.php
+++ packages/infolists/src/Components/Concerns/HasState.php
@@ -69,7 +69,7 @@ trait HasState
 
             $state = $containerState instanceof Model ?
                 $this->getStateFromRecord($containerState) :
-                data_get($containerState, $this->getStatePath(false));
+                data_get($containerState, $this->getStatePath(isAbsolute: false));
         }
 
         if (is_string($state) && ($separator = $this->getSeparator())) {
