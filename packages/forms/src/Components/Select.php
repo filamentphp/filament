737895ea3f67aec17da210d702aa896f4a2df3e9
Dan Harrin <git@danharrin.com>
2026-01-01 17:04:57 +0000

    fix: `MorphToSelect` create form with relationship

--- packages/forms/src/Components/Select.php
+++ packages/forms/src/Components/Select.php
@@ -231,9 +231,7 @@ class Select extends Field implements Contracts\CanDisableOptions, Contracts\Has
         $action = Action::make($this->getCreateOptionActionName())
             ->label(__('filament-forms::components.select.actions.create_option.label'))
             ->schema(static function (Select $component, Schema $schema): array | Schema | null {
-                return $component->getCreateOptionActionForm($schema->model(
-                    $component->getRelationship() ? $component->getRelationship()->getModel()::class : null,
-                ));
+                return $component->getCreateOptionActionForm($schema);
             })
             ->action(static function (Action $action, array $arguments, Select $component, array $data, Schema $schema): void {
                 if (! $component->getCreateOptionUsing()) {
