10e70e7f737366e7729ba00c72ba1a9f59fc1a72
webard <b.gajda@webard.me>
2026-01-04 22:40:09 +0000

    fix

--- packages/panels/src/helpers.php
+++ packages/panels/src/helpers.php
@@ -52,7 +52,7 @@ if (! function_exists('Filament\get_authorization_response')) {
 
             throw new LogicException(blank($policyClass)
                 ? "Strict authorization mode is enabled, but no policy was found for [{$model}]."
-                : 'Strict authorization mode is enabled, but no [' . enum_value($action) . "] method was found on [{$policyClass}].");
+                : 'Strict authorization mode is enabled, but no [' . enum_value($action) . "()] method was found on [{$policyClass}].");
         }
 
         /** @var bool | Response | null $response */
