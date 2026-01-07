7ec7e01ad5fe1279379b9c474149d211a6723be2
Chris Page <sirhc.page@gmail.com>
2026-01-07 09:44:46 +0000

    Added robots meta tag to prevent panel pages being indexed

--- packages/panels/resources/views/components/layout/base.blade.php
+++ packages/panels/resources/views/components/layout/base.blade.php
@@ -21,6 +21,7 @@
         <meta charset="utf-8" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="noindex,nofollow" />
 
         @if ($favicon = filament()->getFavicon())
             <link rel="icon" href="{{ $favicon }}" />
