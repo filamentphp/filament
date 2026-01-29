3b73ecd5f60c059d2de244b5a67ac7349936eba3
Dan Harrin <git@danharrin.com>
2026-01-29 13:26:50 +0000

    Update QueryBuilder.php

--- packages/tables/src/Filters/QueryBuilder.php
+++ packages/tables/src/Filters/QueryBuilder.php
@@ -69,7 +69,7 @@ class QueryBuilder extends BaseFilter
             $ruleBuilderBlockContainer = $ruleBuilder->getChildSchema($ruleIndex);
 
             if (! $ruleBuilderBlockContainer) {
-                throw new LogicException('No query builder block found for [' . $rule['type'] ?? $ruleIndex . '].');
+                throw new LogicException('No query builder block found for [' . ($rule['type'] ?? $ruleIndex) . '].');
             }
 
             if ($rule['type'] === RuleBuilder::OR_BLOCK_NAME) {
@@ -137,7 +137,7 @@ class QueryBuilder extends BaseFilter
             $ruleBuilderBlockContainer = $ruleBuilder->getChildSchema($ruleIndex);
 
             if (! $ruleBuilderBlockContainer) {
-                throw new LogicException('No query builder block found for [' . $rule['type'] ?? $ruleIndex . '].');
+                throw new LogicException('No query builder block found for [' . ($rule['type'] ?? $ruleIndex) . '].');
             }
 
             if ($rule['type'] === RuleBuilder::OR_BLOCK_NAME) {
@@ -176,7 +176,7 @@ class QueryBuilder extends BaseFilter
             $ruleBuilderBlockContainer = $ruleBuilder->getChildSchema($ruleIndex);
 
             if (! $ruleBuilderBlockContainer) {
-                throw new LogicException('No query builder block found for [' . $rule['type'] ?? $ruleIndex . '].');
+                throw new LogicException('No query builder block found for [' . ($rule['type'] ?? $ruleIndex) . '].');
             }
 
             if ($rule['type'] === RuleBuilder::OR_BLOCK_NAME) {
@@ -222,7 +222,7 @@ class QueryBuilder extends BaseFilter
             $ruleBuilderBlockContainer = $ruleBuilder->getChildSchema($ruleIndex);
 
             if (! $ruleBuilderBlockContainer) {
-                throw new LogicException('No query builder block found for [' . $rule['type'] ?? $ruleIndex . '].');
+                throw new LogicException('No query builder block found for [' . ($rule['type'] ?? $ruleIndex) . '].');
             }
 
             if ($rule['type'] === RuleBuilder::OR_BLOCK_NAME) {
