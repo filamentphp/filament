dcd7414c6a7f1e1678a19f13332802dcd51b5616
Dan Harrin <git@danharrin.com>
2026-01-30 09:31:22 +0000

    tweaks

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -1280,6 +1280,13 @@
                             </table>
                         @endif
                     @elseif ((! ($content || $hasColumnsLayout)) && ($records !== null))
+                        @php
+                            $sortableColumns = $isStackedOnMobile ? array_filter(
+                                $columns,
+                                fn (\Filament\Tables\Columns\Column $column): bool => $column->isSortable(),
+                            ) : [];
+                        @endphp
+
                         <table
                             @class([
                                 'fi-ta-table',
@@ -1287,6 +1294,143 @@
                             ])
                         >
                             <thead>
+                                @if ($isStackedOnMobile && count($sortableColumns) && (! $isReordering))
+                                    <tr class="fi-ta-stacked-header-row">
+                                        <th
+                                            colspan="100%"
+                                            class="fi-ta-stacked-header-cell"
+                                        >
+                                            @if ($isSelectionEnabled && ($maxSelectableRecords !== 1) && (! $selectsGroupsOnly))
+                                                <input
+                                                    aria-label="{{ __('filament-tables::table.fields.bulk_select_page.label') }}"
+                                                    type="checkbox"
+                                                    @if ($isSelectionDisabled)
+                                                        disabled
+                                                    @elseif ($maxSelectableRecords)
+                                                        x-bind:disabled="
+                                                            const recordsOnPage = getRecordsOnPage()
+
+                                                            return recordsOnPage.length && ! areRecordsToggleable(recordsOnPage)
+                                                        "
+                                                    @endif
+                                                    x-bind:checked="
+                                                        const recordsOnPage = getRecordsOnPage()
+
+                                                        if (recordsOnPage.length && areRecordsSelected(recordsOnPage)) {
+                                                            $el.checked = true
+
+                                                            return 'checked'
+                                                        }
+
+                                                        $el.checked = false
+
+                                                        return null
+                                                    "
+                                                    x-on:click="toggleSelectRecordsOnPage"
+                                                    {{-- Make sure the "checked" state gets re-evaluated after a Livewire request: --}}
+                                                    wire:key="{{ $this->getId() }}.table.bulk-select-page.checkbox.stacked.{{ \Illuminate\Support\Str::random() }}"
+                                                    wire:loading.attr="disabled"
+                                                    wire:target="{{ implode(',', \Filament\Tables\Table::LOADING_TARGETS) }}"
+                                                    class="fi-ta-page-checkbox fi-checkbox-input"
+                                                />
+                                            @endif
+
+                                            <div
+                                                x-data="{
+                                                    sort: $wire.$entangle('tableSort', true),
+                                                    column: null,
+                                                    direction: null,
+                                                }"
+                                                x-init="
+                                                    if (sort) {
+                                                        ;[column, direction] = sort.split(':')
+                                                        direction ??= 'asc'
+                                                    }
+
+                                                    $watch('sort', function () {
+                                                        if (! sort) {
+                                                            return
+                                                        }
+
+                                                        ;[column, direction] = sort.split(':')
+                                                        direction ??= 'asc'
+                                                    })
+
+                                                    $watch('direction', function () {
+                                                        sort = column ? `${column}:${direction}` : null
+                                                    })
+
+                                                    $watch('column', function (newColumn, oldColumn) {
+                                                        if (! newColumn) {
+                                                            direction = null
+                                                            sort = column ? `${column}:${direction}` : null
+
+                                                            return
+                                                        }
+
+                                                        if (oldColumn) {
+                                                            sort = column ? `${column}:${direction}` : null
+
+                                                            return
+                                                        }
+
+                                                        direction = 'asc'
+                                                        sort = column ? `${column}:${direction}` : null
+                                                    })
+                                                "
+                                                class="fi-ta-stacked-sorting"
+                                            >
+                                                <label>
+                                                    <x-filament::input.wrapper
+                                                        :prefix="__('filament-tables::table.sorting.fields.column.label')"
+                                                    >
+                                                        <x-filament::input.select
+                                                            x-model="column"
+                                                        >
+                                                            <option value="">
+                                                                {{ $defaultSortOptionLabel }}
+                                                            </option>
+
+                                                            @foreach ($sortableColumns as $sortableColumn)
+                                                                <option
+                                                                    value="{{ $sortableColumn->getName() }}"
+                                                                >
+                                                                    {{ $sortableColumn->getLabel() }}
+                                                                </option>
+                                                            @endforeach
+                                                        </x-filament::input.select>
+                                                    </x-filament::input.wrapper>
+                                                </label>
+
+                                                <label
+                                                    x-cloak
+                                                    x-show="column"
+                                                >
+                                                    <span class="fi-sr-only">
+                                                        {{ __('filament-tables::table.sorting.fields.direction.label') }}
+                                                    </span>
+
+                                                    <x-filament::input.wrapper>
+                                                        <x-filament::input.select
+                                                            x-model="direction"
+                                                        >
+                                                            <option value="asc">
+                                                                {{ __('filament-tables::table.sorting.fields.direction.options.asc') }}
+                                                            </option>
+
+                                                            <option
+                                                                value="desc"
+                                                            >
+                                                                {{ __('filament-tables::table.sorting.fields.direction.options.desc') }}
+                                                            </option>
+                                                        </x-filament::input.select>
+                                                    </x-filament::input.wrapper>
+                                                </label>
+                                            </div>
+                                        </th>
+                                    </tr>
+                                @endif
+
                                 @if ($hasColumnGroups)
                                     <tr class="fi-ta-table-head-groups-row">
                                         @if (count($records))
@@ -2049,7 +2193,6 @@
 
                                                     @if (count($defaultRecordActions) && $recordActionsPosition === RecordActionsPosition::AfterColumns && (! $isReordering))
                                                         <td class="fi-ta-cell">
-                                                            {!! $isStackedOnMobile ? '<div class="fi-ta-actions-label">' . e(__('filament-tables::table.columns.actions.label', [], 2)) . '</div>' : '' !!}
                                                             <div
                                                                 @class([
                                                                     'fi-ta-actions',
