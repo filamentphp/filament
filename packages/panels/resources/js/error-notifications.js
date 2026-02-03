78a4151d7ce78f759ec31f78e7abbdbfff73426c
Dan Harrin <git@danharrin.com>
2026-02-03 10:04:29 +0000

    Update error-notifications.js

--- packages/panels/resources/js/error-notifications.js
+++ packages/panels/resources/js/error-notifications.js
@@ -1,28 +1,49 @@
 document.addEventListener('livewire:init', () => {
-    Livewire.hook('request', ({ payload, fail }) => {
-        fail(({ status, preventDefault }) => {
+    Livewire.interceptRequest(({ request, onError, onFailure }) => {
+        onError(({ response, preventDefault }) => {
             const errorNotifications = window.filamentErrorNotifications
 
             if (!errorNotifications) {
                 return
             }
 
-            if (JSON.parse(payload).components.length === 1) {
-                for (const component of JSON.parse(payload).components) {
-                    if (
-                        JSON.parse(component.snapshot).data
-                            .isFilamentNotificationsComponent
-                    ) {
-                        return
+            try {
+                const payload = request?.payload
+                if (payload && JSON.parse(payload).components.length === 1) {
+                    for (const component of JSON.parse(payload).components) {
+                        if (
+                            JSON.parse(component.snapshot).data
+                                .isFilamentNotificationsComponent
+                        ) {
+                            return
+                        }
                     }
                 }
+            } catch (error) {
+                //
             }
 
             preventDefault()
 
+            const status = response?.status ?? ''
             const errorNotification =
                 errorNotifications[status] ?? errorNotifications['']
 
+            new FilamentNotification()
+                .title(errorNotification.title)
+                .body(errorNotification.body)
+                .danger()
+                .send()
+        })
+
+        onFailure(() => {
+            const errorNotifications = window.filamentErrorNotifications
+
+            if (!errorNotifications) {
+                return
+            }
+
+            const errorNotification = errorNotifications['']
             new FilamentNotification()
                 .title(errorNotification.title)
                 .body(errorNotification.body)
