13ab3dc9677ed4399ecd87a439b2deacf9cc65e6
Nouman Ahmad <31096570+NoumanAhmad448@users.noreply.github.com>
2026-02-28 15:44:36 +0800

    Adding Navbar Logo in the sidebar (#1)
    * Update filament.php
    
    Signed-off-by: Nouman Ahmad <31096570+NoumanAhmad448@users.noreply.github.com>
    
    * Update brand.blade.php
    
    Signed-off-by: Nouman Ahmad <31096570+NoumanAhmad448@users.noreply.github.com>
    
    * Update filament.php
    
    Signed-off-by: Nouman Ahmad <31096570+NoumanAhmad448@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Nouman Ahmad <31096570+NoumanAhmad448@users.noreply.github.com>

--- packages/admin/resources/views/components/brand.blade.php
+++ packages/admin/resources/views/components/brand.blade.php
@@ -7,4 +7,12 @@
     >
         {{ $brand }}
     </div>
+@elseif (filled($brand = config('filament.brand_logo')))
+    <a @class([
+        'filament-brand leading-5 tracking-tight',
+        'dark:text-white' => config('filament.dark_mode'),
+    ]) href="{{ config("filament.home_url") }}">
+        <img src="{{ $brand' }}" alt="Website Logo"
+            width="{{ config('filament.brand_logo_width') }}">
+    </a>
 @endif
