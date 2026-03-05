2ca350380b61db7c324f4f052e80df0b2eadd12c
PeopleSea <70972819+People-Sea@users.noreply.github.com>
2026-03-06 00:08:53 +0800

    fix: Escape URL in `generate_href_html()` output

--- packages/support/src/helpers.php
+++ packages/support/src/helpers.php
@@ -133,7 +133,7 @@ if (! function_exists('Filament\Support\generate_href_html')) {
             return new HtmlString('');
         }
 
-        $html = "href=\"{$url}\"";
+        $html = 'href="' . e($url) . '"';
 
         if ($shouldOpenInNewTab) {
             $html .= ' target="_blank"';
@@ -141,7 +141,7 @@ if (! function_exists('Filament\Support\generate_href_html')) {
             if (FilamentView::hasSpaPrefetching()) {
                 $html .= ' wire:navigate.hover';
             } elseif ($hasNestedClickEventHandler) {
-                $html .= ' x-on:click="if (! ($event.altKey || $event.ctrlKey || $event.metaKey || $event.shiftKey)) { $event.preventDefault(); Alpine.navigate(' . "'{$url}'" . ') }"';
+                $html .= ' x-on:click="if (! ($event.altKey || $event.ctrlKey || $event.metaKey || $event.shiftKey)) { $event.preventDefault(); Alpine.navigate($el.getAttribute(\'href\')) }"';
             } else {
                 $html .= ' wire:navigate';
             }
