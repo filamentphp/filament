1477965f90db147b26549e0cba57f11f71acd21e
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-03-24 02:19:38 +0500

    Added LoadingIndicator import in SupportServiceProvider.php

--- packages/support/src/SupportServiceProvider.php
+++ packages/support/src/SupportServiceProvider.php
@@ -18,6 +18,7 @@ use Filament\Support\Commands\OptimizeCommand;
 use Filament\Support\Commands\UpgradeCommand;
 use Filament\Support\Components\ComponentManager;
 use Filament\Support\Components\Contracts\ScopedComponentManager;
+use Filament\Support\Contracts\LoadingIndicator;
 use Filament\Support\Enums\GridDirection;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentColor;
