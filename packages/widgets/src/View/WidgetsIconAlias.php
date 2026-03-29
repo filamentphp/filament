a82b9b80149679b7fdeca5bf4be75b4808414419
ahmed-rashad-alnaggar <131385452+ahmed-rashad-alnaggar@users.noreply.github.com>
2026-03-29 03:56:09 +0200

    Add CHART_WIDGET_EMPTY_STATE constant to WidgetsIconAlias
    Signed-off-by: ahmed-rashad-alnaggar <131385452+ahmed-rashad-alnaggar@users.noreply.github.com>

--- packages/widgets/src/View/WidgetsIconAlias.php
+++ packages/widgets/src/View/WidgetsIconAlias.php
@@ -5,4 +5,6 @@ namespace Filament\Widgets\View;
 class WidgetsIconAlias
 {
     const CHART_WIDGET_FILTER = 'widgets::chart-widget.filter';
+
+    const CHART_WIDGET_EMPTY_STATE = 'widgets::chart-widget.empty-state';
 }
