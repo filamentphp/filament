a0ac3384f0265fbec0ac71f255cbb658b2a9da31
Dan Harrin <git@danharrin.com>
2026-04-13 22:27:55 +0100

    Update helpers.php

--- packages/support/src/helpers.php
+++ packages/support/src/helpers.php
@@ -245,12 +245,10 @@ if (! function_exists('Filament\Support\generate_search_column_expression')) {
     {
         $driverName = $databaseConnection->getDriverName();
 
-        // Support laravel table prefix with pgsql
-        $tablePrefix = $databaseConnection->getTablePrefix();
-        if ($driverName === 'pgsql' && filled($tablePrefix) && str_contains($column, '.')) {
-            $column = $tablePrefix . $column;
+        if ($driverName === 'pgsql' && str_contains($column, '.')) {
+            $column = $databaseConnection->getTablePrefix() . $column;
         }
-        
+
         $column = match ($driverName) {
             'pgsql' => (
                 str($column)->contains('->')
