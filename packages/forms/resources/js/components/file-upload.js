821928a0f3e02febdd87fc609303ed283485fd45
Gaspar Sukk <gaspar@update.ee>
2026-04-17 20:09:12 +0300

    feat: Register FilePond Estonian (et-ee) locale

--- packages/forms/resources/js/components/file-upload.js
+++ packages/forms/resources/js/components/file-upload.js
@@ -926,6 +926,7 @@ import de from 'filepond/locale/de-de'
 import el from 'filepond/locale/el-el'
 import en from 'filepond/locale/en-en'
 import es from 'filepond/locale/es-es'
+import et from 'filepond/locale/et-ee'
 import fa from 'filepond/locale/fa_ir'
 import fi from 'filepond/locale/fi-fi'
 import fr from 'filepond/locale/fr-fr'
@@ -968,6 +969,7 @@ const locales = {
     el,
     en,
     es,
+    et,
     fa,
     fi,
     fr,
