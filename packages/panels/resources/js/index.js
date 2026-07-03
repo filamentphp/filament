edb105fa7ce0ce5135e131f02ccd683cd0f20c5d
PeopleSea <70972819+People-Sea@users.noreply.github.com>
2026-07-03 19:25:14 +0800

    fix: remove obsolete history-state.js wrapper on v5

--- packages/panels/resources/js/index.js
+++ packages/panels/resources/js/index.js
@@ -2,7 +2,6 @@ import Mousetrap from '@danharrin/alpine-mousetrap'
 import sidebar from './stores/sidebar.js'
 import './dark-mode.js'
 import './error-notifications.js'
-import './history-state.js'
 import './scroll-sidebar.js'
 import './unsaved-changes-alert.js'
 
