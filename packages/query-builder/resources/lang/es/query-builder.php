62de40ce3c4cf6c6ffb0791001394aabae21fa45
Luis Parrado <luisprmat@gmail.com>
2026-07-17 11:03:59 -0500

    Update Spanish translations (#20209)

--- packages/query-builder/resources/lang/es/query-builder.php
+++ packages/query-builder/resources/lang/es/query-builder.php
@@ -14,6 +14,10 @@ return [
 
             'label' => 'Grupos',
 
+            'group' => [
+                'label' => 'Grupo',
+            ],
+
             'block' => [
                 'label' => 'Disyunción (O)',
                 'or' => 'O',
