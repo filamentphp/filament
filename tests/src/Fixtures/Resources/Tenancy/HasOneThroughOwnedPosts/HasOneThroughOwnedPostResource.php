6b57f08c0608b06bfecf169e136b5af284f55312
Dan Harrin <git@danharrin.com>
2026-09-15 13:40:44 +0000

    Simplify tenancy relationship tests

--- tests/src/Fixtures/Resources/Tenancy/HasOneThroughOwnedPosts/HasOneThroughOwnedPostResource.php
+++ tests/src/Fixtures/Resources/Tenancy/HasOneThroughOwnedPosts/HasOneThroughOwnedPostResource.php
@@ -3,29 +3,11 @@
 namespace Filament\Tests\Fixtures\Resources\Tenancy\HasOneThroughOwnedPosts;
 
 use Filament\Resources\Resource;
-use Filament\Schemas\Schema;
-use Filament\Tables\Table;
 use Filament\Tests\Fixtures\Models\Post;
 
 class HasOneThroughOwnedPostResource extends Resource
 {
     protected static ?string $model = Post::class;
 
-    // Ownership through a read-only Laravel `HasOneThrough` relationship
-    // (post -> author -> team), which cannot `save()` the tenant.
     protected static ?string $tenantOwnershipRelationshipName = 'teamThroughAuthor';
-
-    protected static ?string $slug = 'has-one-through-owned-posts';
-
-    public static function form(Schema $form): Schema
-    {
-        return $form
-            ->components([]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([]);
-    }
 }
