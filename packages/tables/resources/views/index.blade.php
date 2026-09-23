4b853aaf5465705983311fae3d514306a82526d9
ukeloop <45733259+ukeloop@users.noreply.github.com>
2026-09-23 16:44:25 +0900

    chore: remove unused imports (#20557)

--- packages/tables/resources/views/index.blade.php
+++ packages/tables/resources/views/index.blade.php
@@ -5,7 +5,6 @@
     use Filament\Actions\BulkActionGroup;
     use Filament\Support\Enums\Alignment;
     use Filament\Support\Enums\IconSize;
-    use Filament\Support\Enums\VerticalAlignment;
     use Filament\Support\Enums\Width;
     use Filament\Support\Facades\FilamentView;
     use Filament\Support\Icons\Heroicon;
@@ -14,9 +13,7 @@
     use Filament\Tables\Columns\Column;
     use Filament\Tables\Columns\ColumnGroup;
     use Filament\Tables\Enums\ColumnManagerLayout;
-    use Filament\Tables\Enums\ColumnManagerResetActionPosition;
     use Filament\Tables\Enums\FiltersLayout;
-    use Filament\Tables\Enums\FiltersResetActionPosition;
     use Filament\Tables\Enums\RecordActionsPosition;
     use Filament\Tables\Enums\RecordCheckboxPosition;
     use Filament\Tables\Filters\Indicator;
